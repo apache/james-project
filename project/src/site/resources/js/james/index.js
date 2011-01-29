97483d79dd499116177492bb60356b25cf54a11a
eric <eric@13f79535-47bb-0310-9956-ffa450edef68>
2011-01-29 06:28:49 +0000

    Add proposal 8 for james logo on index page of web site (JAMES-1091)
    git-svn-id: https://svn.apache.org/repos/asf/james/project/trunk@1064966 13f79535-47bb-0310-9956-ffa450edef68

--- project/src/site/resources/js/james/index.js
+++ project/src/site/resources/js/james/index.js
@@ -31,7 +31,7 @@ function initIndexPage() {
       switchLogo('images/james-project-logo.gif');
     });
 /*    
-    for (var i=0; i < 8; i++) {
+    for (var i=0; i < 9; i++) {
       $('#james-logo-' + i + '-preview').click(function() {
         switchLogo('logo-call/james-logo-'+ i + '.png');
       });
@@ -63,6 +63,10 @@ function initIndexPage() {
 
     $('#james-logo-7-preview').click(function() {
         switchLogo('logo-call/james-logo-7.png');
+    });
+    
+    $('#james-logo-8-preview').click(function() {
+        switchLogo('logo-call/james-logo-8.png');
     });
     
     if (window.location.hash == '#logo') {
