e6672ba8241f2e7ad7908579836da5a8e6e7af99
Matthieu Baechler <matthieu@apache.org>
2015-11-02 13:42:26 +0000

    JAMES-1595 refactor CollectionToMapping function
    git-svn-id: https://svn.apache.org/repos/asf/james/project/trunk@1711972 13f79535-47bb-0310-9956-ffa450edef68

---
+++ server/data/data-library/src/test/java/org/apache/james/rrt/lib/RecipientRewriteTableUtilTest.java
@@ -0,0 +1,34 @@
+package org.apache.james.rrt.lib;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RecipientRewriteTableUtilTest {
+
+    @Test(expected=NullPointerException.class)
+    public void collectionToMappingShouldThrowWhenNull() {
+        RecipientRewriteTableUtil.CollectionToMapping(null);
+    }
+    
+    @Test
+    public void collectionToMappingShouldReturnEmptyStringWhenEmptyCollection() {
+        String actual = RecipientRewriteTableUtil.CollectionToMapping(ImmutableList.<String>of());
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    public void collectionToMappingShouldReturnSimpleValueWhenSingleElementCollection() {
+        String actual = RecipientRewriteTableUtil.CollectionToMapping(ImmutableList.of("value"));
+        assertThat(actual).isEqualTo("value");
+    }
+
+    @Test
+    public void collectionToMappingShouldReturnSeparatedValuesWhenSeveralElementsCollection() {
+        String actual = RecipientRewriteTableUtil.CollectionToMapping(ImmutableList.of("value1", "value2"));
+        assertThat(actual).isEqualTo("value1;value2");
+    }
+
+}
