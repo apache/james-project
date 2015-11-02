24d60e5ed0d0937b2246179e9979886c5051d72c
matthieu <matthieu@13f79535-47bb-0310-9956-ffa450edef68>
2015-11-02 13:48:06 +0000

    JAMES-1595 Mapping now have a getType method
    git-svn-id: https://svn.apache.org/repos/asf/james/project/trunk@1711990 13f79535-47bb-0310-9956-ffa450edef68

--- server/data/data-api/src/main/java/org/apache/james/rrt/lib/Mapping.java
+++ server/data/data-api/src/main/java/org/apache/james/rrt/lib/Mapping.java
@@ -23,6 +23,10 @@ package org.apache.james.rrt.lib;
 
 public interface Mapping {
 
+    enum Type { Regex, Domain, Error, Address };
+    
+    Type getType();
+    
     String asString();
 
     boolean hasDomain();
