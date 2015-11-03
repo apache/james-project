794b982547a613df2e961fab139c6855b806b49d
Matthieu Baechler <matthieu.baechler@gmail.com>
2015-11-03 15:28:48 +0100

    fixup! JAMES-1626 support IMAP, SMTP, POP and LMTP protocols in cassandra-guice application

--- server/container/cassandra-guice/src/test/java/org/apache/james/CassandraJamesServerTest.java
+++ server/container/cassandra-guice/src/test/java/org/apache/james/CassandraJamesServerTest.java
@@ -90,31 +90,31 @@ public class CassandraJamesServerTest {
     }
 
     @Test
-    public void connectIMAPServerShouldThrowWhenNoCredentials() throws Exception {
+    public void connectIMAPServerShouldSendShabangOnConnect() throws Exception {
         socketChannel.connect(new InetSocketAddress("127.0.0.1", IMAP_PORT));
         assertThat(getServerConnectionResponse(socketChannel)).startsWith("* OK JAMES IMAP4rev1 Server");
     }
 
     @Test
-    public void connectOnSecondaryIMAPServerIMAPServerShouldThrowWhenNoCredentials() throws Exception {
+    public void connectOnSecondaryIMAPServerIMAPServerShouldSendShabangOnConnect() throws Exception {
         socketChannel.connect(new InetSocketAddress("127.0.0.1", IMAP_PORT_SSL));
         assertThat(getServerConnectionResponse(socketChannel)).startsWith("* OK JAMES IMAP4rev1 Server");
     }
 
     @Test
-    public void connectPOP3ServerShouldThrowWhenNoCredentials() throws Exception {
+    public void connectPOP3ServerShouldSendShabangOnConnect() throws Exception {
         socketChannel.connect(new InetSocketAddress("127.0.0.1", POP3_PORT));
         assertThat(getServerConnectionResponse(socketChannel)).contains("POP3 server (JAMES POP3 Server ) ready");
     }
 
     @Test
-    public void connectSMTPServerShouldNotThrowWhenNoCredentials() throws Exception {
+    public void connectSMTPServerShouldSendShabangOnConnect() throws Exception {
         socketChannel.connect(new InetSocketAddress("127.0.0.1", SMTP_PORT));
         assertThat(getServerConnectionResponse(socketChannel)).startsWith("220 JAMES Linagora's SMTP awesome Server");
     }
 
     @Test
-    public void connectLMTPServerShouldNotThrowWhenNoCredentials() throws Exception {
+    public void connectLMTPServerShouldSendShabangOnConnect() throws Exception {
         socketChannel.connect(new InetSocketAddress("127.0.0.1", LMTP_PORT));
         assertThat(getServerConnectionResponse(socketChannel)).contains("LMTP Server (JAMES Protocols Server) ready");
     }
