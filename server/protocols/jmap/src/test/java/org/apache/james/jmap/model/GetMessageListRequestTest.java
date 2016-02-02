00199af12528560997107c2beec2134cf6699f87
Laura Royet <lroyet@linagora.com>
2016-02-02 17:32:30 +0100

    JAMES-1678 GetMailboxMessageListRequestTest renamed to GetMessageListRequestTest

--- server/protocols/jmap/src/test/java/org/apache/james/jmap/model/GetMessageListRequestTest.java
+++ server/protocols/jmap/src/test/java/org/apache/james/jmap/model/GetMessageListRequestTest.java
@@ -29,7 +29,7 @@ import org.junit.Test;
 
 import com.google.common.collect.ImmutableList;
 
-public class GetMailboxMessageListRequestTest {
+public class GetMessageListRequestTest {
 
     @Test(expected=IllegalStateException.class)
     public void builderShouldThrowWhenPositionIsNegative() {
