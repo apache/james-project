9df364760ad59ce9b7876abc6d49e93f7f729516
Benoit Tellier <btellier@linagora.com>
2016-08-31 14:00:11 +0700

    MAILET-121 Provide a tool to read emails from matcher condition
    Code factorization and testing between SenderIs and RecipientIs

---
+++ mailet/standard/src/test/java/org/apache/james/transport/matchers/util/MailAddressCollectionReaderTest.java
@@ -0,0 +1,85 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.transport.matchers.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.james.transport.matchers.utils.MailAddressCollectionReader;
+import org.apache.mailet.MailAddress;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class MailAddressCollectionReaderTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void readShouldThrowOnNullInput() {
+        expectedException.expect(IllegalArgumentException.class);
+        MailAddressCollectionReader.read(null);
+    }
+
+    @Test
+    public void readShouldThrowOnEmptyInput() {
+        expectedException.expect(IllegalArgumentException.class);
+        MailAddressCollectionReader.read("");
+    }
+
+    @Test
+    public void readShouldThrowOnInvalidEmail() {
+        expectedException.expect(RuntimeException.class);
+        MailAddressCollectionReader.read("not_valid");
+    }
+
+    @Test
+    public void readShouldThrowOnInvalidEmailOnSecondPosition() {
+        expectedException.expect(RuntimeException.class);
+        MailAddressCollectionReader.read("valid@apache.org, not_valid");
+    }
+
+    @Test
+    public void readShouldParseOneEmail() throws Exception {
+        MailAddress mailAddress = new MailAddress("valid@apache.org");
+
+        assertThat(MailAddressCollectionReader.read(mailAddress.toString()))
+            .containsExactly(mailAddress);
+    }
+
+    @Test
+    public void readShouldParseTwoEmail() throws Exception {
+        MailAddress mailAddress1 = new MailAddress("valid@apache.org");
+        MailAddress mailAddress2 = new MailAddress("bis@apache.org");
+
+        assertThat(MailAddressCollectionReader.read(mailAddress1.toString() + ", " + mailAddress2.toString()))
+            .containsExactly(mailAddress1, mailAddress2);
+    }
+
+    @Test
+    public void readShouldRemoveDuplicates() throws Exception {
+        MailAddress mailAddress = new MailAddress("valid@apache.org");
+
+        assertThat(MailAddressCollectionReader.read(mailAddress.toString() + ", " + mailAddress.toString()))
+            .containsExactly(mailAddress);
+    }
+
+
+}
