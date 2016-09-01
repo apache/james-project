b626c0c90ef57e0293ab9463304cac4832dfef4a
Benoit Tellier <btellier@linagora.com>
2016-09-01 15:15:29 +0700

    MAILET-124 Provide tests for Xor composite matcher

--- server/mailet/mailetcontainer-camel/src/test/java/org/apache/james/mailetcontainer/impl/matchers/XorTest.java
+++ server/mailet/mailetcontainer-camel/src/test/java/org/apache/james/mailetcontainer/impl/matchers/XorTest.java
@@ -18,51 +18,84 @@
  ****************************************************************/
 package org.apache.james.mailetcontainer.impl.matchers;
 
+import static org.apache.mailet.base.MailAddressFixture.ANY_AT_JAMES;
+import static org.apache.mailet.base.MailAddressFixture.OTHER_AT_JAMES;
+import static org.apache.mailet.base.MailAddressFixture.ANY_AT_JAMES2;
+import static org.apache.mailet.base.MailAddressFixture.OTHER_AT_JAMES2;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.apache.mailet.Mail;
 import org.apache.mailet.MailAddress;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import org.apache.mailet.Matcher;
+import org.apache.mailet.base.test.FakeMail;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.mail.MessagingException;
-import java.util.Collection;
-import java.util.Iterator;
+import com.google.common.collect.ImmutableList;
 
-public class XorTest extends BaseMatchersTest {
+public class XorTest {
 
-    @Override
+    private Xor testee;
+    private Matcher matcher1;
+    private Matcher matcher2;
+    private Mail mail;
+
     @Before
     public void setUp() throws Exception {
-        super.setUp();
-        setupCompositeMatcher("Xor", Xor.class);
-    }
+        matcher1 = mock(Matcher.class);
+        matcher2 = mock(Matcher.class);
 
-    // test if all recipients was returned
-    @Test
-    public void testIntersectSame() throws MessagingException {
-        setupChild("RecipientIsRegex=test@james.apache.org");
-        setupChild("RecipientIsRegex=test@james.apache.org");
+        testee = new Xor();
 
-        Collection<MailAddress> matchedRecipients = matcher.match(mockedMail);
-
-        assertNotNull(matchedRecipients);
-        assertEquals(0, matchedRecipients.size());
+        mail = FakeMail.builder().recipients(ANY_AT_JAMES, OTHER_AT_JAMES, ANY_AT_JAMES2, OTHER_AT_JAMES2).build();
     }
 
     @Test
-    public void testNoIntersect() throws MessagingException {
-        setupChild("RecipientIsRegex=test@james.apache.org");
-        setupChild("RecipientIsRegex=test2@james.apache.org");
+    public void shouldReturnNoResultWhenNoMatcherSpecified() throws Exception {
+        assertThat(testee.match(mail)).isNull();
+    }
 
-        Collection<MailAddress> matchedRecipients = matcher.match(mockedMail);
+    @Test
+    public void shouldReturnMatchResultWhenOnlyOneMatcher() throws Exception {
+        when(matcher1.match(mail)).thenReturn(ImmutableList.of(ANY_AT_JAMES, ANY_AT_JAMES2));
 
-        assertNotNull(matchedRecipients);
-        assertEquals(2, matchedRecipients.size());
+        testee.add(matcher1);
 
-        Iterator<MailAddress> iterator = matchedRecipients.iterator();
-        MailAddress address = (MailAddress) iterator.next();
-        assertEquals(address, "test@james.apache.org");
-        address = (MailAddress) iterator.next();
-        assertEquals(address, "test2@james.apache.org");
+        assertThat(testee.match(mail)).containsOnly(ANY_AT_JAMES, ANY_AT_JAMES2);
+    }
+
+    @Test
+    public void shouldPerformXorWhenTwoMatchers() throws Exception {
+        when(matcher1.match(mail)).thenReturn(ImmutableList.of(ANY_AT_JAMES, ANY_AT_JAMES2));
+        when(matcher2.match(mail)).thenReturn(ImmutableList.of(ANY_AT_JAMES, OTHER_AT_JAMES));
+
+        testee.add(matcher1);
+        testee.add(matcher2);
+
+        assertThat(testee.match(mail)).containsOnly(ANY_AT_JAMES2, OTHER_AT_JAMES);
+    }
+
+    @Test
+    public void shouldAcceptEmptyResults() throws Exception {
+        when(matcher1.match(mail)).thenReturn(ImmutableList.of(ANY_AT_JAMES, ANY_AT_JAMES2));
+        when(matcher2.match(mail)).thenReturn(ImmutableList.<MailAddress>of());
+
+        testee.add(matcher1);
+        testee.add(matcher2);
+
+        assertThat(testee.match(mail)).containsOnly(ANY_AT_JAMES, ANY_AT_JAMES2);
+    }
+
+    @Test
+    public void shouldAcceptNullResults() throws Exception {
+        when(matcher1.match(mail)).thenReturn(ImmutableList.of(ANY_AT_JAMES, ANY_AT_JAMES2));
+        when(matcher2.match(mail)).thenReturn(null);
+
+        testee.add(matcher1);
+        testee.add(matcher2);
+
+        assertThat(testee.match(mail)).containsOnly(ANY_AT_JAMES, ANY_AT_JAMES2);
     }
 }
