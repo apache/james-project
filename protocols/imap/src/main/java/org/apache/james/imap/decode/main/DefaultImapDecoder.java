dd3566255591e5c03fcd41d3b9f17ff59ca7493a
Benoit Tellier <btellier@linagora.com>
2016-10-21 18:41:07 +0200

    JAMES-1846 Improve IMAP logging
    We should be able to know wich command is being used after UID command

--- protocols/imap/src/main/java/org/apache/james/imap/decode/main/DefaultImapDecoder.java
+++ protocols/imap/src/main/java/org/apache/james/imap/decode/main/DefaultImapDecoder.java
@@ -18,6 +18,7 @@
  ****************************************************************/
 package org.apache.james.imap.decode.main;
 
+import org.apache.james.imap.api.ImapConstants;
 import org.apache.james.imap.api.ImapMessage;
 import org.apache.james.imap.api.ImapSessionState;
 import org.apache.james.imap.api.display.HumanReadableText;
