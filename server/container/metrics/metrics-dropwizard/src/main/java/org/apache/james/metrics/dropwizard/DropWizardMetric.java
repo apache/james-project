0254c439cc28a9e29aa4b798de988d2995237897
Benoit Tellier <btellier@linagora.com>
2016-11-24 11:54:20 +0700

    JAMES-1868 Add a Metric implementation with DropWizard-metrics

---
+++ server/container/metrics/metrics-dropwizard/src/main/java/org/apache/james/metrics/dropwizard/DropWizardMetric.java
@@ -0,0 +1,43 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.metrics.dropwizard;
+
+import org.apache.james.metrics.api.Metric;
+
+import com.codahale.metrics.Counter;
+
+public class DropWizardMetric implements Metric {
+
+    private final Counter counter;
+
+    public DropWizardMetric(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void increment() {
+        counter.inc();
+    }
+
+    @Override
+    public void decrement() {
+        counter.dec();
+    }
+}
