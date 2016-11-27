76ed6b137dc308abda851f67559231afeb2a4c53
luo877280 <176157898@qq.com>
2016-11-27 16:14:09 +0800

    test add file

---
+++ server/app/src/main/java/org/apache/james/app/spring/TT.java
@@ -0,0 +1,10 @@
+package org.apache.james.app.spring;
+
+public class TT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("..............");
+	}
+
+}
