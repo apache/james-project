ad2a163db757bc058ab019a3ef632d23a39f043d
Benoit Tellier <btellier@linagora.com>
2016-12-09 09:38:53 +0700

    JAMES-1885: Write test for Side effects expected from MessageIdManager

---
+++ mailbox/store/src/test/java/org/apache/james/mailbox/store/AbstractMessageIdManagerSideEffectTest.java
@@ -0,0 +1,324 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailbox.store;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import javax.mail.Flags;
+
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.MessageIdManager;
+import org.apache.james.mailbox.MessageManager;
+import org.apache.james.mailbox.MessageManager.FlagsUpdateMode;
+import org.apache.james.mailbox.MessageUid;
+import org.apache.james.mailbox.exception.MailboxException;
+import org.apache.james.mailbox.exception.OverQuotaException;
+import org.apache.james.mailbox.model.FetchGroupImpl;
+import org.apache.james.mailbox.model.MailboxId;
+import org.apache.james.mailbox.model.MessageId;
+import org.apache.james.mailbox.model.MessageResult;
+import org.apache.james.mailbox.model.Quota;
+import org.apache.james.mailbox.model.QuotaRoot;
+import org.apache.james.mailbox.model.UpdatedFlags;
+import org.apache.james.mailbox.quota.QuotaManager;
+import org.apache.james.mailbox.store.event.MailboxEventDispatcher;
+import org.apache.james.mailbox.store.mail.model.Mailbox;
+import org.apache.james.mailbox.store.quota.QuotaImpl;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+
+public abstract class AbstractMessageIdManagerSideEffectTest {
+
+    private static final Quota OVER_QUOTA = QuotaImpl.quota(102, 100);
+    public static final Flags FLAGS = new Flags();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    private MessageIdManager messageIdManager;
+    private MailboxEventDispatcher dispatcher;
+    private MailboxSession session;
+    private Mailbox mailbox1;
+    private Mailbox mailbox2;
+    private Mailbox mailbox3;
+    private QuotaManager quotaManager;
+    private MessageIdManagerTestSystem testingData;
+
+    protected abstract MessageIdManagerTestSystem createTestingData(QuotaManager quotaManager, MailboxEventDispatcher dispatcher) throws Exception;
+
+    @Before
+    public void setUp() throws Exception {
+        dispatcher = mock(MailboxEventDispatcher.class);
+        session = mock(MailboxSession.class);
+        quotaManager = mock(QuotaManager.class);
+
+        testingData = createTestingData(quotaManager, dispatcher);
+        messageIdManager = testingData.getMessageIdManager();
+        mailbox1 = testingData.getMailbox1();
+        mailbox2 = testingData.getMailbox2();
+        mailbox3 = testingData.getMailbox3();
+    }
+
+    @After
+    public void tearDown() {
+        testingData.clean();
+    }
+
+    @Test
+    public void deleteShouldCallEventDispatcher() throws Exception {
+        givenUnlimitedQuota();
+        MessageId messageId = testingData.persist(mailbox1.getMailboxId(), FLAGS);
+        reset(dispatcher);
+
+        MessageResult messageResult = messageIdManager.getMessages(ImmutableList.of(messageId), FetchGroupImpl.MINIMAL, session).get(0);
+        SimpleMessageMetaData simpleMessageMetaData = fromMessageResult(messageId, messageResult);
+
+        messageIdManager.delete(messageId, ImmutableList.of(mailbox1.getMailboxId()), session);
+
+        verify(dispatcher).expunged(session, simpleMessageMetaData, mailbox1);
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void deleteShouldNotCallEventDispatcherWhenMessageIsInWrongMailbox() throws Exception {
+        givenUnlimitedQuota();
+        MessageId messageId = testingData.persist(mailbox2.getMailboxId(), FLAGS);
+        reset(dispatcher);
+
+        messageIdManager.delete(messageId, ImmutableList.of(mailbox1.getMailboxId()), session);
+
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setInMailboxesShouldNotCallDispatcherWhenMessageAlreadyInMailbox() throws Exception {
+        givenUnlimitedQuota();
+        MessageId messageId = testingData.persist(mailbox1.getMailboxId(), FLAGS);
+        reset(dispatcher);
+
+        messageIdManager.setInMailboxes(messageId, ImmutableList.of(mailbox1.getMailboxId()), session);
+
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setInMailboxesShouldCallDispatcher() throws Exception {
+        givenUnlimitedQuota();
+        MessageId messageId = testingData.persist(mailbox2.getMailboxId(), FLAGS);
+        reset(dispatcher);
+
+        messageIdManager.setInMailboxes(messageId, ImmutableList.of(mailbox1.getMailboxId()), session);
+
+        MessageResult messageResult = FluentIterable
+            .from(messageIdManager.getMessages(ImmutableList.of(messageId), FetchGroupImpl.MINIMAL, session))
+            .filter(inMailbox(mailbox1.getMailboxId()))
+            .get(0);
+        SimpleMessageMetaData simpleMessageMetaData = fromMessageResult(messageId, messageResult);
+
+        verify(dispatcher).added(session, simpleMessageMetaData, mailbox1);
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setInMailboxesShouldCallDispatcherWithMultipleMailboxes() throws Exception {
+        givenUnlimitedQuota();
+        MessageId messageId = testingData.persist(mailbox2.getMailboxId(), FLAGS);
+        reset(dispatcher);
+
+        messageIdManager.setInMailboxes(messageId, ImmutableList.of(mailbox1.getMailboxId(), mailbox3.getMailboxId()), session);
+
+        List<MessageResult> messageResults = messageIdManager.getMessages(ImmutableList.of(messageId), FetchGroupImpl.MINIMAL, session);
+        MessageResult messageResultMaibox1 = FluentIterable
+            .from(messageResults)
+            .filter(inMailbox(mailbox1.getMailboxId()))
+            .get(0);
+        SimpleMessageMetaData metadataMailbox1 = fromMessageResult(messageId, messageResultMaibox1);
+        MessageResult messageResultMaibox3 = FluentIterable
+            .from(messageResults)
+            .filter(inMailbox(mailbox3.getMailboxId()))
+            .get(0);
+        SimpleMessageMetaData metadataMailbox3 = fromMessageResult(messageId, messageResultMaibox3);
+
+        verify(dispatcher).added(session, metadataMailbox1, mailbox1);
+        verify(dispatcher).added(session, metadataMailbox3, mailbox3);
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setInMailboxesShouldThrowExceptionWhenOverQuota() throws Exception {
+        MessageId messageId = testingData.persist(mailbox2.getMailboxId(), FLAGS);
+        reset(dispatcher);
+        when(quotaManager.getStorageQuota(any(QuotaRoot.class))).thenReturn(QuotaImpl.unlimited());
+        when(quotaManager.getMessageQuota(any(QuotaRoot.class))).thenReturn(OVER_QUOTA);
+        when(quotaManager.getStorageQuota(any(QuotaRoot.class))).thenReturn(QuotaImpl.unlimited());
+
+        expectedException.expect(OverQuotaException.class);
+
+        messageIdManager.setInMailboxes(messageId, ImmutableList.<MailboxId>of(mailbox1.getMailboxId()), session);
+    }
+
+    @Test
+    public void setFlagsShouldNotDispatchWhenFlagAlreadySet() throws Exception {
+        givenUnlimitedQuota();
+        Flags newFlags = new Flags(Flags.Flag.SEEN);
+        MessageId messageId = testingData.persist(mailbox2.getMailboxId(), newFlags);
+        reset(dispatcher);
+
+        messageIdManager.setFlags(newFlags, MessageManager.FlagsUpdateMode.ADD, messageId, ImmutableList.of(mailbox2.getMailboxId()), session);
+
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setFlagsShouldNotDispatchWhenMessageAlreadyInMailbox() throws Exception {
+        givenUnlimitedQuota();
+        Flags newFlags = new Flags(Flags.Flag.SEEN);
+        MessageId messageId = testingData.persist(mailbox1.getMailboxId(), newFlags);
+        messageIdManager.setInMailboxes(messageId, ImmutableList.of(mailbox2.getMailboxId()), session);
+        reset(dispatcher);
+
+        messageIdManager.setFlags(newFlags, MessageManager.FlagsUpdateMode.ADD, messageId, ImmutableList.of(mailbox1.getMailboxId(), mailbox2.getMailboxId()), session);
+
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setFlagsShouldNotDispatchWhenMessageDoesNotBelongToMailbox() throws Exception {
+        givenUnlimitedQuota();
+        Flags newFlags = new Flags(Flags.Flag.SEEN);
+        MessageId messageId = testingData.persist(mailbox1.getMailboxId(), FLAGS);
+        reset(dispatcher);
+
+        messageIdManager.setFlags(newFlags, MessageManager.FlagsUpdateMode.ADD, messageId, ImmutableList.of(mailbox2.getMailboxId()), session);
+
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setFlagsShouldNotDispatchWhenEmptyMailboxes() throws Exception {
+        givenUnlimitedQuota();
+        Flags newFlags = new Flags(Flags.Flag.SEEN);
+        MessageId messageId = testingData.persist(mailbox1.getMailboxId(), FLAGS);
+        reset(dispatcher);
+
+        messageIdManager.setFlags(newFlags, MessageManager.FlagsUpdateMode.ADD, messageId, ImmutableList.<MailboxId>of(), session);
+
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setFlagsShouldDispatchWhenMessageBelongsToAllMailboxes() throws Exception {
+        givenUnlimitedQuota();
+        Flags newFlags = new Flags(Flags.Flag.SEEN);
+        MessageId messageId = testingData.persist(mailbox1.getMailboxId(), FLAGS);
+        messageIdManager.setInMailboxes(messageId, ImmutableList.of(mailbox2.getMailboxId()), session);
+        reset(dispatcher);
+
+        messageIdManager.setFlags(newFlags, MessageManager.FlagsUpdateMode.ADD, messageId, ImmutableList.of(mailbox1.getMailboxId(), mailbox2.getMailboxId()), session);
+
+        verify(dispatcher, times(2)).flagsUpdated(eq(session), any(MessageUid.class), any(Mailbox.class), any(UpdatedFlags.class));
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setFlagsShouldDispatchWhenMessageBelongsToTheMailboxes() throws Exception {
+        givenUnlimitedQuota();
+        MessageId messageId = testingData.persist(mailbox2.getMailboxId(), FLAGS);
+        reset(dispatcher);
+
+        Flags newFlags = new Flags(Flags.Flag.SEEN);
+        messageIdManager.setFlags(newFlags, MessageManager.FlagsUpdateMode.ADD, messageId, ImmutableList.of(mailbox1.getMailboxId(), mailbox2.getMailboxId()), session);
+
+        List<MessageResult> messages = messageIdManager.getMessages(ImmutableList.of(messageId), FetchGroupImpl.MINIMAL, session);
+        assertThat(messages).hasSize(1);
+        MessageResult messageResult = messages.get(0);
+        MessageUid messageUid = messageResult.getUid();
+        long modSeq = messageResult.getModSeq();
+        UpdatedFlags updatedFlags = new UpdatedFlags(messageUid, modSeq, FLAGS, newFlags);
+
+        verify(dispatcher, times(1)).flagsUpdated(session, messageUid, mailbox2, updatedFlags);
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void deleteShouldNotDispatchEventWhenMessageDoesNotExist() throws Exception {
+        givenUnlimitedQuota();
+        MessageId messageId = testingData.createNotUsedMessageId();
+
+        messageIdManager.delete(messageId, ImmutableList.of(mailbox1.getMailboxId()), session);
+
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setFlagsShouldNotDispatchEventWhenMessageDoesNotExist() throws Exception {
+        givenUnlimitedQuota();
+        MessageId messageId = testingData.createNotUsedMessageId();
+
+        messageIdManager.setFlags(FLAGS, FlagsUpdateMode.ADD, messageId, ImmutableList.of(mailbox2.getMailboxId()), session);
+
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    @Test
+    public void setInMailboxesShouldNotDispatchEventWhenMessageDoesNotExist() throws Exception {
+        givenUnlimitedQuota();
+        MessageId messageId = testingData.createNotUsedMessageId();
+
+        messageIdManager.setInMailboxes(messageId, ImmutableList.of(mailbox1.getMailboxId()), session);
+
+        verifyNoMoreInteractions(dispatcher);
+    }
+
+    private void givenUnlimitedQuota() throws MailboxException {
+        when(quotaManager.getMessageQuota(any(QuotaRoot.class))).thenReturn(QuotaImpl.unlimited());
+        when(quotaManager.getStorageQuota(any(QuotaRoot.class))).thenReturn(QuotaImpl.unlimited());
+    }
+
+    private Predicate<MessageResult> inMailbox(final MailboxId mailboxId) {
+        return new Predicate<MessageResult>() {
+            @Override
+            public boolean apply(MessageResult input) {
+                return input.getMailboxId().equals(mailboxId);
+            }
+        };
+    }
+
+    private SimpleMessageMetaData fromMessageResult(MessageId messageId, MessageResult messageResult) {
+        return new SimpleMessageMetaData(messageResult.getUid(), messageResult.getModSeq(), messageResult.getFlags(), messageResult.getSize(), messageResult.getInternalDate(), messageId);
+    }
+}
---
+++ mailbox/store/src/test/java/org/apache/james/mailbox/store/MessageIdManagerTestSystem.java
@@ -0,0 +1,73 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailbox.store;
+
+import javax.mail.Flags;
+
+import org.apache.james.mailbox.MessageIdManager;
+import org.apache.james.mailbox.model.MailboxId;
+import org.apache.james.mailbox.model.MessageId;
+import org.apache.james.mailbox.store.mail.model.Mailbox;
+
+public abstract class MessageIdManagerTestSystem {
+    private final MessageIdManager messageIdManager;
+    private final Mailbox mailbox1;
+    private final Mailbox mailbox2;
+    private final Mailbox mailbox3;
+
+    public MessageIdManagerTestSystem(MessageIdManager messageIdManager, Mailbox mailbox1, Mailbox mailbox2, Mailbox mailbox3) {
+        this.messageIdManager = messageIdManager;
+        this.mailbox1 = mailbox1;
+        this.mailbox2 = mailbox2;
+        this.mailbox3 = mailbox3;
+    }
+
+    public MessageIdManager getMessageIdManager() {
+        return messageIdManager;
+    }
+
+    public Mailbox getMailbox1() {
+        return mailbox1;
+    }
+
+    public Mailbox getMailbox2() {
+        return mailbox2;
+    }
+
+    public Mailbox getMailbox3() {
+        return mailbox3;
+    }
+
+    /**
+     * Should take care of find returning the MailboxMessage
+     * Should take care of findMailboxes returning the mailbox the message is in
+     * Should persist flags 
+     * Should keep track of flag state for setFlags
+     * 
+     * @param mailboxId
+     * @param flags
+     * @return the id of persisted message
+     */
+    public abstract MessageId persist(MailboxId mailboxId, Flags flags);
+
+    public abstract MessageId createNotUsedMessageId();
+
+    public abstract void clean();
+}
