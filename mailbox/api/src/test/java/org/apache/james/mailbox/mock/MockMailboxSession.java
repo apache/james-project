cc8bac6884908ff33960bfe4d3cc4e2113a81480
Quynh Nguyen <qnguyen@linagora.com>
2016-12-19 16:38:02 +0700

    JAMES-1888: MockMailboxSession allows to create system session

--- mailbox/api/src/test/java/org/apache/james/mailbox/mock/MockMailboxSession.java
+++ mailbox/api/src/test/java/org/apache/james/mailbox/mock/MockMailboxSession.java
@@ -39,6 +39,7 @@ public class MockMailboxSession implements MailboxSession{
     private final static Random RANDOM = new Random();
 
     private final long sessionId = RANDOM.nextLong();
+    private SessionType type = SessionType.User;
     
     public MockMailboxSession(final String username) {
         this.user = new User() {
@@ -64,7 +65,11 @@ public class MockMailboxSession implements MailboxSession{
             }
         };
     }
-    
+
+    public MockMailboxSession(final String username, SessionType type) {
+        this(username);
+        this.type = type;
+    }
     public void close() {
         this.close = true;
     }
@@ -106,7 +111,7 @@ public class MockMailboxSession implements MailboxSession{
 	}
 
     public SessionType getType() {
-        return SessionType.User;
+        return type;
     }
 
 }
