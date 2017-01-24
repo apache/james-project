d4fb7f832af111bcd09b6182ab3ceb3c42693119
Quynh Nguyen <qnguyen@linagora.com>
2017-01-24 13:43:16 +0700

    JAMES-1869 MPT testing with Cassandra should be error with long mailbox name

--- mpt/impl/imap-mailbox/cassandra/src/test/java/org/apache/james/mpt/imapmailbox/cassandra/CassandraMailboxTest.java
+++ mpt/impl/imap-mailbox/cassandra/src/test/java/org/apache/james/mpt/imapmailbox/cassandra/CassandraMailboxTest.java
@@ -20,9 +20,58 @@
 package org.apache.james.mpt.imapmailbox.cassandra;
 
 import org.apache.james.mpt.imapmailbox.AbstractMailboxTest;
+import org.apache.james.mpt.imapmailbox.suite.MailboxWithLongNameError;
+import org.apache.james.mpt.imapmailbox.suite.AuthenticatedState;
+import org.apache.james.mpt.imapmailbox.suite.ConcurrentSessions;
+import org.apache.james.mpt.imapmailbox.suite.Events;
+import org.apache.james.mpt.imapmailbox.suite.Expunge;
+import org.apache.james.mpt.imapmailbox.suite.Fetch;
+import org.apache.james.mpt.imapmailbox.suite.FetchBodySection;
+import org.apache.james.mpt.imapmailbox.suite.FetchBodyStructure;
+import org.apache.james.mpt.imapmailbox.suite.FetchHeaders;
+import org.apache.james.mpt.imapmailbox.suite.Listing;
+import org.apache.james.mpt.imapmailbox.suite.MailboxAnnotation;
+import org.apache.james.mpt.imapmailbox.suite.Move;
+import org.apache.james.mpt.imapmailbox.suite.NonAuthenticatedState;
+import org.apache.james.mpt.imapmailbox.suite.PartialFetch;
+import org.apache.james.mpt.imapmailbox.suite.QuotaTest;
+import org.apache.james.mpt.imapmailbox.suite.Rename;
+import org.apache.james.mpt.imapmailbox.suite.Search;
+import org.apache.james.mpt.imapmailbox.suite.Security;
+import org.apache.james.mpt.imapmailbox.suite.Select;
+import org.apache.james.mpt.imapmailbox.suite.SelectedInbox;
+import org.apache.james.mpt.imapmailbox.suite.SelectedState;
+import org.apache.james.mpt.imapmailbox.suite.UidSearch;
+import org.apache.james.mpt.imapmailbox.suite.UserFlagsSupport;
 import org.apache.james.mpt.onami.test.annotation.GuiceModules;
+import org.junit.runners.Suite.SuiteClasses;
 
 @GuiceModules({ CassandraMailboxTestModule.class })
+@SuiteClasses({
+    AuthenticatedState.class,
+    ConcurrentSessions.class,
+    Events.class,
+    Expunge.class,
+    Fetch.class,
+    FetchBodySection.class,
+    FetchBodyStructure.class,
+    FetchHeaders.class,
+    Listing.class,
+    NonAuthenticatedState.class,
+    PartialFetch.class,
+    Rename.class,
+    Search.class,
+    Security.class,
+    Select.class,
+    QuotaTest.class,
+    UserFlagsSupport.class,
+    Move.class,
+    SelectedInbox.class,
+    SelectedState.class,
+    UidSearch.class,
+    MailboxAnnotation.class,
+    MailboxWithLongNameError.class
+})
 public class CassandraMailboxTest extends AbstractMailboxTest {
 
 }
