f096bddfa198bb55c68f362a0a71094cc8db3c7d
Benoit Tellier <btellier@linagora.com>
2017-02-21 13:13:56 +0700

    JAMES-1945 CompletableFutureUtil::allOf should preserve order

--- server/container/util-java8/src/test/java/org/apache/james/util/CompletableFutureUtilTest.java
+++ server/container/util-java8/src/test/java/org/apache/james/util/CompletableFutureUtilTest.java
@@ -56,6 +56,37 @@ public class CompletableFutureUtilTest {
             .containsOnly(value1, value2, value3);
     }
 
+
+    @Test
+    public void allOfShouldPreserveOrder() {
+        long value1 = 18L;
+        long value2 = 19L;
+        long value3 = 20L;
+        long value4 = 21L;
+        long value5 = 22L;
+        long value6 = 23L;
+        long value7 = 24L;
+        long value8 = 25L;
+        long value9 = 26L;
+        long value10 = 27L;
+        assertThat(
+            CompletableFutureUtil.allOf(
+                Stream.of(
+                    CompletableFuture.completedFuture(value1),
+                    CompletableFuture.completedFuture(value2),
+                    CompletableFuture.completedFuture(value3),
+                    CompletableFuture.completedFuture(value4),
+                    CompletableFuture.completedFuture(value5),
+                    CompletableFuture.completedFuture(value6),
+                    CompletableFuture.completedFuture(value7),
+                    CompletableFuture.completedFuture(value8),
+                    CompletableFuture.completedFuture(value9),
+                    CompletableFuture.completedFuture(value10)))
+                .join()
+                .collect(Guavate.toImmutableList()))
+            .containsExactly(value1, value2, value3, value4, value5, value6, value7, value8, value9, value10);
+    }
+
     @Test
     public void allOfShouldWorkOnVeryLargeStream() {
         CompletableFutureUtil.allOf(
