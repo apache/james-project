d43ce78c6b20f4e38e69376fb77ad6003ae4bb8a
benwa <btellier@linagora.com>
2017-02-22 10:59:51 +0700

    JAMES-1945 Introduce JamesCollectors::chunker

---
+++ server/container/util-java8/src/main/java/org/apache/james/util/streams/JamesCollectors.java
@@ -0,0 +1,36 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.util.streams;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import com.google.common.base.Preconditions;
+
+public class JamesCollectors {
+    public static <D> Collector<D, ?, Map<Integer, List<D>>> chunker(int chunkSize) {
+        Preconditions.checkArgument(chunkSize > 0, "ChunkSize should be strictly positive");
+        AtomicInteger counter = new AtomicInteger(-1);
+        return Collectors.groupingBy(x -> counter.incrementAndGet() / chunkSize);
+    }
+}
