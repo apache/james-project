1e2d9f3b9c2be0a4e379ccddea8154053306a166
Antoine Duprat <aduprat@linagora.com>
2017-02-23 16:02:44 +0100

    JAMES-1950 Introduce metrics logger implementation

---
+++ metrics/metrics-logger/src/main/java/org/apache/james/metrics/logger/DefaultMetricFactory.java
@@ -0,0 +1,41 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.metrics.logger;
+
+import org.apache.james.metrics.api.Metric;
+import org.apache.james.metrics.api.MetricFactory;
+import org.apache.james.metrics.api.TimeMetric;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultMetricFactory implements MetricFactory {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(DefaultMetricFactory.class);
+
+    @Override
+    public Metric generate(String name) {
+        return new DefaultMetric();
+    }
+
+    @Override
+    public TimeMetric timer(String name) {
+        return new DefaultTimeMetric(name);
+    }
+
+}
---
+++ metrics/metrics-logger/src/main/java/org/apache/james/metrics/logger/DefaultTimeMetric.java
@@ -0,0 +1,49 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.metrics.logger;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.james.metrics.api.TimeMetric;
+
+import com.google.common.base.Stopwatch;
+
+public class DefaultTimeMetric implements TimeMetric {
+
+    private final String name;
+    private final Stopwatch stopwatch;
+
+    public DefaultTimeMetric(String name) {
+        this.name = name;
+        this.stopwatch = Stopwatch.createStarted();
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public long stopAndPublish() {
+        long elapsed = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+        DefaultMetricFactory.LOGGER.info("Time spent in " + name + ": " + elapsed + " ms.");
+        return elapsed;
+    }
+
+}
