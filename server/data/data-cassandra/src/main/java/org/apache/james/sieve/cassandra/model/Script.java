e4e1462a85151401e80b6ddb9bd801b31941f3bc
benwa <btellier@linagora.com>
2017-03-16 20:24:24 +0700

    JAMES-1968 Script model

---
+++ server/data/data-cassandra/src/main/java/org/apache/james/sieve/cassandra/model/Script.java
@@ -0,0 +1,115 @@
+
+
+package org.apache.james.sieve.cassandra.model;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import org.apache.james.sieverepository.api.ScriptSummary;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+
+public class Script {
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String name;
+        private String content;
+        private Optional<Boolean> isActive = Optional.empty();
+        private Optional<Long> size = Optional.empty();
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder copyOf(Script script) {
+            this.name = script.getName();
+            this.content = script.getContent();
+            this.isActive = Optional.of(script.isActive());
+            this.size = Optional.of(script.getSize());
+            return this;
+        }
+
+        public Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder size(long size) {
+            this.size = Optional.of(size);
+            return this;
+        }
+
+        public Builder isActive(boolean isActive) {
+            this.isActive = Optional.of(isActive);
+            return this;
+        }
+
+        public Script build() {
+            Preconditions.checkState(name != null);
+            Preconditions.checkState(content != null);
+            Preconditions.checkState(isActive.isPresent());
+
+            return new Script(name,
+                content,
+                isActive.get(),
+                size.orElse((long) content.getBytes(Charsets.UTF_8).length));
+        }
+
+    }
+
+    private final String name;
+    private final String content;
+    private final boolean isActive;
+    private final long size;
+
+    private Script(String name, String content, boolean isActive, long size) {
+        this.name = name;
+        this.content = content;
+        this.isActive = isActive;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public ScriptSummary toSummary() {
+        return new ScriptSummary(name, isActive);
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o instanceof Script) {
+            Script that = (Script) o;
+
+            return Objects.equals(this.name, that.name)
+                && Objects.equals(this.content, that.content)
+                && Objects.equals(this.isActive, that.isActive)
+                && Objects.equals(this.size, that.size);
+        }
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(name, content, isActive, size);
+    }
+}
---
+++ server/data/data-cassandra/src/test/java/org/apache/james/sieve/cassandra/model/ScriptTest.java
@@ -0,0 +1,234 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.sieve.cassandra.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.james.sieverepository.api.ScriptSummary;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.google.common.base.Charsets;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+public class ScriptTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void shouldMatchBeanContract() {
+        EqualsVerifier.forClass(Script.class).verify();
+    }
+
+    @Test
+    public void buildShouldThrowOnMissingContent() {
+        expectedException.expect(IllegalStateException.class);
+
+        Script.builder()
+            .name("name")
+            .isActive(false)
+            .build();
+    }
+
+    @Test
+    public void buildShouldThrowOnMissingActivation() {
+        expectedException.expect(IllegalStateException.class);
+
+        Script.builder()
+            .name("name")
+            .content("content")
+            .build();
+    }
+
+    @Test
+    public void buildShouldThrowOnMissingName() {
+        expectedException.expect(IllegalStateException.class);
+
+        Script.builder()
+            .content("content")
+            .isActive(false)
+            .build();
+    }
+
+    @Test
+    public void buildShouldPreserveName() {
+        String name = "name";
+        assertThat(
+            Script.builder()
+                .name(name)
+                .content("content")
+                .isActive(false)
+                .build()
+                .getName())
+            .isEqualTo(name);
+    }
+
+    @Test
+    public void buildShouldPreserveContent() {
+        String content = "content";
+        assertThat(
+            Script.builder()
+                .name("name")
+                .content(content)
+                .isActive(false)
+                .build()
+                .getContent())
+            .isEqualTo(content);
+    }
+
+    @Test
+    public void buildShouldPreserveActiveWhenFalse() {
+        assertThat(
+            Script.builder()
+                .name("name")
+                .content("content")
+                .isActive(false)
+                .build()
+                .isActive())
+            .isFalse();
+    }
+
+    @Test
+    public void buildShouldPreserveActiveWhenTrue() {
+        assertThat(
+            Script.builder()
+                .name("name")
+                .content("content")
+                .isActive(true)
+                .build()
+                .isActive())
+            .isTrue();
+    }
+
+    @Test
+    public void buildShouldComputeSizeWhenAbsent() {
+        String content = "content";
+        assertThat(
+            Script.builder()
+                .name("name")
+                .content(content)
+                .isActive(true)
+                .build()
+                .getSize())
+            .isEqualTo(content.getBytes(Charsets.UTF_8).length);
+    }
+
+    @Test
+    public void buildShouldPreserveSize() {
+        long size = 48L;
+        assertThat(
+            Script.builder()
+                .name("name")
+                .content("content")
+                .isActive(true)
+                .size(size)
+                .build()
+                .getSize())
+            .isEqualTo(size);
+    }
+
+    @Test
+    public void toSummaryShouldWork() {
+        String name = "name";
+        boolean isActive = true;
+        assertThat(
+            Script.builder()
+                .name(name)
+                .content("content")
+                .isActive(isActive)
+                .size(48L)
+                .build()
+                .toSummary())
+            .isEqualTo(new ScriptSummary(name, isActive));
+    }
+
+    @Test
+    public void copyOfShouldAllowModifyingName() {
+        String content = "content";
+        String newName = "newName";
+
+        Script originalScript = Script.builder()
+            .name("name")
+            .content(content)
+            .isActive(true)
+            .build();
+
+        assertThat(
+            Script.builder()
+                .copyOf(originalScript)
+                .name(newName)
+                .build())
+            .isEqualTo(Script.builder()
+                .name(newName)
+                .content(content)
+                .isActive(true)
+                .build());
+    }
+
+    @Test
+    public void copyOfShouldAllowModifyingActivation() {
+        String content = "content";
+        String name = "name";
+
+        Script originalScript = Script.builder()
+            .name(name)
+            .content(content)
+            .isActive(true)
+            .build();
+
+        assertThat(
+            Script.builder()
+                .copyOf(originalScript)
+                .isActive(false)
+                .build())
+            .isEqualTo(Script.builder()
+                .name(name)
+                .content(content)
+                .isActive(false)
+                .build());
+    }
+
+    @Test
+    public void copyOfShouldAllowModifyingContent() {
+        String name = "name";
+        String content = "content";
+
+        Script originalScript = Script.builder()
+            .name(name)
+            .content(content)
+            .isActive(true)
+            .build();
+
+        assertThat(
+            Script.builder()
+                .copyOf(originalScript)
+                .content(content)
+                .build())
+            .isEqualTo(Script.builder()
+                .name(name)
+                .content(content)
+                .isActive(true)
+                .build());
+    }
+
+}
