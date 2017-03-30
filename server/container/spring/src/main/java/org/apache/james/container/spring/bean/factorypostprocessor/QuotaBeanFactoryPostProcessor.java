54c6402fdbb40532b32db3092abbdeeb30d68126
benwa <btellier@linagora.com>
2017-03-30 08:02:19 +0700

    MAILBOX-291 Review constants in Quota Bean spring stuff

--- server/container/spring/src/main/java/org/apache/james/container/spring/bean/factorypostprocessor/QuotaBeanFactoryPostProcessor.java
+++ server/container/spring/src/main/java/org/apache/james/container/spring/bean/factorypostprocessor/QuotaBeanFactoryPostProcessor.java
@@ -30,19 +30,21 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 
 public class QuotaBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
-    private static final String IN_MEMORY = "inmemory";
-    private static final String CASSANDRA = "cassandra";
-    private static final String FAKE = "fake";
+    private static final String IN_MEMORY_IMPLEMENTATION = "inmemory";
+    private static final String CASSANDRA_IMPLEMENTATION = "cassandra";
+    private static final String FAKE_IMPLEMENTATION = "fake";
     private static final String MAX_QUOTA_MANAGER = "maxQuotaManager";
-    private static final String CURRENT_QUOTA_MANAGER = "currentQuotaManager";
-    private static final String QUOTA_MANAGER = "quotaManager";
-    private static final String QUOTA_UPDATER = "quotaUpdater";
+    private static final String JPA_IMPLEMENTATION = "jpa";
+    private static final String DEFAULT_IMPLEMENTATION = "default";
+
+    private static final String QUOTA_MANAGER_BEAN = "quotaManager";
+    private static final String QUOTA_UPDATER_BEAN = "quotaUpdater";
+    private static final String QUOTA_ROOT_RESOLVER_BEAN = "quotaRootResolver";
+    private static final String CURRENT_QUOTA_MANAGER_BEAN = "currentQuotaManager";
+    private static final String UPDATES_BEAN = "updates";
+
     private static final String PROVIDER = "provider";
-    private static final String DEFAULT = "default";
-    private static final String UPDATES = "updates";
-    private static final String QUOTA_ROOT_RESOLVER = "quotaRootResolver";
     private static final String EVENT = "event";
-    public static final String JPA = "jpa";
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
@@ -50,11 +52,11 @@ public class QuotaBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
         try {
             HierarchicalConfiguration config = confProvider.getConfiguration("quota");
 
-            String quotaRootResolver = config.configurationAt(QUOTA_ROOT_RESOLVER).getString(PROVIDER, DEFAULT);
-            String currentQuotaManager = config.configurationAt(CURRENT_QUOTA_MANAGER).getString(PROVIDER, "none");
-            String maxQuotaManager = config.configurationAt(MAX_QUOTA_MANAGER).getString(PROVIDER, FAKE);
-            String quotaManager = config.configurationAt(QUOTA_MANAGER).getString(PROVIDER, FAKE);
-            String quotaUpdater = config.configurationAt(UPDATES).getString(PROVIDER, FAKE);
+            String quotaRootResolver = config.configurationAt(QUOTA_ROOT_RESOLVER_BEAN).getString(PROVIDER, DEFAULT_IMPLEMENTATION);
+            String currentQuotaManager = config.configurationAt(CURRENT_QUOTA_MANAGER_BEAN).getString(PROVIDER, "none");
+            String maxQuotaManager = config.configurationAt(MAX_QUOTA_MANAGER).getString(PROVIDER, FAKE_IMPLEMENTATION);
+            String quotaManager = config.configurationAt(QUOTA_MANAGER_BEAN).getString(PROVIDER, FAKE_IMPLEMENTATION);
+            String quotaUpdater = config.configurationAt(UPDATES_BEAN).getString(PROVIDER, FAKE_IMPLEMENTATION);
 
             BeanDefinitionRegistry registry = (BeanDefinitionRegistry) beanFactory;
 
@@ -70,32 +72,32 @@ public class QuotaBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
     private void registerAliasForQuotaUpdater(String quotaUpdater, BeanDefinitionRegistry registry) {
         if (quotaUpdater.equalsIgnoreCase(EVENT)) {
-            registry.registerAlias("eventQuotaUpdater", QUOTA_UPDATER);
-        } else if (quotaUpdater.equalsIgnoreCase(FAKE)) {
-            registry.registerAlias("noQuotaUpdater", QUOTA_UPDATER);
+            registry.registerAlias("eventQuotaUpdater", QUOTA_UPDATER_BEAN);
+        } else if (quotaUpdater.equalsIgnoreCase(FAKE_IMPLEMENTATION)) {
+            registry.registerAlias("noQuotaUpdater", QUOTA_UPDATER_BEAN);
         } else {
             throw new FatalBeanException("Unreadable value for Quota Updater : " + quotaUpdater);
         }
     }
 
     private void registerAliasForQuotaManager(String quotaManager, BeanDefinitionRegistry registry) {
-        if (quotaManager.equalsIgnoreCase(FAKE)) {
-            registry.registerAlias("noQuotaManager", QUOTA_MANAGER);
+        if (quotaManager.equalsIgnoreCase(FAKE_IMPLEMENTATION)) {
+            registry.registerAlias("noQuotaManager", QUOTA_MANAGER_BEAN);
         } else if (quotaManager.equalsIgnoreCase("store")) {
-            registry.registerAlias("storeQuotaManager", QUOTA_MANAGER);
+            registry.registerAlias("storeQuotaManager", QUOTA_MANAGER_BEAN);
         } else {
             throw new FatalBeanException("Unreadable value for Quota Manager : " + quotaManager);
         }
     }
 
     private void registerAliasForMaxQuotaManager(String maxQuotaManager, BeanDefinitionRegistry registry) {
-        if (maxQuotaManager.equalsIgnoreCase(FAKE)) {
+        if (maxQuotaManager.equalsIgnoreCase(FAKE_IMPLEMENTATION)) {
             registry.registerAlias("noMaxQuotaManager", MAX_QUOTA_MANAGER);
-        } else if (maxQuotaManager.equalsIgnoreCase(IN_MEMORY)) {
+        } else if (maxQuotaManager.equalsIgnoreCase(IN_MEMORY_IMPLEMENTATION)) {
             registry.registerAlias("inMemoryMaxQuotaManager", MAX_QUOTA_MANAGER);
-        } else if (maxQuotaManager.equalsIgnoreCase(CASSANDRA)) {
+        } else if (maxQuotaManager.equalsIgnoreCase(CASSANDRA_IMPLEMENTATION)) {
             registry.registerAlias("cassandraMaxQuotaManager", MAX_QUOTA_MANAGER);
-        } else if (maxQuotaManager.equalsIgnoreCase(JPA)) {
+        } else if (maxQuotaManager.equalsIgnoreCase(JPA_IMPLEMENTATION)) {
             registry.registerAlias("jpaMaxQuotaManager", MAX_QUOTA_MANAGER);
         } else {
             throw new FatalBeanException("Unreadable value for Max Quota Manager : " + maxQuotaManager);
@@ -103,20 +105,20 @@ public class QuotaBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
     }
 
     private void registerAliasForCurrentQuotaManager(String currentQuotaManager, BeanDefinitionRegistry registry) {
-        if (currentQuotaManager.equalsIgnoreCase(IN_MEMORY)) {
-            registry.registerAlias("inMemoryCurrentQuotaManager", CURRENT_QUOTA_MANAGER);
-        } else if (currentQuotaManager.equalsIgnoreCase(CASSANDRA)) {
-            registry.registerAlias("cassandraCurrentQuotaManager", CURRENT_QUOTA_MANAGER);
-        }  else if (currentQuotaManager.equalsIgnoreCase(JPA)) {
-            registry.registerAlias("jpaCurrentQuotaManager", CURRENT_QUOTA_MANAGER);
+        if (currentQuotaManager.equalsIgnoreCase(IN_MEMORY_IMPLEMENTATION)) {
+            registry.registerAlias("inMemoryCurrentQuotaManager", CURRENT_QUOTA_MANAGER_BEAN);
+        } else if (currentQuotaManager.equalsIgnoreCase(CASSANDRA_IMPLEMENTATION)) {
+            registry.registerAlias("cassandraCurrentQuotaManager", CURRENT_QUOTA_MANAGER_BEAN);
+        }  else if (currentQuotaManager.equalsIgnoreCase(JPA_IMPLEMENTATION)) {
+            registry.registerAlias("jpaCurrentQuotaManager", CURRENT_QUOTA_MANAGER_BEAN);
         } else if (! currentQuotaManager.equalsIgnoreCase("none")) {
             throw new FatalBeanException("Unreadable value for Current Quota Manager : " + currentQuotaManager);
         }
     }
 
     private void registerAliasForQuotaRootResolver(String quotaRootResolver, BeanDefinitionRegistry registry) {
-        if (quotaRootResolver.equals(DEFAULT)) {
-            registry.registerAlias("defaultQuotaRootResolver", QUOTA_ROOT_RESOLVER);
+        if (quotaRootResolver.equals(DEFAULT_IMPLEMENTATION)) {
+            registry.registerAlias("defaultQuotaRootResolver", QUOTA_ROOT_RESOLVER_BEAN);
         } else {
             throw new FatalBeanException("Unreadable value for QUOTA ROOT resolver : " + quotaRootResolver);
         }
