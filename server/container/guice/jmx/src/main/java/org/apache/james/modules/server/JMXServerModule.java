336d02e7a70b5947b4c236ef74b9e85906c7f013
benwa <btellier@linagora.com>
2017-03-31 19:03:37 +0700

    JAMES-1982 Allow Quota and Sieve administration threw JMX

--- server/container/guice/jmx/src/main/java/org/apache/james/modules/server/JMXServerModule.java
+++ server/container/guice/jmx/src/main/java/org/apache/james/modules/server/JMXServerModule.java
@@ -26,6 +26,8 @@ import org.apache.james.adapter.mailbox.MailboxCopierManagementMBean;
 import org.apache.james.adapter.mailbox.MailboxManagerManagement;
 import org.apache.james.adapter.mailbox.MailboxManagerManagementMBean;
 import org.apache.james.adapter.mailbox.MailboxManagerResolver;
+import org.apache.james.adapter.mailbox.QuotaManagement;
+import org.apache.james.adapter.mailbox.QuotaManagementMBean;
 import org.apache.james.adapter.mailbox.ReIndexerManagement;
 import org.apache.james.adapter.mailbox.ReIndexerManagementMBean;
 import org.apache.james.domainlist.api.DomainListManagementMBean;
@@ -39,6 +41,8 @@ import org.apache.james.mailetcontainer.api.jmx.MailSpoolerMBean;
 import org.apache.james.mailetcontainer.impl.JamesMailSpooler;
 import org.apache.james.rrt.api.RecipientRewriteTableManagementMBean;
 import org.apache.james.rrt.lib.RecipientRewriteTableManagement;
+import org.apache.james.sieverepository.api.SieveRepositoryManagementMBean;
+import org.apache.james.sieverepository.lib.SieveRepositoryManagement;
 import org.apache.james.user.api.UsersRepositoryManagementMBean;
 import org.apache.james.user.lib.UsersRepositoryManagement;
 import org.apache.james.utils.ConfigurationPerformer;
@@ -60,6 +64,8 @@ public class JMXServerModule extends AbstractModule {
     private static final String JMX_COMPONENT_NAME_MAILBOXMANAGERBEAN = "org.apache.james:type=component,name=mailboxmanagerbean";
     private static final String JMX_COMPONENT_MAILBOXCOPIER = "org.apache.james:type=component,name=mailboxcopier";
     private static final String JMX_COMPONENT_REINDEXER = "org.apache.james:type=component,name=reindexerbean";
+    private final static String JMX_COMPONENT_QUOTA = "org.apache.james:type=component,name=quotamanagerbean";
+    private final static String JMX_COMPONENT_SIEVE = "org.apache.james:type=component,name=sievemanagerbean";
 
     @Override
     protected void configure() {
@@ -73,6 +79,8 @@ public class JMXServerModule extends AbstractModule {
         bind(MailSpoolerMBean.class).to(JamesMailSpooler.class);
         bind(ReIndexer.class).annotatedWith(Names.named("reindexer")).to(ReIndexerImpl.class);
         bind(ReIndexerManagementMBean.class).to(ReIndexerManagement.class);
+        bind(QuotaManagementMBean.class).to(QuotaManagement.class);
+        bind(SieveRepositoryManagementMBean.class).to(SieveRepositoryManagement.class);
         Multibinder.newSetBinder(binder(), ConfigurationPerformer.class).addBinding().to(JMXModuleConfigurationPerformer.class);
     }
 
@@ -86,6 +94,8 @@ public class JMXServerModule extends AbstractModule {
         private final MailboxManagerManagementMBean mailboxManagerManagementMBean;
         private final MailboxCopierManagementMBean mailboxCopierManagementMBean;
         private final ReIndexerManagementMBean reIndexerManagementMBean;
+        private final QuotaManagementMBean quotaManagementMBean;
+        private final SieveRepositoryManagementMBean sieveRepositoryManagementMBean;
 
         @Inject
         public JMXModuleConfigurationPerformer(JMXServer jmxServer,
@@ -94,7 +104,9 @@ public class JMXServerModule extends AbstractModule {
                                                RecipientRewriteTableManagementMBean recipientRewriteTableManagementMBean,
                                                MailboxManagerManagementMBean mailboxManagerManagementMBean,
                                                MailboxCopierManagementMBean mailboxCopierManagementMBean,
-                                               ReIndexerManagementMBean reIndexerManagementMBean) {
+                                               ReIndexerManagementMBean reIndexerManagementMBean,
+                                               QuotaManagementMBean quotaManagementMBean,
+                                               SieveRepositoryManagementMBean sieveRepositoryManagementMBean) {
             this.jmxServer = jmxServer;
             this.domainListManagementMBean = domainListManagementMBean;
             this.usersRepositoryManagementMBean = usersRepositoryManagementMBean;
@@ -102,6 +114,8 @@ public class JMXServerModule extends AbstractModule {
             this.mailboxManagerManagementMBean = mailboxManagerManagementMBean;
             this.mailboxCopierManagementMBean = mailboxCopierManagementMBean;
             this.reIndexerManagementMBean = reIndexerManagementMBean;
+            this.quotaManagementMBean = quotaManagementMBean;
+            this.sieveRepositoryManagementMBean = sieveRepositoryManagementMBean;
         }
 
         @Override
@@ -114,6 +128,8 @@ public class JMXServerModule extends AbstractModule {
                 jmxServer.register(JMX_COMPONENT_NAME_MAILBOXMANAGERBEAN, mailboxManagerManagementMBean);
                 jmxServer.register(JMX_COMPONENT_MAILBOXCOPIER, mailboxCopierManagementMBean);
                 jmxServer.register(JMX_COMPONENT_REINDEXER, reIndexerManagementMBean);
+                jmxServer.register(JMX_COMPONENT_QUOTA, quotaManagementMBean);
+                jmxServer.register(JMX_COMPONENT_SIEVE, sieveRepositoryManagementMBean);
             } catch (Exception e) {
                 Throwables.propagate(e);
             }
