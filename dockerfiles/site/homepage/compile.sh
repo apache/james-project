c5e8a2b026c5375f367431ee5095ba2313efffe4
quynhn <qnguyen@linagora.com>
2017-04-11 16:19:33 +0700

    JAMES-1994 Add more docker site to build the homepage

---
+++ dockerfiles/site/homepage/compile.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -e
+#
+
+printUsage() {
+   echo "Usage : "
+   echo "./compile.sh SHA1"
+   echo "    SHA1: SHA1 to build (optional)"
+   exit 1
+}
+
+ORIGIN=/origin
+DESTINATION=/destination
+
+for arg in "$@"
+do
+   case $arg in
+      -*)
+         echo "Invalid option: -$OPTARG"
+         printUsage
+         ;;
+      *)
+         if ! [ -z "$1" ]; then
+            SHA1=$1
+         fi
+         ;;
+   esac
+   if [ "0" -lt "$#" ]; then
+      shift
+   fi
+done
+
+if [ -z "$SHA1" ]; then
+   SHA1=master
+fi
+
+# Sources retrieval
+git clone $ORIGIN/.
+git checkout $SHA1
+
+# Compilation /james-project/src/jekyll/_site
+
+jekyll build --source src/homepage --destination $DESTINATION
+
