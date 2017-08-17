bc50d81afd4eec1dbd9246060b5b6822005b5340
benwa <btellier@linagora.com>
2017-08-17 10:25:55 +0700

    JAMES-2122 Adding a log on Cassandra cluster fallback

--- backends-common/cassandra/src/main/java/org/apache/james/backends/cassandra/init/ClusterBuilder.java
+++ backends-common/cassandra/src/main/java/org/apache/james/backends/cassandra/init/ClusterBuilder.java
@@ -23,6 +23,8 @@ import java.util.Collection;
 import java.util.Optional;
 
 import org.apache.james.util.Host;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.PoolingOptions;
@@ -32,7 +34,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 
 public class ClusterBuilder {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClusterBuilder.class);
     private final static String DEFAULT_CLUSTER_IP = "localhost";
     public static final int DEFAULT_CASSANDRA_PORT = 9042;
 
@@ -189,8 +191,14 @@ public class ClusterBuilder {
     }
 
     private Collection<Host> getServersFromHostAndPort() {
-        String host = this.host.orElse(DEFAULT_CLUSTER_IP);
-        int port = this.port.orElse(DEFAULT_CASSANDRA_PORT);
+        String host = this.host.orElseGet(() -> {
+            LOGGER.info("No cassandra host specified. Falling back to {}", DEFAULT_CLUSTER_IP);
+            return DEFAULT_CLUSTER_IP;
+        });
+        int port = this.port.orElseGet(() -> {
+            LOGGER.info("No cassandra port specified. Falling back to {}", DEFAULT_CASSANDRA_PORT);
+            return DEFAULT_CASSANDRA_PORT;
+        });
 
         return ImmutableList.of(Host.from(host, port));
     }
