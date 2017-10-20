4667548abe259c69cf1b3e4dc89f422da944c711
quynhn <qnguyen@linagora.com>
2017-10-20 17:55:50 +0700

    JAMES-2183 Allows checking message by its id and mailbox

--- server/testing/src/main/java/org/apache/james/utils/IMAPMessageReader.java
+++ server/testing/src/main/java/org/apache/james/utils/IMAPMessageReader.java
@@ -22,9 +22,11 @@ package org.apache.james.utils;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.apache.commons.net.imap.IMAPClient;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Splitter;
 
 public class IMAPMessageReader implements Closeable {
@@ -32,6 +34,11 @@ public class IMAPMessageReader implements Closeable {
     private static final String INBOX = "INBOX";
 
     private final IMAPClient imapClient;
+
+    @VisibleForTesting
+    IMAPMessageReader(IMAPClient imapClient) {
+        this.imapClient = imapClient;
+    }
 
     public IMAPMessageReader(String host, int port) throws IOException {
         imapClient = new IMAPClient();
@@ -63,6 +70,22 @@ public class IMAPMessageReader implements Closeable {
         imapClient.fetch("1:1", "ALL");
         return imapClient.getReplyString()
             .contains("OK FETCH completed");
+    }
+
+    public boolean userReceivedMessageWithFlagsInMailbox(String user, String password, String mailbox, String flags) throws IOException {
+        connectAndSelect(user, password, mailbox);
+        imapClient.fetch("1:1", "ALL");
+        String replyString = imapClient.getReplyString();
+        return isCompletedWithFlags(flags, replyString);
+    }
+
+    @VisibleForTesting
+    boolean isCompletedWithFlags(String flags, String replyString) {
+        return replyString.contains("OK FETCH completed") &&
+            Splitter.on(" ")
+            .splitToList(flags)
+            .stream()
+            .allMatch(s -> replyString.contains(s));
     }
 
     public boolean userGetNotifiedForNewMessagesWhenSelectingMailbox(String user, String password, int numOfNewMessage, String mailboxName) throws IOException {
