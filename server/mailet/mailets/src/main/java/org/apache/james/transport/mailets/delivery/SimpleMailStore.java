03e3851ba841cf724ba18e730cf3740b6f232ba9
Daniel Trebbien <dtrebbien@gmail.com>
2017-10-20 10:31:53 -0500

    JAMES-2199 Use a placeholder

--- server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/delivery/SimpleMailStore.java
+++ server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/delivery/SimpleMailStore.java
@@ -110,9 +110,7 @@ public class SimpleMailStore implements MailStore {
         try {
             return usersRepository.getUser(recipient);
         } catch (UsersRepositoryException e) {
-            if (LOGGER.isWarnEnabled()) {
-                LOGGER.warn("Unable to retrieve username for " + recipient.asPrettyString(), e);
-            }
+            LOGGER.warn("Unable to retrieve username for {}", recipient.asPrettyString(), e);
             return recipient.toString();
         }
     }
