a7959458f4a7605544209ebfcc1ec8a3a147e50b
Raphael Ouazana <raphael.ouazana@linagora.com>
2017-12-07 15:08:41 +0100

    JAMES-2254 Introduce RPM packaging

--- dockerfiles/packaging/package.sh
+++ dockerfiles/packaging/package.sh
@@ -14,10 +14,6 @@ fi
 
 RELEASE=$1
 ITERATION=$2
-cp /jars/james-server-cassandra-guice.jar /debian/package/usr/share/james/james-server.jar
-cp -r /jars/james-server-cassandra-guice.lib /debian/package/usr/share/james/
-cp /jars/james-server-cli.jar /debian/package/usr/share/james/james-cli.jar
-cp -r /jars/james-server-cli.lib /debian/package/usr/share/james/
 
 fpm -s dir -t deb \
  -n james \
@@ -27,7 +23,6 @@ fpm -s dir -t deb \
  -C package \
  --deb-systemd james.service \
  --after-install james.postinst \
- --before-remove james.prerm \
  --provides mail-transport-agent \
  --provides default-mta \
  --iteration $ITERATION \
@@ -39,4 +34,28 @@ fpm -s dir -t deb \
  --category web \
  .
 
-cp /debian/james*.deb /result/
+#Workaround waiting for https://github.com/jordansissel/fpm/issues/1163 to be released
+cp james.service package/usr/share/james
+
+fpm -s dir -t rpm \
+ -n james \
+ -v $RELEASE \
+ -a x86_64 \
+ -d java-1.8.0-openjdk-headless \
+ -C package \
+ --after-install james.rpm.postinst \
+ --after-upgrade james.rpm.postupgrade \
+ --after-remove james.rpm.postremove \
+ --provides mail-transport-agent \
+ --provides default-mta \
+ --iteration $ITERATION \
+ --license http://www.apache.org/licenses/LICENSE-2.0 \
+ --description "$(printf "James stands for Java Apache Mail Enterprise Server!\nIt has a modular architecture based on a rich set of modern and efficient components which provides at the end complete, stable, secure and extendable Mail Servers running on the JVM.")" \
+ --vendor "Apache" \
+ --maintainer "Apache" \
+ --url http://james.apache.org/ \
+ --category web \
+ .
+
+cp /packages/james*.deb /result/
+cp /packages/james*.rpm /result/
