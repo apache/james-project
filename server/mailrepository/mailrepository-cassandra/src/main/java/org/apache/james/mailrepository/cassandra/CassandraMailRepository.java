a7aad0bee8cca3335bcd81d49d71f442cf498aa2
Raphael Ouazana <raphael.ouazana@linagora.com>
2018-01-23 17:00:09 +0100

    JAMES-2291 Introduce skeleton project for Cassandra mail repository

---
+++ server/mailrepository/mailrepository-cassandra/src/main/java/org/apache/james/mailrepository/cassandra/CassandraMailRepository.java
@@ -0,0 +1,75 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailrepository.cassandra;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.mail.MessagingException;
+
+import org.apache.james.mailrepository.api.MailRepository;
+import org.apache.mailet.Mail;
+
+public class CassandraMailRepository implements MailRepository {
+
+    public CassandraMailRepository() {
+    }
+
+    @Override
+    public void store(Mail mail) {
+    }
+
+    @Override
+    public Iterator<String> list() {
+        return null;
+    }
+
+    @Override
+    public Mail retrieve(String key) {
+        return null;
+    }
+
+    @Override
+    public void remove(Mail mail) {
+    }
+
+    @Override
+    public void remove(Collection<Mail> toRemove) {
+    }
+
+    @Override
+    public void remove(String key) {
+    }
+
+    @Override
+    public long size() throws MessagingException {
+        return 0;
+    }
+
+    @Override
+    public boolean lock(String key) {
+        return false;
+    }
+
+    @Override
+    public boolean unlock(String key) {
+        return false;
+    }
+}
