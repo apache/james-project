fac7e596baf4e54ec94a5d612bf2e6c9da91c1fa
benwa <btellier@linagora.com>
2018-01-25 10:22:00 +0700

    JAMES-2294 Fix taskIdDTO Location header

--- server/protocols/webadmin/webadmin-core/src/test/java/org/apache/james/webadmin/dto/TaskIdDtoTest.java
+++ server/protocols/webadmin/webadmin-core/src/test/java/org/apache/james/webadmin/dto/TaskIdDtoTest.java
@@ -20,34 +20,31 @@
 package org.apache.james.webadmin.dto;
 
 import static org.eclipse.jetty.http.HttpHeader.LOCATION;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import java.util.UUID;
 
 import org.apache.james.task.TaskId;
-import org.apache.james.webadmin.routes.TasksRoutes;
 import org.eclipse.jetty.http.HttpStatus;
+import org.junit.Test;
 
 import spark.Response;
 
-public class TaskIdDto {
+public class TaskIdDtoTest {
+    private static final String UID_VALUE = "ce5316cb-c924-40eb-9ca0-c5828e276297";
 
-    public static TaskIdDto respond(Response response, TaskId taskId) {
-        response.status(HttpStatus.CREATED_201);
-        response.header(LOCATION.asString(), TasksRoutes.BASE + "/" + taskId.getValue());
-        return TaskIdDto.from(taskId);
+    @Test
+    public void respondShouldReturnCreatedWithTaskIdHeader() {
+        Response response = mock(Response.class);
+        TaskId taskId = new TaskId(UUID.fromString(UID_VALUE));
+
+        TaskIdDto.respond(response, taskId);
+
+        verify(response).status(HttpStatus.CREATED_201);
+        verify(response).header(LOCATION.asString(), "/tasks/" + UID_VALUE);
+        verifyNoMoreInteractions(response);
     }
 
-    public static TaskIdDto from(TaskId id) {
-        return new TaskIdDto(id.getValue());
-    }
-
-    private final UUID uuid;
-
-    public TaskIdDto(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    public UUID getTaskId() {
-        return uuid;
-    }
 }
