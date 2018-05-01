8202f46b1442c30c48b786bd6dbf894d2ee014a0
Simon Levesque <simon@foilen.com>
2018-05-01 07:30:13 -0400

    JAMES-2386 Datasource validation is configurable

--- server/container/guice/jpa-guice/src/main/java/org/apache/james/JPAConfiguration.java
+++ server/container/guice/jpa-guice/src/main/java/org/apache/james/JPAConfiguration.java
@@ -18,10 +18,13 @@
  ****************************************************************/
 package org.apache.james;
 
+import org.apache.james.backends.jpa.JPAConstants;
+
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 
 public class JPAConfiguration {
+
     public static Builder builder() {
         return new Builder();
     }
@@ -29,6 +32,10 @@ public class JPAConfiguration {
     public static class Builder {
         private String driverName;
         private String driverURL;
+        private boolean testOnBorrow;
+        private int validationQueryTimeoutSec = JPAConstants.VALIDATION_NO_TIMEOUT;
+        private String validationQuery;
+
 
         public Builder driverName(String driverName) {
             this.driverName = driverName;
@@ -40,19 +47,41 @@ public class JPAConfiguration {
             return this;
         }
 
+        public Builder testOnBorrow(boolean testOnBorrow) {
+            this.testOnBorrow = testOnBorrow;
+            return this;
+        }
+
+        public Builder validationQueryTimeoutSec(int validationQueryTimeoutSec) {
+            this.validationQueryTimeoutSec = validationQueryTimeoutSec;
+            return this;
+        }
+
+        public Builder validationQuery(String validationQuery) {
+            this.validationQuery = validationQuery;
+            return this;
+        }
+
         public JPAConfiguration build() {
             Preconditions.checkNotNull(driverName);
             Preconditions.checkNotNull(driverURL);
-            return new JPAConfiguration(driverName, driverURL);
+            return new JPAConfiguration(driverName, driverURL, testOnBorrow, validationQueryTimeoutSec, validationQuery);
         }
     }
 
     private final String driverName;
     private final String driverURL;
+    private final boolean testOnBorrow;
+    private final int validationQueryTimeoutSec;
+    private final String validationQuery;
 
-    @VisibleForTesting JPAConfiguration(String driverName, String driverURL) {
+    @VisibleForTesting
+    JPAConfiguration(String driverName, String driverURL, boolean testOnBorrow, int validationQueryTimeoutSec, String validationQuery) {
         this.driverName = driverName;
         this.driverURL = driverURL;
+        this.testOnBorrow = testOnBorrow;
+        this.validationQueryTimeoutSec = validationQueryTimeoutSec;
+        this.validationQuery = validationQuery;
     }
 
     public String getDriverName() {
@@ -61,6 +90,18 @@ public class JPAConfiguration {
 
     public String getDriverURL() {
         return driverURL;
+    }
+
+    public boolean isTestOnBorrow() {
+        return testOnBorrow;
+    }
+
+    public int getValidationQueryTimeoutSec() {
+        return validationQueryTimeoutSec;
+    }
+
+    public String getValidationQuery() {
+        return validationQuery;
     }
 
 }
