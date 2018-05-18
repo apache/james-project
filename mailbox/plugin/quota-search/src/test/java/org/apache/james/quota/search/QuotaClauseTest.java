fec172dc5464368f4c7793df71ba5f376c8ffe65
Antoine Duprat <aduprat@linagora.com>
2018-05-18 12:50:38 +0200

    JAMES-2400 Introduce QuotaQuery

---
+++ mailbox/plugin/quota-search/src/test/java/org/apache/james/quota/search/QuotaClauseTest.java
@@ -0,0 +1,121 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.quota.search;
+
+import static org.apache.james.quota.search.QuotaBoundaryFixture._50;
+import static org.apache.james.quota.search.QuotaBoundaryFixture._75;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+
+import org.apache.james.core.Domain;
+import org.junit.jupiter.api.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+public class QuotaClauseTest implements QuotaClause {
+    
+    @Test
+    public void lessThanShouldMatchBeanContract() {
+        EqualsVerifier.forClass(QuotaClause.LessThan.class)
+            .allFieldsShouldBeUsed()
+            .verify();
+    }
+    
+    @Test
+    public void moreThanShouldMatchBeanContract() {
+        EqualsVerifier.forClass(QuotaClause.MoreThan.class)
+            .allFieldsShouldBeUsed()
+            .verify();
+    }
+    
+    @Test
+    public void hasDomainShouldMatchBeanContract() {
+        EqualsVerifier.forClass(QuotaClause.HasDomain.class)
+            .allFieldsShouldBeUsed()
+            .verify();
+    }
+    
+    @Test
+    public void andShouldMatchBeanContract() {
+        EqualsVerifier.forClass(QuotaClause.And.class)
+            .allFieldsShouldBeUsed()
+            .verify();
+    }
+
+    @Test
+    public void lessThanClauseShouldThrowWhenQuotaThresholdIsNull() {
+        assertThatThrownBy(() -> QuotaClause.lessThan(null))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void moreThanClauseShouldThrowWhenQuotaThresholdIsNull() {
+        assertThatThrownBy(() -> QuotaClause.moreThan(null))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void hasDomainClauseShouldThrowWhenDomainIsNull() {
+        assertThatThrownBy(() -> QuotaClause.hasDomain(null))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void andClauseShouldThrowWhenDomainIsNull() {
+        List<QuotaClause> clauses = null;
+        assertThatThrownBy(() -> QuotaClause.and(clauses))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void lessThanClauseShouldInstanciate() {
+        QuotaClause.LessThan lessThanQuery = QuotaClause.lessThan(_50);
+
+        assertThat(lessThanQuery.getQuotaBoundary()).isEqualTo(_50);
+    }
+
+    @Test
+    public void moreThanClauseShouldInstanciate() {
+        QuotaClause.MoreThan moreThanQuery = QuotaClause.moreThan(_50);
+
+        assertThat(moreThanQuery.getQuotaBoundary()).isEqualTo(_50);
+    }
+
+    @Test
+    public void hasDomainClauseShouldInstanciate() {
+        Domain domain = Domain.of("domain.org");
+
+        QuotaClause.HasDomain hasDomainQuery = QuotaClause.hasDomain(domain);
+
+        assertThat(hasDomainQuery.getDomain()).isEqualTo(domain);
+    }
+
+    @Test
+    public void andClauseShouldInstanciate() {
+        QuotaClause.MoreThan first = QuotaClause.moreThan(_50);
+        QuotaClause.MoreThan second = QuotaClause.moreThan(_75);
+
+        QuotaClause.And andQuery = QuotaClause.and(first, second);
+
+        assertThat(andQuery.getClauses())
+            .containsExactly(first, second);
+    }
+}
---
+++ mailbox/plugin/quota-search/src/test/java/org/apache/james/quota/search/QuotaQueryTest.java
@@ -0,0 +1,76 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.quota.search;
+
+import static org.apache.james.quota.search.QuotaBoundaryFixture._50;
+import static org.apache.james.quota.search.QuotaBoundaryFixture._75;
+
+import org.apache.james.core.Domain;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+public class QuotaQueryTest {
+
+    @Test
+    public void shouldMatchBeanContract() {
+        EqualsVerifier.forClass(QuotaQuery.class)
+            .allFieldsShouldBeUsed()
+            .verify();
+    }
+
+    @Test
+    public void builderShouldProvideDefaults() {
+        QuotaQuery quotaQuery = QuotaQuery.builder()
+            .build();
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(quotaQuery.getClause()).isEqualTo(QuotaClause.and());
+        softly.assertThat(quotaQuery.getLimit()).isEqualTo(Limit.unlimited());
+        softly.assertThat(quotaQuery.getOffset()).isEqualTo(Offset.none());
+        softly.assertAll();
+    }
+
+    @Test
+    public void builderShouldUseProvidedValues() {
+        Limit limit = Limit.of(10);
+
+        Offset offset = Offset.of(50);
+        QuotaQuery quotaQuery = QuotaQuery.builder()
+            .withLimit(limit)
+            .withOffset(offset)
+            .lessThan(_75)
+            .moreThan(_50)
+            .hasDomain(Domain.LOCALHOST)
+            .build();
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(quotaQuery.getClause())
+            .isEqualTo(QuotaClause.and(
+                QuotaClause.lessThan(_75),
+                QuotaClause.moreThan(_50),
+                QuotaClause.hasDomain(Domain.LOCALHOST)));
+        softly.assertThat(quotaQuery.getLimit()).isEqualTo(limit);
+        softly.assertThat(quotaQuery.getOffset()).isEqualTo(offset);
+        softly.assertAll();
+    }
+
+}
