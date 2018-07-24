e6381b41f527674e5d0a5bd4335c4ad7907a5fa9
Gautier DI FOLCO <gdifolco@linagora.com>
2018-07-24 14:38:53 +0200

    JAMES-2497 Remove h2

--- backends-common/jpa/src/test/java/org/apache/james/backends/jpa/JpaTestCluster.java
+++ backends-common/jpa/src/test/java/org/apache/james/backends/jpa/JpaTestCluster.java
@@ -38,8 +38,8 @@ public class JpaTestCluster {
 
     public static JpaTestCluster create(List<Class<?>> clazz) {
         HashMap<String, String> properties = new HashMap<>();
-        properties.put("openjpa.ConnectionDriverName", org.h2.Driver.class.getName());
-        properties.put("openjpa.ConnectionURL", "jdbc:h2:mem:mailboxintegrationtesting;DB_CLOSE_DELAY=-1"); // Memory H2 database
+        properties.put("openjpa.ConnectionDriverName", org.apache.derby.jdbc.EmbeddedDriver.class.getName());
+        properties.put("openjpa.ConnectionURL", "jdbc:derby:memory:mailboxintegrationtesting;create=true"); // Memory Derby database
         properties.put("openjpa.jdbc.SynchronizeMappings", "buildSchema(ForeignKeys=true)"); // Create Foreign Keys
         properties.put("openjpa.jdbc.MappingDefaults", "ForeignKeyDeleteAction=restrict, JoinForeignKeyDeleteAction=restrict");
         properties.put("openjpa.jdbc.SchemaFactory", "native(ForeignKeys=true)");
