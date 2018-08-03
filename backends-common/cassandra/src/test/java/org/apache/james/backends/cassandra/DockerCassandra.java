de0fa8a3df69f50cbc0684dfb1b911ad497856d7
Benoit Tellier <btellier@linagora.com>
2018-08-03 16:34:18 +0700

    JAMES-2514 Update to latest Cassandra 3.11.3

--- backends-common/cassandra/src/test/java/org/apache/james/backends/cassandra/DockerCassandra.java
+++ backends-common/cassandra/src/test/java/org/apache/james/backends/cassandra/DockerCassandra.java
@@ -50,10 +50,10 @@ public class DockerCassandra {
         int cassandraMemory = 1000;
         long cassandraContainerMemory = Float.valueOf(cassandraMemory * 1.2f * 1024 * 1024L).longValue();
         cassandraContainer = new GenericContainer<>(
-            new ImageFromDockerfile("cassandra_3_11_1", deleteOnExit)
+            new ImageFromDockerfile("cassandra_3_11_3", deleteOnExit)
                 .withDockerfileFromBuilder(builder ->
                     builder
-                        .from("cassandra:3.11.1")
+                        .from("cassandra:3.11.3")
                         .env("ENV CASSANDRA_CONFIG", "/etc/cassandra")
                         .run("echo \"-Xms" + cassandraMemory + "M\" >> " + JVM_OPTIONS)
                         .run("echo \"-Xmx" + cassandraMemory + "M\" >> " + JVM_OPTIONS)
