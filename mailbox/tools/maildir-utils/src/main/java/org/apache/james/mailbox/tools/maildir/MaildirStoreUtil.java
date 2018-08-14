77fdac54977be411208fb5a2abf5e500fb8db4c2
Benoit Tellier <btellier@linagora.com>
2018-08-14 17:21:56 +0700

    JAMES-2521 Move Maildir utils as a separate tool project

--- mailbox/tools/maildir-utils/src/main/java/org/apache/james/mailbox/tools/maildir/MaildirStoreUtil.java
+++ mailbox/tools/maildir-utils/src/main/java/org/apache/james/mailbox/tools/maildir/MaildirStoreUtil.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
-package org.apache.james.adapter.mailbox.maildir;
+package org.apache.james.mailbox.tools.maildir;
 
 import java.io.FileNotFoundException;
 
