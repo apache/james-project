58f0f800632189fda5fd1a4b9d5cfd89e991f2c4
Matthieu Baechler <matthieu@apache.org>
2018-08-22 16:08:23 +0200

    JAMES-2528 implementation of a FilteringManagement API using Event Sourcing

---
+++ server/data/data-jmap/src/main/java/org/apache/james/jmap/api/filtering/impl/DefineRulesCommand.java
@@ -0,0 +1,76 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.api.filtering.impl;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.james.core.User;
+import org.apache.james.eventsourcing.Command;
+import org.apache.james.jmap.api.filtering.Rule;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+
+public class DefineRulesCommand implements Command {
+
+    private final User user;
+    private final List<Rule> rules;
+
+    public DefineRulesCommand(User user, List<Rule> rules) {
+        Preconditions.checkNotNull(user);
+        Preconditions.checkNotNull(rules);
+
+        this.user = user;
+        this.rules = rules;
+    }
+
+    public List<Rule> getRules() {
+        return rules;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o instanceof DefineRulesCommand) {
+            DefineRulesCommand that = (DefineRulesCommand) o;
+
+            return Objects.equals(this.user, that.user)
+                && Objects.equals(this.rules, that.rules);
+        }
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(user, rules);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("user", user)
+            .add("rules", rules)
+            .toString();
+    }
+}
---
+++ server/data/data-jmap/src/main/java/org/apache/james/jmap/api/filtering/impl/FilteringAggregate.java
@@ -0,0 +1,90 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.api.filtering.impl;
+
+import java.util.List;
+
+import org.apache.james.eventsourcing.Event;
+import org.apache.james.eventsourcing.eventstore.History;
+import org.apache.james.jmap.api.filtering.Rule;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+
+public class FilteringAggregate {
+
+    public static FilteringAggregate load(FilteringAggregateId aggregateId, History eventsOfAggregate) {
+        return new FilteringAggregate(aggregateId, eventsOfAggregate);
+    }
+
+    private static class State {
+
+        static State initial() {
+            return new State(ImmutableList.of());
+        }
+
+        final ImmutableList<Rule> rules;
+
+        private State(ImmutableList<Rule> rules) {
+            this.rules = rules;
+        }
+
+        State set(ImmutableList<Rule> rules) {
+            return new State(rules);
+        }
+    }
+
+    private final FilteringAggregateId aggregateId;
+    private final History history;
+    private State state;
+
+    private FilteringAggregate(FilteringAggregateId aggregateId, History history) {
+        this.aggregateId = aggregateId;
+        this.state = State.initial();
+        history.getEvents().forEach(this::apply);
+        this.history = history;
+    }
+
+    public List<? extends Event> defineRules(List<Rule> rules) {
+        Preconditions.checkArgument(shouldNotContainDuplicates(rules));
+        ImmutableList<RuleSetDefined> events = ImmutableList.of(
+            new RuleSetDefined(aggregateId, history.getNextEventId(), ImmutableList.copyOf(rules)));
+        events.forEach(this::apply);
+        return events;
+    }
+
+    private boolean shouldNotContainDuplicates(List<Rule> rules) {
+        long uniqueIdCount = rules.stream()
+            .map(Rule::getId)
+            .distinct()
+            .count();
+        return uniqueIdCount == rules.size();
+    }
+
+    public List<Rule> listRules() {
+        return state.rules;
+    }
+
+    private void apply(Event event) {
+        if (event instanceof RuleSetDefined) {
+            state = state.set(((RuleSetDefined)event).getRules());
+        }
+    }
+}
