6d0f22de8c20172dfe3402b085f61035fd38d6b0
Benoit Tellier <btellier@linagora.com>
2018-09-06 12:14:12 +0700

    JAMES-2541 Implement a MimeMessageStore on top of the blob store

---
+++ server/blob/mail-store/src/main/java/org/apache/james/blob/mail/MimeMessageStore.java
@@ -0,0 +1,133 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.blob.mail;
+
+import static org.apache.commons.io.output.NullOutputStream.NULL_OUTPUT_STREAM;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.james.blob.api.BlobStore;
+import org.apache.james.blob.api.Store;
+import org.apache.james.util.BodyOffsetInputStream;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+
+public class MimeMessageStore extends Store.Impl<MimeMessage> {
+    public static final BlobType HEADER_BLOB_TYPE = new BlobType("mailHeader");
+    public static final BlobType BODY_BLOB_TYPE = new BlobType("mailBody");
+
+    static class MailEncoder implements Encoder<MimeMessage> {
+        @Override
+        public Map<BlobType, InputStream> encode(MimeMessage message) {
+            try {
+                byte[] messageAsArray = messageToArray(message);
+                int bodyStartOctet = computeBodyStartOctet(messageAsArray);
+                return ImmutableMap.of(
+                    HEADER_BLOB_TYPE, new ByteArrayInputStream(getHeaderBytes(messageAsArray, bodyStartOctet)),
+                    BODY_BLOB_TYPE, new ByteArrayInputStream(getBodyBytes(messageAsArray, bodyStartOctet)));
+            } catch (MessagingException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private static byte[] messageToArray(MimeMessage message) throws IOException, MessagingException {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            message.writeTo(byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
+        }
+
+        private static byte[] getHeaderBytes(byte[] messageContentAsArray, int bodyStartOctet) {
+            ByteBuffer headerContent = ByteBuffer.wrap(messageContentAsArray, 0, bodyStartOctet);
+            byte[] headerBytes = new byte[bodyStartOctet];
+            headerContent.get(headerBytes);
+            return headerBytes;
+        }
+
+        private static byte[] getBodyBytes(byte[] messageContentAsArray, int bodyStartOctet) {
+            if (bodyStartOctet < messageContentAsArray.length) {
+                ByteBuffer bodyContent = ByteBuffer.wrap(messageContentAsArray,
+                    bodyStartOctet,
+                    messageContentAsArray.length - bodyStartOctet);
+                byte[] bodyBytes = new byte[messageContentAsArray.length - bodyStartOctet];
+                bodyContent.get(bodyBytes);
+                return bodyBytes;
+            } else {
+                return new byte[] {};
+            }
+        }
+
+        private static int computeBodyStartOctet(byte[] messageAsArray) throws IOException {
+            try (BodyOffsetInputStream bodyOffsetInputStream =
+                     new BodyOffsetInputStream(new ByteArrayInputStream(messageAsArray))) {
+                consume(bodyOffsetInputStream);
+
+                if (bodyOffsetInputStream.getBodyStartOffset() == -1) {
+                    return 0;
+                }
+                return (int) bodyOffsetInputStream.getBodyStartOffset();
+            }
+        }
+
+        private static void consume(InputStream in) throws IOException {
+            IOUtils.copy(in, NULL_OUTPUT_STREAM);
+        }
+    }
+
+    static class MailDecoder implements Decoder<MimeMessage> {
+        @Override
+        public MimeMessage decode(Map<BlobType, InputStream> streams) {
+            Preconditions.checkNotNull(streams);
+            Preconditions.checkArgument(streams.containsKey(HEADER_BLOB_TYPE));
+            Preconditions.checkArgument(streams.containsKey(BODY_BLOB_TYPE));
+
+            return toMimeMessage(
+                new SequenceInputStream(
+                    streams.get(HEADER_BLOB_TYPE),
+                    streams.get(BODY_BLOB_TYPE)));
+        }
+
+        private MimeMessage toMimeMessage(InputStream inputStream) {
+            try {
+                return new MimeMessage(Session.getInstance(new Properties()), inputStream);
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Inject
+    public MimeMessageStore(BlobStore blobStore) {
+        super(new MailEncoder(), new MailDecoder(), blobStore);
+    }
+}
---
+++ server/blob/mail-store/src/test/java/org/apache/james/blob/mail/MimeMessageStoreTest.java
@@ -0,0 +1,130 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.blob.mail;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import javax.mail.internet.MimeMessage;
+
+import org.apache.james.blob.api.BlobId;
+import org.apache.james.blob.api.HashBlobId;
+import org.apache.james.blob.api.Store;
+import org.apache.james.blob.memory.MemoryBlobStore;
+import org.apache.james.core.builder.MimeMessageBuilder;
+import org.apache.james.util.MimeMessageUtil;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MimeMessageStoreTest {
+    private static final HashBlobId.Factory BLOB_ID_FACTORY = new HashBlobId.Factory();
+
+    private MimeMessageStore testee;
+    private MemoryBlobStore blobStore;
+
+    @BeforeEach
+    void setUp() {
+        blobStore = new MemoryBlobStore(BLOB_ID_FACTORY);
+        testee = new MimeMessageStore(blobStore);
+    }
+
+    @Test
+    void saveShouldThrowWhenNull() {
+        assertThatThrownBy(() -> testee.save(null))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void readShouldThrowWhenNull() {
+        assertThatThrownBy(() -> testee.read(null))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void mailStoreShouldPreserveContent() throws Exception {
+        MimeMessage message = MimeMessageBuilder.mimeMessageBuilder()
+            .addFrom("any@any.com")
+            .addToRecipient("toddy@any.com")
+            .setSubject("Important Mail")
+            .setText("Important mail content")
+            .build();
+
+        Map<Store.BlobType, BlobId> parts = testee.save(message);
+
+        MimeMessage retrievedMessage = testee.read(parts);
+
+        assertThat(MimeMessageUtil.asString(retrievedMessage))
+            .isEqualTo(MimeMessageUtil.asString(message));
+    }
+
+    @Test
+    void mailStoreShouldPreserveMailWithoutBody() throws Exception {
+        MimeMessage message = MimeMessageBuilder.mimeMessageBuilder()
+            .addFrom("any@any.com")
+            .addToRecipient("toddy@any.com")
+            .setSubject("Important Mail")
+            .build();
+
+        Map<Store.BlobType, BlobId> parts = testee.save(message);
+
+        MimeMessage retrievedMessage = testee.read(parts);
+
+        assertThat(MimeMessageUtil.asString(retrievedMessage))
+            .isEqualTo(MimeMessageUtil.asString(message));
+    }
+
+    @Test
+    void saveShouldSeparateHeadersAndBodyInDifferentBlobs() throws Exception {
+        MimeMessage message = MimeMessageBuilder.mimeMessageBuilder()
+            .addHeader("Date", "Thu, 6 Sep 2018 13:29:13 +0700 (ICT)")
+            .addHeader("Message-ID", "<84739718.0.1536215353507@localhost.localdomain>")
+            .addFrom("any@any.com")
+            .addToRecipient("toddy@any.com")
+            .setSubject("Important Mail")
+            .setText("Important mail content")
+            .build();
+
+        Map<Store.BlobType, BlobId> parts = testee.save(message);
+
+        SoftAssertions.assertSoftly(
+            softly -> {
+                softly.assertThat(parts).containsKeys(MimeMessageStore.HEADER_BLOB_TYPE, MimeMessageStore.BODY_BLOB_TYPE);
+
+                BlobId headerBlobId = parts.get(MimeMessageStore.HEADER_BLOB_TYPE);
+                BlobId bodyBlobId = parts.get(MimeMessageStore.BODY_BLOB_TYPE);
+
+                softly.assertThat(new String(blobStore.readBytes(headerBlobId).join(), StandardCharsets.UTF_8))
+                    .isEqualTo("Date: Thu, 6 Sep 2018 13:29:13 +0700 (ICT)\r\n" +
+                        "From: any@any.com\r\n" +
+                        "To: toddy@any.com\r\n" +
+                        "Message-ID: <84739718.0.1536215353507@localhost.localdomain>\r\n" +
+                        "Subject: Important Mail\r\n" +
+                        "MIME-Version: 1.0\r\n" +
+                        "Content-Type: text/plain; charset=UTF-8\r\n" +
+                        "Content-Transfer-Encoding: 7bit\r\n\r\n");
+                softly.assertThat(new String(blobStore.readBytes(bodyBlobId).join(), StandardCharsets.UTF_8))
+                    .isEqualTo("Important mail content");
+            });
+    }
+}
