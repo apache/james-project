7c57ae178a75ccbb63e12df59ed431f31ee644d6
Jean Helou <jhe@codamens.fr>
2018-09-10 16:08:07 +0200

    JAMES-2525 enforce singleton swift container for test

--- server/blob/blob-objectstorage/src/test/java/org/apache/james/blob/objectstorage/DockerSwiftExtension.java
+++ server/blob/blob-objectstorage/src/test/java/org/apache/james/blob/objectstorage/DockerSwiftExtension.java
@@ -19,73 +19,27 @@
 
 package org.apache.james.blob.objectstorage;
 
-import static org.testcontainers.containers.wait.strategy.Wait.forHttp;
-
-import java.net.URI;
-
-import org.apache.james.util.docker.RateLimiters;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.Container;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 
 public class DockerSwiftExtension implements ParameterResolver, BeforeAllCallback,
     AfterAllCallback {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DockerSwiftExtension.class);
-    private static final String SWIFT_DOCKER_IMAGE = "jeantil/openstack-keystone-swift:pike";
-    private static final int SWIFT_PORT = 8080;
-    private static final int KEYSTONE_ADMIN_PORT = 35357;
 
-    private final GenericContainer<?> swiftContainer;
-
+    private final DockerSwiftRule swiftContainer;
     private DockerSwift dockerSwift;
 
     public DockerSwiftExtension() {
-        swiftContainer = new GenericContainer<>(SWIFT_DOCKER_IMAGE)
-            .withExposedPorts(KEYSTONE_ADMIN_PORT)
-            .withExposedPorts(SWIFT_PORT)
-            .waitingFor(
-                new WaitAllStrategy().withStrategy(
-                    forHttp("/v3")
-                        .forPort(KEYSTONE_ADMIN_PORT)
-                        .forStatusCode(200)
-                        .withRateLimiter(RateLimiters.DEFAULT)
-                ).withStrategy(
-                    forHttp("/info")
-                        .forPort(SWIFT_PORT)
-                        .forStatusCode(200)
-                        .withRateLimiter(RateLimiters.DEFAULT)
-                )
-            );
+        swiftContainer = new DockerSwiftRule();
     }
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
         swiftContainer.start();
-        Integer keystonePort = swiftContainer.getMappedPort(KEYSTONE_ADMIN_PORT);
-        Integer swiftPort = swiftContainer.getMappedPort(SWIFT_PORT);
-        String containerIpAddress = swiftContainer.getContainerIpAddress();
-        Container.ExecResult execResult = swiftContainer.execInContainer("/swift/bin/register-swift-endpoint.sh", "http://" + containerIpAddress + ":" + swiftPort);
-        if (!execResult.getStdout().isEmpty()) {
-            LOGGER.debug(execResult.getStdout());
-        }
-        if (!execResult.getStderr().isEmpty()) {
-            LOGGER.error(execResult.getStderr());
-        }
-        URI keystoneV2Endpoint =
-            URI.create("http://" + containerIpAddress + ":" + keystonePort + "/v2.0");
-        URI keystoneV3Endpoint =
-            URI.create("http://" + containerIpAddress + ":" + keystonePort + "/v3");
-        URI swiftEndpoint =
-            URI.create("http://" + containerIpAddress + ":" + swiftPort + "/auth/v1.0");
-        dockerSwift = new DockerSwift(keystoneV2Endpoint, keystoneV3Endpoint, swiftEndpoint);
+        dockerSwift = swiftContainer.dockerSwift();
     }
 
     @Override
