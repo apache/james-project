cdf0bd4c04e7b97d2624c12cad1838308e353095
Antoine Duprat <aduprat@linagora.com>
2018-10-10 15:24:26 +0200

    JAMES-2559 Add original branch parameter in merging tool

--- dockerfiles/merge/merge.sh
+++ dockerfiles/merge/merge.sh
@@ -2,30 +2,39 @@
 
 printUsage() {
    echo "Usage : "
-   echo "./merge.sh SHA1 RESULTING_BRANCH"
-   echo "    SHA1: SHA1 to merge with master"
+   echo "./merge.sh SHA1 RESULTING_BRANCH ORIGINAL_BRANCH"
+   echo "    SHA1: SHA1 to merge with the branch"
    echo "    RESULTING_BRANCH : Resulting branch of the merge"
+   echo "    ORIGINAL_BRANCH: the original branch used for merge (if none, then `master` will be used)"
    exit 1
 }
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
     printUsage
 fi
 
 SHA1=$1
 RESULTING_BRANCH=$2
+if [ "$#" -eq 3 ]; then
+    ORIGINAL_BRANCH=$3
+else
+    ORIGINAL_BRANCH=master
+fi
 
 APACHE_REPO=`git remote show | grep apache || true`
 if [ -z "$APACHE_REPO" ]; then
     git remote add apache https://github.com/apache/james-project.git
 fi 
+# Getting original branch from apache repo
 git fetch apache
-git checkout apache/master -b master
+git checkout apache/$ORIGINAL_BRANCH -B $ORIGINAL_BRANCH
 
+# Getting the branch to be merged from /origin
 git fetch origin
 git checkout $SHA1
 git checkout -b SHA1_BRANCH
 
-git checkout master
+# Merging the branch to be merged in the original branch
+git checkout $ORIGINAL_BRANCH
 git checkout -b $RESULTING_BRANCH
 git merge --no-edit SHA1_BRANCH
