962683d38715e7a40286e422fc0487c495a47c32
Benoit Tellier <btellier@linagora.com>
2018-10-11 09:19:44 +0700

    JAMES-2555 Proves our fix helps reindexing big mailboxes
    Before the fix the added test was failing with OutOfMemory exception after 300
    messages. Now it passes easily 1000 messages.

---
+++ mailbox/tools/indexer/src/test/java/org/apache/mailbox/tools/indexer/CassandraReIndexerImplTest.java
@@ -0,0 +1,119 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.mailbox.tools.indexer;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+import org.apache.james.backends.cassandra.CassandraCluster;
+import org.apache.james.backends.cassandra.DockerCassandraRule;
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.MessageManager;
+import org.apache.james.mailbox.cassandra.CassandraMailboxManager;
+import org.apache.james.mailbox.cassandra.CassandraMailboxManagerProvider;
+import org.apache.james.mailbox.cassandra.mail.MailboxAggregateModule;
+import org.apache.james.mailbox.indexer.ReIndexer;
+import org.apache.james.mailbox.model.MailboxPath;
+import org.apache.james.mailbox.store.MailboxSessionMapperFactory;
+import org.apache.james.mailbox.store.mail.model.Mailbox;
+import org.apache.james.mailbox.store.mail.model.MailboxMessage;
+import org.apache.james.mailbox.store.search.ListeningMessageSearchIndex;
+import org.apache.james.util.concurrency.ConcurrentTestRunner;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import com.google.common.base.Strings;
+
+public class CassandraReIndexerImplTest {
+    private static final String USERNAME = "benwa@apache.org";
+    public static final MailboxPath INBOX = MailboxPath.forUser(USERNAME, "INBOX");
+    private CassandraMailboxManager mailboxManager;
+    private ListeningMessageSearchIndex messageSearchIndex;
+
+    private ReIndexer reIndexer;
+
+    @ClassRule
+    public static DockerCassandraRule cassandraServer = new DockerCassandraRule();
+
+    private static CassandraCluster cassandra;
+
+    @BeforeClass
+    public static void setUpClass() {
+        cassandra = CassandraCluster.create(MailboxAggregateModule.MODULE_WITH_QUOTA, cassandraServer.getHost());
+    }
+
+    @Before
+    public void setUp() {
+        mailboxManager = CassandraMailboxManagerProvider.provideMailboxManager(cassandra.getConf(), cassandra.getTypesProvider());
+        MailboxSessionMapperFactory mailboxSessionMapperFactory = mailboxManager.getMapperFactory();
+        messageSearchIndex = mock(ListeningMessageSearchIndex.class);
+        reIndexer = new ReIndexerImpl(mailboxManager, messageSearchIndex, mailboxSessionMapperFactory);
+    }
+
+    @After
+    public void tearDown() {
+        cassandra.clearTables();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        cassandra.closeCluster();
+    }
+
+    @Test
+    public void reIndexShouldBeWellPerformed() throws Exception {
+        // Given a mailbox with 1000 messages * 150 KB
+        MailboxSession systemSession = mailboxManager.createSystemSession(USERNAME);
+        mailboxManager.createMailbox(INBOX, systemSession);
+
+        byte[] bigBody = (Strings.repeat("header: value\r\n", 10000) + "\r\nbody").getBytes(StandardCharsets.UTF_8);
+
+        int threadCount = 10;
+        int operationCount = 100;
+        MessageManager mailbox = mailboxManager.getMailbox(INBOX, systemSession);
+        ConcurrentTestRunner.builder()
+            .operation((a, b) -> mailbox
+                .appendMessage(
+                    MessageManager.AppendCommand.builder().build(bigBody),
+                    systemSession))
+            .threadCount(threadCount)
+            .operationCount(operationCount)
+            .runSuccessfullyWithin(Duration.ofMinutes(10));
+
+        // When We re-index
+        reIndexer.reIndex(INBOX);
+
+        // The indexer is called for each message
+        verify(messageSearchIndex).deleteAll(any(MailboxSession.class), any(Mailbox.class));
+        verify(messageSearchIndex, times(threadCount * operationCount))
+            .add(any(MailboxSession.class), any(Mailbox.class),any(MailboxMessage.class));
+        verifyNoMoreInteractions(messageSearchIndex);
+    }
+}
