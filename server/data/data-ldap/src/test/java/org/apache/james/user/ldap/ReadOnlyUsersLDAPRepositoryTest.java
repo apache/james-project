ffded8479d75afe29abacc2bb66b5b0eeebd81b1
datph <dphamhoang@linagora.com>
2018-11-29 15:37:46 +0700

    JAMES-2615 Merge data-ldap and data-ldap-integration-test

--- server/data/data-ldap/src/test/java/org/apache/james/user/ldap/ReadOnlyUsersLDAPRepositoryTest.java
+++ server/data/data-ldap/src/test/java/org/apache/james/user/ldap/ReadOnlyUsersLDAPRepositoryTest.java
@@ -19,28 +19,54 @@
 
 package org.apache.james.user.ldap;
 
+import static org.apache.james.user.ldap.DockerLdapSingleton.ADMIN_PASSWORD;
+import static org.apache.james.user.ldap.DockerLdapSingleton.DOMAIN;
+import static org.apache.james.user.ldap.DockerLdapSingleton.JAMES_USER;
+import static org.apache.james.user.ldap.DockerLdapSingleton.PASSWORD;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
-import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.ConversionException;
 import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.apache.commons.configuration.plist.PropertyListConfiguration;
+import org.apache.james.core.MailAddress;
 import org.apache.james.domainlist.api.DomainList;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ReadOnlyUsersLDAPRepositoryTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReadOnlyUsersLDAPRepositoryTest.class);
+    private static final String JAMES_USER_MAIL = JAMES_USER + "@" + DOMAIN;
+    private static final String UNKNOWN = "unknown";
+    private static final String BAD_PASSWORD = "badpassword";
+
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
+
+    @ClassRule
+    public static LdapGenericContainer ldapContainer = LdapGenericContainer.builder()
+        .domain(DOMAIN)
+        .password(ADMIN_PASSWORD)
+        .build();
+
+    private ReadOnlyUsersLDAPRepository ldapRepository;
     private DomainList domainList;
 
     @Before
     public void setUp() {
         domainList = mock(DomainList.class);
+    }
+
+    private void startUsersRepository(HierarchicalConfiguration ldapRepositoryConfiguration) throws Exception {
+        ldapRepository = new ReadOnlyUsersLDAPRepository(domainList);
+        ldapRepository.configure(ldapRepositoryConfiguration);
+        ldapRepository.init();
     }
 
     @Test
@@ -85,11 +111,11 @@ public class ReadOnlyUsersLDAPRepositoryTest {
         usersLDAPRepository.configure(configuration);
     }
 
-    private HierarchicalConfiguration ldapRepositoryConfiguration() throws ConfigurationException {
+    private HierarchicalConfiguration ldapRepositoryConfiguration() {
         PropertyListConfiguration configuration = new PropertyListConfiguration();
-        configuration.addProperty("[@ldapHost]", "ldap://127.0.0.1");
+        configuration.addProperty("[@ldapHost]", ldapContainer.getLdapHost());
         configuration.addProperty("[@principal]", "cn=admin\\,dc=james\\,dc=org");
-        configuration.addProperty("[@credentials]", "secret");
+        configuration.addProperty("[@credentials]", ADMIN_PASSWORD);
         configuration.addProperty("[@userBase]", "ou=People\\,dc=james\\,dc=org");
         configuration.addProperty("[@userIdAttribute]", "uid");
         configuration.addProperty("[@userObjectClass]", "inetOrgPerson");
@@ -100,4 +126,104 @@ public class ReadOnlyUsersLDAPRepositoryTest {
         return configuration;
     }
 
+    private HierarchicalConfiguration ldapRepositoryConfigurationWithVirtualHosting() {
+        PropertyListConfiguration configuration = new PropertyListConfiguration();
+        configuration.addProperty("[@ldapHost]", ldapContainer.getLdapHost());
+        configuration.addProperty("[@principal]", "cn=admin\\,dc=james\\,dc=org");
+        configuration.addProperty("[@credentials]", ADMIN_PASSWORD);
+        configuration.addProperty("[@userBase]", "ou=People\\,dc=james\\,dc=org");
+        configuration.addProperty("[@userIdAttribute]", "mail");
+        configuration.addProperty("[@userObjectClass]", "inetOrgPerson");
+        configuration.addProperty("[@maxRetries]", "4");
+        configuration.addProperty("[@retryStartInterval]", "0");
+        configuration.addProperty("[@retryMaxInterval]", "8");
+        configuration.addProperty("[@retryIntervalScale]", "1000");
+        configuration.addProperty("supportsVirtualHosting", true);
+        return configuration;
+    }
+
+    @Test
+    public void knownUserShouldBeAbleToLogInWhenPasswordIsCorrect() throws Exception {
+        startUsersRepository(ldapRepositoryConfiguration());
+        assertThat(ldapRepository.test(JAMES_USER, PASSWORD)).isTrue();
+    }
+
+    @Test
+    public void knownUserShouldNotBeAbleToLogInWhenPasswordIsNotCorrect() throws Exception {
+        startUsersRepository(ldapRepositoryConfiguration());
+        assertThat(ldapRepository.test(JAMES_USER, BAD_PASSWORD)).isFalse();
+    }
+
+    @Test
+    public void unknownUserShouldNotBeAbleToLogIn() throws Exception {
+        startUsersRepository(ldapRepositoryConfiguration());
+        assertThat(ldapRepository.test(UNKNOWN, BAD_PASSWORD)).isFalse();
+    }
+
+    @Test
+    public void unknownUserShouldNotBeAbleToLogInWhenPasswordIsCorrect() throws Exception {
+        startUsersRepository(ldapRepositoryConfiguration());
+        assertThat(ldapRepository.test(UNKNOWN, PASSWORD)).isFalse();
+    }
+
+    @Test
+    public void knownUserShouldBeAbleToLogInWhenPasswordIsCorrectWithVirtualHosting() throws Exception {
+        startUsersRepository(ldapRepositoryConfigurationWithVirtualHosting());
+        assertThat(ldapRepository.test(JAMES_USER_MAIL, PASSWORD)).isTrue();
+    }
+
+    @Test
+    public void testShouldStillWorksAfterRestartingLDAP() throws Exception {
+        startUsersRepository(ldapRepositoryConfigurationWithVirtualHosting());
+        ldapRepository.test(JAMES_USER_MAIL, PASSWORD);
+
+        ldapContainer.pause();
+        try {
+            ldapRepository.test(JAMES_USER_MAIL, PASSWORD);
+        } catch (Exception e) {
+            LOGGER.info("This exception is expected as we shut down the LDAP and forced its use", e);
+        }
+        ldapContainer.unpause();
+
+        assertThat(ldapRepository.test(JAMES_USER_MAIL, PASSWORD)).isTrue();
+    }
+
+
+    @Test
+    public void knownUserShouldNotBeAbleToLogInWhenPasswordIsNotCorrectWithVirtualHosting() throws Exception {
+        startUsersRepository(ldapRepositoryConfigurationWithVirtualHosting());
+        assertThat(ldapRepository.test(JAMES_USER, BAD_PASSWORD)).isFalse();
+    }
+
+    @Test
+    public void unknownUserShouldNotBeAbleToLogInWithVirtualHosting() throws Exception {
+        startUsersRepository(ldapRepositoryConfigurationWithVirtualHosting());
+        assertThat(ldapRepository.test(UNKNOWN, BAD_PASSWORD)).isFalse();
+    }
+
+    @Test
+    public void unknownUserShouldNotBeAbleToLogInWhenPasswordIsCorrectWithVirtualHosting() throws Exception {
+        startUsersRepository(ldapRepositoryConfigurationWithVirtualHosting());
+        assertThat(ldapRepository.test(UNKNOWN, PASSWORD)).isFalse();
+    }
+
+    @Test
+    public void containsWithGetUserShouldBeTrue() throws Exception {
+        startUsersRepository(ldapRepositoryConfiguration());
+        assertThat(ldapRepository.contains(ldapRepository.getUser(new MailAddress(JAMES_USER_MAIL)))).isTrue();
+    }
+
+    @Test
+    public void containsWithGetUserShouldBeTrueWithVirtualHosting() throws Exception {
+        startUsersRepository(ldapRepositoryConfigurationWithVirtualHosting());
+        assertThat(ldapRepository.contains(ldapRepository.getUser(new MailAddress(JAMES_USER_MAIL)))).isTrue();
+    }
+
+    @Test
+    public void specialCharacterInUserInputShouldBeSanitized() throws Exception {
+        String patternMatchingMultipleUsers = "j*";
+
+        startUsersRepository(ldapRepositoryConfigurationWithVirtualHosting());
+        assertThat(ldapRepository.test(patternMatchingMultipleUsers, PASSWORD)).isFalse();
+    }
 }
