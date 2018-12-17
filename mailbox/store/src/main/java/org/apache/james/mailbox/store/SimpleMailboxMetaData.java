89917a80718f98db584211ecb005261d293205d2
Benoit Tellier <btellier@linagora.com>
2018-12-17 10:55:53 +0700

    MAILBOX-359 Remove never used factory method in SimpleMailboxMetaData

--- mailbox/store/src/main/java/org/apache/james/mailbox/store/SimpleMailboxMetaData.java
+++ mailbox/store/src/main/java/org/apache/james/mailbox/store/SimpleMailboxMetaData.java
@@ -25,19 +25,10 @@ import org.apache.james.mailbox.model.MailboxMetaData;
 import org.apache.james.mailbox.model.MailboxPath;
 
 public class SimpleMailboxMetaData implements MailboxMetaData, Comparable<MailboxMetaData> {
-
-    public static MailboxMetaData createNoSelect(MailboxPath path, MailboxId mailboxId, char delimiter) {
-        return new SimpleMailboxMetaData(path, mailboxId, delimiter, Children.CHILDREN_ALLOWED_BUT_UNKNOWN, Selectability.NOSELECT);
-    }
-
     private final MailboxPath path;
-
     private final char delimiter;
-
     private final Children inferiors;
-
     private final Selectability selectability;
-
     private final MailboxId mailboxId;
 
     public SimpleMailboxMetaData(MailboxPath path, MailboxId mailboxId, char delimiter) {
