fc2c0c09237831ec6bfc6a076dfe638137e8cee4
Rémi Kowalski <rkowalski@linagora.com>
2019-05-21 11:42:06 +0200

    JAMES-2777 create a MemoryTaskManagerWorker responsible to execute the tasks

---
+++ server/task/src/main/java/org/apache/james/task/MemoryTaskManagerWorker.java
@@ -0,0 +1,114 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.task;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import org.apache.james.util.MDCBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import reactor.core.publisher.Mono;
+
+public class MemoryTaskManagerWorker implements TaskManagerWorker {
+    private static final boolean INTERRUPT_IF_RUNNING = true;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MemoryTaskManagerWorker.class);
+    private final ConcurrentHashMap<TaskId, CompletableFuture<Task.Result>> idToFuture = new ConcurrentHashMap<>();
+
+    @Override
+    public Mono<Task.Result> executeTask(TaskWithId taskWithId, Consumer<TaskExecutionDetailsUpdater> updateDetails) {
+        CompletableFuture<Task.Result> futureResult = CompletableFuture.supplyAsync(() -> runWithMdc(taskWithId, updateDetails));
+
+        idToFuture.put(taskWithId.getId(), futureResult);
+
+        Mono<Task.Result> result = Mono.<Task.Result>fromFuture(futureResult)
+            .doOnError(res -> failed(updateDetails,
+                (logger, details) -> logger.error("Task was partially performed. Check logs for more details")))
+            .doOnTerminate(() -> idToFuture.remove(taskWithId.getId()));
+
+        return result;
+    }
+
+    private Task.Result runWithMdc(TaskWithId taskWithId, Consumer<TaskExecutionDetailsUpdater> updateDetails) {
+        return MDCBuilder.withMdc(
+            MDCBuilder.create()
+                .addContext(Task.TASK_ID, taskWithId.getId())
+                .addContext(Task.TASK_TYPE, taskWithId.getTask().type())
+                .addContext(Task.TASK_DETAILS, taskWithId.getTask().details()),
+            () -> run(taskWithId, updateDetails));
+    }
+
+
+    private Task.Result run(TaskWithId taskWithId, Consumer<TaskExecutionDetailsUpdater> updateDetails) {
+        updateDetails.accept(TaskExecutionDetails::start);
+        try {
+            return taskWithId.getTask()
+                .run()
+                .onComplete(() -> success(updateDetails))
+                .onFailure(() -> failed(updateDetails, (logger, details) -> logger.error("Task was partially performed. Check logs for more details" + details.getTaskId())));
+        } catch (Exception e) {
+            failed(updateDetails,
+                (logger, executionDetails) -> logger.error("Error while running task", executionDetails, e));
+            return Task.Result.PARTIAL;
+        }
+    }
+
+    @Override
+    public void cancelTask(TaskId id, Consumer<TaskExecutionDetailsUpdater> updateDetails) {
+        Optional.ofNullable(idToFuture.remove(id))
+            .ifPresent(future -> {
+                updateDetails.accept(details -> {
+                    if (details.getStatus().equals(TaskManager.Status.WAITING) || details.getStatus().equals(TaskManager.Status.IN_PROGRESS)) {
+                        return details.cancel();
+                    }
+                    return details;
+                });
+                future.cancel(INTERRUPT_IF_RUNNING);
+            });
+    }
+
+    private void success(Consumer<TaskExecutionDetailsUpdater> updateDetails) {
+        updateDetails.accept(currentDetails -> {
+            if (!wasCancelled(currentDetails)) {
+                LOGGER.info("Task success");
+                return currentDetails.completed();
+            }
+            return currentDetails;
+        });
+    }
+
+    private void failed(Consumer<TaskExecutionDetailsUpdater> updateDetails, BiConsumer<Logger, TaskExecutionDetails> logOperation) {
+        updateDetails.accept(currentDetails -> {
+            if (!wasCancelled(currentDetails)) {
+                logOperation.accept(LOGGER, currentDetails);
+                return currentDetails.failed();
+            }
+            return currentDetails;
+        });
+    }
+
+    private boolean wasCancelled(TaskExecutionDetails details) {
+        return details.getStatus() == TaskManager.Status.CANCELLED;
+    }
+
+}
