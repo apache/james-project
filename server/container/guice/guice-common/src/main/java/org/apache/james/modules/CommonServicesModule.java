60d325a8d3ffac5f640bdda41cd28ca410627a4d
Tran Tien Duc <dtran@linagora.com>
2019-05-22 11:19:13 +0700

    JAMES-2767 AsyncTask is not used anywhere else except ES-2 modules
    Remove it

--- server/container/guice/guice-common/src/main/java/org/apache/james/modules/CommonServicesModule.java
+++ server/container/guice/guice-common/src/main/java/org/apache/james/modules/CommonServicesModule.java
@@ -24,7 +24,6 @@ import javax.inject.Singleton;
 
 import org.apache.james.filesystem.api.FileSystem;
 import org.apache.james.filesystem.api.JamesDirectoriesProvider;
-import org.apache.james.modules.server.AsyncTasksExecutorModule;
 import org.apache.james.modules.server.DNSServiceModule;
 import org.apache.james.modules.server.DropWizardMetricsModule;
 import org.apache.james.modules.server.TaskManagerModule;
@@ -58,7 +57,6 @@ public class CommonServicesModule extends AbstractModule {
         install(new StartablesModule());
         install(new PreDestroyModule());
         install(new DNSServiceModule());
-        install(new AsyncTasksExecutorModule());
         install(new DropWizardMetricsModule());
         install(new TaskManagerModule());
         install(new CleanupTaskModule());
