51e2cbe2a4603747a5ff533926448fb4db106e6e
Rémi Kowalski <rkowalski@linagora.com>
2019-05-22 16:47:01 +0200

    JAMES-2777 use junit 5 in server.task project

--- server/task/src/test/java/org/apache/james/task/TaskTest.java
+++ server/task/src/test/java/org/apache/james/task/TaskTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class TaskTest {
 
