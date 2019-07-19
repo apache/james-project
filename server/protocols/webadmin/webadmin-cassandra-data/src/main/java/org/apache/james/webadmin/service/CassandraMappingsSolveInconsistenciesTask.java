8d187c926ccbcdd7b743800123696a4321b0a39b
Gautier DI FOLCO <gdifolco@linagora.com>
2019-07-19 15:47:47 +0200

    JAMES-2813 Add serialization for CassandraMappingsSolveInconsistenciesTask

--- server/protocols/webadmin/webadmin-cassandra-data/src/main/java/org/apache/james/webadmin/service/CassandraMappingsSolveInconsistenciesTask.java
+++ server/protocols/webadmin/webadmin-cassandra-data/src/main/java/org/apache/james/webadmin/service/CassandraMappingsSolveInconsistenciesTask.java
@@ -20,18 +20,46 @@
 package org.apache.james.webadmin.service;
 
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 import javax.inject.Inject;
 
+import org.apache.james.json.DTOModule;
 import org.apache.james.rrt.cassandra.CassandraMappingsSourcesDAO;
 import org.apache.james.rrt.cassandra.migration.MappingsSourcesMigration;
+import org.apache.james.server.task.json.dto.TaskDTO;
+import org.apache.james.server.task.json.dto.TaskDTOModule;
 import org.apache.james.task.Task;
 import org.apache.james.task.TaskExecutionDetails;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import reactor.core.publisher.Mono;
 
 public class CassandraMappingsSolveInconsistenciesTask implements Task {
     public static final String TYPE = "cassandraMappingsSolveInconsistencies";
+
+    private static class CassandraMappingsSolveInconsistenciesTaskDTO implements TaskDTO {
+
+        private final String type;
+
+        public CassandraMappingsSolveInconsistenciesTaskDTO(@JsonProperty("type") String type) {
+            this.type = type;
+        }
+
+        @Override
+        public String getType() {
+            return type;
+        }
+    }
+
+    public static final BiFunction<MappingsSourcesMigration, CassandraMappingsSourcesDAO, TaskDTOModule<CassandraMappingsSolveInconsistenciesTask, CassandraMappingsSolveInconsistenciesTaskDTO>> MODULE = (mappingsSourcesMigration, cassandraMappingsSourcesDAO) ->
+        DTOModule
+            .forDomainObject(CassandraMappingsSolveInconsistenciesTask.class)
+            .convertToDTO(CassandraMappingsSolveInconsistenciesTaskDTO.class)
+            .toDomainObjectConverter(dto -> new CassandraMappingsSolveInconsistenciesTask(mappingsSourcesMigration, cassandraMappingsSourcesDAO))
+            .toDTOConverter((domainObject, typeName) -> new CassandraMappingsSolveInconsistenciesTaskDTO(typeName))
+            .typeName(TYPE)
+            .withFactory(TaskDTOModule::new);
 
     private final Task mappingsSourcesMigration;
     private final CassandraMappingsSourcesDAO cassandraMappingsSourcesDAO;
