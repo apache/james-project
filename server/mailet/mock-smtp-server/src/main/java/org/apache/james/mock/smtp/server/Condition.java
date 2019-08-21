0b62377c2930f3f2bad009db2d9b2c945f478118
Tran Tien Duc <dtran@linagora.com>
2019-08-21 10:22:48 +0700

    JAMES-2864 Condition POJO for matching SMTP lines

---
+++ server/mailet/mock-smtp-server/src/main/java/org/apache/james/mock/smtp/server/Condition.java
@@ -0,0 +1,39 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mock.smtp.server;
+
+import com.google.common.base.Preconditions;
+
+class Condition {
+    private final Operator operator;
+    private final String matchingValue;
+
+    Condition(Operator operator, String matchingValue) {
+        Preconditions.checkNotNull(operator);
+        Preconditions.checkNotNull(matchingValue);
+
+        this.operator = operator;
+        this.matchingValue = matchingValue;
+    }
+
+    boolean matches(String line) {
+        return operator.matches(line, matchingValue);
+    }
+}
---
+++ server/mailet/mock-smtp-server/src/test/java/org/apache/james/mock/smtp/server/ConditionTest.java
@@ -0,0 +1,63 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mock.smtp.server;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class ConditionTest {
+    @Test
+    void constructorShouldThrowWhenNullOperator() {
+        assertThatThrownBy(() -> new Condition(null, "matchingValue"))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void constructorShouldThrowWhenNullMatchingValue() {
+        assertThatThrownBy(() -> new Condition(Operator.CONTAINS, null))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void matchesShouldReturnTrueWhenOperatorMatches() {
+        Condition condition = new Condition(Operator.CONTAINS, "match me");
+
+        assertThat(condition.matches("this contains match me string"))
+            .isTrue();
+    }
+
+    @Test
+    void matchesShouldReturnFalseWhenOperatorDoesNotMatch() {
+        Condition condition = new Condition(Operator.CONTAINS, "match me");
+
+        assertThat(condition.matches("this contains another string"))
+            .isFalse();
+    }
+
+    @Test
+    void matchesShouldThrowWhenNullLine() {
+        Condition condition = new Condition(Operator.CONTAINS, "match me");
+
+        assertThatThrownBy(() -> condition.matches(null))
+            .isInstanceOf(NullPointerException.class);
+    }
+}
