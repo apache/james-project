09a896b53866c6f542c38a58ee94187ac5dcbb45
Gautier DI FOLCO <gdifolco@linagora.com>
2019-09-04 14:06:01 +0200

    JAMES-2813 Add MailboxPathV2MigrationTask.AdditionalInformation serialization

--- mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/mail/migration/MailboxPathV2MigrationTaskSerializationTest.java
+++ mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/mail/migration/MailboxPathV2MigrationTaskSerializationTest.java
@@ -26,7 +26,9 @@ import static org.mockito.Mockito.mock;
 
 import java.io.IOException;
 
+import org.apache.james.server.task.json.JsonTaskAdditionalInformationsSerializer;
 import org.apache.james.server.task.json.JsonTaskSerializer;
+import org.apache.james.task.TaskExecutionDetails;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
@@ -35,8 +37,11 @@ class MailboxPathV2MigrationTaskSerializationTest {
     private static final MailboxPathV2Migration MIGRATION = mock(MailboxPathV2Migration.class);
     private static final MailboxPathV2Migration.MailboxPathV2MigrationTask TASK = new MailboxPathV2Migration.MailboxPathV2MigrationTask(MIGRATION);
     private static final String SERIALIZED_TASK = "{\"type\": \"Cassandra_mailboxPathV2Migration\"}";
+    private static final MailboxPathV2Migration.AdditionalInformation DETAILS = new MailboxPathV2Migration.AdditionalInformation(42L, 10);
+    private static final String SERIALIZED_ADDITIONAL_INFORMATION = "{\"remainingCount\":42,\"initialCount\":10}";
 
     private static final JsonTaskSerializer TASK_SERIALIZER = new JsonTaskSerializer(MailboxPathV2MigrationTaskDTO.MODULE.apply(MIGRATION));
+    private static final JsonTaskAdditionalInformationsSerializer JSON_TASK_ADDITIONAL_INFORMATIONS_SERIALIZER = new JsonTaskAdditionalInformationsSerializer(MailboxPathV2MigrationTaskAdditionalInformationDTO.MODULE);
 
     @Test
     void taskShouldBeSerializable() throws JsonProcessingException {
@@ -48,5 +53,20 @@ class MailboxPathV2MigrationTaskSerializationTest {
     void taskShouldBeDeserializable() throws IOException {
         assertThat(TASK_SERIALIZER.deserialize(SERIALIZED_TASK))
             .isEqualToComparingFieldByField(TASK);
+    }
+
+    @Test
+    void additionalInformationShouldBeSerializable() throws JsonProcessingException {
+        assertThatJson(JSON_TASK_ADDITIONAL_INFORMATIONS_SERIALIZER.serialize(DETAILS)).isEqualTo(SERIALIZED_ADDITIONAL_INFORMATION);
+    }
+
+    @Test
+    void additonalInformationShouldBeDeserializable() throws IOException {
+        TaskExecutionDetails.AdditionalInformation deserialized = JSON_TASK_ADDITIONAL_INFORMATIONS_SERIALIZER.deserialize("Cassandra_mailboxPathV2Migration", SERIALIZED_ADDITIONAL_INFORMATION);
+        assertThat(deserialized).isInstanceOf(MailboxPathV2Migration.AdditionalInformation.class);
+
+        MailboxPathV2Migration.AdditionalInformation additionalInformation = (MailboxPathV2Migration.AdditionalInformation) deserialized;
+        assertThat(additionalInformation.getRemainingCount()).isEqualTo(DETAILS.getRemainingCount());
+        assertThat(additionalInformation.getInitialCount()).isEqualTo(DETAILS.getInitialCount());
     }
 }
