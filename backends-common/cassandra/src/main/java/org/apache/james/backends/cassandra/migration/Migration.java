a30e5711a16a6f510acc74918aa221cd7e0ceec4
Rémi KOWALSKI <rkowalski@linagora.com>
2019-10-02 10:05:14 +0200

    JAMES-2813 add task type for Migration subtasks

--- backends-common/cassandra/src/main/java/org/apache/james/backends/cassandra/migration/Migration.java
+++ backends-common/cassandra/src/main/java/org/apache/james/backends/cassandra/migration/Migration.java
@@ -20,13 +20,24 @@
 package org.apache.james.backends.cassandra.migration;
 
 import org.apache.james.task.Task;
+import org.apache.james.task.TaskType;
 
 public interface Migration {
 
     void apply() throws InterruptedException;
 
     default Task asTask() {
-        return this::runTask;
+        return new Task() {
+            @Override
+            public Result run() throws InterruptedException {
+                return runTask();
+            }
+
+            @Override
+            public TaskType type() {
+                return TaskType.of("migration_sub_task");
+            }
+        };
     }
 
     default Task.Result runTask() throws InterruptedException {
