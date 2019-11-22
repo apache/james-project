9bdd121f6e0ab9a3506483c0f6ae09e28058e2b7
Rene Cordier <rcordier@linagora.com>
2019-11-22 13:20:04 +0700

    JAMES-2987 Moving remaining test classes in message.view package to JUnit 5

--- server/protocols/jmap-draft/src/test/java/org/apache/james/jmap/draft/model/message/view/MessageFullViewTest.java
+++ server/protocols/jmap-draft/src/test/java/org/apache/james/jmap/draft/model/message/view/MessageFullViewTest.java
@@ -19,6 +19,7 @@
 package org.apache.james.jmap.draft.model.message.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.Instant;
 import java.util.Optional;
@@ -31,86 +32,153 @@ import org.apache.james.jmap.draft.model.Keywords;
 import org.apache.james.jmap.draft.model.Number;
 import org.apache.james.mailbox.inmemory.InMemoryId;
 import org.apache.james.mailbox.model.TestMessageId;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
-public class MessageFullViewTest {
+class MessageFullViewTest {
 
-    
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenIdIsNull() {
-        MessageFullView.builder().build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenBlobIdIsNull() {
-        MessageFullView.builder().id(TestMessageId.of(1)).build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenThreadIdIsNull() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenThreadIdIsEmpty() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("").build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenMailboxIdsIsNull() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("threadId").build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenHeadersIsNull() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("threadId").fluentMailboxIds().build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenSubjectIsNull() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("threadId").fluentMailboxIds().headers(ImmutableMap.of()).build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenSubjectIsEmpty() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("threadId").fluentMailboxIds().headers(ImmutableMap.of())
-            .subject("").build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenSizeIsNull() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("threadId").fluentMailboxIds().headers(ImmutableMap.of())
-            .subject("subject").build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenDateIsNull() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("threadId").fluentMailboxIds().headers(ImmutableMap.of())
-            .subject("subject").size(123).build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenPreviewIsNull() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("threadId").fluentMailboxIds().headers(ImmutableMap.of())
-            .subject("subject").size(123).date(Instant.now()).build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenPreviewIsEmpty() {
-        MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("threadId").fluentMailboxIds().headers(ImmutableMap.of())
-            .subject("subject").size(123).date(Instant.now()).preview("").build();
+    @Test
+    void buildShouldThrowWhenIdIsNull() {
+        assertThatThrownBy(() -> MessageFullView.builder().build())
+            .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    public void buildShouldWorkWhenMandatoryFieldsArePresent() {
+    void buildShouldThrowWhenBlobIdIsNull() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenThreadIdIsNull() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenThreadIdIsEmpty() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("")
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenMailboxIdsIsNull() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("threadId")
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenHeadersIsNull() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("threadId")
+                .fluentMailboxIds()
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenSubjectIsNull() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("threadId")
+                .fluentMailboxIds()
+                .headers(ImmutableMap.of())
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenSubjectIsEmpty() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("threadId")
+                .fluentMailboxIds()
+                .headers(ImmutableMap.of())
+                .subject("")
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenSizeIsNull() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("threadId")
+                .fluentMailboxIds()
+                .headers(ImmutableMap.of())
+                .subject("subject")
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenDateIsNull() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("threadId")
+                .fluentMailboxIds()
+                .headers(ImmutableMap.of())
+                .subject("subject")
+                .size(123)
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenPreviewIsNull() {
+        assertThatThrownBy(() -> MessageFullView.builder().id(TestMessageId.of(1)).blobId(BlobId.of("blobId")).threadId("threadId").fluentMailboxIds().headers(ImmutableMap.of())
+            .subject("subject").size(123).date(Instant.now()).build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenPreviewIsEmpty() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("threadId")
+                .fluentMailboxIds()
+                .headers(ImmutableMap.of())
+                .subject("subject")
+                .size(123)
+                .date(Instant.now())
+                .preview("")
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldWorkWhenMandatoryFieldsArePresent() {
         Instant currentDate = Instant.now();
         Number messageSize = Number.fromLong(123);
-        MessageFullView expected = new MessageFullView(TestMessageId.of(1), BlobId.of("blobId"), "threadId", ImmutableList.of(InMemoryId.of(456)), Optional.empty(), false, ImmutableMap.of("key", "value"), Optional.empty(),
-                ImmutableList.of(), ImmutableList.of(), ImmutableList.of(), ImmutableList.of(), "subject", currentDate, messageSize, "preview", Optional.empty(), Optional.empty(),
-                ImmutableList.of(), ImmutableMap.of(), Keywords.DEFAULT_VALUE);
+
+        MessageFullView expected = new MessageFullView(TestMessageId.of(1), BlobId.of("blobId"), "threadId",
+            ImmutableList.of(InMemoryId.of(456)), Optional.empty(), false, ImmutableMap.of("key", "value"),
+            Optional.empty(), ImmutableList.of(), ImmutableList.of(), ImmutableList.of(), ImmutableList.of(),
+            "subject", currentDate, messageSize, "preview", Optional.empty(), Optional.empty(),
+            ImmutableList.of(), ImmutableMap.of(), Keywords.DEFAULT_VALUE);
+
         MessageFullView tested = MessageFullView.builder()
                 .id(TestMessageId.of(1))
                 .blobId(BlobId.of("blobId"))
@@ -125,33 +193,37 @@ public class MessageFullViewTest {
         assertThat(tested).isEqualToComparingFieldByField(expected);
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenAttachedMessageIsNotMatchingAttachments() {
+    @Test
+    void buildShouldThrowWhenAttachedMessageIsNotMatchingAttachments() {
         Attachment simpleAttachment = Attachment.builder().blobId(BlobId.of("blobId")).type("type").name("name").size(123).build();
         ImmutableList<Attachment> attachments = ImmutableList.of(simpleAttachment);
+
         SubMessage simpleMessage = SubMessage.builder()
                 .headers(ImmutableMap.of("key", "value"))
                 .subject("subject")
                 .date(Instant.now())
                 .build();
+
         ImmutableMap<BlobId, SubMessage> attachedMessages = ImmutableMap.of(BlobId.of("differentBlobId"), simpleMessage);
-        MessageFullView.builder()
-            .id(TestMessageId.of(1))
-            .blobId(BlobId.of("blobId"))
-            .threadId("threadId")
-            .mailboxId(InMemoryId.of(456))
-            .headers(ImmutableMap.of("key", "value"))
-            .subject("subject")
-            .size(123)
-            .date(Instant.now())
-            .preview("preview")
-            .attachments(attachments)
-            .attachedMessages(attachedMessages)
-            .build();
+
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("threadId")
+                .mailboxId(InMemoryId.of(456))
+                .headers(ImmutableMap.of("key", "value"))
+                .subject("subject")
+                .size(123)
+                .date(Instant.now())
+                .preview("preview")
+                .attachments(attachments)
+                .attachedMessages(attachedMessages)
+                .build())
+            .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    public void buildShouldWorkWhenAllFieldsArePresent() {
+    void buildShouldWorkWhenAllFieldsArePresent() {
         Emailer from = Emailer.builder().name("from").email("from@domain").build();
         ImmutableList<Emailer> to = ImmutableList.of(Emailer.builder().name("to").email("to@domain").build());
         ImmutableList<Emailer> cc = ImmutableList.of(Emailer.builder().name("cc").email("cc@domain").build());
@@ -160,11 +232,13 @@ public class MessageFullViewTest {
         Instant currentDate = Instant.now();
         Attachment simpleAttachment = Attachment.builder().blobId(BlobId.of("blobId")).type("type").name("name").size(123).build();
         ImmutableList<Attachment> attachments = ImmutableList.of(simpleAttachment);
+
         SubMessage simpleMessage = SubMessage.builder()
                 .headers(ImmutableMap.of("key", "value"))
                 .subject("subject")
                 .date(currentDate)
                 .build();
+
         ImmutableMap<BlobId, SubMessage> attachedMessages = ImmutableMap.of(BlobId.of("blobId"), simpleMessage);
 
         Keywords keywords = Keywords.strictFactory()
@@ -193,6 +267,7 @@ public class MessageFullViewTest {
             attachments,
             attachedMessages,
             keywords);
+
         MessageFullView tested = MessageFullView.builder()
             .id(TestMessageId.of(1))
             .blobId(BlobId.of("blobId"))
@@ -215,31 +290,33 @@ public class MessageFullViewTest {
             .attachments(attachments)
             .attachedMessages(attachedMessages)
             .build();
+
         assertThat(tested).isEqualToComparingFieldByField(expected);
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenOneAttachedMessageIsNotInAttachments() throws Exception {
-        MessageFullView.builder()
-            .id(TestMessageId.of(1))
-            .blobId(BlobId.of("blobId"))
-            .threadId("threadId")
-            .mailboxId(InMemoryId.of(456))
-            .headers(ImmutableMap.of("key", "value"))
-            .subject("subject")
-            .size(1)
-            .date(Instant.now())
-            .preview("preview")
-            .attachedMessages(ImmutableMap.of(BlobId.of("key"), SubMessage.builder()
-                    .headers(ImmutableMap.of("key", "value"))
-                    .subject("subject")
-                    .date(Instant.now())
-                    .build()))
-            .build();
+    @Test
+    void buildShouldThrowWhenOneAttachedMessageIsNotInAttachments() {
+        assertThatThrownBy(() -> MessageFullView.builder()
+                .id(TestMessageId.of(1))
+                .blobId(BlobId.of("blobId"))
+                .threadId("threadId")
+                .mailboxId(InMemoryId.of(456))
+                .headers(ImmutableMap.of("key", "value"))
+                .subject("subject")
+                .size(1)
+                .date(Instant.now())
+                .preview("preview")
+                .attachedMessages(ImmutableMap.of(BlobId.of("key"), SubMessage.builder()
+                        .headers(ImmutableMap.of("key", "value"))
+                        .subject("subject")
+                        .date(Instant.now())
+                        .build()))
+                .build())
+            .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    public void buildShouldNotThrowWhenOneAttachedMessageIsInAttachments() throws Exception {
+    void buildShouldNotThrowWhenOneAttachedMessageIsInAttachments() {
         MessageFullView.builder()
             .id(TestMessageId.of(1))
             .blobId(BlobId.of("blobId"))
@@ -264,7 +341,7 @@ public class MessageFullViewTest {
     }
 
     @Test
-    public void hasAttachmentShouldReturnFalseWhenNoAttachment() throws Exception {
+    void hasAttachmentShouldReturnFalseWhenNoAttachment() {
         MessageFullView message = MessageFullView.builder()
             .id(TestMessageId.of(1))
             .blobId(BlobId.of("blobId"))
@@ -282,7 +359,7 @@ public class MessageFullViewTest {
     }
 
     @Test
-    public void hasAttachmentShouldReturnFalseWhenAllAttachmentsAreInline() throws Exception {
+    void hasAttachmentShouldReturnFalseWhenAllAttachmentsAreInline() {
         MessageFullView message = MessageFullView.builder()
             .id(TestMessageId.of(1))
             .blobId(BlobId.of("blobId"))
@@ -314,7 +391,7 @@ public class MessageFullViewTest {
     }
 
     @Test
-    public void hasAttachmentShouldReturnTrueWhenOneAttachmentIsNotInline() throws Exception {
+    void hasAttachmentShouldReturnTrueWhenOneAttachmentIsNotInline() {
         MessageFullView message = MessageFullView.builder()
             .id(TestMessageId.of(1))
             .blobId(BlobId.of("blobId"))
@@ -352,7 +429,7 @@ public class MessageFullViewTest {
     }
 
     @Test
-    public void hasAttachmentShouldReturnTrueWhenAllAttachmentsAreNotInline() throws Exception {
+    void hasAttachmentShouldReturnTrueWhenAllAttachmentsAreNotInline() {
         MessageFullView message = MessageFullView.builder()
             .id(TestMessageId.of(1))
             .blobId(BlobId.of("blobId"))
--- server/protocols/jmap-draft/src/test/java/org/apache/james/jmap/draft/model/message/view/SubMessageTest.java
+++ server/protocols/jmap-draft/src/test/java/org/apache/james/jmap/draft/model/message/view/SubMessageTest.java
@@ -19,6 +19,7 @@
 package org.apache.james.jmap.draft.model.message.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.Instant;
 import java.util.Optional;
@@ -26,70 +27,90 @@ import java.util.Optional;
 import org.apache.james.jmap.draft.model.Attachment;
 import org.apache.james.jmap.draft.model.BlobId;
 import org.apache.james.jmap.draft.model.Emailer;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
-public class SubMessageTest {
+class SubMessageTest {
 
     private static final Optional<String> DEFAULT_TEXT_BODY = Optional.of("textBody");
     private static final Optional<String> DEFAULT_HTML_BODY = Optional.of("htmlBody");
 
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenHeadersIsNull() {
-        SubMessage.builder().build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenSubjectIsNull() {
-        SubMessage.builder().headers(ImmutableMap.of()).build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenSubjectIsEmpty() {
-        SubMessage.builder().headers(ImmutableMap.of()).subject("").build();
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenDateIsNull() {
-        SubMessage.builder().headers(ImmutableMap.of()).subject("subject").build();
+    @Test
+    void buildShouldThrowWhenHeadersIsNull() {
+        assertThatThrownBy(() -> SubMessage.builder().build())
+            .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    public void buildShouldWorkWhenMandatoryFieldsArePresent() {
+    void buildShouldThrowWhenSubjectIsNull() {
+        assertThatThrownBy(() -> SubMessage.builder()
+                .headers(ImmutableMap.of())
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenSubjectIsEmpty() {
+        assertThatThrownBy(() -> SubMessage.builder()
+                .headers(ImmutableMap.of())
+                .subject("")
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldThrowWhenDateIsNull() {
+        assertThatThrownBy(() -> SubMessage.builder()
+                .headers(ImmutableMap.of())
+                .subject("subject")
+                .build())
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void buildShouldWorkWhenMandatoryFieldsArePresent() {
         Instant currentDate = Instant.now();
-        SubMessage expected = new SubMessage(ImmutableMap.of("key", "value"), Optional.empty(), ImmutableList.of(), ImmutableList.of(), ImmutableList.of(), ImmutableList.of(),
-                "subject", currentDate, Optional.empty(), Optional.empty(), ImmutableList.of(), ImmutableMap.of());
+
+        SubMessage expected = new SubMessage(ImmutableMap.of("key", "value"), Optional.empty(),
+            ImmutableList.of(), ImmutableList.of(), ImmutableList.of(), ImmutableList.of(), "subject",
+            currentDate, Optional.empty(), Optional.empty(), ImmutableList.of(), ImmutableMap.of());
+
         SubMessage tested = SubMessage.builder()
             .headers(ImmutableMap.of("key", "value"))
             .subject("subject")
             .date(currentDate)
             .build();
+
         assertThat(tested).isEqualToComparingFieldByField(expected);
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void buildShouldThrowWhenAttachedMessageIsNotMatchingAttachments() {
+    @Test
+    void buildShouldThrowWhenAttachedMessageIsNotMatchingAttachments() {
         Attachment simpleAttachment = Attachment.builder().blobId(BlobId.of("blobId")).type("type").name("name").size(123).build();
         ImmutableList<Attachment> attachments = ImmutableList.of(simpleAttachment);
+
         SubMessage simpleMessage = SubMessage.builder()
                 .headers(ImmutableMap.of("key", "value"))
                 .subject("subject")
                 .date(Instant.now())
                 .build();
+
         ImmutableMap<BlobId, SubMessage> attachedMessages = ImmutableMap.of(BlobId.of("differentBlobId"), simpleMessage);
-        SubMessage.builder()
-            .headers(ImmutableMap.of("key", "value"))
-            .subject("subject")
-            .date(Instant.now())
-            .attachments(attachments)
-            .attachedMessages(attachedMessages)
-            .build();
+
+        assertThatThrownBy(() -> SubMessage.builder()
+                .headers(ImmutableMap.of("key", "value"))
+                .subject("subject")
+                .date(Instant.now())
+                .attachments(attachments)
+                .attachedMessages(attachedMessages)
+                .build())
+            .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    public void buildShouldWorkWhenAllFieldsArePresent() {
+    void buildShouldWorkWhenAllFieldsArePresent() {
         Emailer from = Emailer.builder().name("from").email("from@domain").build();
         ImmutableList<Emailer> to = ImmutableList.of(Emailer.builder().name("to").email("to@domain").build());
         ImmutableList<Emailer> cc = ImmutableList.of(Emailer.builder().name("cc").email("cc@domain").build());
@@ -98,12 +119,15 @@ public class SubMessageTest {
         Instant currentDate = Instant.now();
         Attachment simpleAttachment = Attachment.builder().blobId(BlobId.of("blobId")).type("type").name("name").size(123).build();
         ImmutableList<Attachment> attachments = ImmutableList.of(simpleAttachment);
+
         SubMessage simpleMessage = SubMessage.builder()
                 .headers(ImmutableMap.of("key", "value"))
                 .subject("subject")
                 .date(currentDate)
                 .build();
+
         ImmutableMap<BlobId, SubMessage> attachedMessages = ImmutableMap.of(BlobId.of("blobId"), simpleMessage);
+
         SubMessage expected = new SubMessage(
                 ImmutableMap.of("key", "value"),
                 Optional.of(from),
@@ -117,6 +141,7 @@ public class SubMessageTest {
                 DEFAULT_HTML_BODY,
                 attachments,
                 attachedMessages);
+
         SubMessage tested = SubMessage.builder()
             .headers(ImmutableMap.of("key", "value"))
             .from(from)
@@ -131,6 +156,7 @@ public class SubMessageTest {
             .attachments(attachments)
             .attachedMessages(attachedMessages)
             .build();
+
         assertThat(tested).isEqualToComparingFieldByField(expected);
     }
 }
