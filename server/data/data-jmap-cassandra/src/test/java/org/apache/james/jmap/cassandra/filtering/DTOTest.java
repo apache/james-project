2a557c122e66b51f5145b17163c54b78ccad4897
Benoit Tellier <btellier@linagora.com>
2019-12-18 16:54:23 +0700

    JAMES-2993 Use JsonSerializationVerifier for JMAP filtering event DTO tests

--- server/data/data-jmap-cassandra/src/test/java/org/apache/james/jmap/cassandra/filtering/DTOTest.java
+++ server/data/data-jmap-cassandra/src/test/java/org/apache/james/jmap/cassandra/filtering/DTOTest.java
@@ -19,101 +19,47 @@
 
 package org.apache.james.jmap.cassandra.filtering;
 
-import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.apache.james.jmap.api.filtering.RuleFixture.RULE_1;
 import static org.apache.james.jmap.api.filtering.RuleFixture.RULE_2;
 import static org.apache.james.jmap.api.filtering.RuleFixture.RULE_FROM;
 import static org.apache.james.jmap.api.filtering.RuleFixture.RULE_RECIPIENT;
 import static org.apache.james.jmap.api.filtering.RuleFixture.RULE_SUBJECT;
 import static org.apache.james.jmap.api.filtering.RuleFixture.RULE_TO;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.apache.james.jmap.cassandra.filtering.FilteringRuleSetDefineDTOModules.FILTERING_RULE_SET_DEFINED;
 
+import org.apache.james.JsonSerializationVerifier;
 import org.apache.james.core.Username;
 import org.apache.james.eventsourcing.EventId;
 import org.apache.james.jmap.api.filtering.impl.FilteringAggregateId;
 import org.apache.james.jmap.api.filtering.impl.RuleSetDefined;
 import org.apache.james.util.ClassLoaderUtils;
-
-import com.fasterxml.jackson.databind.jsontype.NamedType;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.guava.GuavaModule;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.google.common.collect.ImmutableList;
 
-public class DTOTest {
+class DTOTest {
     static final String EVENT_JSON = ClassLoaderUtils.getSystemResourceAsString("json/event.json");
     static final String EVENT_EMPTY_JSON = ClassLoaderUtils.getSystemResourceAsString("json/eventEmpty.json");
     static final String EVENT_COMPLEX_JSON = ClassLoaderUtils.getSystemResourceAsString("json/eventComplex.json");
-    static final FilteringRuleSetDefinedDTO SIMPLE_DTO = FilteringRuleSetDefinedDTO.from(
-            new RuleSetDefined(
+    static final RuleSetDefined SIMPLE_RULE = new RuleSetDefined(
                     new FilteringAggregateId(Username.of("Bart")),
                     EventId.first(),
-                    ImmutableList.of(RULE_1, RULE_2)));
-    static final FilteringRuleSetDefinedDTO EMPTY_DTO = FilteringRuleSetDefinedDTO.from(
-            new RuleSetDefined(
+                    ImmutableList.of(RULE_1, RULE_2));
+    static final RuleSetDefined EMPTY_RULE = new RuleSetDefined(
                     new FilteringAggregateId(Username.of("Bart")),
                     EventId.first(),
-                    ImmutableList.of()));
-    static final FilteringRuleSetDefinedDTO COMPLEX_DTO = FilteringRuleSetDefinedDTO.from(
-            new RuleSetDefined(
+                    ImmutableList.of());
+    static final RuleSetDefined COMPLEX_RULE =  new RuleSetDefined(
                     new FilteringAggregateId(Username.of("Bart")),
                     EventId.first(),
-                    ImmutableList.of(RULE_FROM, RULE_RECIPIENT, RULE_SUBJECT, RULE_TO)));
-
-    private ObjectMapper objectMapper;
-
-    @BeforeEach
-    void setUp() {
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new Jdk8Module());
-        objectMapper.registerModule(new GuavaModule());
-        objectMapper.registerSubtypes(
-            new NamedType(FilteringRuleSetDefinedDTO.class, "filtering-rule-set-defined"));
-
-    }
+                    ImmutableList.of(RULE_FROM, RULE_RECIPIENT, RULE_SUBJECT, RULE_TO));
 
     @Test
-    void shouldSerializeDTO() throws Exception {
-        String serializedEvent = objectMapper.writeValueAsString(SIMPLE_DTO);
-
-        assertThatJson(serializedEvent).isEqualTo(EVENT_JSON);
-    }
-
-    @Test
-    void shouldDeserializeDTO() throws Exception {
-        FilteringRuleSetDefinedDTO dto = objectMapper.readValue(EVENT_JSON, FilteringRuleSetDefinedDTO.class);
-
-        assertThat(dto).isEqualTo(SIMPLE_DTO);
-    }
-
-    @Test
-    void shouldSerializeEmptyDTO() throws Exception {
-        String serializedEvent = objectMapper.writeValueAsString(EMPTY_DTO);
-
-        assertThatJson(serializedEvent).isEqualTo(EVENT_EMPTY_JSON);
-    }
-
-    @Test
-    void shouldDeserializeEmptyDTO() throws Exception {
-        FilteringRuleSetDefinedDTO dto = objectMapper.readValue(EVENT_EMPTY_JSON, FilteringRuleSetDefinedDTO.class);
-
-        assertThat(dto).isEqualTo(EMPTY_DTO);
-    }
-
-    @Test
-    void shouldSerializeComplexDTO() throws Exception {
-        String serializedEvent = objectMapper.writeValueAsString(COMPLEX_DTO);
-
-        assertThatJson(serializedEvent).isEqualTo(EVENT_COMPLEX_JSON);
-    }
-
-    @Test
-    void shouldDeserializeComplexDTO() throws Exception {
-        FilteringRuleSetDefinedDTO dto = objectMapper.readValue(EVENT_COMPLEX_JSON, FilteringRuleSetDefinedDTO.class);
-
-        assertThat(dto).isEqualTo(COMPLEX_DTO);
+    void shouldSerializeRule() throws Exception {
+        JsonSerializationVerifier.dtoModule(FILTERING_RULE_SET_DEFINED)
+            .bean(EMPTY_RULE).json(EVENT_EMPTY_JSON)
+            .bean(SIMPLE_RULE).json(EVENT_JSON)
+            .bean(COMPLEX_RULE).json(EVENT_COMPLEX_JSON)
+            .verify();
     }
 }
