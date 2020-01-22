2759aa2e36c2051c7d871586c954ab753552444c
Rene Cordier <rcordier@linagora.com>
2020-01-22 10:39:26 +0700

    [Refactoring] Migrate ElasticSearchQuotaMailboxListenerTest to JUnit 5

--- mailbox/plugin/quota-search-elasticsearch/src/test/java/org/apache/james/quota/search/elasticsearch/events/ElasticSearchQuotaMailboxListenerTest.java
+++ mailbox/plugin/quota-search-elasticsearch/src/test/java/org/apache/james/quota/search/elasticsearch/events/ElasticSearchQuotaMailboxListenerTest.java
@@ -26,7 +26,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 
-import org.apache.james.backends.es.DockerElasticSearchRule;
+import org.apache.james.backends.es.DockerElasticSearchExtension;
 import org.apache.james.backends.es.ElasticSearchConfiguration;
 import org.apache.james.backends.es.ElasticSearchIndexer;
 import org.apache.james.backends.es.NodeMappingFactory;
@@ -44,24 +44,25 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
-public class ElasticSearchQuotaMailboxListenerTest {
-    private static Event.EventId EVENT_ID = Event.EventId.of("6e0dd59d-660e-4d9b-b22f-0354479f47b4");
+class ElasticSearchQuotaMailboxListenerTest {
+    static Event.EventId EVENT_ID = Event.EventId.of("6e0dd59d-660e-4d9b-b22f-0354479f47b4");
 
-    private static final int BATCH_SIZE = 1;
+    static final int BATCH_SIZE = 1;
 
-    @Rule
-    public DockerElasticSearchRule elasticSearch = new DockerElasticSearchRule();
-    private ElasticSearchQuotaMailboxListener quotaMailboxListener;
-    private RestHighLevelClient client;
+    @RegisterExtension
+    DockerElasticSearchExtension elasticSearch = new DockerElasticSearchExtension();
 
-    @Before
-    public void setUp() throws IOException {
-        client = elasticSearch.clientProvider().get();
+    ElasticSearchQuotaMailboxListener quotaMailboxListener;
+    RestHighLevelClient client;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        client = elasticSearch.getDockerElasticSearch().clientProvider().get();
 
         QuotaSearchIndexCreationUtil.prepareDefaultClient(client, ElasticSearchConfiguration.builder()
             .addHost(elasticSearch.getDockerElasticSearch().getHttpHost())
@@ -75,19 +76,19 @@ public class ElasticSearchQuotaMailboxListenerTest {
             new UserRoutingKeyFactory());
     }
 
-    @After
-    public void tearDown() throws IOException {
+    @AfterEach
+    void tearDown() throws IOException {
         client.close();
     }
 
     @Test
-    public void deserializeElasticSearchQuotaMailboxListenerGroup() throws Exception {
+    void deserializeElasticSearchQuotaMailboxListenerGroup() throws Exception {
         assertThat(Group.deserialize("org.apache.james.quota.search.elasticsearch.events.ElasticSearchQuotaMailboxListener$ElasticSearchQuotaMailboxListenerGroup"))
             .isEqualTo(new ElasticSearchQuotaMailboxListener.ElasticSearchQuotaMailboxListenerGroup());
     }
 
     @Test
-    public void eventShouldIndexEventWhenQuotaEvent() throws Exception {
+    void eventShouldIndexEventWhenQuotaEvent() throws Exception {
         quotaMailboxListener.event(EventFactory.quotaUpdated()
             .eventId(EVENT_ID)
             .user(BOB_USERNAME)
