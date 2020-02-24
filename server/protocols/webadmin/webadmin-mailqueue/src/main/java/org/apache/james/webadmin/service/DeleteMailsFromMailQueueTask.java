69de2ec081dec4511614e89a7a0887329be4377f
Matthieu Baechler <matthieu@apache.org>
2020-02-24 12:00:37 +0100

    [Refactoring] rename DeleteMailsFromMailQueueTask fields to make it clear arguments are optionals

--- server/protocols/webadmin/webadmin-mailqueue/src/main/java/org/apache/james/webadmin/service/DeleteMailsFromMailQueueTask.java
+++ server/protocols/webadmin/webadmin-mailqueue/src/main/java/org/apache/james/webadmin/service/DeleteMailsFromMailQueueTask.java
@@ -50,15 +50,15 @@ public class DeleteMailsFromMailQueueTask implements Task {
         private final Instant timestamp;
 
         public AdditionalInformation(MailQueueName mailQueueName, long initialCount, long remainingCount,
-                                     Optional<MailAddress> maybeSender, Optional<String> maybeName,
-                                     Optional<MailAddress> maybeRecipient, Instant timestamp) {
+                                     Optional<MailAddress> optionalSender, Optional<String> optionalName,
+                                     Optional<MailAddress> optionalRecipient, Instant timestamp) {
             this.mailQueueName = mailQueueName;
             this.initialCount = initialCount;
             this.remainingCount = remainingCount;
 
-            sender = maybeSender.map(MailAddress::asString);
-            name = maybeName;
-            recipient = maybeRecipient.map(MailAddress::asString);
+            sender = optionalSender.map(MailAddress::asString);
+            name = optionalName;
+            recipient = optionalRecipient.map(MailAddress::asString);
             this.timestamp = timestamp;
         }
 
@@ -105,9 +105,9 @@ public class DeleteMailsFromMailQueueTask implements Task {
 
     public static final TaskType TYPE = TaskType.of("delete-mails-from-mail-queue");
 
-    private final Optional<MailAddress> maybeSender;
-    private final Optional<String> maybeName;
-    private final Optional<MailAddress> maybeRecipient;
+    private final Optional<MailAddress> optionalSender;
+    private final Optional<String> optionalName;
+    private final Optional<MailAddress> optionalRecipient;
     private final MailQueueFactory factory;
     private final MailQueueName queueName;
     private Optional<Long> initialCount;
@@ -115,17 +115,18 @@ public class DeleteMailsFromMailQueueTask implements Task {
     private Optional<TaskExecutionDetails.AdditionalInformation> lastAdditionalInformation;
 
     public DeleteMailsFromMailQueueTask(MailQueueName queueName, MailQueueFactory factory,
-                                        Optional<MailAddress> maybeSender,
-                                        Optional<String> maybeName, Optional<MailAddress> maybeRecipient) {
+                                        Optional<MailAddress> optionalSender,
+                                        Optional<String> optionalName,
+                                        Optional<MailAddress> optionalRecipient) {
         this.factory = factory;
         this.queueName = queueName;
         Preconditions.checkArgument(
-            Booleans.countTrue(maybeSender.isPresent(), maybeName.isPresent(), maybeRecipient.isPresent()) == 1,
+            Booleans.countTrue(optionalSender.isPresent(), optionalName.isPresent(), optionalRecipient.isPresent()) == 1,
             "You should provide one and only one of the query parameters 'sender', 'name' or 'recipient'.");
 
-        this.maybeSender = maybeSender;
-        this.maybeName = maybeName;
-        this.maybeRecipient = maybeRecipient;
+        this.optionalSender = optionalSender;
+        this.optionalName = optionalName;
+        this.optionalRecipient = optionalRecipient;
         this.initialCount = Optional.empty();
         this.queue = Optional.empty();
         this.lastAdditionalInformation = Optional.empty();
@@ -137,11 +138,11 @@ public class DeleteMailsFromMailQueueTask implements Task {
         try (ManageableMailQueue queue = factory.create(queueName)) {
             this.initialCount = Optional.of(getRemainingSize(queue));
             this.queue = Optional.of(queue);
-            maybeSender.ifPresent(Throwing.consumer(
+            optionalSender.ifPresent(Throwing.consumer(
                 (MailAddress sender) -> queue.remove(ManageableMailQueue.Type.Sender, sender.asString())));
-            maybeName.ifPresent(Throwing.consumer(
+            optionalName.ifPresent(Throwing.consumer(
                 (String name) -> queue.remove(ManageableMailQueue.Type.Name, name)));
-            maybeRecipient.ifPresent(Throwing.consumer(
+            optionalRecipient.ifPresent(Throwing.consumer(
                 (MailAddress recipient) -> queue.remove(ManageableMailQueue.Type.Recipient, recipient.asString())));
 
             this.lastAdditionalInformation = details();
@@ -165,15 +166,15 @@ public class DeleteMailsFromMailQueueTask implements Task {
     }
 
     Optional<String> getMaybeName() {
-        return maybeName;
+        return optionalName;
     }
 
     Optional<MailAddress> getMaybeRecipient() {
-        return maybeRecipient;
+        return optionalRecipient;
     }
 
     Optional<MailAddress> getMaybeSender() {
-        return maybeSender;
+        return optionalSender;
     }
 
     @Override
@@ -184,8 +185,8 @@ public class DeleteMailsFromMailQueueTask implements Task {
                 new AdditionalInformation(
                     queueName,
                     initialCount.get(),
-                    getRemainingSize(queue), maybeSender,
-                    maybeName, maybeRecipient, Clock.systemUTC().instant())));
+                    getRemainingSize(queue), optionalSender,
+                    optionalName, optionalRecipient, Clock.systemUTC().instant())));
     }
 
     public long getRemainingSize(ManageableMailQueue queue) {
