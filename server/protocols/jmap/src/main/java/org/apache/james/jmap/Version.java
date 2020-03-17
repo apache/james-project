43bfc1d60716dedc2c884f60c402bdb023dc8c0d
Rene Cordier <rcordier@linagora.com>
2020-03-17 14:47:15 +0700

    JAMES-3092 Implement Version, Verb and Endpoint

---
+++ server/protocols/jmap/src/main/java/org/apache/james/jmap/Version.java
@@ -0,0 +1,70 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap;
+
+import java.util.List;
+import java.util.Objects;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+
+public class Version {
+    public static final Version DRAFT = new Version("draft");
+    public static final Version RFC8621 = new Version("rfc-8621");
+
+    private static final List<Version> AVAILABLE_VERSIONS = ImmutableList.of(
+        DRAFT,
+        RFC8621
+    );
+
+    public static Version of(String version) {
+        Preconditions.checkNotNull(version);
+
+        return AVAILABLE_VERSIONS.stream()
+            .filter(jmapVersion -> jmapVersion.version.equalsIgnoreCase(version))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(version + " is not a supported version"));
+    }
+
+    private final String version;
+
+    Version(String version) {
+        this.version = version;
+    }
+
+    public String asString() {
+        return version;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o instanceof Version) {
+            Version version = (Version) o;
+
+            return Objects.equals(this.version, version.version);
+        }
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(version);
+    }
+}
---
+++ server/protocols/jmap/src/test/java/org/apache/james/jmap/VersionTest.java
@@ -0,0 +1,64 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+class VersionTest {
+    @Test
+    void shouldRespectBeanContract() {
+        EqualsVerifier.forClass(Version.class)
+            .verify();
+    }
+
+    @Test
+    void ofShouldReturnCorrectValue() {
+        String version = "rfc-8621";
+
+        assertThat(Version.of(version)).isEqualTo(Version.RFC8621);
+    }
+
+    @Test
+    void ofShouldThrowWhenVersionNotKnown() {
+        String version = "unknown";
+
+        assertThatThrownBy(() -> Version.of(version))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void ofShouldThrowWhenVersionIsNull() {
+        assertThatThrownBy(() -> Version.of(null))
+            .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void ofShouldThrowWhenVersionIsEmpty() {
+        String version = "";
+
+        assertThatThrownBy(() -> Version.of(version))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
