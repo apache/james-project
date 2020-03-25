492f3827032ad95cf0b0040e86ff456cc7272d14
Tran Tien Duc <dtran@linagora.com>
2020-03-25 20:42:26 +0700

    JAMES-2890 uncomment RequestObjectSerializationTest

--- server/protocols/jmap-rfc-8621/src/test/scala/org/apache/james/jmap/json/RequestObjectSerializationTest.scala
+++ server/protocols/jmap-rfc-8621/src/test/scala/org/apache/james/jmap/json/RequestObjectSerializationTest.scala
@@ -20,8 +20,7 @@
 package org.apache.james.jmap.json
 
 import eu.timepit.refined.auto._
-//import org.apache.james.jmap.json.Invocation._
-//import org.apache.james.jmap.json.RequestObject._
+import org.apache.james.jmap.model.CapabilityIdentifier.CapabilityIdentifier
 import org.apache.james.jmap.model.CreatedIds.{ClientId, ServerId}
 import org.apache.james.jmap.model.Id.Id
 import org.apache.james.jmap.model.Invocation.{Arguments, MethodCallId, MethodName}
@@ -31,194 +30,126 @@ import play.api.libs.json._
 
 class RequestObjectSerializationTest extends PlaySpec {
   private val id: Id = "aHR0cHM6Ly93d3cuYmFzZTY0ZW5jb2RlLm9yZy8"
-//
-//  "Deserialize Capability" must {
-//    "succeed when deserialize from JsString" in {
-//      val capabilityJsValue: JsValue = JsString("org.com.test.1")
-//      val actualCapability = Json.fromJson[Capability](capabilityJsValue)
-//
-//      actualCapability must be(JsSuccess(Capability("org.com.test.1")))
-//      actualCapability.get mustBe a[Capability]
-//    }
-//
-//    "be instanceOf JsError when deserialize with wrong value type" in {
-//      val capabilityJsValue: JsValue = JsBoolean(true)
-//      Json.fromJson[Capability](capabilityJsValue) mustBe a[JsError]
-//    }
-//
-//    "succeed with MethodName value class type but not same instanceOf Capability" in {
-//      val capabilityJsValue: JsValue = JsString("org.com.test.1")
-//      val actualCapability = Json.fromJson[MethodName](capabilityJsValue).get
-//
-//      actualCapability must not be a[Capability]
-//      actualCapability mustBe a[MethodName]
-//    }
-//  }
-//
-//  "Serialize Capability" must {
-//    "succeed when write to string" in {
-//      val capability: Capability = Capability("org.com.test.1")
-//      val expectedCapability: JsValue = JsString("org.com.test.1")
-//
-//      Json.toJson[Capability](capability) must be(expectedCapability)
-//    }
-//  }
-//
-//  "Deserialize RequestObject" must {
-//    "succeed when deserialize from JsString without CreatedIds" in {
-//      val methodName: MethodName = MethodName("Core/echo")
-//      val argument: Arguments = Arguments(Json.obj("arg1" -> "arg1data", "arg2" -> "arg2data"))
-//      val methodCallId: MethodCallId = MethodCallId("c1")
-//      val expectedInvocation: Invocation = Invocation(methodName, argument, methodCallId)
-//
-//      deserialize(
-//        """
-//          |{
-//          |  "using": [ "urn:ietf:params:jmap:core"],
-//          |  "methodCalls": [
-//          |    [ "Core/echo", {
-//          |      "arg1": "arg1data",
-//          |      "arg2": "arg2data"
-//          |    }, "c1" ]
-//          |  ]
-//          |}
-//          |""".stripMargin) must be(
-//        JsSuccess(RequestObject(
-//          using = Seq(Capability("urn:ietf:params:jmap:core")),
-//          methodCalls = Seq(expectedInvocation))))
-//    }
-//
-//    "succeed when deserialize from JsString withCreatedIds" in {
-//      val methodName: MethodName = MethodName("Core/echo")
-//      val argument: Arguments = Arguments(Json.obj("arg1" -> "arg1data", "arg2" -> "arg2data"))
-//      val methodCallId: MethodCallId = MethodCallId("c1")
-//      val expectedInvocation: Invocation = Invocation(methodName, argument, methodCallId)
-//      val expectedCreatedIds: CreatedIds = CreatedIds(Map(
-//        ClientId(id) -> ServerId(id)
-//      ))
-//
-//      deserialize(
-//        """
-//          |{
-//          |  "using": [ "urn:ietf:params:jmap:core"],
-//          |  "methodCalls": [
-//          |    [ "Core/echo", {
-//          |      "arg1": "arg1data",
-//          |      "arg2": "arg2data"
-//          |    }, "c1" ]
-//          |  ],
-//          |  "createdIds":{"aHR0cHM6Ly93d3cuYmFzZTY0ZW5jb2RlLm9yZy8":"aHR0cHM6Ly93d3cuYmFzZTY0ZW5jb2RlLm9yZy8"}
-//          |}
-//          |""".stripMargin) must be(
-//        JsSuccess(RequestObject(
-//          using = Seq(Capability("urn:ietf:params:jmap:core")),
-//          methodCalls = Seq(expectedInvocation),
-//          Option(expectedCreatedIds))))
-//    }
-//
-//    "succeed with many Capability, methodCalls without CreatedIds" in {
-//      val expectedInvocation1: Invocation = Invocation(MethodName("Core/echo"),
-//        Arguments(Json.obj("arg1" -> "arg1data", "arg2" -> "arg2data")), MethodCallId("c1"))
-//      val expectedInvocation2: Invocation = Invocation(MethodName("Core/echo2"),
-//        Arguments(Json.obj("arg3" -> "arg3data", "arg4" -> "arg4data")), MethodCallId("c2"))
-//
-//      deserialize(
-//        """
-//          |{
-//          |  "using": [ "urn:ietf:params:jmap:core", "urn:ietf:params:jmap:core2"],
-//          |  "methodCalls": [
-//          |    [ "Core/echo", {
-//          |      "arg1": "arg1data",
-//          |      "arg2": "arg2data"
-//          |    }, "c1" ],
-//          |    [ "Core/echo2", {
-//          |      "arg3": "arg3data",
-//          |      "arg4": "arg4data"
-//          |    }, "c2" ]
-//          |  ]
-//          |}
-//          |""".stripMargin) must be(
-//        JsSuccess(RequestObject(
-//          using = Seq(Capability("urn:ietf:params:jmap:core"), Capability("urn:ietf:params:jmap:core2")),
-//          methodCalls = Seq(expectedInvocation1, expectedInvocation2))))
-//    }
-//  }
+  private val createdIds: CreatedIds = CreatedIds(Map(ClientId(id) -> ServerId(id)))
+  private val coreIdentifier: CapabilityIdentifier = "urn:ietf:params:jmap:core"
+  private val mailIdentifier: CapabilityIdentifier = "urn:ietf:params:jmap:mail"
+  private val invocation1: Invocation = Invocation(
+    methodName = MethodName("Core/echo1"),
+    arguments = Arguments(Json.obj("arg1" -> "arg1data", "arg2" -> "arg2data")),
+    methodCallId = MethodCallId("c1"))
+  private val invocation2: Invocation = Invocation(
+    methodName = MethodName("Core/echo2"),
+    arguments = Arguments(Json.obj("arg3" -> "arg3data", "arg4" -> "arg4data")),
+    methodCallId = MethodCallId("c2")
+  )
+
+  "Deserialize RequestObject" must {
+    "succeed when deserialize from JsString without CreatedIds" in {
+      val expectedRequestObject = RequestObject(
+        using = Seq(coreIdentifier),
+        methodCalls = Seq(invocation1),
+        createdIds = Option.empty)
+
+      new Serializer()
+        .deserializeRequestObject(
+          """
+            |{
+            |  "using": [ "urn:ietf:params:jmap:core"],
+            |  "methodCalls": [
+            |    [ "Core/echo1", {
+            |      "arg1": "arg1data",
+            |      "arg2": "arg2data"
+            |    }, "c1" ]
+            |  ]
+            |}
+            |""".stripMargin) must equal(JsSuccess(expectedRequestObject))
+    }
+
+    "succeed when deserialize from JsString with CreatedIds" in {
+      val expectedRequestObject = RequestObject(
+        using = Seq(coreIdentifier),
+        methodCalls = Seq(invocation1),
+        createdIds = Option(createdIds))
+
+      new Serializer()
+        .deserializeRequestObject(
+          """
+            |{
+            |  "using": [ "urn:ietf:params:jmap:core"],
+            |  "methodCalls": [
+            |    [ "Core/echo1", {
+            |      "arg1": "arg1data",
+            |      "arg2": "arg2data"
+            |    }, "c1" ]
+            |  ],
+            |  "createdIds":{"aHR0cHM6Ly93d3cuYmFzZTY0ZW5jb2RlLm9yZy8":"aHR0cHM6Ly93d3cuYmFzZTY0ZW5jb2RlLm9yZy8"}
+            |}
+            |""".stripMargin) must equal(JsSuccess(expectedRequestObject))
+    }
+
+    "succeed with many Capability, methodCalls without CreatedIds" in {
+      val expectedRequestObject = RequestObject(
+        using = Seq(coreIdentifier, mailIdentifier),
+        methodCalls = Seq(invocation1, invocation2),
+        createdIds = Option.empty)
+
+      new Serializer()
+        .deserializeRequestObject(
+          """
+            |{
+            |  "using": [ "urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
+            |  "methodCalls": [
+            |    [ "Core/echo1", {
+            |      "arg1": "arg1data",
+            |      "arg2": "arg2data"
+            |    }, "c1" ],
+            |    [ "Core/echo2", {
+            |      "arg3": "arg3data",
+            |      "arg4": "arg4data"
+            |    }, "c2" ]
+            |  ]
+            |}
+            |""".stripMargin) must equal(JsSuccess(expectedRequestObject))
+    }
+  }
 
   "Serialize RequestObject" must {
-//    "succeed when write to string without CreatedIds" in {
-//      val methodName: MethodName = MethodName("Core/echo")
-//      val argument: Arguments = Arguments(Json.obj("arg1" -> "arg1data", "arg2" -> "arg2data"))
-//      val methodCallId: MethodCallId = MethodCallId("c1")
-//      val invocation: Invocation = Invocation(methodName, argument, methodCallId)
-//
-//      val requestObject: RequestObject = RequestObject(
-//        using = Seq(Capability("urn:ietf:params:jmap:core"), Capability("urn:ietf:params:jmap:core2")),
-//        methodCalls = Seq(invocation))
-//      val expectedValue = Json.prettyPrint(Json.parse(
-//        """
-//          |{
-//          |  "using": [ "urn:ietf:params:jmap:core", "urn:ietf:params:jmap:core2"],
-//          |  "methodCalls": [
-//          |    [ "Core/echo", {
-//          |      "arg1": "arg1data",
-//          |      "arg2": "arg2data"
-//          |    }, "c1" ]
-//          |  ]
-//          |}
-//          |""".stripMargin))
-//
-//      Json.toJson(requestObject) must be(Json.parse(expectedValue))
-//    }
-//
-//    "succeed when write to string with CreatedIds" in {
-//      val methodName: MethodName = MethodName("Core/echo")
-//      val argument: Arguments = Arguments(Json.obj("arg1" -> "arg1data", "arg2" -> "arg2data"))
-//      val methodCallId: MethodCallId = MethodCallId("c1")
-//      val invocation: Invocation = Invocation(methodName, argument, methodCallId)
-//      val createdIds: CreatedIds = CreatedIds(Map(
-//        ClientId(id) -> ServerId(id)
-//      ))
-//      val requestObject: RequestObject = RequestObject(
-//        using = Seq(Capability("urn:ietf:params:jmap:core"), Capability("urn:ietf:params:jmap:core2")),
-//        methodCalls = Seq(invocation),
-//        Option(createdIds))
-//
-//      val expectedValue = Json.prettyPrint(Json.parse(
-//        """
-//          |{
-//          |  "using": [ "urn:ietf:params:jmap:core", "urn:ietf:params:jmap:core2"],
-//          |  "methodCalls": [
-//          |    [ "Core/echo", {
-//          |      "arg1": "arg1data",
-//          |      "arg2": "arg2data"
-//          |    }, "c1" ]
-//          |  ],
-//          |  "createdIds":{"aHR0cHM6Ly93d3cuYmFzZTY0ZW5jb2RlLm9yZy8":"aHR0cHM6Ly93d3cuYmFzZTY0ZW5jb2RlLm9yZy8"}
-//          |}
-//          |""".stripMargin))
-//
-//      Json.toJson(requestObject) must be(Json.parse(expectedValue))
-//    }
-
-     "succeed when write to string with CreatedIds" in {
-      val methodName: MethodName = MethodName("Core/echo")
-      val argument: Arguments = Arguments(Json.obj("arg1" -> "arg1data", "arg2" -> "arg2data"))
-      val methodCallId: MethodCallId = MethodCallId("c1")
-      val invocation: Invocation = Invocation(methodName, argument, methodCallId)
-      val createdIds: CreatedIds = CreatedIds(Map(
-        ClientId(id) -> ServerId(id)
-      ))
-      val requestObject: RequestObject = RequestObject(
-        using = Seq("urn:ietf:params:jmap:core", "urn:ietf:params:jmap:core2"),
-        methodCalls = Seq(invocation),
-        Option(createdIds))
+    "succeed when write to string without CreatedIds" in {
+      val actualValue = new Serializer().serialize(
+        RequestObject(
+          using = Seq(coreIdentifier, mailIdentifier),
+          methodCalls = Seq(invocation1),
+          createdIds = Option.empty))
 
       val expectedValue = Json.prettyPrint(Json.parse(
         """
           |{
-          |  "using": [ "urn:ietf:params:jmap:core", "urn:ietf:params:jmap:core2"],
+          |  "using": [ "urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
           |  "methodCalls": [
-          |    [ "Core/echo", {
+          |    [ "Core/echo1", {
+          |      "arg1": "arg1data",
+          |      "arg2": "arg2data"
+          |    }, "c1" ]
+          |  ]
+          |}
+          |""".stripMargin))
+
+      Json.parse(actualValue) must equal(Json.parse(expectedValue))
+    }
+
+    "succeed when write to string with CreatedIds" in {
+      val actualValue = new Serializer().serialize(
+        RequestObject(
+          using = Seq(coreIdentifier, mailIdentifier),
+          methodCalls = Seq(invocation1),
+          createdIds = Option(createdIds)))
+
+      val expectedValue = Json.prettyPrint(Json.parse(
+        """
+          |{
+          |  "using": [ "urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
+          |  "methodCalls": [
+          |    [ "Core/echo1", {
           |      "arg1": "arg1data",
           |      "arg2": "arg2data"
           |    }, "c1" ]
@@ -227,8 +158,7 @@ class RequestObjectSerializationTest extends PlaySpec {
           |}
           |""".stripMargin))
 
-       Json.parse(new Serializer().serialize(requestObject)) must equal(Json.parse(expectedValue))
+      Json.parse(actualValue) must equal(Json.parse(expectedValue))
     }
   }
-
 }
