6507a5c05e9a6f88ad7fa1f8f6b2920efcc2988c
ducnv <duc91.nvd@gmail.com>
2020-04-10 17:17:50 +0700

    JAMES-2888: add BadRequestException removed by rebase work

---
+++ server/protocols/jmap-draft/src/main/java/org/apache/james/jmap/draft/exceptions/BadRequestException.java
@@ -0,0 +1,12 @@
+package org.apache.james.jmap.draft.exceptions;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
