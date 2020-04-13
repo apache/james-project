d02792264dc55dceb0643b5922c61b74092e9682
Benoit Tellier <btellier@linagora.com>
2020-04-13 14:18:02 +0700

    [REFACTORING] Remove unused CassandraAttachmentDAO
    Also remove associated table.
    
    Since schema version 5 is required, this DAO is no longer required.

--- mailbox/cassandra/src/main/java/org/apache/james/mailbox/cassandra/modules/CassandraAttachmentModule.java
+++ mailbox/cassandra/src/main/java/org/apache/james/mailbox/cassandra/modules/CassandraAttachmentModule.java
@@ -20,7 +20,6 @@
 package org.apache.james.mailbox.cassandra.modules;
 
 import static com.datastax.driver.core.DataType.bigint;
-import static com.datastax.driver.core.DataType.blob;
 import static com.datastax.driver.core.DataType.text;
 import static com.datastax.driver.core.DataType.uuid;
 
@@ -28,21 +27,13 @@ import org.apache.james.backends.cassandra.components.CassandraModule;
 import org.apache.james.backends.cassandra.utils.CassandraConstants;
 import org.apache.james.mailbox.cassandra.table.CassandraAttachmentMessageIdTable;
 import org.apache.james.mailbox.cassandra.table.CassandraAttachmentOwnerTable;
-import org.apache.james.mailbox.cassandra.table.CassandraAttachmentTable;
 import org.apache.james.mailbox.cassandra.table.CassandraAttachmentV2Table;
 
 import com.datastax.driver.core.schemabuilder.SchemaBuilder;
 
 public interface CassandraAttachmentModule {
 
-    CassandraModule MODULE = CassandraModule.table(CassandraAttachmentTable.TABLE_NAME)
-        .comment("Holds attachment for fast attachment retrieval")
-        .statement(statement -> statement
-            .addPartitionKey(CassandraAttachmentTable.ID, text())
-            .addColumn(CassandraAttachmentTable.PAYLOAD, blob())
-            .addColumn(CassandraAttachmentTable.TYPE, text())
-            .addColumn(CassandraAttachmentTable.SIZE, bigint()))
-        .table(CassandraAttachmentV2Table.TABLE_NAME)
+    CassandraModule MODULE = CassandraModule.table(CassandraAttachmentV2Table.TABLE_NAME)
         .comment("Holds attachment for fast attachment retrieval. Content of messages is stored" +
             "in `blobs` and `blobparts` tables.")
         .options(options -> options
@@ -55,6 +46,7 @@ public interface CassandraAttachmentModule {
             .addColumn(CassandraAttachmentV2Table.BLOB_ID, text())
             .addColumn(CassandraAttachmentV2Table.TYPE, text())
             .addColumn(CassandraAttachmentV2Table.SIZE, bigint()))
+
         .table(CassandraAttachmentMessageIdTable.TABLE_NAME)
         .comment("Holds ids of messages owning the attachment")
         .options(options -> options
@@ -65,6 +57,7 @@ public interface CassandraAttachmentModule {
             .addPartitionKey(CassandraAttachmentMessageIdTable.ATTACHMENT_ID_AS_UUID, uuid())
             .addColumn(CassandraAttachmentMessageIdTable.ATTACHMENT_ID, text())
             .addClusteringColumn(CassandraAttachmentMessageIdTable.MESSAGE_ID, text()))
+
         .table(CassandraAttachmentOwnerTable.TABLE_NAME)
         .comment("Holds explicit owners of some attachments")
         .options(options -> options
