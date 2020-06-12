c6842072d05c013705c6bd3ef23af462609246e3
Rene Cordier <rcordier@linagora.com>
2020-06-12 10:03:51 +0700

    fixup! JAMES-1481 Change default SMTP greetings in sample configurations

--- server/container/guice/guice-common/src/test/java/org/apache/james/JamesServerContract.java
+++ server/container/guice/guice-common/src/test/java/org/apache/james/JamesServerContract.java
@@ -90,7 +90,7 @@ public interface JamesServerContract {
     default void connectSMTPServerShouldSendShabangOnConnect(GuiceJamesServer jamesServer) throws Exception {
         try (SocketChannel socketChannel = SocketChannel.open()) {
             socketChannel.connect(new InetSocketAddress(JAMES_SERVER_HOST, jamesServer.getProbe(SmtpGuiceProbe.class).getSmtpPort().getValue()));
-            assertThat(getServerConnectionResponse(socketChannel)).startsWith("220 JAMES Linagora's SMTP awesome Server");
+            assertThat(getServerConnectionResponse(socketChannel)).startsWith("220 Apache JAMES awesome SMTP Server");
         }
     }
 
