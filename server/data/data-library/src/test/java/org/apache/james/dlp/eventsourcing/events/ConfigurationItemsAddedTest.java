0572e22f755591732e76572f5b6ec56adc315c0a
Rene Cordier <rcordier@linagora.com>
2020-08-13 15:38:32 +0700

    [Refactoring] Migrate ConfigurationItemsAddedTest to JUnit5

--- server/data/data-library/src/test/java/org/apache/james/dlp/eventsourcing/events/ConfigurationItemsAddedTest.java
+++ server/data/data-library/src/test/java/org/apache/james/dlp/eventsourcing/events/ConfigurationItemsAddedTest.java
@@ -25,40 +25,40 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.apache.james.core.Domain;
 import org.apache.james.dlp.eventsourcing.aggregates.DLPAggregateId;
 import org.apache.james.eventsourcing.EventId;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.google.common.collect.ImmutableList;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 
-public class ConfigurationItemsAddedTest {
+class ConfigurationItemsAddedTest {
 
     @Test
-    public void shouldMatchBeanContract() {
+    void shouldMatchBeanContract() {
         EqualsVerifier.forClass(ConfigurationItemsAdded.class)
             .verify();
     }
 
     @Test
-    public void constructorShouldThrowWhenNullAggregateId() {
+    void constructorShouldThrowWhenNullAggregateId() {
         assertThatThrownBy(() -> new ConfigurationItemsAdded(null, EventId.first(), ImmutableList.of(RULE)))
             .isInstanceOf(NullPointerException.class);
     }
 
     @Test
-    public void constructorShouldThrowWhenNullEventId() {
+    void constructorShouldThrowWhenNullEventId() {
         assertThatThrownBy(() -> new ConfigurationItemsAdded(new DLPAggregateId(Domain.LOCALHOST), null, ImmutableList.of(RULE)))
             .isInstanceOf(NullPointerException.class);
     }
 
     @Test
-    public void constructorShouldThrowWhenNullRules() {
+    void constructorShouldThrowWhenNullRules() {
         assertThatThrownBy(() -> new ConfigurationItemsAdded(new DLPAggregateId(Domain.LOCALHOST), EventId.first(), null))
             .isInstanceOf(NullPointerException.class);
     }
 
     @Test
-    public void constructorShouldThrowWhenEmptyRulesList() {
+    void constructorShouldThrowWhenEmptyRulesList() {
         assertThatThrownBy(() -> new ConfigurationItemsAdded(new DLPAggregateId(Domain.LOCALHOST), EventId.first(), ImmutableList.of()))
             .isInstanceOf(IllegalArgumentException.class);
     }
