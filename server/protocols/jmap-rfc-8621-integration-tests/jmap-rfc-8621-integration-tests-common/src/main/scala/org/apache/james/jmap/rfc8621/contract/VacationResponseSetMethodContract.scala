9caf1e55747a0db68c78b6f85a74f60e9f89a674
LanKhuat <dlkhuat@linagora.com>
2020-08-26 14:58:11 +0700

    JAMES-3366 VacationResponse/set implementation

---
+++ server/protocols/jmap-rfc-8621-integration-tests/jmap-rfc-8621-integration-tests-common/src/main/scala/org/apache/james/jmap/rfc8621/contract/VacationResponseSetMethodContract.scala
@@ -0,0 +1,521 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *  http://www.apache.org/licenses/LICENSE-2.0                  *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.rfc8621.contract
+
+import io.netty.handler.codec.http.HttpHeaderNames.ACCEPT
+import io.restassured.RestAssured.{`given`, requestSpecification}
+import io.restassured.http.ContentType.JSON
+import net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson
+import org.apache.http.HttpStatus.SC_OK
+import org.apache.james.GuiceJamesServer
+import org.apache.james.jmap.api.vacation.AccountId
+import org.apache.james.jmap.draft.JmapGuiceProbe
+import org.apache.james.jmap.http.UserCredential
+import org.apache.james.jmap.rfc8621.contract.Fixture._
+import org.apache.james.utils.DataProbeImpl
+import org.assertj.core.api.SoftAssertions.assertSoftly
+import org.junit.jupiter.api.{BeforeEach, Disabled, Test}
+
+trait VacationResponseSetMethodContract {
+  @BeforeEach
+  def setUp(server: GuiceJamesServer): Unit = {
+    server.getProbe(classOf[DataProbeImpl])
+      .fluent
+      .addDomain(DOMAIN.asString)
+      .addUser(BOB.asString, BOB_PASSWORD)
+
+    requestSpecification = baseRequestSpecBuilder(server)
+      .setAuth(authScheme(UserCredential(BOB, BOB_PASSWORD)))
+      .build
+  }
+
+  @Test
+  def updateShouldSucceed(server: GuiceJamesServer): Unit = {
+    val request =
+      s"""
+         |{
+         |   "using": [ "urn:ietf:params:jmap:core",
+         |     "urn:ietf:params:jmap:mail",
+         |     "urn:ietf:params:jmap:vacationresponse" ],
+         |   "methodCalls": [
+         |       ["VacationResponse/set",
+         |           {
+         |                "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |                "update": {
+         |                    "singleton": {
+         |                      "isEnabled": true,
+         |                      "fromDate": "2014-10-30T14:12:00Z",
+         |                      "toDate": "2014-11-30T14:12:00Z",
+         |                      "subject": "I am in vacation",
+         |                      "textBody": "I'm currently enjoying life. Please disturb me later",
+         |                      "htmlBody": "I'm currently enjoying <b>life</b>. <br/>Please disturb me later"
+         |                    }
+         |                }
+         |           },
+         |    "c1"]]
+         |}
+         |""".stripMargin
+
+    val response = `given`
+        .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+        .body(request)
+      .when
+        .post
+      .`then`
+        .log().ifValidationFails()
+        .statusCode(SC_OK)
+        .contentType(JSON)
+        .extract
+        .body
+        .asString
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |"sessionState": "75128aab4b1b",
+         |  "methodResponses": [
+         |    ["VacationResponse/set", {
+         |      "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |      "newState": "000001",
+         |      "updated": {
+         |        "singleton": {}
+         |      }
+         |    }, "c1"]
+         |  ]
+         |}""".stripMargin)
+
+    val vacation = server.getProbe(classOf[JmapGuiceProbe]).retrieveVacation(AccountId.fromUsername(BOB))
+
+    //Should be replaced with vacation/get method when available
+    assertSoftly(softly => {
+      softly.assertThat(vacation.isEnabled).isEqualTo(true)
+      softly.assertThat(vacation.getFromDate.get()).isEqualTo("2014-10-30T14:12:00Z")
+      softly.assertThat(vacation.getToDate.get()).isEqualTo("2014-11-30T14:12:00Z")
+      softly.assertThat(vacation.getSubject.get()).isEqualTo("I am in vacation")
+      softly.assertThat(vacation.getTextBody.get()).isEqualTo("I'm currently enjoying life. Please disturb me later")
+      softly.assertThat(vacation.getHtmlBody.get()).isEqualTo("I'm currently enjoying <b>life</b>. <br/>Please disturb me later")
+    })
+  }
+
+  @Test
+  def updateUpdateShouldBeIdempotentWhenEmptyPatchObject(server: GuiceJamesServer): Unit = {
+    val request =
+      s"""
+         |{
+         |   "using": [ "urn:ietf:params:jmap:core",
+         |     "urn:ietf:params:jmap:mail",
+         |     "urn:ietf:params:jmap:vacationresponse" ],
+         |   "methodCalls": [
+         |       ["VacationResponse/set",
+         |           {
+         |                "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |                "update": {
+         |                    "singleton": {
+         |                    }
+         |                }
+         |           },
+         |    "c1"]]
+         |}
+         |""".stripMargin
+
+    val response = `given`
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .body(request)
+      .when
+      .post
+      .`then`
+      .log().ifValidationFails()
+      .statusCode(SC_OK)
+      .contentType(JSON)
+      .extract
+      .body
+      .asString
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |"sessionState": "75128aab4b1b",
+         |  "methodResponses": [
+         |    ["VacationResponse/set", {
+         |      "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |      "newState": "000001",
+         |      "updated": {
+         |        "singleton": {}
+         |      }
+         |    }, "c1"]
+         |  ]
+         |}""".stripMargin)
+
+    val vacation = server.getProbe(classOf[JmapGuiceProbe]).retrieveVacation(AccountId.fromUsername(BOB))
+
+    //Should be replaced with vacation/get method when available
+    assertSoftly(softly => {
+      softly.assertThat(vacation.isEnabled).isEqualTo(false)
+      softly.assertThat(vacation.getFromDate).isEmpty
+      softly.assertThat(vacation.getToDate).isEmpty
+      softly.assertThat(vacation.getSubject).isEmpty
+      softly.assertThat(vacation.getTextBody).isEmpty
+      softly.assertThat(vacation.getHtmlBody).isEmpty
+    })
+  }
+
+  @Test
+  def partialUpdateShouldSucceed(server: GuiceJamesServer): Unit = {
+    val request =
+      s"""
+         |{
+         |   "using": [ "urn:ietf:params:jmap:core",
+         |     "urn:ietf:params:jmap:mail",
+         |     "urn:ietf:params:jmap:vacationresponse" ],
+         |   "methodCalls": [
+         |       ["VacationResponse/set",
+         |           {
+         |                "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |                "update": {
+         |                    "singleton": {
+         |                      "htmlBody": "I'm currently enjoying <b>life</b>. <br/>Please disturb me later"
+         |                    }
+         |                }
+         |           },
+         |    "c1"]]
+         |}
+         |""".stripMargin
+
+    val response = `given`
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .body(request)
+    .when
+      .post
+    .`then`
+      .log().ifValidationFails()
+      .statusCode(SC_OK)
+      .contentType(JSON)
+      .extract
+      .body
+      .asString
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |"sessionState": "75128aab4b1b",
+         |  "methodResponses": [
+         |    ["VacationResponse/set", {
+         |      "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |      "newState": "000001",
+         |      "updated": {
+         |        "singleton": {}
+         |      }
+         |    }, "c1"]
+         |  ]
+         |}""".stripMargin)
+  }
+
+  @Test
+  def partialUpdateShouldNotAffectOtherFields(server: GuiceJamesServer): Unit = {
+    val request =
+      s"""
+         |{
+         |   "using": [ "urn:ietf:params:jmap:core",
+         |     "urn:ietf:params:jmap:mail",
+         |     "urn:ietf:params:jmap:vacationresponse" ],
+         |   "methodCalls": [
+         |       ["VacationResponse/set",
+         |           {
+         |                "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |                "update": {
+         |                    "singleton": {
+         |                      "htmlBody": "I'm currently enjoying <b>life</b>. <br/>Please disturb me later"
+         |                    }
+         |                }
+         |           },
+         |    "c1"]]
+         |}
+         |""".stripMargin
+
+    val response = `given`
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .body(request)
+    .when
+      .post
+    .`then`
+      .log().ifValidationFails()
+      .statusCode(SC_OK)
+      .contentType(JSON)
+      .extract
+      .body
+      .asString
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |"sessionState": "75128aab4b1b",
+         |  "methodResponses": [
+         |    ["VacationResponse/set", {
+         |      "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |      "newState": "000001",
+         |      "updated": {
+         |        "singleton": {}
+         |      }
+         |    }, "c1"]
+         |  ]
+         |}""".stripMargin)
+
+    val vacation = server.getProbe(classOf[JmapGuiceProbe]).retrieveVacation(AccountId.fromUsername(BOB))
+
+    //Should be replaced with vacation/get method when available
+    assertSoftly(softly => {
+      softly.assertThat(vacation.isEnabled).isEqualTo(false)
+      softly.assertThat(vacation.getFromDate).isEmpty
+      softly.assertThat(vacation.getToDate).isEmpty
+      softly.assertThat(vacation.getSubject).isEmpty
+      softly.assertThat(vacation.getTextBody).isEmpty
+      softly.assertThat(vacation.getHtmlBody.get()).isEqualTo("I'm currently enjoying <b>life</b>. <br/>Please disturb me later")
+    })
+  }
+
+  @Test
+  def updateShouldFailWhenInvalidKey(server: GuiceJamesServer): Unit = {
+    val request =
+      s"""
+         |{
+         |   "using": [ "urn:ietf:params:jmap:core",
+         |     "urn:ietf:params:jmap:mail",
+         |     "urn:ietf:params:jmap:vacationresponse" ],
+         |   "methodCalls": [
+         |       ["VacationResponse/set",
+         |           {
+         |                "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |                "update": {
+         |                    "invalid": {
+         |                      "htmlBody": "I'm currently enjoying <b>life</b>. <br/>Please disturb me later"
+         |                    }
+         |                }
+         |           },
+         |    "c1"]]
+         |}
+         |""".stripMargin
+
+    val response = `given`
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .body(request)
+      .when
+      .post
+      .`then`
+      .log().ifValidationFails()
+      .statusCode(SC_OK)
+      .contentType(JSON)
+      .extract
+      .body
+      .asString
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |	"sessionState": "75128aab4b1b",
+         |	"methodResponses": [
+         |		[
+         |			"VacationResponse/set",
+         |			{
+         |				"accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |				"newState": "000001",
+         |				"notUpdated": {
+         |					"singleton": {
+         |						"type": "invalidArguments",
+         |						"description": "id must be singleton"
+         |					}
+         |				}
+         |			},
+         |			"c1"
+         |		]
+         |	]
+         |}""".stripMargin)
+  }
+
+  @Test
+  def updateShouldFailWhenIdIsPresent(server: GuiceJamesServer): Unit = {
+    val request =
+      s"""
+         |{
+         |   "using": [ "urn:ietf:params:jmap:core",
+         |     "urn:ietf:params:jmap:mail",
+         |     "urn:ietf:params:jmap:vacationresponse" ],
+         |   "methodCalls": [
+         |       ["VacationResponse/set",
+         |           {
+         |                "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |                "update": {
+         |                    "singleton": {
+         |                      "id": "singleton",
+         |                      "htmlBody": "I'm currently enjoying <b>life</b>. <br/>Please disturb me later"
+         |                    }
+         |                }
+         |           },
+         |    "c1"]]
+         |}
+         |""".stripMargin
+
+    val response = `given`
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .body(request)
+    .when
+      .post
+    .`then`
+      .log().ifValidationFails()
+      .statusCode(SC_OK)
+      .contentType(JSON)
+      .extract
+      .body
+      .asString
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |	"sessionState": "75128aab4b1b",
+         |	"methodResponses": [
+         |		[
+         |			"VacationResponse/set",
+         |			{
+         |				"accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |				"newState": "000001",
+         |				"notUpdated": {
+         |					"singleton": {
+         |						"type": "invalidArguments",
+         |						"description": "id is server-set thus cannot be changed"
+         |					}
+         |				}
+         |			},
+         |			"c1"
+         |		]
+         |	]
+         |}""".stripMargin)
+  }
+
+  @Test
+  def updateShouldFailWhenInvalidDate(server: GuiceJamesServer): Unit = {
+    val request =
+      s"""
+         |{
+         |   "using": [ "urn:ietf:params:jmap:core",
+         |     "urn:ietf:params:jmap:mail",
+         |     "urn:ietf:params:jmap:vacationresponse" ],
+         |   "methodCalls": [
+         |       ["VacationResponse/set",
+         |           {
+         |                "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |                "update": {
+         |                    "singleton": {
+         |                      "fromDate": "2014/12/30"
+         |                    }
+         |                }
+         |           },
+         |    "c1"]]
+         |}
+         |""".stripMargin
+
+    val response = `given`
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .body(request)
+    .when
+      .post
+    .`then`
+      .log().ifValidationFails()
+      .statusCode(SC_OK)
+      .contentType(JSON)
+      .extract
+      .body
+      .asString
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |	"sessionState": "75128aab4b1b",
+         |	"methodResponses": [
+         |		[
+         |			"VacationResponse/set",
+         |			{
+         |				"accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |				"newState": "000001",
+         |				"notUpdated": {
+         |					"singleton": {
+         |						"type": "invalidArguments",
+         |						"description": "java.time.format.DateTimeParseException: Text '2014/12/30' could not be parsed at index 4"
+         |					}
+         |				}
+         |			},
+         |			"c1"
+         |		]
+         |	]
+         |}""".stripMargin)
+  }
+
+  @Test
+  @Disabled("not implemented yet")
+  def updateShouldFailWhenMultiplePatchObjects(server: GuiceJamesServer): Unit = {
+    val request =
+      s"""
+         |{
+         |   "using": [ "urn:ietf:params:jmap:core",
+         |     "urn:ietf:params:jmap:mail",
+         |     "urn:ietf:params:jmap:vacationresponse" ],
+         |   "methodCalls": [
+         |       ["VacationResponse/set",
+         |           {
+         |                "accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |                "update": {
+         |                    "singleton": {
+         |                      "htmlBody": "I'm currently enjoying <b>life</b>. <br/>Please disturb me later"
+         |                    },
+         |                    "singleton": {
+         |                      "htmlBody": "I'm currently enjoying <b>life</b>. <br/>Please disturb me later"
+         |                    }
+         |                }
+         |           },
+         |    "c1"]]
+         |}
+         |""".stripMargin
+
+    val response = `given`
+      .header(ACCEPT.toString, ACCEPT_RFC8621_VERSION_HEADER)
+      .body(request)
+    .when
+      .post
+    .`then`
+      .log().ifValidationFails()
+      .statusCode(SC_OK)
+      .contentType(JSON)
+      .extract
+      .body
+      .asString
+
+    assertThatJson(response).isEqualTo(
+      s"""{
+         |	"sessionState": "75128aab4b1b",
+         |	"methodResponses": [
+         |		[
+         |			"VacationResponse/set",
+         |			{
+         |				"accountId": "29883977c13473ae7cb7678ef767cbfbaffc8a44a6e463d971d23a65c1dc4af6",
+         |				"newState": "000001",
+         |				"notUpdated": {
+         |					"singleton": {
+         |						"type": "invalidArguments",
+         |						"description": "java.time.format.DateTimeParseException: Text '2014/12/30' could not be parsed at index 4"
+         |					}
+         |				}
+         |			},
+         |			"c1"
+         |		]
+         |	]
+         |}""".stripMargin)
+  }
+}
---
+++ server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/method/VacationResponseSetMethod.scala
@@ -0,0 +1,107 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *  http://www.apache.org/licenses/LICENSE-2.0                  *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.method
+
+import eu.timepit.refined.auto._
+import javax.inject.Inject
+import org.apache.james.jmap.api.vacation.{AccountId, VacationRepository}
+import org.apache.james.jmap.json.Serializer
+import org.apache.james.jmap.mail.SetErrorDescription
+import org.apache.james.jmap.model.CapabilityIdentifier.CapabilityIdentifier
+import org.apache.james.jmap.model.Invocation.{Arguments, MethodName}
+import org.apache.james.jmap.model.{Invocation, State}
+import org.apache.james.jmap.routes.ProcessingContext
+import org.apache.james.jmap.vacation.{VacationResponseSetError, VacationResponseSetRequest, VacationResponseSetResponse, VacationResponseUpdateResponse}
+import org.apache.james.mailbox.MailboxSession
+import org.apache.james.metrics.api.MetricFactory
+import org.apache.james.util.date.ZonedDateTimeProvider
+import org.reactivestreams.Publisher
+import play.api.libs.json.{JsError, JsObject, JsSuccess}
+import reactor.core.scala.publisher.SMono
+
+sealed trait UpdateResult {
+  def updated: Option[Map[String, VacationResponseUpdateResponse]]
+  def notUpdated: Option[Map[String, VacationResponseSetError]]
+}
+case class UpdateSuccess() extends UpdateResult {
+  override def updated: Option[Map[String, VacationResponseUpdateResponse]] = Some(Map("singleton" -> VacationResponseUpdateResponse(JsObject(Seq()))))
+
+  override def notUpdated: Option[Map[String, VacationResponseSetError]] = None
+}
+case class UpdateFailure(exception: Throwable) extends UpdateResult {
+  override def updated: Option[Map[String, VacationResponseUpdateResponse]] = None
+
+  override def notUpdated: Option[Map[String, VacationResponseSetError]] = Some(Map("singleton" -> asSetError(exception)))
+
+  def asSetError(exception: Throwable): VacationResponseSetError = exception match {
+      case e: IllegalArgumentException => VacationResponseSetError.invalidArgument(Some(SetErrorDescription(e.getMessage)))
+      case e: Throwable => VacationResponseSetError.serverFail(Some(SetErrorDescription(e.getMessage)))
+    }
+}
+
+class VacationResponseSetMethod @Inject() (serializer: Serializer,
+                                           vacationRepository: VacationRepository,
+                                           zonedDateTimeProvider: ZonedDateTimeProvider,
+                                           metricFactory: MetricFactory) extends Method {
+  override val methodName: MethodName = MethodName("VacationResponse/set")
+
+  override def process(capabilities: Set[CapabilityIdentifier],
+                       invocation: Invocation,
+                       mailboxSession: MailboxSession,
+                       processingContext: ProcessingContext): Publisher[Invocation] = {
+    metricFactory.decoratePublisherWithTimerMetricLogP99(JMAP_RFC8621_PREFIX + methodName.value,
+    asVacationResponseSetRequest(invocation.arguments)
+      .flatMap(vacationResponseSetRequest => update(mailboxSession, vacationResponseSetRequest)
+        .map(updateResult => createResponse(invocation, vacationResponseSetRequest, updateResult))))
+  }
+
+  private def update(mailboxSession: MailboxSession, vacationResponseSetRequest: VacationResponseSetRequest): SMono[UpdateResult] = {
+    vacationResponseSetRequest.parsePatch()
+      .flatMap(patch => patch.asVacationPatch)
+      .fold(e => SMono.just(UpdateFailure(e)),
+        patch => SMono.fromPublisher(vacationRepository.modifyVacation(toVacationAccountId(mailboxSession), patch))
+          .`then`(SMono.just(UpdateSuccess())))
+  }
+
+  private def toVacationAccountId(mailboxSession: MailboxSession): AccountId = {
+    AccountId.fromUsername(mailboxSession.getUser)
+  }
+
+  private def asVacationResponseSetRequest(arguments: Arguments): SMono[VacationResponseSetRequest] = {
+    serializer.deserializeVacationResponseSetRequest(arguments.value) match {
+      case JsSuccess(vacationResponseSetRequest, _) => SMono.just(vacationResponseSetRequest)
+      case errors: JsError => SMono.raiseError(new IllegalArgumentException(serializer.serialize(errors).toString))
+    }
+  }
+
+  private def createResponse(invocation: Invocation,
+                             vacationResponseSetRequest: VacationResponseSetRequest,
+                             updateResult: UpdateResult): Invocation = {
+    val response = VacationResponseSetResponse(
+      accountId = vacationResponseSetRequest.accountId,
+      newState = State.INSTANCE,
+      updated = updateResult.updated,
+      notUpdated = updateResult.notUpdated)
+
+    Invocation(methodName,
+      Arguments(serializer.serialize(response).as[JsObject]),
+      invocation.methodCallId)
+  }
+}
