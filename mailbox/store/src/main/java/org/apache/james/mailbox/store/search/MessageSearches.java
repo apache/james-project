5eaec3db728f6b2d84e96cf6a8586e5457592771
Rémi Kowalski <rkowalski@linagora.com>
2020-09-18 15:01:58 +0200

    JAMES-3380 use non am/pm dependent hour format

--- mailbox/store/src/main/java/org/apache/james/mailbox/store/search/MessageSearches.java
+++ mailbox/store/src/main/java/org/apache/james/mailbox/store/search/MessageSearches.java
@@ -635,20 +635,20 @@ public class MessageSearches implements Iterable<SimpleMessageSearchIndex.Search
 
     private SimpleDateFormat createFormat(DateResolution dateResolution) {
         switch (dateResolution) {
-        case Year:
-            return new SimpleDateFormat("yyyy");
-        case Month:
-            return new SimpleDateFormat("yyyyMM");
-        case Day:
-            return new SimpleDateFormat("yyyyMMdd");
-        case Hour:
-            return new SimpleDateFormat("yyyyMMddhh");
-        case Minute:
-            return new SimpleDateFormat("yyyyMMddhhmm");
-        case Second:
-            return new SimpleDateFormat("yyyyMMddhhmmss");
-        default:
-            return new SimpleDateFormat("yyyyMMddhhmmssSSS");
+            case Year:
+                return new SimpleDateFormat("yyyy");
+            case Month:
+                return new SimpleDateFormat("yyyyMM");
+            case Day:
+                return new SimpleDateFormat("yyyyMMdd");
+            case Hour:
+                return new SimpleDateFormat("yyyyMMddkk");
+            case Minute:
+                return new SimpleDateFormat("yyyyMMddkkmm");
+            case Second:
+                return new SimpleDateFormat("yyyyMMddkkmmss");
+            default:
+                return new SimpleDateFormat("yyyyMMddkkmmssSSS");
         }
     }
 
