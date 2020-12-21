d3bfd8e171ce9ac1e42559f3736931a50b6ed5a6
Benoit Tellier <btellier@linagora.com>
2020-12-21 11:05:46 +0700

    JAMES-3469 EmailChangeRepository interface

---
+++ server/data/data-jmap/src/main/java/org/apache/james/jmap/api/change/EmailChangeRepository.java
@@ -0,0 +1,38 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.api.change;
+
+import java.util.Optional;
+
+import org.apache.james.jmap.api.model.AccountId;
+
+import reactor.core.publisher.Mono;
+
+public interface EmailChangeRepository {
+    Mono<Void> save(EmailChange change);
+
+    Mono<EmailChanges> getSinceState(AccountId accountId, State state, Optional<Limit> maxIdsToReturn);
+
+    Mono<EmailChanges> getSinceStateWithDelegation(AccountId accountId, State state, Optional<Limit> maxIdsToReturn);
+
+    Mono<State> getLatestState(AccountId accountId);
+
+    Mono<State> getLatestStateWithDelegation(AccountId accountId);
+}
