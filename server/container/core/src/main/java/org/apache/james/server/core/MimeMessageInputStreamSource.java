d926bd1bf2d844dd5e6a11e58719d632b2abb5ac
Benoit Tellier <btellier@linagora.com>
2020-12-28 18:06:54 +0700

    fixup! JAMES-3477 MimeMessageInputStreamSource do not need ordering for opened streams

--- server/container/core/src/main/java/org/apache/james/server/core/MimeMessageInputStreamSource.java
+++ server/container/core/src/main/java/org/apache/james/server/core/MimeMessageInputStreamSource.java
@@ -23,9 +23,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.mail.MessagingException;
