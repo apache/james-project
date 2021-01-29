83229ad5af61a1558e0025191a505cb8fe65d791
Benoit Tellier <btellier@linagora.com>
2021-01-29 16:25:47 +0700

    JAMES-3497 Upgrade openjpa 3.1.0 -> 3.1.2

--- dockerfiles/run/guice/jpa/destination/run_james.sh
+++ dockerfiles/run/guice/jpa/destination/run_james.sh
@@ -2,4 +2,4 @@
 if [ "$GLOWROOT_ACTIVATED" == "true" ]; then
     GLOWROOT_OPTIONS=-javaagent:/root/glowroot/glowroot.jar
 fi
-java -classpath '/root/james-server.jar:/root/james-server-jpa-guice.lib/*' -javaagent:/root/james-server-jpa-guice.lib/openjpa-3.1.0.jar $GLOWROOT_OPTIONS -Dlogback.configurationFile=/root/conf/logback.xml -Dworking.directory=/root/ $JVM_OPTIONS org.apache.james.JPAJamesServerMain
+java -classpath '/root/james-server.jar:/root/james-server-jpa-guice.lib/*' -javaagent:/root/james-server-jpa-guice.lib/openjpa-3.1.2.jar $GLOWROOT_OPTIONS -Dlogback.configurationFile=/root/conf/logback.xml -Dworking.directory=/root/ $JVM_OPTIONS org.apache.james.JPAJamesServerMain
