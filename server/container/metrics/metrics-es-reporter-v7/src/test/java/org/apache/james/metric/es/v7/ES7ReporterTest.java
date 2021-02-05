f1800604572cd464dd6ba69beaaf257aebcc9b2d
andreasvaldma <andreas.valdmanortal.com>
2021-02-05 14:44:57 +0200

    JAMES-3492, update ES6 test, explicitly set image to ES6, create test for ES7

--- server/container/metrics/metrics-es-reporter-v7/src/test/java/org/apache/james/metric/es/v7/ES7ReporterTest.java
+++ server/container/metrics/metrics-es-reporter-v7/src/test/java/org/apache/james/metric/es/v7/ES7ReporterTest.java
@@ -20,12 +20,10 @@
 package org.apache.james.metric.es.v7;
 
 import org.apache.james.backends.es.v7.DockerElasticSearch;
-import org.apache.james.util.docker.Images;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-class ES6ReporterTest extends ESReporterContract {
+class ES7ReporterTest extends ESReporterContract {
 
     @RegisterExtension
-    static DockerElasticSearchExtension testExtension = new DockerElasticSearchExtension(
-            new DockerElasticSearch.NoAuth(Images.ELASTICSEARCH_6));
+    static DockerElasticSearchExtension testExtension = new DockerElasticSearchExtension(new DockerElasticSearch.NoAuth());
 }
