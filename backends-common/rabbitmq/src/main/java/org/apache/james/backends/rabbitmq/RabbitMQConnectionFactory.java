d2e1abf6acdd0790936af30c8409215f5efba2eb
andreasvaldma <andreas.valdmanortal.com>
2021-02-23 19:26:58 +0200

    JAMES-3502, add rabbitmq tls support in connection factory

--- backends-common/rabbitmq/src/main/java/org/apache/james/backends/rabbitmq/RabbitMQConnectionFactory.java
+++ backends-common/rabbitmq/src/main/java/org/apache/james/backends/rabbitmq/RabbitMQConnectionFactory.java
@@ -18,18 +18,33 @@
  ****************************************************************/
 package org.apache.james.backends.rabbitmq;
 
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.time.Duration;
+import java.util.Optional;
 
 import javax.inject.Inject;
+import javax.net.ssl.SSLContext;
 
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import org.apache.commons.lang3.NotImplementedException;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.util.retry.Retry;
 
+import static org.apache.james.backends.rabbitmq.RabbitMQConfiguration.SSLConfiguration.*;
+
 public class RabbitMQConnectionFactory {
+
+    private static final TrustStrategy TRUST_ALL = (x509Certificates, authType) -> true;
 
     private final ConnectionFactory connectionFactory;
 
@@ -37,8 +52,8 @@ public class RabbitMQConnectionFactory {
 
     @Inject
     public RabbitMQConnectionFactory(RabbitMQConfiguration rabbitMQConfiguration) {
-        this.connectionFactory = from(rabbitMQConfiguration);
         this.configuration = rabbitMQConfiguration;
+        this.connectionFactory = from(rabbitMQConfiguration);
     }
 
     private ConnectionFactory from(RabbitMQConfiguration rabbitMQConfiguration) {
@@ -54,10 +69,81 @@ public class RabbitMQConnectionFactory {
             connectionFactory.setUsername(rabbitMQConfiguration.getManagementCredentials().getUser());
             connectionFactory.setPassword(String.valueOf(rabbitMQConfiguration.getManagementCredentials().getPassword()));
 
+            if (configuration.useSsl()) setupSslConfiguration(connectionFactory);
+
             return connectionFactory;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void setupSslConfiguration(ConnectionFactory connectionFactory) {
+        try {
+            connectionFactory.useSslProtocol(sslContext(configuration));
+            setupHostNameVerification(connectionFactory);
+        } catch (KeyManagementException | NoSuchAlgorithmException | CertificateException | KeyStoreException | IOException | UnrecoverableKeyException e) {
+            throw new RuntimeException("Cannot set SSL options to the connection factory", e);
+        }
+    }
+
+    private SSLContext sslContext(RabbitMQConfiguration configuration) throws KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, IOException, UnrecoverableKeyException {
+        SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
+
+        RabbitMQConfiguration.SSLConfiguration sslConfiguration = configuration.getSslConfiguration();
+
+        setupSslValidationStrategy(sslContextBuilder, sslConfiguration);
+
+        setupClientCertificateAuthentication(sslContextBuilder, sslConfiguration);
+
+        return sslContextBuilder.build();
+
+    }
+
+    private void setupClientCertificateAuthentication(SSLContextBuilder sslContextBuilder, RabbitMQConfiguration.SSLConfiguration sslConfiguration) throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException, CertificateException, IOException {
+        Optional<SSLKeyStore> keyStore = sslConfiguration.getKeyStore();
+
+        if (keyStore.isPresent()) {
+            SSLKeyStore sslKeyStore = keyStore.get();
+
+            sslContextBuilder.loadKeyMaterial(sslKeyStore.getFile(), sslKeyStore.getPassword(), null);
+        }
+    }
+
+    private void setupSslValidationStrategy(SSLContextBuilder sslContextBuilder, RabbitMQConfiguration.SSLConfiguration sslConfiguration) throws NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException {
+        SSLValidationStrategy strategy = sslConfiguration
+                .getStrategy();
+
+        switch (strategy) {
+            case DEFAULT:
+                break;
+            case IGNORE:
+                sslContextBuilder.loadTrustMaterial(TRUST_ALL);
+                break;
+            case OVERRIDE:
+                applyTrustStore(sslContextBuilder);
+                break;
+            default:
+                throw new NotImplementedException(
+                        String.format("unrecognized strategy '%s'", strategy.name()));
+        }
+    }
+
+    private SSLContextBuilder applyTrustStore(SSLContextBuilder sslContextBuilder) throws CertificateException, NoSuchAlgorithmException,
+            KeyStoreException, IOException {
+
+        SSLTrustStore trustStore = configuration.getSslConfiguration()
+                .getTrustStore()
+                .orElseThrow(() -> new IllegalStateException("SSLTrustStore cannot to be empty"));
+
+        return sslContextBuilder
+                .loadTrustMaterial(trustStore.getFile(), trustStore.getPassword());
+    }
+
+    private void setupHostNameVerification(ConnectionFactory connectionFactory) {
+        HostNameVerifier hostNameVerifier = configuration.getSslConfiguration()
+                .getHostNameVerifier();
+
+        if (hostNameVerifier == HostNameVerifier.DEFAULT) connectionFactory.enableHostnameVerification();
     }
 
     Connection create() {
@@ -66,6 +152,6 @@ public class RabbitMQConnectionFactory {
 
     Mono<Connection> connectionMono() {
         return Mono.fromCallable(connectionFactory::newConnection)
-            .retryWhen(Retry.backoff(configuration.getMaxRetries(), Duration.ofMillis(configuration.getMinDelayInMs())).scheduler(Schedulers.elastic()));
+                .retryWhen(Retry.backoff(configuration.getMaxRetries(), Duration.ofMillis(configuration.getMinDelayInMs())).scheduler(Schedulers.elastic()));
     }
 }
