92b283d5053caf83d1bd924cdb0137774688b4b3
andreasvaldma <andreas.valdmanortal.com>
2021-02-23 19:26:38 +0200

    JAMES-3502, add rabbitmq tls support

--- backends-common/rabbitmq/src/main/java/org/apache/james/backends/rabbitmq/RabbitMQConfiguration.java
+++ backends-common/rabbitmq/src/main/java/org/apache/james/backends/rabbitmq/RabbitMQConfiguration.java
@@ -18,17 +18,274 @@
  ****************************************************************/
 package org.apache.james.backends.rabbitmq;
 
+import java.io.File;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.apache.commons.configuration2.Configuration;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 
+import static org.apache.james.backends.rabbitmq.RabbitMQConfiguration.SSLConfiguration.*;
+import static org.apache.james.backends.rabbitmq.RabbitMQConfiguration.SSLConfiguration.SSLValidationStrategy.*;
+
 public class RabbitMQConfiguration {
+
+    public static class SSLConfiguration {
+
+        public enum SSLValidationStrategy {
+            DEFAULT,
+            IGNORE,
+            OVERRIDE;
+
+            static SSLValidationStrategy from(String rawValue) {
+                Preconditions.checkNotNull(rawValue);
+
+                return Stream.of(values())
+                        .filter(strategy -> strategy.name().equalsIgnoreCase(rawValue))
+                        .findAny()
+                        .orElseThrow(() -> new IllegalArgumentException(String.format("invalid strategy '%s'", rawValue)));
+
+            }
+        }
+
+        public enum HostNameVerifier {
+            DEFAULT,
+            ACCEPT_ANY_HOSTNAME;
+
+            static HostNameVerifier from(String rawValue) {
+                Preconditions.checkNotNull(rawValue);
+
+                return Stream.of(values())
+                        .filter(verifier -> verifier.name().equalsIgnoreCase(rawValue))
+                        .findAny()
+                        .orElseThrow(() -> new IllegalArgumentException(String.format("invalid HostNameVerifier '%s'", rawValue)));
+
+            }
+        }
+
+        public static class SSLKeyStore {
+
+            public static SSLKeyStore of(String filePath, String password) {
+                return new SSLKeyStore(filePath, password);
+            }
+
+            private final File file;
+            private final char[] password;
+
+            private SSLKeyStore(String filePath, String password) {
+                Preconditions.checkNotNull(filePath, "%s cannot be null when %s is specified",
+                        SSL_KEY_STORE_PATH, SSL_KEY_STORE_PASSWORD);
+                Preconditions.checkNotNull(password,
+                        "%s cannot be null when %s is specified",
+                        SSL_KEY_STORE_PASSWORD, SSL_KEY_STORE_PATH);
+                Preconditions.checkArgument(Files.exists(Paths.get(filePath)),
+                        "the file '%s' from property '%s' doesn't exist", filePath, SSL_KEY_STORE_PATH);
+
+                this.file = new File(filePath);
+                this.password = password.toCharArray();
+            }
+
+            public File getFile() {
+                return file;
+            }
+
+            public char[] getPassword() {
+                return password;
+            }
+
+            @Override
+            public final boolean equals(Object o) {
+                if (o instanceof SSLKeyStore) {
+                    SSLKeyStore that = (SSLKeyStore) o;
+
+                    return Objects.equals(this.file, that.file)
+                            && Arrays.equals(this.password, that.password);
+                }
+                return false;
+            }
+
+            @Override
+            public final int hashCode() {
+                return Objects.hash(file, Arrays.hashCode(password));
+            }
+        }
+
+
+        public static class SSLTrustStore {
+
+            public static SSLTrustStore of(String filePath, String password) {
+                return new SSLTrustStore(filePath, password);
+            }
+
+            private final File file;
+            private final char[] password;
+
+            private SSLTrustStore(String filePath, String password) {
+                Preconditions.checkNotNull(filePath, "%s cannot be null when %s is specified",
+                        SSL_TRUST_STORE_PATH, SSL_TRUST_STORE_PASSWORD);
+                Preconditions.checkNotNull(password,
+                        "%s cannot be null when %s is specified",
+                        SSL_TRUST_STORE_PASSWORD, SSL_TRUST_STORE_PATH);
+                Preconditions.checkArgument(Files.exists(Paths.get(filePath)),
+                        "the file '%s' from property '%s' doesn't exist", filePath, SSL_TRUST_STORE_PATH);
+
+                this.file = new File(filePath);
+                this.password = password.toCharArray();
+            }
+
+            public File getFile() {
+                return file;
+            }
+
+            public char[] getPassword() {
+                return password;
+            }
+
+            @Override
+            public final boolean equals(Object o) {
+                if (o instanceof SSLTrustStore) {
+                    SSLTrustStore that = (SSLTrustStore) o;
+
+                    return Objects.equals(this.file, that.file)
+                            && Arrays.equals(this.password, that.password);
+                }
+                return false;
+            }
+
+            @Override
+            public final int hashCode() {
+                return Objects.hash(file, Arrays.hashCode(password));
+            }
+        }
+
+        static class Builder {
+
+            interface RequireSSLStrategyTrustStore {
+                RequireHostNameVerifier sslStrategy(SSLValidationStrategy strategy, Optional<SSLTrustStore> trustStore);
+
+                default RequireHostNameVerifier strategyIgnore() {
+                    return sslStrategy(SSLValidationStrategy.IGNORE, Optional.empty());
+                }
+
+                default RequireHostNameVerifier strategyOverride(SSLTrustStore trustStore) {
+                    return sslStrategy(OVERRIDE, Optional.of(trustStore));
+                }
+
+                default RequireHostNameVerifier strategyDefault() {
+                    return sslStrategy(SSLValidationStrategy.DEFAULT, Optional.empty());
+                }
+            }
+
+            interface RequireHostNameVerifier {
+                ReadyToBuild hostNameVerifier(HostNameVerifier hostNameVerifier);
+
+                default ReadyToBuild acceptAnyHostNameVerifier() {
+                    return hostNameVerifier(HostNameVerifier.ACCEPT_ANY_HOSTNAME);
+                }
+
+                default ReadyToBuild defaultHostNameVerifier() {
+                    return hostNameVerifier(HostNameVerifier.DEFAULT);
+                }
+            }
+
+            static class ReadyToBuild {
+                private final SSLValidationStrategy sslValidationStrategy;
+                private final HostNameVerifier hostNameVerifier;
+                private Optional<SSLTrustStore> sslTrustStore;
+                private Optional<SSLKeyStore> sslKeyStore;
+
+                private ReadyToBuild(SSLValidationStrategy sslValidationStrategy, HostNameVerifier hostNameVerifier, Optional<SSLTrustStore> sslTrustStore) {
+                    this.sslValidationStrategy = sslValidationStrategy;
+                    this.hostNameVerifier = hostNameVerifier;
+                    this.sslTrustStore = sslTrustStore;
+                }
+
+                public ReadyToBuild sslKeyStore(Optional<SSLKeyStore> sslKeyStore) {
+                    this.sslKeyStore = sslKeyStore;
+                    return this;
+                }
+
+                public SSLConfiguration build() {
+                    return new SSLConfiguration(sslValidationStrategy, hostNameVerifier, sslTrustStore, sslKeyStore);
+                }
+            }
+        }
+
+        static SSLConfiguration defaultBehavior() {
+            return new SSLConfiguration(SSLValidationStrategy.DEFAULT, HostNameVerifier.DEFAULT, Optional.empty(), Optional.empty());
+        }
+
+        static Builder.RequireSSLStrategyTrustStore builder() {
+            return (strategy, trustStore) -> hostNameVerifier -> new Builder.ReadyToBuild(strategy, hostNameVerifier, trustStore);
+        }
+
+        private final SSLValidationStrategy strategy;
+        private final HostNameVerifier hostNameVerifier;
+        private final Optional<SSLTrustStore> trustStore;
+        private final Optional<SSLKeyStore> keyStore;
+
+        private SSLConfiguration(SSLValidationStrategy strategy, HostNameVerifier hostNameVerifier, Optional<SSLTrustStore> trustStore, Optional<SSLKeyStore> keyStore) {
+            Preconditions.checkNotNull(strategy);
+            Preconditions.checkNotNull(trustStore);
+            Preconditions.checkNotNull(hostNameVerifier);
+            Preconditions.checkArgument(strategy != OVERRIDE || trustStore.isPresent(),  "%s strategy requires trustStore to be present", OVERRIDE.name());
+
+            this.strategy = strategy;
+            this.trustStore = trustStore;
+            this.keyStore = keyStore;
+            this.hostNameVerifier = hostNameVerifier;
+        }
+
+        public SSLValidationStrategy getStrategy() {
+            return strategy;
+        }
+
+        public Optional<SSLTrustStore> getTrustStore() {
+            return trustStore;
+        }
+
+        public Optional<SSLKeyStore> getKeyStore() {
+            return keyStore;
+        }
+
+        public HostNameVerifier getHostNameVerifier() {
+            return hostNameVerifier;
+        }
+
+        @Override
+        public final boolean equals(Object o) {
+            if (o instanceof SSLConfiguration) {
+                SSLConfiguration that = (SSLConfiguration) o;
+
+                return Objects.equals(this.strategy, that.strategy)
+                        && Objects.equals(this.trustStore, that.trustStore)
+                        && Objects.equals(this.hostNameVerifier, that.hostNameVerifier);
+            }
+            return false;
+        }
+
+        @Override
+        public final int hashCode() {
+            return Objects.hash(strategy, trustStore, hostNameVerifier);
+        }
+    }
+
+    private static String USE_SSL = "ssl.enabled";
+
+    private static String SSL_TRUST_STORE_PATH = "ssl.truststore";
+    private static String SSL_TRUST_STORE_PASSWORD = "ssl.truststore.password";
+    private static String SSL_VALIDATION_STRATEGY = "ssl.validation.strategy";
+    private static String SSL_HOSTNAME_VERIFIER = "ssl.hostname.verifier";
+
+    private static String SSL_KEY_STORE_PATH = "ssl.keystore";
+    private static String SSL_KEY_STORE_PASSWORD = "ssl.keystore.password";
 
     public static class ManagementCredentials {
 
@@ -114,6 +371,8 @@ public class RabbitMQConfiguration {
         private Optional<Integer> handshakeTimeoutInMs;
         private Optional<Integer> shutdownTimeoutInMs;
         private Optional<Integer> networkRecoveryIntervalInMs;
+        private Optional<Boolean> useSsl;
+        private Optional<SSLConfiguration> sslConfiguration;
 
         private Builder(URI amqpUri, URI managementUri, ManagementCredentials managementCredentials) {
             this.amqpUri = amqpUri;
@@ -126,6 +385,8 @@ public class RabbitMQConfiguration {
             this.handshakeTimeoutInMs = Optional.empty();
             this.shutdownTimeoutInMs = Optional.empty();
             this.networkRecoveryIntervalInMs = Optional.empty();
+            this.useSsl = Optional.empty();
+            this.sslConfiguration = Optional.empty();
         }
 
         public Builder maxRetries(int maxRetries) {
@@ -163,6 +424,16 @@ public class RabbitMQConfiguration {
             return this;
         }
 
+        public Builder useSsl(Boolean useSsl) {
+            this.useSsl = Optional.ofNullable(useSsl);
+            return this;
+        }
+
+        public Builder sslConfiguration(SSLConfiguration sslConfiguration) {
+            this.sslConfiguration = Optional.of(sslConfiguration);
+            return this;
+        }
+
         public RabbitMQConfiguration build() {
             Preconditions.checkNotNull(amqpUri, "'amqpUri' should not be null");
             Preconditions.checkNotNull(managementUri, "'managementUri' should not be null");
@@ -176,9 +447,12 @@ public class RabbitMQConfiguration {
                     channelRpcTimeoutInMs.orElse(DEFAULT_CHANNEL_RPC_TIMEOUT),
                     handshakeTimeoutInMs.orElse(DEFAULT_HANDSHAKE_TIMEOUT),
                     shutdownTimeoutInMs.orElse(DEFAULT_SHUTDOWN_TIMEOUT),
-                    networkRecoveryIntervalInMs.orElse(DEFAULT_NETWORK_RECOVERY_INTERVAL)
+                    networkRecoveryIntervalInMs.orElse(DEFAULT_NETWORK_RECOVERY_INTERVAL),
+                    useSsl.orElse(false),
+                    sslConfiguration.orElse(defaultBehavior())
                 );
         }
+
     }
 
     private static final String URI_PROPERTY_NAME = "uri";
@@ -197,11 +471,16 @@ public class RabbitMQConfiguration {
         Preconditions.checkState(!Strings.isNullOrEmpty(managementUriAsString), "You need to specify the management URI of RabbitMQ");
         URI managementUri = checkURI(managementUriAsString);
 
+        Boolean useSsl = configuration.getBoolean(USE_SSL, false);
+
         ManagementCredentials managementCredentials = ManagementCredentials.from(configuration);
         return builder()
             .amqpUri(amqpUri)
             .managementUri(managementUri)
             .managementCredentials(managementCredentials)
+            .useSsl(useSsl)
+            .sslConfiguration(sslTrustConfiguration(configuration))
+            .sslConfiguration(sslTrustConfiguration(configuration))
             .build();
     }
 
@@ -213,6 +492,46 @@ public class RabbitMQConfiguration {
         }
     }
 
+    private static SSLConfiguration sslTrustConfiguration(Configuration configuration) {
+        SSLValidationStrategy sslStrategy = Optional
+                .ofNullable(configuration.getString(SSL_VALIDATION_STRATEGY))
+                .map(SSLValidationStrategy::from)
+                .orElse(SSLValidationStrategy.DEFAULT);
+
+        HostNameVerifier hostNameVerifier = Optional
+                .ofNullable(configuration.getString(SSL_HOSTNAME_VERIFIER))
+                .map(HostNameVerifier::from)
+                .orElse(HostNameVerifier.DEFAULT);
+
+        return SSLConfiguration.builder()
+                .sslStrategy(sslStrategy, getSSLTrustStore(configuration))
+                .hostNameVerifier(hostNameVerifier)
+                .sslKeyStore(getSSLKeyStore(configuration))
+                .build();
+    }
+
+    private static Optional<SSLTrustStore> getSSLTrustStore(Configuration configuration) {
+        String trustStorePath = configuration.getString(SSL_TRUST_STORE_PATH);
+        String trustStorePassword = configuration.getString(SSL_TRUST_STORE_PASSWORD);
+
+        if (trustStorePath == null && trustStorePassword == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(SSLTrustStore.of(trustStorePath, trustStorePassword));
+    }
+
+    private static Optional<SSLKeyStore> getSSLKeyStore(Configuration configuration) {
+        String keyStorePath = configuration.getString(SSL_KEY_STORE_PATH);
+        String keyStorePassword = configuration.getString(SSL_KEY_STORE_PASSWORD);
+
+        if (keyStorePath == null && keyStorePassword == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(SSLKeyStore.of(keyStorePath, keyStorePassword));
+    }
+
     private final URI uri;
     private final URI managementUri;
     private final int maxRetries;
@@ -222,12 +541,14 @@ public class RabbitMQConfiguration {
     private final int handshakeTimeoutInMs;
     private final int shutdownTimeoutInMs;
     private final int networkRecoveryIntervalInMs;
+    private final Boolean useSsl;
+    private final SSLConfiguration sslConfiguration;
 
 
     private final ManagementCredentials managementCredentials;
 
     private RabbitMQConfiguration(URI uri, URI managementUri, ManagementCredentials managementCredentials, int maxRetries, int minDelayInMs,
-                                  int connectionTimeoutInMs, int channelRpcTimeoutInMs, int handshakeTimeoutInMs, int shutdownTimeoutInMs, int networkRecoveryIntervalInMs) {
+                                  int connectionTimeoutInMs, int channelRpcTimeoutInMs, int handshakeTimeoutInMs, int shutdownTimeoutInMs, int networkRecoveryIntervalInMs, Boolean useSsl, SSLConfiguration sslConfiguration) {
         this.uri = uri;
         this.managementUri = managementUri;
         this.managementCredentials = managementCredentials;
@@ -238,6 +559,8 @@ public class RabbitMQConfiguration {
         this.handshakeTimeoutInMs = handshakeTimeoutInMs;
         this.shutdownTimeoutInMs = shutdownTimeoutInMs;
         this.networkRecoveryIntervalInMs = networkRecoveryIntervalInMs;
+        this.useSsl = useSsl;
+        this.sslConfiguration = sslConfiguration;
     }
 
     public URI getUri() {
@@ -278,6 +601,14 @@ public class RabbitMQConfiguration {
 
     public ManagementCredentials getManagementCredentials() {
         return managementCredentials;
+    }
+
+    public Boolean useSsl() {
+        return useSsl;
+    }
+
+    public SSLConfiguration getSslConfiguration() {
+        return sslConfiguration;
     }
 
     @Override
