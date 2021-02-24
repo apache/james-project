90d84820328fa14eed9bb16f097cafb1080f13d1
andreasvaldma <andreas.valdmanortal.com>
2021-02-24 20:52:10 +0200

    JAMES-3502, set keystore Optional.empty() as default (client cert authentication)

--- backends-common/rabbitmq/src/main/java/org/apache/james/backends/rabbitmq/RabbitMQConfiguration.java
+++ backends-common/rabbitmq/src/main/java/org/apache/james/backends/rabbitmq/RabbitMQConfiguration.java
@@ -205,6 +205,7 @@ public class RabbitMQConfiguration {
                     this.sslValidationStrategy = sslValidationStrategy;
                     this.hostNameVerifier = hostNameVerifier;
                     this.sslTrustStore = sslTrustStore;
+                    this.sslKeyStore = Optional.empty();
                 }
 
                 public ReadyToBuild sslKeyStore(Optional<SSLKeyStore> sslKeyStore) {
