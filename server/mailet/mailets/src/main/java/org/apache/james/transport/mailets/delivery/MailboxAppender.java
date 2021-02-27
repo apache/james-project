a1595afa6b66adbeee5f2e381d3e988fc0f4fcd8
Benoit Tellier <btellier@linagora.com>
2021-02-27 14:45:31 +0700

    JAMES-3506 Avoid multiple mailbox reads upon mail receival
    As we later read the mailbox we can save a "exist" check (~4ms)

--- server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/delivery/MailboxAppender.java
+++ server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/delivery/MailboxAppender.java
@@ -29,6 +29,7 @@ import org.apache.james.mailbox.MessageManager;
 import org.apache.james.mailbox.MessageManager.AppendResult;
 import org.apache.james.mailbox.exception.MailboxException;
 import org.apache.james.mailbox.exception.MailboxExistsException;
+import org.apache.james.mailbox.exception.MailboxNotFoundException;
 import org.apache.james.mailbox.model.ComposedMessageId;
 import org.apache.james.mailbox.model.MailboxPath;
 import org.apache.james.server.core.MimeMessageInputStream;
@@ -36,8 +37,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Strings;
-
-import reactor.core.publisher.Mono;
 
 public class MailboxAppender {
     private static final Logger LOGGER = LoggerFactory.getLogger(MailboxAppender.class);
@@ -78,8 +77,7 @@ public class MailboxAppender {
     }
 
     private AppendResult appendMessageToMailbox(MimeMessage mail, MailboxSession session, MailboxPath path) throws MailboxException, MessagingException {
-        createMailboxIfNotExist(session, path);
-        final MessageManager mailbox = mailboxManager.getMailbox(path, session);
+        MessageManager mailbox = createMailboxIfNotExist(session, path);
         if (mailbox == null) {
             throw new MessagingException("Mailbox " + path + " for user " + session.getUser().asString() + " was not found on this server.");
         }
@@ -89,12 +87,16 @@ public class MailboxAppender {
             session);
     }
 
-    private void createMailboxIfNotExist(MailboxSession session, MailboxPath path) throws MailboxException {
-        if (!Mono.from(mailboxManager.mailboxExists(path, session)).block()) {
+    private MessageManager createMailboxIfNotExist(MailboxSession session, MailboxPath path) throws MailboxException {
+        try {
+            return mailboxManager.getMailbox(path, session);
+        } catch (MailboxNotFoundException e) {
             try {
                 mailboxManager.createMailbox(path, session);
-            } catch (MailboxExistsException e) {
+                return mailboxManager.getMailbox(path, session);
+            } catch (MailboxExistsException exist) {
                 LOGGER.info("Mailbox {} have been created concurrently", path);
+                return mailboxManager.getMailbox(path, session);
             }
         }
     }
