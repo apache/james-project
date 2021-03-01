7b99d165d118389bd314e4484c474bf9a2f0c84f
Benoit Tellier <btellier@linagora.com>
2021-03-01 10:58:44 +0700

    JAMES-3507 Test bed for IMAP Netty bindings

---
+++ server/protocols/protocols-imap4/src/test/java/org/apache/james/imapserver/netty/IMAPServerTest.java
@@ -0,0 +1,196 @@
+package org.apache.james.imapserver.netty;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.convert.DisabledListDelimiterHandler;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.io.FileHandler;
+import org.apache.james.core.Username;
+import org.apache.james.imap.encode.main.DefaultImapEncoderFactory;
+import org.apache.james.imap.main.DefaultImapDecoderFactory;
+import org.apache.james.imap.processor.main.DefaultImapProcessorFactory;
+import org.apache.james.mailbox.inmemory.manager.InMemoryIntegrationResources;
+import org.apache.james.mailbox.store.FakeAuthenticator;
+import org.apache.james.mailbox.store.FakeAuthorizator;
+import org.apache.james.mailbox.store.StoreSubscriptionManager;
+import org.apache.james.metrics.tests.RecordingMetricFactory;
+import org.apache.james.util.ClassLoaderUtils;
+import org.apache.james.utils.TestIMAPClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+class IMAPServerTest {
+    private static final String _129K_MESSAGE = "header: value\r\n" + "012345678\r\n".repeat(13107);
+    private static final String _65K_MESSAGE = "header: value\r\n" + "012345678\r\n".repeat(6553);
+    private static final Username USER = Username.of("user@domain.org");
+    private static final String USER_PASS = "pass";
+
+    private static XMLConfiguration getConfig(InputStream configStream) throws ConfigurationException {
+        FileBasedConfigurationBuilder<XMLConfiguration> builder = new FileBasedConfigurationBuilder<>(XMLConfiguration.class)
+            .configure(new Parameters()
+                .xml()
+                .setListDelimiterHandler(new DisabledListDelimiterHandler()));
+        XMLConfiguration xmlConfiguration = builder.getConfiguration();
+        FileHandler fileHandler = new FileHandler(xmlConfiguration);
+        fileHandler.load(configStream);
+        try {
+            configStream.close();
+        } catch (IOException ignored) {
+            // Ignored
+        }
+
+        return xmlConfiguration;
+    }
+
+    @RegisterExtension
+    public TestIMAPClient testIMAPClient = new TestIMAPClient();
+
+    private IMAPServer createImapServer(String configurationFile) throws Exception {
+        FakeAuthenticator authenticator = new FakeAuthenticator();
+        authenticator.addUser(USER, USER_PASS);
+
+        InMemoryIntegrationResources resources = InMemoryIntegrationResources.builder()
+            .authenticator(authenticator)
+            .authorizator(FakeAuthorizator.defaultReject())
+            .inVmEventBus()
+            .defaultAnnotationLimits()
+            .defaultMessageParser()
+            .scanningSearchIndex()
+            .noPreDeletionHooks()
+            .storeQuotaManager()
+            .build();
+        RecordingMetricFactory metricFactory = new RecordingMetricFactory();
+        IMAPServer imapServer = new IMAPServer(
+            DefaultImapDecoderFactory.createDecoder(),
+            new DefaultImapEncoderFactory().buildImapEncoder(),
+            DefaultImapProcessorFactory.createXListSupportingProcessor(
+                resources.getMailboxManager(),
+                resources.getEventBus(),
+                new StoreSubscriptionManager(resources.getMailboxManager().getMapperFactory()),
+                null,
+                resources.getQuotaManager(),
+                resources.getQuotaRootResolver(),
+                metricFactory),
+            new ImapMetrics(metricFactory));
+
+        imapServer.configure(getConfig(ClassLoaderUtils.getSystemResourceAsSharedStream(configurationFile)));
+        imapServer.init();
+
+        return imapServer;
+    }
+
+    @Nested
+    class NoLimit {
+        IMAPServer imapServer;
+
+        @BeforeEach
+        void beforeEach() throws Exception {
+            imapServer = createImapServer("imapServerNoLimits.xml");
+        }
+
+        @AfterEach
+        void tearDown() {
+            imapServer.destroy();
+        }
+
+        @Test
+        void smallAppendsShouldWork() {
+            int port = imapServer.getListenAddresses().get(0).getPort();
+            System.out.println(port);
+
+            assertThatCode(() ->
+                testIMAPClient.connect("127.0.0.1", port)
+                    .login(USER.asString(), USER_PASS)
+                    .append("INBOX", "header: value\r\n\r\nBODY"))
+                .doesNotThrowAnyException();
+        }
+
+        @Disabled("JAMES-3507 Bad file management, scoped to command parsing, causes this to fail")
+        @Test
+        void mediumAppendsShouldWork() {
+            int port = imapServer.getListenAddresses().get(0).getPort();
+            System.out.println(port);
+
+            assertThatCode(() ->
+                testIMAPClient.connect("127.0.0.1", port)
+                    .login(USER.asString(), USER_PASS)
+                    .append("INBOX", _65K_MESSAGE))
+                .doesNotThrowAnyException();
+        }
+
+        @Test
+        void largeAppendsShouldBeRejected() {
+            int port = imapServer.getListenAddresses().get(0).getPort();
+            System.out.println(port);
+
+            assertThatCode(() ->
+                testIMAPClient.connect("127.0.0.1", port)
+                    .login(USER.asString(), USER_PASS)
+                    .append("INBOX", _129K_MESSAGE))
+                .isInstanceOf(RuntimeException.class)
+            .hasMessageContaining("NO APPEND failed");
+        }
+    }
+
+    @Disabled("Broken literalSizeLimit option")
+    @Nested
+    class Limit {
+        IMAPServer imapServer;
+
+        @BeforeEach
+        void beforeEach() throws Exception {
+            imapServer = createImapServer("imapServer.xml");
+        }
+
+        @AfterEach
+        void tearDown() {
+            imapServer.destroy();
+        }
+
+        @Test
+        void smallAppendsShouldWork() {
+            int port = imapServer.getListenAddresses().get(0).getPort();
+            System.out.println(port);
+
+            assertThatCode(() ->
+                testIMAPClient.connect("127.0.0.1", port)
+                    .login(USER.asString(), USER_PASS)
+                    .append("INBOX", "header: value\r\n\r\nBODY"))
+                .doesNotThrowAnyException();
+        }
+
+        @Test
+        void mediumAppendsShouldWork() {
+            int port = imapServer.getListenAddresses().get(0).getPort();
+            System.out.println(port);
+
+            assertThatCode(() ->
+                testIMAPClient.connect("127.0.0.1", port)
+                    .login(USER.asString(), USER_PASS)
+                    .append("INBOX", _65K_MESSAGE))
+                .doesNotThrowAnyException();
+        }
+
+        @Test
+        void largeAppendsShouldWork() {
+            int port = imapServer.getListenAddresses().get(0).getPort();
+            System.out.println(port);
+
+            assertThatCode(() ->
+                testIMAPClient.connect("127.0.0.1", port)
+                    .login(USER.asString(), USER_PASS)
+                    .append("INBOX", _129K_MESSAGE))
+                .doesNotThrowAnyException();
+        }
+    }
+}
