e984390853279bbf7fc7045061e751551becd45a
andreasvaldma <andreas.valdmanortal.com>
2021-03-10 20:02:22 +0200

    JAMES-3492, change ElasticSearchStartUpCheck.java to use v7

--- server/container/guice/elasticsearch/src/main/java/org/apache/james/modules/mailbox/ElasticSearchStartUpCheck.java
+++ server/container/guice/elasticsearch/src/main/java/org/apache/james/modules/mailbox/ElasticSearchStartUpCheck.java
@@ -28,7 +28,6 @@ import org.apache.james.backends.es.v7.ReactorElasticSearchClient;
 import org.apache.james.lifecycle.api.StartUpCheck;
 import org.elasticsearch.Version;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.core.MainResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,7 +35,7 @@ public class ElasticSearchStartUpCheck implements StartUpCheck {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchConfiguration.class);
 
-    private static final Version RECOMMENDED_ES_VERSION = Version.fromString("6.3.2");
+    private static final Version RECOMMENDED_ES_VERSION = Version.V_7_10_2;
     private static final String VERSION_CHECKING_ERROR_MESSAGE = "Error when checking ES version";
 
     public static final String CHECK_NAME = "ElasticSearchStartUpCheck";
@@ -51,25 +50,27 @@ public class ElasticSearchStartUpCheck implements StartUpCheck {
     @Override
     public CheckResult check() {
         try {
-            MainResponse.Version esVersion = client.info(RequestOptions.DEFAULT)
-                .getVersion();
 
+            Version esVersion = Version.fromString(client.info(RequestOptions.DEFAULT).getVersion().getNumber());
+
+            if (esVersion.isCompatible(RECOMMENDED_ES_VERSION)) {
                 return CheckResult.builder()
                     .checkName(checkName())
                     .resultType(ResultType.GOOD)
                     .build();
+            }
 
-//            String esVersionCompatibilityWarn = String.format(
-//                "ES version(%s) is not compatible with the recommendation(%s)",
-//                esVersion.toString(),
-//                RECOMMENDED_ES_VERSION.toString());
-//            LOGGER.warn(esVersionCompatibilityWarn);
-//
-//            return CheckResult.builder()
-//                .checkName(checkName())
-//                .resultType(ResultType.BAD)
-//                .description(esVersionCompatibilityWarn)
-//                .build();
+            String esVersionCompatibilityWarn = String.format(
+                "ES version(%s) is not compatible with the recommendation(%s)",
+                esVersion.toString(),
+                RECOMMENDED_ES_VERSION.toString());
+            LOGGER.warn(esVersionCompatibilityWarn);
+
+            return CheckResult.builder()
+                .checkName(checkName())
+                .resultType(ResultType.BAD)
+                .description(esVersionCompatibilityWarn)
+                .build();
         } catch (IOException e) {
             LOGGER.error(VERSION_CHECKING_ERROR_MESSAGE, e);
             return CheckResult.builder()
