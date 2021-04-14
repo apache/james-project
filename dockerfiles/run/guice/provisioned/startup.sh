e4aa88d486bea70129cb2911502d066ced8abc8e
Benoit Tellier <btellier@linagora.com>
2021-04-14 11:56:00 +0700

    [FIX] update openjpa version in startup.sh for demo image

--- dockerfiles/run/guice/provisioned/startup.sh
+++ dockerfiles/run/guice/provisioned/startup.sh
@@ -2,4 +2,4 @@
 
 wait-for-it.sh --host=localhost --port=9999 --strict --timeout=0 -- ./initialdata.sh &
 
-java -classpath '/root/james-server.jar:/root/james-server-jpa-guice.lib/*' -javaagent:/root/james-server-jpa-guice.lib/openjpa-3.1.0.jar -Dlogback.configurationFile=/root/conf/logback.xml -Dworking.directory=/root/ org.apache.james.JPAJamesServerMain
+java -classpath '/root/james-server.jar:/root/james-server-jpa-guice.lib/*' -javaagent:/root/james-server-jpa-guice.lib/openjpa-3.1.2.jar -Dlogback.configurationFile=/root/conf/logback.xml -Dworking.directory=/root/ org.apache.james.JPAJamesServerMain
