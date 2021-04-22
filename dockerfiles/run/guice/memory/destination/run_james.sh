6b7a0deb69159e4ad73ffe6368d91e3e4ed66657
Benoit Tellier <btellier@linagora.com>
2021-04-22 09:16:18 +0700

    JAMES-3261 Get rid of old dockerfiles for James memory

--- dockerfiles/run/guice/memory/destination/run_james.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$GLOWROOT_ACTIVATED" == "true" ]; then
-    GLOWROOT_OPTIONS=-javaagent:/root/glowroot/glowroot.jar
-fi
-java -Dlogback.configurationFile=/root/conf/logback.xml -Dworking.directory=/root/ $JVM_OPTIONS $GLOWROOT_OPTIONS -jar james-server-memory-guice.jar
