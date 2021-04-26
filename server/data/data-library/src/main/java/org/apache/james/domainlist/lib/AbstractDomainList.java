f93cdfc8ff70ca569e77eed79e7496a64785f507
Benoit Tellier <btellier@linagora.com>
2021-04-26 23:39:38 +0700

    JAMES-3467 Domain cache should be refreshed periodically under read load
    The documentation states:
    
    ```
    Expiracy of the cache. Longer means less reads are performed to the backend but writes will take longer to propagate.
    ```
    
    Which implies writes will e visible after the expiracy period.
    
    Sadly it was not the case as the added test proves it...

--- server/data/data-library/src/main/java/org/apache/james/domainlist/lib/AbstractDomainList.java
+++ server/data/data-library/src/main/java/org/apache/james/domainlist/lib/AbstractDomainList.java
@@ -93,7 +93,7 @@ public abstract class AbstractDomainList implements DomainList, Configurable {
         this.configuration = domainListConfiguration;
 
         this.cache = CacheBuilder.newBuilder()
-            .expireAfterAccess(configuration.getCacheExpiracy())
+            .expireAfterWrite(configuration.getCacheExpiracy())
             .build(new CacheLoader<>() {
                 @Override
                 public Boolean load(Domain key) throws DomainListException {
