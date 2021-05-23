30fce7c806dec97e7581a0507489d5803c762573
Benoit Tellier <btellier@linagora.com>
2021-05-23 10:15:08 +0700

    [PERFORMANCE] Further reactify PostDequeueDecorator
    This allows a single .block call.

--- server/protocols/jmap-draft/src/main/java/org/apache/james/jmap/draft/send/exception/MailShouldBeInOutboxException.java
+++ server/protocols/jmap-draft/src/main/java/org/apache/james/jmap/draft/send/exception/MailShouldBeInOutboxException.java
@@ -21,7 +21,7 @@ package org.apache.james.jmap.draft.send.exception;
 import org.apache.james.mailbox.model.MessageId;
 import org.apache.james.queue.api.MailQueue.MailQueueException;
 
-public class MailShouldBeInOutboxException extends MailQueueException {
+public class MailShouldBeInOutboxException extends RuntimeException {
 
     public MailShouldBeInOutboxException(MessageId messageId) {
         super("Message with Id " + messageId + " can't be found in outbox");
