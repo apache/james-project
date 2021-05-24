79ee79ee10c492889f373270abfad37a8c3b343b
Tellier Benoit <btellier@linagora.com>
2021-05-24 16:16:45 +0700

    [PERFORMANCE] Cache and reuse Object mappers for writing JMAP responses (#440)
    * [REFACTORING] Cache and reuse Object mappers for writing JMAP responses
    
    Clients use a close ensemble of properties combination in JMAP responses.
    This enable reuse of jackson assemblies and achieve significant speedup
    of JMAP responses serialisation.
    
    Note that protections against hash collision are setted up.

--- server/protocols/jmap-draft/src/main/java/org/apache/james/jmap/draft/methods/JmapResponseWriterImpl.java
+++ server/protocols/jmap-draft/src/main/java/org/apache/james/jmap/draft/methods/JmapResponseWriterImpl.java
@@ -19,11 +19,14 @@
 
 package org.apache.james.jmap.draft.methods;
 
+import java.time.Duration;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.james.jmap.draft.json.ObjectMapperFactory;
 import org.apache.james.jmap.draft.model.InvocationResponse;
 import org.apache.james.jmap.draft.model.Property;
@@ -33,40 +36,95 @@ import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.PropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.github.fge.lambdas.Throwing;
 import com.github.steveash.guavate.Guavate;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 import reactor.core.publisher.Flux;
 
 public class JmapResponseWriterImpl implements JmapResponseWriter {
-
     public static final String PROPERTIES_FILTER = "propertiesFilter";
+
+    private static class CachedObjectMapper {
+        private final ObjectMapper objectMapper;
+        private final Optional<? extends Set<? extends Property>> properties;
+        private final Optional<? extends Set<? extends Property>> subProperties;
+
+        private CachedObjectMapper(ObjectMapper objectMapper,
+                                   Optional<? extends Set<? extends Property>> properties,
+                                   Optional<? extends Set<? extends Property>> subProperties) {
+            this.objectMapper = objectMapper;
+            this.properties = properties;
+            this.subProperties = subProperties;
+        }
+
+        Optional<ObjectMapper> cachedMapperIfApplicable(JmapResponse jmapResponse) {
+            if (jmapResponse.getProperties().equals(properties)
+                && jmapResponse.getFilterProvider().map(Pair::getKey).equals(subProperties)) {
+
+                return Optional.of(objectMapper);
+            }
+            return Optional.empty();
+        }
+    }
+
+
     private final ObjectMapper objectMapper;
+    private final Cache<Long, CachedObjectMapper> writerCache;
 
     @Inject
     public JmapResponseWriterImpl(ObjectMapperFactory objectMapperFactory) {
         this.objectMapper = objectMapperFactory.forWriting();
+
+        writerCache = CacheBuilder.newBuilder()
+            .maximumSize(128)
+            .expireAfterAccess(Duration.ofMinutes(15))
+            .build();
     }
 
     @Override
     public Flux<InvocationResponse> formatMethodResponse(Flux<JmapResponse> jmapResponses) {
-        return jmapResponses.map(jmapResponse -> {
-            ObjectMapper objectMapper = newConfiguredObjectMapper(jmapResponse);
+        return jmapResponses.map(Throwing.function(jmapResponse -> {
+            ObjectMapper objectMapper = retrieveObjectMapperFromCache(jmapResponse)
+                .cachedMapperIfApplicable(jmapResponse)
+                .orElseGet(() -> newConfiguredObjectMapper(jmapResponse));
 
             return new InvocationResponse(
                     jmapResponse.getResponseName(),
                     objectMapper.valueToTree(jmapResponse.getResponse()),
                     jmapResponse.getMethodCallId());
-        });
+        }));
     }
-    
+
+    private CachedObjectMapper retrieveObjectMapperFromCache(JmapResponse jmapResponse) throws ExecutionException {
+        return writerCache.get(computeCachingKey(jmapResponse), () -> new CachedObjectMapper(
+            newConfiguredObjectMapper(jmapResponse),
+            jmapResponse.getProperties(),
+            jmapResponse.getFilterProvider().map(Pair::getKey)));
+    }
+
     private ObjectMapper newConfiguredObjectMapper(JmapResponse jmapResponse) {
         FilterProvider filterProvider = jmapResponse
-                .getFilterProvider()
-                .orElseGet(SimpleFilterProvider::new)
-                .setDefaultFilter(SimpleBeanPropertyFilter.serializeAll())
-                .addFilter(PROPERTIES_FILTER, getPropertiesFilter(jmapResponse.getProperties()));
-        
+            .getFilterProvider()
+            .map(Pair::getValue)
+            .orElseGet(SimpleFilterProvider::new)
+            .setDefaultFilter(SimpleBeanPropertyFilter.serializeAll())
+            .addFilter(PROPERTIES_FILTER, getPropertiesFilter(jmapResponse.getProperties()));
+
         return objectMapper.copy().setFilterProvider(filterProvider);
+
+    }
+
+    private long computeCachingKey(JmapResponse jmapResponse) {
+        long lowBits = jmapResponse.getProperties().hashCode();
+        long highBits = jmapResponse.getFilterProvider()
+            .map(Pair::getKey)
+            .map(Object::hashCode)
+            .map(i -> (long) i)
+            .orElse((long) jmapResponse.getResponseName().hashCode());
+
+        return lowBits + (highBits >> 32);
     }
     
     private PropertyFilter getPropertiesFilter(Optional<? extends Set<? extends Property>> properties) {
