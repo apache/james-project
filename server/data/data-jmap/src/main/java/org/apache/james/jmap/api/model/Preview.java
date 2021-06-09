1f5d02d1fe305cdd188d19ce697840ecb1a914d9
Benoit Tellier <btellier@linagora.com>
2021-06-09 10:13:37 +0700

    fixup! fixup! JAMES-2989 Preview should not normalize spaces of the entire body

--- server/data/data-jmap/src/main/java/org/apache/james/jmap/api/model/Preview.java
+++ server/data/data-jmap/src/main/java/org/apache/james/jmap/api/model/Preview.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.inject.Inject;
 
