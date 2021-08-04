a7724744df644fbd1f763be0673d75d190324e68
Benoit Tellier <btellier@linagora.com>
2021-08-04 17:27:23 +0700

    JAMES-3544 Implement a distributed UploadRepository

---
+++ server/data/data-jmap-cassandra/src/main/java/org/apache/james/jmap/cassandra/upload/CassandraUploadRepository.java
@@ -0,0 +1,68 @@
+package org.apache.james.jmap.cassandra.upload;
+
+import static org.apache.james.blob.api.BlobStore.StoragePolicy.LOW_COST;
+
+import java.io.InputStream;
+
+import org.apache.james.blob.api.BlobStore;
+import org.apache.james.blob.api.BucketName;
+import org.apache.james.core.Username;
+import org.apache.james.jmap.api.model.Upload;
+import org.apache.james.jmap.api.model.UploadId;
+import org.apache.james.jmap.api.model.UploadMetaData;
+import org.apache.james.jmap.api.model.UploadNotFoundException;
+import org.apache.james.jmap.api.upload.UploadRepository;
+import org.apache.james.mailbox.model.ContentType;
+import org.reactivestreams.Publisher;
+
+import com.datastax.driver.core.utils.UUIDs;
+import com.google.common.io.CountingInputStream;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class CassandraUploadRepository implements UploadRepository {
+    private final UploadDAO uploadDAO;
+    private final BlobStore blobStore;
+    private final BucketNameGenerator bucketNameGenerator;
+
+    public CassandraUploadRepository(UploadDAO uploadDAO, BlobStore blobStore, BucketNameGenerator bucketNameGenerator) {
+        this.uploadDAO = uploadDAO;
+        this.blobStore = blobStore;
+        this.bucketNameGenerator = bucketNameGenerator;
+    }
+
+    @Override
+    public Publisher<UploadId> upload(InputStream data, ContentType contentType, Username user) {
+        UploadId uploadId = generateId();
+        UploadBucketName uploadBucketName = bucketNameGenerator.current();
+        BucketName bucketName = uploadBucketName.asBucketName();
+
+        return Mono.fromCallable(() -> new CountingInputStream(data))
+            .flatMap(countingInputStream -> Mono.from(blobStore.save(bucketName, countingInputStream, LOW_COST))
+                .map(blobId -> new UploadDAO.UploadRepresentation(uploadId, bucketName, blobId, contentType, countingInputStream.getCount(), user))
+                .flatMap(upload -> uploadDAO.save(upload).thenReturn(upload.getId())));
+    }
+
+    @Override
+    public Publisher<Upload> retrieve(UploadId id, Username user) {
+        return uploadDAO.retrieve(id)
+            .filter(upload -> upload.getUser().equals(user))
+            .map(upload -> Upload.from(
+                UploadMetaData.from(id, upload.getContentType(), upload.getSize(), upload.getBlobId()),
+                () -> blobStore.read(upload.getBucketName(), upload.getBlobId(), LOW_COST)))
+            .switchIfEmpty(Mono.error(() -> new UploadNotFoundException(id)));
+    }
+
+    public Mono<Void> purge() {
+        return Flux.from(blobStore.listBuckets())
+            .<UploadBucketName>handle((bucketName, sink) -> UploadBucketName.ofBucket(bucketName).ifPresentOrElse(sink::next, sink::complete))
+            .filter(bucketNameGenerator.evictionPredicate())
+            .concatMap(bucket -> blobStore.deleteBucket(bucket.asBucketName()))
+            .then();
+    }
+
+    private UploadId generateId() {
+        return UploadId.from(UUIDs.timeBased());
+    }
+}
---
+++ server/data/data-jmap-cassandra/src/test/java/org/apache/james/jmap/cassandra/upload/CassandraUploadRepositoryTest.java
@@ -0,0 +1,80 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.cassandra.upload;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.Clock;
+import java.time.Duration;
+
+import org.apache.james.backends.cassandra.CassandraClusterExtension;
+import org.apache.james.blob.api.BucketName;
+import org.apache.james.blob.api.HashBlobId;
+import org.apache.james.blob.memory.MemoryBlobStoreDAO;
+import org.apache.james.core.Username;
+import org.apache.james.jmap.api.model.UploadId;
+import org.apache.james.jmap.api.model.UploadNotFoundException;
+import org.apache.james.jmap.api.upload.UploadRepository;
+import org.apache.james.jmap.api.upload.UploadRepositoryContract;
+import org.apache.james.mailbox.model.ContentType;
+import org.apache.james.server.blob.deduplication.DeDuplicationBlobStore;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.datastax.driver.core.utils.UUIDs;
+
+import reactor.core.publisher.Mono;
+
+class CassandraUploadRepositoryTest implements UploadRepositoryContract {
+    @RegisterExtension
+    static CassandraClusterExtension cassandra = new CassandraClusterExtension(UploadModule.MODULE);
+    private CassandraUploadRepository testee;
+
+    @BeforeEach
+    void setUp() {
+        testee = new CassandraUploadRepository(new UploadDAO(cassandra.getCassandraCluster().getConf(),
+            new HashBlobId.Factory(),
+            new UploadConfiguration(Duration.ofSeconds(5))),
+            new DeDuplicationBlobStore(new MemoryBlobStoreDAO(), BucketName.of("default"), new HashBlobId.Factory()),
+            new BucketNameGenerator(Clock.systemUTC()));
+    }
+
+    @Override
+    public UploadId randomUploadId() {
+        return UploadId.from(UUIDs.timeBased());
+    }
+
+    @Override
+    public UploadRepository testee() {
+        return testee;
+    }
+
+    @Test
+    void uploadShouldExpire() throws Exception {
+        Username bob = Username.of("bob");
+        UploadId id = Mono.from(testee.upload(data(), ContentType.of("text/plain"), bob)).block();
+
+        Thread.sleep(6000);
+
+        assertThatThrownBy(() -> Mono.from(testee.retrieve(id, bob)).blockOptional())
+            .isInstanceOf(UploadNotFoundException.class);
+    }
+}
