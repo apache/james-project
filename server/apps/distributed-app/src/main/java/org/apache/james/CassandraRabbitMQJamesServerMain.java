1e0c8ec6c0730d6f10c5af65cd55c42dcc892465
Tung TRAN <vttran@linagora.com>
2021-08-11 17:03:36 +0700

    JAMES-3544 - Webadmin - UploadRepositoryCleanup integration to James

--- server/apps/distributed-app/src/main/java/org/apache/james/CassandraRabbitMQJamesServerMain.java
+++ server/apps/distributed-app/src/main/java/org/apache/james/CassandraRabbitMQJamesServerMain.java
@@ -67,6 +67,7 @@ import org.apache.james.modules.server.ElasticSearchMetricReporterModule;
 import org.apache.james.modules.server.InconsistencyQuotasSolvingRoutesModule;
 import org.apache.james.modules.server.JMXServerModule;
 import org.apache.james.modules.server.JmapTasksModule;
+import org.apache.james.modules.server.JmapUploadCleanupModule;
 import org.apache.james.modules.server.MailQueueRoutesModule;
 import org.apache.james.modules.server.MailRepositoriesRoutesModule;
 import org.apache.james.modules.server.MailboxRoutesModule;
@@ -97,6 +98,7 @@ public class CassandraRabbitMQJamesServerMain implements JamesServerMain {
         new DLPRoutesModule(),
         new InconsistencyQuotasSolvingRoutesModule(),
         new InconsistencySolvingRoutesModule(),
+        new JmapUploadCleanupModule(),
         new JmapTasksModule(),
         new MailboxesExportRoutesModule(),
         new MailboxRoutesModule(),
