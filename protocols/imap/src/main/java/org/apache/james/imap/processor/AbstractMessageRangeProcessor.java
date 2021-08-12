2cb4b44ef15f084bddc0a9649d2357352f25c83c
Benoit Tellier <btellier@linagora.com>
2021-08-12 13:40:36 +0700

    fixup! fixup! fixup! [REFACTORING] Method return Publishers should avoid typed exceptions

--- protocols/imap/src/main/java/org/apache/james/imap/processor/AbstractMessageRangeProcessor.java
+++ protocols/imap/src/main/java/org/apache/james/imap/processor/AbstractMessageRangeProcessor.java
@@ -88,10 +88,13 @@ public abstract class AbstractMessageRangeProcessor<R extends AbstractMessageRan
                     getOperationName(), session.getSelected().getMailboxId(), targetMailbox, request.getIdSet(), e);
             no(request, responder, HumanReadableText.GENERIC_FAILURE_DURING_PROCESSING);
         } catch (Exception e) {
-            if (e.getCause() instanceof MailboxException)
-            LOGGER.error("{} failed from mailbox {} to {} for sequence-set {}",
-                getOperationName(), session.getSelected().getMailboxId(), targetMailbox, request.getIdSet(), e.getCause());
-            no(request, responder, HumanReadableText.GENERIC_FAILURE_DURING_PROCESSING);
+            if (e.getCause() instanceof MailboxException) {
+                LOGGER.error("{} failed from mailbox {} to {} for sequence-set {}",
+                    getOperationName(), session.getSelected().getMailboxId(), targetMailbox, request.getIdSet(), e.getCause());
+                no(request, responder, HumanReadableText.GENERIC_FAILURE_DURING_PROCESSING);
+            } else {
+                throw e;
+            }
         }
     }
 
