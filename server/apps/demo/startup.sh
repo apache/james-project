a4573f3132f4d0cb7bd3dbdc8733d613103e9573
Benoit Tellier <btellier@linagora.com>
2021-08-29 19:09:15 +0700

    JAMES-3603 Adapt demo docker file to latest

--- server/apps/demo/startup.sh
+++ server/apps/demo/startup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ ! -e /root/conf/private.key ]]; then
+    openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Apache/L=Fundation/O=/CN=james.apache.org" -keyout /root/conf/private.key -out /root/conf/private.csr
+fi
+
 wait-for-it.sh --host=localhost --port=9999 --strict --timeout=0 -- ./initialdata.sh &
 
-java -classpath '/root/james-server.jar:/root/james-server-jpa-guice.lib/*' -javaagent:/root/james-server-jpa-guice.lib/openjpa-3.1.2.jar -Dlogback.configurationFile=/root/conf/logback.xml -Dworking.directory=/root/ org.apache.james.JPAJamesServerMain
+java -Djdk.tls.ephemeralDHKeySize=2048 \
+     -classpath '/root/resources:/root/classes:/root/libs/*' \
+     -javaagent:/root/libs/openjpa-3.1.2.jar \
+     -Dlogback.configurationFile=/root/conf/logback.xml \
+      -Dworking.directory=/root/ org.apache.james.JPAJamesServerMain
