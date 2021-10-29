bbd4c9b7282a9fceee17fe840915dd0434e4caec
vttran <vttran@linagora.com>
2021-10-29 09:24:17 +0700

    JAMES-3539 - WebPushClient (#711)

---
+++ server/protocols/jmap-rfc-8621/src/test/scala/org/apache/james/jmap/push_subscription/PushServerExtension.scala
@@ -0,0 +1,82 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ * http://www.apache.org/licenses/LICENSE-2.0                   *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.push_subscription
+
+import org.junit.jupiter.api.extension.{AfterEachCallback, BeforeEachCallback, ExtensionContext, ParameterContext, ParameterResolver}
+import org.mockserver.configuration.ConfigurationProperties
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.integration.ClientAndServer.startClientAndServer
+import org.mockserver.model.HttpRequest.request
+import org.mockserver.model.HttpResponse.response
+import org.mockserver.model.NottableString.{not, string}
+
+import java.net.URL
+import java.time.Clock
+import java.util.UUID
+
+class PushServerExtension extends BeforeEachCallback with AfterEachCallback with ParameterResolver {
+  var mockServer: ClientAndServer = _
+
+  override def afterEach(extensionContext: ExtensionContext): Unit = mockServer.close()
+
+  override def supportsParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext): Boolean =
+    parameterContext.getParameter.getType eq classOf[ClientAndServer]
+
+  override def resolveParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext): AnyRef =
+    mockServer
+
+  override def beforeEach(extensionContext: ExtensionContext): Unit = {
+    mockServer = startClientAndServer(0)
+    ConfigurationProperties.logLevel("WARN")
+    MockPushServer.appendSpec(mockServer)
+  }
+
+  def getBaseUrl: URL = new URL(s"http://127.0.0.1:${mockServer.getLocalPort}")
+}
+
+object MockPushServer {
+  def appendSpec(mockServer: ClientAndServer): Unit = {
+    mockServer
+      .when(request.withHeader(not("TTL")))
+      .respond(response.withStatusCode(400).withBody("missing TTL header"))
+
+    mockServer
+      .when(request.withHeader(not("Content-type")))
+      .respond(response.withStatusCode(400).withBody("Content-type is missing or invalid"))
+
+    mockServer
+      .when(request.withHeader(string("Content-type"), not("application/json charset=utf-8")))
+      .respond(response.withStatusCode(400).withBody("Content-type is missing or invalid"))
+
+    mockServer
+      .when(request
+        .withPath("/push")
+        .withMethod("POST")
+        .withHeader(string("Content-type"), string("application/json charset=utf-8"))
+        .withHeader(string("Urgency"))
+        .withHeader(string("Topic"))
+        .withHeader(string("TTL")))
+      .respond(response
+        .withStatusCode(201)
+        .withHeader("Location", String.format("https://push.example.net/message/%s", UUID.randomUUID))
+        .withHeader("Date", Clock.systemUTC.toString)
+        .withBody(UUID.randomUUID.toString))
+  }
+}
---
+++ server/protocols/jmap-rfc-8621/src/test/scala/org/apache/james/jmap/push_subscription/WebPushClientContract.scala
@@ -0,0 +1,118 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ * http://www.apache.org/licenses/LICENSE-2.0                   *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.push_subscription
+
+import org.apache.james.jmap.api.model.PushSubscriptionServerURL
+import org.apache.james.jmap.push_subscription.WebPushClientTestFixture.PUSH_REQUEST_SAMPLE
+import org.assertj.core.api.Assertions.{assertThatCode, assertThatThrownBy}
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.model.HttpRequest.request
+import org.mockserver.model.HttpResponse.response
+import org.mockserver.verify.VerificationTimes
+import reactor.core.scala.publisher.SMono
+import reactor.core.scheduler.Schedulers
+
+import java.net.URL
+import java.nio.charset.StandardCharsets
+
+object WebPushClientTestFixture {
+  val PUSH_CLIENT_CONFIGURATION: PushClientConfiguration =
+    PushClientConfiguration(
+      maxTimeoutSeconds = Some(10),
+      maxConnections = Some(10),
+      maxRetryTimes = Some(10),
+      requestPerSeconds = Some(10),
+      scheduler = Schedulers.elastic())
+
+  val PUSH_REQUEST_SAMPLE: PushRequest = PushRequest(
+    ttl = PushTTL.validate(15).toOption.get,
+    topic = PushTopic.validate("topicabc").toOption,
+    urgency = Some(High),
+    payload = "Content123".getBytes(StandardCharsets.UTF_8))
+}
+
+trait WebPushClientContract {
+  def testee: WebPushClient
+
+  def pushServerBaseUrl: URL
+
+  private def getPushSubscriptionServerURL: PushSubscriptionServerURL =
+    PushSubscriptionServerURL.from(s"${pushServerBaseUrl.toString}/push").get
+
+  @Test
+  def pushValidRequestShouldNotThrowException(): Unit = {
+    assertThatCode(() => SMono.fromPublisher(testee.push(getPushSubscriptionServerURL, PUSH_REQUEST_SAMPLE))
+      .block())
+      .doesNotThrowAnyException()
+  }
+
+  @Test
+  def pushValidRequestShouldHTTPCallToPushServer(pushServer: ClientAndServer): Unit = {
+    SMono.fromPublisher(testee.push(getPushSubscriptionServerURL, PUSH_REQUEST_SAMPLE))
+      .block()
+    pushServer.verify(request()
+      .withPath("/push")
+      .withHeader("TTL", "15")
+      .withHeader("Topic", "topicabc")
+      .withHeader("Urgency", "High")
+      .withBody(new String(PUSH_REQUEST_SAMPLE.payload, StandardCharsets.UTF_8)),
+      VerificationTimes.atLeast(1))
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = Array(500, 501, 502, 503, 504))
+  def pushRequestShouldThrowWhenPushServerReturnFailHTTPStatus(httpErrorCode: Int, pushServer: ClientAndServer): Unit = {
+    pushServer
+      .when(request
+        .withPath("/invalid"))
+      .respond(response.withStatusCode(httpErrorCode))
+
+    assertThatThrownBy(() => SMono.fromPublisher(
+      testee.push(PushSubscriptionServerURL.from(s"${pushServerBaseUrl.toString}/invalid").get,
+        PUSH_REQUEST_SAMPLE))
+      .block())
+      .isInstanceOf(classOf[WebPushTemporarilyUnavailableException])
+
+    pushServer.verify(request()
+      .withPath("/invalid"),
+      VerificationTimes.atLeast(1))
+  }
+
+  @Test
+  def pushRequestShouldParserErrorResponseFromPushServerWhenFail(pushServer: ClientAndServer): Unit = {
+    pushServer
+      .when(request
+        .withPath("/invalid"))
+      .respond(response
+        .withStatusCode(500)
+        .withBody("Request did not validate 123"))
+
+    assertThatThrownBy(() => SMono.fromPublisher(
+      testee.push(PushSubscriptionServerURL.from(s"${pushServerBaseUrl.toString}/invalid").get,
+        PUSH_REQUEST_SAMPLE))
+      .block())
+      .hasMessageContaining("Request did not validate 123")
+  }
+}
+
+
