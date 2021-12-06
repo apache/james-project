8039e544c95099ca70fa76bef6fb0c4dd9200b64
Benoit Tellier <btellier@linagora.com>
2021-12-06 15:52:12 +0700

    JAMES-3680 SMTPServer: Use an enum for authRequired
    Instead of a collection of integers

--- server/container/guice/protocols/smtp/src/main/java/org/apache/james/modules/protocols/SmtpGuiceProbe.java
+++ server/container/guice/protocols/smtp/src/main/java/org/apache/james/modules/protocols/SmtpGuiceProbe.java
@@ -18,6 +18,8 @@
  ****************************************************************/
 package org.apache.james.modules.protocols;
 
+import static org.apache.james.smtpserver.netty.SMTPServer.AuthenticationRequired.REQUIRED;
+
 import java.net.InetSocketAddress;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -63,7 +65,7 @@ public class SmtpGuiceProbe implements GuiceProbe {
     }
 
     public Port getSmtpAuthRequiredPort() {
-        return getPort(server -> ((SMTPServer) server).getAuthRequired() == SMTPServer.AUTH_REQUIRED);
+        return getPort(server -> ((SMTPServer) server).getAuthRequired().equals(REQUIRED));
     }
 
     private Port getPort(Predicate<? super AbstractConfigurableAsyncServer> filter) {
