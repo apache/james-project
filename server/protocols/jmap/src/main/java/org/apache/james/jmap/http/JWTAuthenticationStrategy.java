7a55a184df3d95a8d00e1d3f060acd6a4b1b51b9
Quan Tran <hqtran@linagora.com>
2021-12-09 10:05:20 +0700

    JAMES-3680 Move JWTAuthenticationStrategy to package "org.apache.james.jmap.http"

--- server/protocols/jmap/src/main/java/org/apache/james/jmap/http/JWTAuthenticationStrategy.java
+++ server/protocols/jmap/src/main/java/org/apache/james/jmap/http/JWTAuthenticationStrategy.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
-package org.apache.james.jmap.jwt;
+package org.apache.james.jmap.http;
 
 import javax.inject.Inject;
 import javax.inject.Named;
