2c20c0e861f378c41b262d831c279b4a515e5b66
Matthieu Baechler <matthieu@apache.org>
2021-12-24 18:34:21 +0100

    JAMES-3687 fixing licenses headers

--- server/queue/queue-pulsar/src/main/scala/org/apache/james/queue/pulsar/EnqueueId.scala
+++ server/queue/queue-pulsar/src/main/scala/org/apache/james/queue/pulsar/EnqueueId.scala
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
-
 package org.apache.james.queue.pulsar
 
 import eu.timepit.refined
