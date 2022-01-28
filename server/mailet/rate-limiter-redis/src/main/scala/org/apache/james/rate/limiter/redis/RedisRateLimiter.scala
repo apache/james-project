2a33a003a4a1d7e8f985a8e266e6538c46817a90
Benoit Tellier <btellier@linagora.com>
2022-01-28 11:19:48 +0700

    JAMES-3693 Support connections to a Redis Cluster
    Changeset tested manually

--- server/mailet/rate-limiter-redis/src/main/scala/org/apache/james/rate/limiter/redis/RedisRateLimiter.scala
+++ server/mailet/rate-limiter-redis/src/main/scala/org/apache/james/rate/limiter/redis/RedisRateLimiter.scala
@@ -19,9 +19,10 @@
 
 package org.apache.james.rate.limiter.redis
 
-import es.moki.ratelimitj.core.limiter.request.{ReactiveRequestRateLimiter, RequestLimitRule}
-import es.moki.ratelimitj.redis.request.{RedisRateLimiterFactory => RateLimitjFactory}
+import es.moki.ratelimitj.core.limiter.request.{AbstractRequestRateLimiterFactory, ReactiveRequestRateLimiter, RequestLimitRule}
+import es.moki.ratelimitj.redis.request.{RedisClusterRateLimiterFactory, RedisSlidingWindowRequestRateLimiter, RedisRateLimiterFactory => RedisSingleInstanceRateLimitjFactory}
 import io.lettuce.core.RedisClient
+import io.lettuce.core.cluster.RedisClusterClient
 import org.apache.james.rate.limiter.api.Increment.Increment
 import org.apache.james.rate.limiter.api.{AcceptableRate, RateExceeded, RateLimiter, RateLimiterFactory, RateLimitingKey, RateLimitingResult, Rule, Rules}
 import org.reactivestreams.Publisher
@@ -30,7 +31,12 @@ import reactor.core.scala.publisher.SMono
 import scala.jdk.CollectionConverters._
 
 class RedisRateLimiterFactory(redisConfiguration: RedisRateLimiterConfiguration) extends RateLimiterFactory {
-  val rateLimitjFactory: RateLimitjFactory = new RateLimitjFactory(RedisClient.create(redisConfiguration.redisURI))
+  val rateLimitjFactory: AbstractRequestRateLimiterFactory[RedisSlidingWindowRequestRateLimiter] =
+    if (redisConfiguration.redisURI.value.size > 1) {
+      new RedisClusterRateLimiterFactory(RedisClusterClient.create(redisConfiguration.redisURI.value.asJava))
+    } else {
+      new RedisSingleInstanceRateLimitjFactory(RedisClient.create(redisConfiguration.redisURI.value.last))
+    }
 
   override def withSpecification(rules: Rules): RateLimiter = {
     val requestRateLimiter: ReactiveRequestRateLimiter = rateLimitjFactory.getInstanceReactive(rules.rules
