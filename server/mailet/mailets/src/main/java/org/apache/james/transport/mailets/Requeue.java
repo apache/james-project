c6a688e76d52146edd3c3798f7037f76ba6e6a56
Tung Van TRAN <vttran@linagora.com>
2022-02-11 12:42:08 +0700

    JAMES-3711 Document for Requeue mailet

--- server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/Requeue.java
+++ server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/Requeue.java
@@ -39,6 +39,34 @@ import org.apache.mailet.base.GenericMailet;
 import com.github.fge.lambdas.Throwing;
 import com.google.common.base.Preconditions;
 
+/**
+ * <p><b>Requeue</b> puts back the email in a queue.
+ * It can be used for throttling when combined with rate limiting mailet. {@link org.apache.james.transport.mailets.PerSenderRateLimit}</p>
+ *
+ * <ul>Here are supported configuration parameters:
+ *      <li><b>queue</b>: a Mail Queue name (optional, default to spool).</li>
+ *      <li><b>processor</b>: a target processor (optional, defaults to root).</li>
+ *      <li><b>delay</b>: a delay when en-queueing mail (optional, defaults to none). Supported units include: s (second), m (minute), h (hour), d (day).</li>
+ *      <li><b>consume</b>: a boolean. true will consume the mail: no further processing would be done on the mail (default)
+ *      while false will continue the processing of the email in addition to the requeue operation, which effectively duplicates the email.</li>
+ *  </ul>
+ *
+ *  <p>For instance, to apply all the examples given above:</p>
+ *
+ *  <pre><code>
+ *  &lt;mailet matcher=&quot;All&quot; class=&quot;Requeue&quot;&gt;
+ *      &lt;queue&gt;spool&lt;/queue&gt;
+ *      &lt;processor&gt;root&lt;/processor&gt;
+ *      &lt;delay&gt;2h&lt;/delay&gt;
+ *      &lt;consume&gt;true&lt;/consume&gt;
+ *  &lt;/mailet&gt;
+ *   </code></pre>
+ *
+ * <p>Note that this is a naive approach: if you have a large number of emails (say N) submitted at once,
+ * you would need O(N) re-queues to eventually send all your emails,
+ * where the constant is the number of mail allowed over the time window.
+ * So this gives an overall complexity of O(N2). </p>
+ */
 public class Requeue extends GenericMailet {
     private final MailQueueFactory<?> mailQueueFactory;
 
