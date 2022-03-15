7222cedec23e006056bfee9be0e5a157075afeac
Benoit Tellier <btellier@linagora.com>
2022-03-15 10:03:48 +0700

    JAMES-1862 IMAP tags should be well sanitized when enforcing STARTTLS command injection checks

--- server/protocols/protocols-imap4/src/main/java/org/apache/james/imapserver/netty/SwitchableLineBasedFrameDecoder.java
+++ server/protocols/protocols-imap4/src/main/java/org/apache/james/imapserver/netty/SwitchableLineBasedFrameDecoder.java
@@ -56,7 +56,7 @@ public class SwitchableLineBasedFrameDecoder extends AllButStartTlsLineBasedChan
 
     protected String removeTag(String input) {
         String trimmedInput = input.trim();
-        int tagEnd = input.indexOf(' ');
+        int tagEnd = trimmedInput.indexOf(' ');
         if (tagEnd < 0) {
             return input;
         }
