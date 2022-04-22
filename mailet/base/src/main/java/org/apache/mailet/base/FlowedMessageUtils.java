7f73560ca0a96434cd2d66603b9673111064ce2f
cketti <ck@cketti.de>
2022-04-22 07:03:30 +0200

    JAMES-3753 Fix FlowedMessageUtils.deflow() (#972)
    (cherry picked from commit d38a35996d693ac927331cfa1238ce5768c0719f)

--- mailet/base/src/main/java/org/apache/mailet/base/FlowedMessageUtils.java
+++ mailet/base/src/main/java/org/apache/mailet/base/FlowedMessageUtils.java
@@ -78,11 +78,11 @@ public final class FlowedMessageUtils {
             String line = i < lines.length ? lines[i] : null;
             int actualQuoteDepth = 0;
             
-            if (line != null && line.length() > 0) {
+            if (line != null) {
                 if (line.equals(RFC2646_SIGNATURE)) {
                     // signature handling (the previous line is not flowed)
                     resultLineFlowed = false;
-                } else if (line.charAt(0) == RFC2646_QUOTE) {
+                } else if (line.length() > 0 && line.charAt(0) == RFC2646_QUOTE) {
                     // Quote
                     actualQuoteDepth = 1;
                     while (actualQuoteDepth < line.length() && line.charAt(actualQuoteDepth) == RFC2646_QUOTE) {
@@ -95,7 +95,7 @@ public final class FlowedMessageUtils {
                     line = line.substring(actualQuoteDepth);
                     
                 } else {
-                    // id quote-depth changes wrt the first line then this is not flowed
+                    // if quote-depth changes wrt the first line then this is not flowed
                     if (resultLineQuoteDepth > 0) {
                         resultLineFlowed = false;
                     }
@@ -107,7 +107,7 @@ public final class FlowedMessageUtils {
                 }
                 
             // if the previous was the last then it was not flowed
-            } else if (line == null) {
+            } else {
                 resultLineFlowed = false;
             }
 
