952c2fd66a24dc24322e38020af5929b3e5ed717
Benoit Tellier <btellier@linagora.com>
2022-05-03 08:37:24 +0700

    JAMES-3747 Make EventBus publishConfirms more resilient

--- backends-common/rabbitmq/src/main/java/org/apache/james/backends/rabbitmq/ReactorRabbitMQChannelPool.java
+++ backends-common/rabbitmq/src/main/java/org/apache/james/backends/rabbitmq/ReactorRabbitMQChannelPool.java
@@ -654,7 +654,7 @@ public class ReactorRabbitMQChannelPool implements ChannelPool, Startable {
     private static final Logger LOGGER = LoggerFactory.getLogger(ReactorRabbitMQChannelPool.class);
     private static final int MAX_CHANNELS_NUMBER = 3;
     private static final int MAX_BORROW_RETRIES = 3;
-    private static final Duration MAX_BORROW_DELAY = Duration.ofSeconds(30);
+    private static final Duration MAX_BORROW_DELAY = Duration.ofSeconds(5);
 
     private final ConcurrentHashMap<Integer, PooledRef<? extends Channel>> refs = new ConcurrentHashMap<>();
     private final Mono<Connection> connectionMono;
