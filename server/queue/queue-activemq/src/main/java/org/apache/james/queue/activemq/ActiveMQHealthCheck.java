60360264f0e00f332f89cdd583d7f976df5594a8
Quan Tran <hqtran@linagora.com>
2022-08-04 11:53:55 +0700

    JAMES-3794 Implement ActiveMQ HealthCheck

---
+++ server/queue/queue-activemq/src/main/java/org/apache/james/queue/activemq/ActiveMQHealthCheck.java
@@ -0,0 +1,66 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.queue.activemq;
+
+import javax.inject.Inject;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Session;
+
+import org.apache.james.core.healthcheck.ComponentName;
+import org.apache.james.core.healthcheck.HealthCheck;
+import org.apache.james.core.healthcheck.Result;
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import reactor.core.publisher.Mono;
+
+public class ActiveMQHealthCheck implements HealthCheck {
+    public static final Logger LOGGER = LoggerFactory.getLogger(ActiveMQHealthCheck.class);
+    public static final ComponentName COMPONENT_NAME = new ComponentName("Embedded ActiveMQ");
+    private final ConnectionFactory connectionFactory;
+
+    @Inject
+    public ActiveMQHealthCheck(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    @Override
+    public ComponentName componentName() {
+        return COMPONENT_NAME;
+    }
+
+    @Override
+    public Publisher<Result> check() {
+        return Mono.fromCallable(() -> {
+            try {
+                Connection connection = connectionFactory.createConnection();
+                Session session = connection.createSession(true, Session.SESSION_TRANSACTED);
+                session.close();
+                return Result.healthy(COMPONENT_NAME);
+            } catch (Exception e) {
+                LOGGER.warn("{} is unhealthy. {}", COMPONENT_NAME.getName(), e.getMessage());
+                return Result.unhealthy(COMPONENT_NAME, e.getMessage());
+            }
+        });
+    }
+}
+
---
+++ server/queue/queue-activemq/src/test/java/org/apache/james/queue/activemq/ActiveMQHealthCheckTest.java
@@ -0,0 +1,70 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.queue.activemq;
+
+import static org.apache.james.queue.activemq.ActiveMQHealthCheck.COMPONENT_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQPrefetchPolicy;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.james.core.healthcheck.Result;
+import org.apache.james.queue.jms.BrokerExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import reactor.core.publisher.Mono;
+
+@ExtendWith(BrokerExtension.class)
+class ActiveMQHealthCheckTest {
+    private ActiveMQHealthCheck testee;
+    private BrokerService broker;
+
+    @BeforeEach
+    void setup(BrokerService broker) {
+        this.broker = broker;
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?create=false");
+        ActiveMQPrefetchPolicy prefetchPolicy = new ActiveMQPrefetchPolicy();
+        prefetchPolicy.setQueuePrefetch(0);
+        connectionFactory.setPrefetchPolicy(prefetchPolicy);
+
+        testee = new ActiveMQHealthCheck(connectionFactory);
+    }
+
+    @Test
+    void componentNameShouldReturnTheRightValue() {
+        assertThat(testee.componentName().getName())
+            .isEqualTo(COMPONENT_NAME.getName());
+    }
+
+    @Test
+    void checkShouldReturnHealthyWhenActiveMQHealthy() {
+        assertThat(Mono.from(testee.check()).block())
+            .isEqualTo(Result.healthy(COMPONENT_NAME));
+    }
+
+    @Test
+    void checkShouldReturnUnHealthyWhenActiveMQDown() throws Exception {
+        broker.stop();
+        assertThat(Mono.from(testee.check()).block().isUnHealthy()).isTrue();
+    }
+}
+
