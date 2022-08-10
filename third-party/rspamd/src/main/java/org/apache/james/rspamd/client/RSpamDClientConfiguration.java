b99732d2d86de2d26d804a9a2d99f2bd686c95ae
Tung Van TRAN <vttran@linagora.com>
2022-08-10 08:27:10 +0700

    JAMES-3775 Guice packaging for RSpamd

--- third-party/rspamd/src/main/java/org/apache/james/rspamd/client/RSpamDClientConfiguration.java
+++ third-party/rspamd/src/main/java/org/apache/james/rspamd/client/RSpamDClientConfiguration.java
@@ -22,8 +22,29 @@ package org.apache.james.rspamd.client;
 import java.net.URL;
 import java.util.Optional;
 
+import org.apache.commons.configuration2.Configuration;
+
+import com.github.fge.lambdas.Throwing;
+import com.google.common.base.Preconditions;
+
 public class RSpamDClientConfiguration {
     public static final Integer DEFAULT_TIMEOUT_IN_SECONDS = 15;
+
+    public static RSpamDClientConfiguration from(Configuration config) {
+        URL rSpamDUrl = Optional.ofNullable(config.getString("rSpamDUrl", null))
+            .filter(s -> !s.isEmpty())
+            .map(Throwing.function(URL::new))
+            .orElseThrow(() -> new IllegalArgumentException("RSpamD's url is invalid."));
+
+        Optional<Integer> rSpamDTimeoutConfigure = Optional.ofNullable(config.getInteger("rSpamDTimeout", null))
+            .map(i -> {
+                Preconditions.checkArgument(i > 0, "rSpamDTimeout should be positive number");
+                return i;
+            });
+
+        String rSpamDPassword = config.getString("rSpamDPassword", "");
+        return new RSpamDClientConfiguration(rSpamDUrl, rSpamDPassword, rSpamDTimeoutConfigure);
+    }
 
     private final URL url;
     private final String password;
