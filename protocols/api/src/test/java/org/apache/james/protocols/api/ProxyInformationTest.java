a0202b5b9a9870577a961cf5a58cc8322545ff56
Benoit Tellier <btellier@linagora.com>
2022-08-18 15:03:48 +0700

    fixup! JAMES-3788 Bean contract: fix ProxyInformation

--- protocols/api/src/test/java/org/apache/james/protocols/api/ProxyInformationTest.java
+++ protocols/api/src/test/java/org/apache/james/protocols/api/ProxyInformationTest.java
@@ -15,7 +15,9 @@
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- ****************************************************************/package org.apache.james.protocols.api;
+ ****************************************************************/
+
+package org.apache.james.protocols.api;
 
 import org.junit.jupiter.api.Test;
 
