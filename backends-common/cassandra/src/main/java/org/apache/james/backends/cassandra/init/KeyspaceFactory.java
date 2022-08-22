427e4838e62fb76ed273e2277de7af30de38f917
Benoit Tellier <btellier@linagora.com>
2022-08-22 12:09:35 +0700

    JAMES-3774 Fasten keyspace creation
    Because CqlSession is immutable, keyspace creation is
    attempted on a dedicated, temporary session. We can
    close that temporary session in an asynchronous fashion,
    which is faster (~1 second)
    
    Also use CqlSession metadata to see if the cluster exists.

--- backends-common/cassandra/src/main/java/org/apache/james/backends/cassandra/init/KeyspaceFactory.java
+++ backends-common/cassandra/src/main/java/org/apache/james/backends/cassandra/init/KeyspaceFactory.java
@@ -19,21 +19,15 @@
 
 package org.apache.james.backends.cassandra.init;
 
-import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal;
-import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
-
 import org.apache.james.backends.cassandra.init.configuration.KeyspaceConfiguration;
 
+import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.querybuilder.SchemaBuilder;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 
 public class KeyspaceFactory {
-    private static final String SYSTEM_SCHEMA = "system_schema";
-    private static final String KEYSPACES = "keyspaces";
-    private static final String KEYSPACE_NAME = "keyspace_name";
-
     public static void createKeyspace(KeyspaceConfiguration configuration, CqlSession session) {
         if (!keyspaceExist(session, configuration.getKeyspace())) {
             session.execute(SchemaBuilder.createKeyspace(configuration.getKeyspace())
@@ -49,18 +43,6 @@ public class KeyspaceFactory {
 
     @VisibleForTesting
     public static boolean keyspaceExist(CqlSession session, String keyspaceName) {
-        long numberOfKeyspaces = session.execute(selectFrom(SYSTEM_SCHEMA, KEYSPACES)
-                .countAll()
-                .whereColumn(KEYSPACE_NAME)
-                    .isEqualTo(literal(keyspaceName))
-            .build())
-            .one()
-            .getLong("count");
-
-        if (numberOfKeyspaces > 1 || numberOfKeyspaces < 0) {
-            throw new IllegalStateException(String.format("unexpected keyspace('%s') count being %d", keyspaceName, numberOfKeyspaces));
-        }
-
-        return numberOfKeyspaces == 1;
+        return session.getMetadata().getKeyspaces().get(CqlIdentifier.fromCql(keyspaceName)) != null;
     }
 }
