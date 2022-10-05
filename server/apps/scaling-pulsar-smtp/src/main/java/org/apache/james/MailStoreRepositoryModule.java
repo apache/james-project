9edaf85cf0ef2334c602dc2e3458eb915e9c0219
Jean Helou <jhe@codamens.fr>
2022-10-05 22:34:15 +0200

    [JAMES-3836] implements guice bindings for BlobMailRepository
    Note that `BlobMailRepository` cannot be loaded through the `GuiceMailRepositoryLoader` since it doesn't take a MailRepositoryUrl as a constructor argument.
    This is why the BlobMailRepositoryModule provides the proper factory binding for use with the `MultiBoundGuiceMailRepositoryLoader`. The use of this binding is demonstrated in `scaling-pulsar-smtp`.

---
+++ server/apps/scaling-pulsar-smtp/src/main/java/org/apache/james/MailStoreRepositoryModule.java
@@ -0,0 +1,82 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james;
+
+import javax.inject.Provider;
+
+import org.apache.commons.configuration2.HierarchicalConfiguration;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.tree.ImmutableNode;
+import org.apache.james.mailrepository.api.MailRepositoryLoader;
+import org.apache.james.mailrepository.api.MailRepositoryStore;
+import org.apache.james.mailrepository.memory.MailRepositoryStoreConfiguration;
+import org.apache.james.mailrepository.memory.MemoryMailRepositoryStore;
+import org.apache.james.modules.mailrepository.guice.MultiBoundGuiceRepositoryLoader;
+import org.apache.james.server.core.configuration.ConfigurationProvider;
+import org.apache.james.utils.GuiceProbe;
+import org.apache.james.utils.InitializationOperation;
+import org.apache.james.utils.InitilizationOperationBuilder;
+import org.apache.james.utils.MailRepositoryProbeImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import com.google.inject.multibindings.Multibinder;
+import com.google.inject.multibindings.ProvidesIntoSet;
+
+public class MailStoreRepositoryModule extends AbstractModule {
+    public static final Logger LOGGER = LoggerFactory.getLogger(MailStoreRepositoryModule.class);
+
+    @Override
+    protected void configure() {
+        bind(MemoryMailRepositoryStore.class).in(Scopes.SINGLETON);
+        bind(MailRepositoryStore.class).to(MemoryMailRepositoryStore.class);
+
+        bind(MultiBoundGuiceRepositoryLoader.class).in(Scopes.SINGLETON);
+        bind(MailRepositoryLoader.class).to(MultiBoundGuiceRepositoryLoader.class);
+
+        Multibinder.newSetBinder(binder(), GuiceProbe.class).addBinding().to(MailRepositoryProbeImpl.class);
+    }
+
+    @Provides
+    @Singleton
+    MailRepositoryStoreConfiguration provideConfiguration(ConfigurationProvider configurationProvider,
+                                                          Provider<MailRepositoryStoreConfiguration.Item> defaultConfigurationItem)
+        throws ConfigurationException {
+
+        HierarchicalConfiguration<ImmutableNode> configuration = configurationProvider.getConfiguration("mailrepositorystore");
+        MailRepositoryStoreConfiguration userConfiguration = MailRepositoryStoreConfiguration.parse(configuration);
+        if (!userConfiguration.getItems().isEmpty()) {
+            return userConfiguration;
+        }
+        LOGGER.warn("Empty MailRepository store configuration supplied. Defaulting to default configuration for this product");
+        return MailRepositoryStoreConfiguration.forItems(defaultConfigurationItem.get());
+    }
+
+    @ProvidesIntoSet
+    InitializationOperation startMailStore(MemoryMailRepositoryStore instance) {
+        return InitilizationOperationBuilder
+            .forClass(MemoryMailRepositoryStore.class)
+            .init(instance::init);
+    }
+}
---
+++ server/container/guice/mailrepository-blob/src/main/java/org/apache/james/modules/mailrepository/BlobstoreMailRepositoryModule.java
@@ -0,0 +1,56 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.modules.mailrepository;
+
+import org.apache.commons.configuration2.BaseHierarchicalConfiguration;
+import org.apache.james.blob.api.BlobId;
+import org.apache.james.blob.api.BlobStoreDAO;
+import org.apache.james.blob.mail.MimeMessageStore;
+import org.apache.james.mailrepository.api.MailRepositoryFactory;
+import org.apache.james.mailrepository.api.Protocol;
+import org.apache.james.mailrepository.blob.BlobMailRepository;
+import org.apache.james.mailrepository.blob.BlobMailRepositoryFactory;
+import org.apache.james.mailrepository.memory.MailRepositoryStoreConfiguration;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.ProvidesIntoSet;
+
+public class BlobstoreMailRepositoryModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(MailRepositoryStoreConfiguration.Item.class)
+                .toInstance(
+                        new MailRepositoryStoreConfiguration.Item(
+                                ImmutableList.of(new Protocol("blob")),
+                                BlobMailRepository.class.getName(),
+                                new BaseHierarchicalConfiguration())
+                );
+    }
+
+    @ProvidesIntoSet()
+    public MailRepositoryFactory blobMailRepository(BlobStoreDAO blobStore,
+                                                    BlobId.Factory blobIdFactory,
+                                                    MimeMessageStore.Factory mimeFactory) {
+        return new BlobMailRepositoryFactory(blobStore, blobIdFactory, mimeFactory);
+    }
+
+}
