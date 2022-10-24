72ce0a699efc91d5f777d31d584f6a69c2f86196
Quan Tran <hqtran@linagora.com>
2022-10-24 11:01:22 +0700

    JAMES-3775 Handle possible empty `rspamdTimeoutInSeconds` when deserializing task additional information

--- third-party/rspamd/src/test/java/org/apache/james/rspamd/task/FeedHamToRspamdTaskAdditionalInformationDTOTest.java
+++ third-party/rspamd/src/test/java/org/apache/james/rspamd/task/FeedHamToRspamdTaskAdditionalInformationDTOTest.java
@@ -19,10 +19,15 @@
 
 package org.apache.james.rspamd.task;
 
+import static org.apache.james.rspamd.task.RunningOptions.DEFAULT_RSPAMD_TIMEOUT;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Optional;
 
 import org.apache.james.JsonSerializationVerifier;
+import org.apache.james.json.JsonGenericSerializer;
 import org.apache.james.util.ClassLoaderUtils;
 import org.junit.jupiter.api.Test;
 
@@ -72,5 +77,43 @@ class FeedHamToRspamdTaskAdditionalInformationDTOTest {
                     Optional.empty())))
             .json(ClassLoaderUtils.getSystemResourceAsString("json/feedHamNonEmptyPeriod.additionalInformation.json"))
             .verify();
+    }
+
+    @Test
+    void shouldWellDeserializeWhenEmptyRspamdTimeoutField() throws Exception {
+        assertThat(JsonGenericSerializer
+            .forModules(FeedHamToRspamdTaskAdditionalInformationDTO.SERIALIZATION_MODULE)
+            .withoutNestedType()
+            .deserialize(ClassLoaderUtils.getSystemResourceAsString("json/feedHamEmptyRspamdTimeout.additionalInformation.json")))
+            .isEqualTo(new FeedHamToRspamdTask.AdditionalInformation(
+                Instant.parse("2007-12-03T10:15:30.00Z"),
+                4,
+                2,
+                1,
+                new RunningOptions(
+                    Optional.of(3600L),
+                    RunningOptions.DEFAULT_MESSAGES_PER_SECOND,
+                    RunningOptions.DEFAULT_SAMPLING_PROBABILITY,
+                    Optional.empty(),
+                    DEFAULT_RSPAMD_TIMEOUT)));
+    }
+
+    @Test
+    void shouldWellDeserializeWhenNonEmptyRspamdTimeoutField() throws Exception {
+        assertThat(JsonGenericSerializer
+            .forModules(FeedHamToRspamdTaskAdditionalInformationDTO.SERIALIZATION_MODULE)
+            .withoutNestedType()
+            .deserialize(ClassLoaderUtils.getSystemResourceAsString("json/feedHamNonEmptyRspamdTimeout.additionalInformation.json")))
+            .isEqualTo(new FeedHamToRspamdTask.AdditionalInformation(
+                Instant.parse("2007-12-03T10:15:30.00Z"),
+                4,
+                2,
+                1,
+                new RunningOptions(
+                    Optional.of(3600L),
+                    RunningOptions.DEFAULT_MESSAGES_PER_SECOND,
+                    RunningOptions.DEFAULT_SAMPLING_PROBABILITY,
+                    Optional.empty(),
+                    Duration.ofSeconds(100))));
     }
 }
--- third-party/rspamd/src/test/java/org/apache/james/rspamd/task/FeedSpamToRspamdTaskAdditionalInformationDTOTest.java
+++ third-party/rspamd/src/test/java/org/apache/james/rspamd/task/FeedSpamToRspamdTaskAdditionalInformationDTOTest.java
@@ -20,12 +20,16 @@
 package org.apache.james.rspamd.task;
 
 import static org.apache.james.rspamd.task.RunningOptions.DEFAULT_MESSAGES_PER_SECOND;
+import static org.apache.james.rspamd.task.RunningOptions.DEFAULT_RSPAMD_TIMEOUT;
 import static org.apache.james.rspamd.task.RunningOptions.DEFAULT_SAMPLING_PROBABILITY;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Optional;
 
 import org.apache.james.JsonSerializationVerifier;
+import org.apache.james.json.JsonGenericSerializer;
 import org.apache.james.util.ClassLoaderUtils;
 import org.junit.jupiter.api.Test;
 
@@ -75,5 +79,43 @@ class FeedSpamToRspamdTaskAdditionalInformationDTOTest {
                     Optional.empty())))
             .json(ClassLoaderUtils.getSystemResourceAsString("json/feedSpamNonEmptyPeriod.additionalInformation.json"))
             .verify();
+    }
+
+    @Test
+    void shouldWellDeserializeWhenEmptyRspamdTimeoutField() throws Exception {
+        assertThat(JsonGenericSerializer
+            .forModules(FeedSpamToRspamdTaskAdditionalInformationDTO.SERIALIZATION_MODULE)
+            .withoutNestedType()
+            .deserialize(ClassLoaderUtils.getSystemResourceAsString("json/feedSpamEmptyRspamdTimeout.additionalInformation.json")))
+            .isEqualTo(new FeedSpamToRspamdTask.AdditionalInformation(
+                Instant.parse("2007-12-03T10:15:30.00Z"),
+                4,
+                2,
+                1,
+                new RunningOptions(
+                    Optional.of(3600L),
+                    DEFAULT_MESSAGES_PER_SECOND,
+                    DEFAULT_SAMPLING_PROBABILITY,
+                    Optional.empty(),
+                    DEFAULT_RSPAMD_TIMEOUT)));
+    }
+
+    @Test
+    void shouldWellDeserializeWhenNonEmptyRspamdTimeoutField() throws Exception {
+        assertThat(JsonGenericSerializer
+            .forModules(FeedSpamToRspamdTaskAdditionalInformationDTO.SERIALIZATION_MODULE)
+            .withoutNestedType()
+            .deserialize(ClassLoaderUtils.getSystemResourceAsString("json/feedSpamNonEmptyRspamdTimeout.additionalInformation.json")))
+            .isEqualTo(new FeedSpamToRspamdTask.AdditionalInformation(
+                Instant.parse("2007-12-03T10:15:30.00Z"),
+                4,
+                2,
+                1,
+                new RunningOptions(
+                    Optional.of(3600L),
+                    DEFAULT_MESSAGES_PER_SECOND,
+                    DEFAULT_SAMPLING_PROBABILITY,
+                    Optional.empty(),
+                    Duration.ofSeconds(100))));
     }
 }
