1ce09283671a1abc9c18e0a36e59d024ef35c5d0
Benoit Tellier <btellier@linagora.com>
2022-11-11 10:04:29 +0700

    fixup! JAMES-3850 [JMAP] Configure `urn:ietf:params:jmap:mail` `maxSizeAttachmentsPerEmail` property

--- server/container/guice/protocols/jmap/src/main/java/org/apache/james/jmap/draft/JMAPModule.java
+++ server/container/guice/protocols/jmap/src/main/java/org/apache/james/jmap/draft/JMAPModule.java
@@ -43,6 +43,7 @@ import org.apache.james.jmap.core.IdentitySortOrderCapabilityFactory$;
 import org.apache.james.jmap.core.JmapQuotaCapabilityFactory$;
 import org.apache.james.jmap.core.JmapRfc8621Configuration;
 import org.apache.james.jmap.core.MDNCapabilityFactory$;
+import org.apache.james.jmap.core.MailCapabilityFactory;
 import org.apache.james.jmap.core.MailCapabilityFactory$;
 import org.apache.james.jmap.core.QuotaCapabilityFactory$;
 import org.apache.james.jmap.core.SharesCapabilityFactory$;
@@ -144,7 +145,6 @@ public class JMAPModule extends AbstractModule {
         supportedVersions.addBinding().toInstance(Version.RFC8621);
 
         Multibinder<CapabilityFactory> supportedCapabilities = Multibinder.newSetBinder(binder(), CapabilityFactory.class);
-        supportedCapabilities.addBinding().toInstance(MailCapabilityFactory$.MODULE$);
         supportedCapabilities.addBinding().toInstance(QuotaCapabilityFactory$.MODULE$);
         supportedCapabilities.addBinding().toInstance(JmapQuotaCapabilityFactory$.MODULE$);
         supportedCapabilities.addBinding().toInstance(IdentitySortOrderCapabilityFactory$.MODULE$);
@@ -168,6 +168,11 @@ public class JMAPModule extends AbstractModule {
             return FILTERING_MAILET_CHECK;
         }
         return ProcessorsCheck.noCheck();
+    }
+
+    @ProvidesIntoSet
+    CapabilityFactory vacationMailetCheck(JmapRfc8621Configuration configuration) {
+        return new MailCapabilityFactory(configuration);
     }
 
     @ProvidesIntoSet
