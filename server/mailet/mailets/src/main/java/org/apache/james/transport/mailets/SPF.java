6529e203ba2c3169ec8537bbf98631e846d0ae6c
ouvtam <ouvtam@8n4.pw>
2022-12-16 11:55:13 +0100

    JAMES-3869 SPF mailet: exclude private networks in addition to 127.0.0.1 and make it configurable

--- server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/SPF.java
+++ server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/SPF.java
@@ -19,19 +19,27 @@
 
 package org.apache.james.transport.mailets;
 
+import java.util.Collection;
+
+import javax.inject.Inject;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.james.dnsservice.api.DNSService;
+import org.apache.james.dnsservice.library.netmatcher.NetMatcher;
 import org.apache.james.jspf.core.Logger;
 import org.apache.james.jspf.executor.SPFResult;
 import org.apache.james.jspf.impl.DefaultSPF;
 import org.apache.mailet.Attribute;
 import org.apache.mailet.AttributeName;
+import org.apache.mailet.AttributeUtils;
 import org.apache.mailet.AttributeValue;
 import org.apache.mailet.Experimental;
 import org.apache.mailet.Mail;
 import org.apache.mailet.base.GenericMailet;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Splitter;
 
 /**
  * Check the ip, sender, helo against SPF. Add the following attributes to the
@@ -50,6 +58,7 @@ import org.slf4j.LoggerFactory;
  * &lt;mailet match="All" class="SPF"&gt;
  *   &lt;addHeader&gt;true&lt;/addHeader&gt;
  *   &lt;debug&gt;false&lt;/debug&gt;
+ *   &lt;ignoreNetworks&gt;127.0.0.0/8&lt;/ignoreNetworks&gt;
  * &lt;/mailet&gt;
  * </pre>
  */
@@ -57,25 +66,44 @@ import org.slf4j.LoggerFactory;
 public class SPF extends GenericMailet {
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(SPF.class);
 
+    private boolean debug = false;
     private boolean addHeader = false;
     private org.apache.james.jspf.impl.SPF spf;
     private static final AttributeName EXPLANATION_ATTRIBUTE = AttributeName.of("org.apache.james.transport.mailets.spf.explanation");
     private static final AttributeName RESULT_ATTRIBUTE = AttributeName.of("org.apache.james.transport.mailets.spf.result");
+    private static final String privateNetworks = "127.0.0.0/8, 10.0.0.0/8, 172.16.0.0/16, 172.17.0.0/16, 172.18.0.0/16, 172.19.0.0/16, 172.20.0.0/16, 172.21.0.0/16, 172.22.0.0/16, 172.23.0.0/16, 172.24.0.0/16, 172.25.0.0/16, 172.26.0.0/16, 172.27.0.0/16, 172.28.0.0/16, 172.29.0.0/16, 172.30.0.0/16, 172.31.0.0/16";
+
+    private final DNSService dnsService;
+    private NetMatcher netMatcher;
+
+    @Inject
+    public SPF(DNSService dnsService) {
+        this.dnsService = dnsService;
+    }
 
     @Override
     public void init() {
+        debug = Boolean.parseBoolean(getInitParameter("debug", "false"));
         addHeader = Boolean.parseBoolean(getInitParameter("addHeader", "false"));
-        SPFLoggerAdapter logger = new SPFLoggerAdapter(Boolean.parseBoolean(getInitParameter("debug", "false")));
+        spf = new DefaultSPF(new SPFLoggerAdapter(debug));
 
-        spf = new DefaultSPF(logger);
+        Collection<String> ignoredNetworks = Splitter.on(',')
+            .trimResults()
+            .omitEmptyStrings()
+            .splitToList(getInitParameter("ignoreNetworks", privateNetworks));
+        netMatcher = new NetMatcher(ignoredNetworks, dnsService);
+
+        LOGGER.info("SPF addHeader={} debug={} ignoreNetworks={}", addHeader, debug, ignoredNetworks);
     }
 
     @Override
     public void service(Mail mail) throws MessagingException {
         String remoteAddr = mail.getRemoteAddr();
-        String helo = mail.getRemoteHost();
 
-        if (!remoteAddr.equals("127.0.0.1")) {
+        if (netMatcher.matchInetNetwork(remoteAddr)) {
+            LOGGER.debug("ignore SPF check for ip:{}", remoteAddr);
+        } else {
+            String helo = mail.getRemoteHost();
             String sender = mail.getMaybeSender().asString("");
             SPFResult result = spf.checkSPF(remoteAddr, sender, helo);
             mail.setAttribute(new Attribute(EXPLANATION_ATTRIBUTE, AttributeValue.of(result.getExplanation())));
