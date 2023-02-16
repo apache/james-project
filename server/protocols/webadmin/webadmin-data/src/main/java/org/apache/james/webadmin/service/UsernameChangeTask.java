d76de2d27b4117ded54a632a83ca4df66a725c46
Benoit Tellier <btellier@linagora.com>
2023-02-16 15:32:33 +0700

    JAMES-3885 Task to change of username
    Simple task wrapper around the service

---
+++ server/protocols/webadmin/webadmin-data/src/main/java/org/apache/james/webadmin/service/UsernameChangeTask.java
@@ -0,0 +1,109 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.webadmin.service;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Map;
+import java.util.Optional;
+
+import org.apache.james.core.Username;
+import org.apache.james.task.Task;
+import org.apache.james.task.TaskExecutionDetails;
+import org.apache.james.task.TaskType;
+import org.apache.james.user.api.UsernameChangeTaskStep;
+
+import reactor.core.publisher.Mono;
+
+public class UsernameChangeTask implements Task {
+    static final TaskType TYPE = TaskType.of("UsernameChangeTask");
+
+    public static class AdditionalInformation implements TaskExecutionDetails.AdditionalInformation {
+        private final Instant timestamp;
+        private final Username oldUser;
+        private final Username newUser;
+        private final Map<UsernameChangeTaskStep.StepName, UsernameChangeService.StepState> status;
+
+        public AdditionalInformation(Instant timestamp, Username oldUser, Username newUser, Map<UsernameChangeTaskStep.StepName, UsernameChangeService.StepState> status) {
+            this.timestamp = timestamp;
+            this.oldUser = oldUser;
+            this.newUser = newUser;
+            this.status = status;
+        }
+
+        public Username getOldUser() {
+            return oldUser;
+        }
+
+        public Username getNewUser() {
+            return newUser;
+        }
+
+        public Map<UsernameChangeTaskStep.StepName, UsernameChangeService.StepState> getStatus() {
+            return status;
+        }
+
+        @Override
+        public Instant timestamp() {
+            return timestamp;
+        }
+    }
+
+    private final Username oldUser;
+    private final Username newUser;
+    private final UsernameChangeService.Performer performer;
+
+    public UsernameChangeTask(UsernameChangeService service, Username oldUser, Username newUser) {
+        this.oldUser = oldUser;
+        this.newUser = newUser;
+
+        this.performer = service.performer();
+    }
+
+
+    @Override
+    public Result run() {
+        return performer.changeUsername(oldUser, newUser)
+            .thenReturn(Result.COMPLETED)
+            .onErrorResume(e -> {
+                LOGGER.error("Error while changing username {} into {}", oldUser.asString(), newUser.asString(), e);
+                return Mono.just(Result.PARTIAL);
+            })
+            .block();
+    }
+
+    @Override
+    public TaskType type() {
+        return TYPE;
+    }
+
+    @Override
+    public Optional<TaskExecutionDetails.AdditionalInformation> details() {
+        return Optional.of(new AdditionalInformation(Clock.systemUTC().instant(), oldUser, newUser, performer.getStatus().getStates()));
+    }
+
+    public Username getOldUser() {
+        return oldUser;
+    }
+
+    public Username getNewUser() {
+        return newUser;
+    }
+}
---
+++ server/protocols/webadmin/webadmin-data/src/main/java/org/apache/james/webadmin/service/UsernameChangeTaskDTO.java
@@ -0,0 +1,72 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.webadmin.service;
+
+import org.apache.james.core.Username;
+import org.apache.james.json.DTOModule;
+import org.apache.james.server.task.json.dto.TaskDTO;
+import org.apache.james.server.task.json.dto.TaskDTOModule;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UsernameChangeTaskDTO implements TaskDTO {
+
+    public static TaskDTOModule<UsernameChangeTask, UsernameChangeTaskDTO> module(UsernameChangeService service) {
+        return DTOModule
+            .forDomainObject(UsernameChangeTask.class)
+            .convertToDTO(UsernameChangeTaskDTO.class)
+            .toDomainObjectConverter(dto -> dto.fromDTO(service))
+            .toDTOConverter(UsernameChangeTaskDTO::toDTO)
+            .typeName(UsernameChangeTask.TYPE.asString())
+            .withFactory(TaskDTOModule::new);
+    }
+
+    public static UsernameChangeTaskDTO toDTO(UsernameChangeTask domainObject, String typeName) {
+        return new UsernameChangeTaskDTO(typeName, domainObject.getOldUser().asString(), domainObject.getNewUser().asString());
+    }
+
+    private final String type;
+    private final String oldUser;
+    private final String newUser;
+
+    public UsernameChangeTaskDTO(@JsonProperty("type") String type,
+                                 @JsonProperty("oldUser") String oldUser,
+                                 @JsonProperty("newUser") String newUser) {
+        this.type = type;
+        this.oldUser = oldUser;
+        this.newUser = newUser;
+    }
+
+    public UsernameChangeTask fromDTO(UsernameChangeService service) {
+        return new UsernameChangeTask(service, Username.of(oldUser), Username.of(newUser));
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    public String getOldUser() {
+        return oldUser;
+    }
+
+    public String getNewUser() {
+        return newUser;
+    }
+}
