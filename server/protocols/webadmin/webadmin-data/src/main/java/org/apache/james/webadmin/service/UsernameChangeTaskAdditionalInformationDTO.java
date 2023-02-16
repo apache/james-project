d3ebe71f4f19bf37b7e4fc2314542b42bdccb5bb
Benoit Tellier <btellier@linagora.com>
2023-02-16 15:32:33 +0700

    JAMES-3885 Task to change of username
    Simple task wrapper around the service

---
+++ server/protocols/webadmin/webadmin-data/src/main/java/org/apache/james/webadmin/service/UsernameChangeTaskAdditionalInformationDTO.java
@@ -0,0 +1,96 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.webadmin.service;
+
+import java.time.Instant;
+import java.util.Map;
+
+import org.apache.james.core.Username;
+import org.apache.james.json.DTOModule;
+import org.apache.james.server.task.json.dto.AdditionalInformationDTO;
+import org.apache.james.server.task.json.dto.AdditionalInformationDTOModule;
+import org.apache.james.user.api.UsernameChangeTaskStep;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableMap;
+
+public class UsernameChangeTaskAdditionalInformationDTO implements AdditionalInformationDTO {
+    public static AdditionalInformationDTOModule<UsernameChangeTask.AdditionalInformation, UsernameChangeTaskAdditionalInformationDTO> module() {
+        return DTOModule.forDomainObject(UsernameChangeTask.AdditionalInformation.class)
+            .convertToDTO(UsernameChangeTaskAdditionalInformationDTO.class)
+            .toDomainObjectConverter(dto -> new UsernameChangeTask.AdditionalInformation(
+                dto.timestamp,
+                Username.of(dto.oldUser),
+                Username.of(dto.newUser),
+                dto.status.entrySet().stream()
+                    .collect(ImmutableMap.toImmutableMap(
+                        entry -> new UsernameChangeTaskStep.StepName(entry.getKey()),
+                        entry -> UsernameChangeService.StepState.valueOf(entry.getValue())))))
+            .toDTOConverter((details, type) -> new UsernameChangeTaskAdditionalInformationDTO(
+                type,
+                details.getOldUser().asString(),
+                details.getNewUser().asString(),
+                details.getStatus().entrySet().stream()
+                    .collect(ImmutableMap.toImmutableMap(
+                        entry -> entry.getKey().asString(),
+                        entry -> entry.getValue().toString())),
+                details.timestamp()))
+            .typeName(UsernameChangeTask.TYPE.asString())
+            .withFactory(AdditionalInformationDTOModule::new);
+    }
+
+    private final String type;
+    private final String oldUser;
+    private final String newUser;
+    private final Map<String, String> status;
+    private final Instant timestamp;
+
+    public UsernameChangeTaskAdditionalInformationDTO(@JsonProperty("type") String type,
+                                                      @JsonProperty("oldUser") String oldUser,
+                                                      @JsonProperty("newUser") String newUser,
+                                                      @JsonProperty("status") Map<String, String> status,
+                                                      @JsonProperty("timestamp") Instant timestamp) {
+        this.type = type;
+        this.oldUser = oldUser;
+        this.newUser = newUser;
+        this.status = status;
+        this.timestamp = timestamp;
+    }
+
+    public String getOldUser() {
+        return oldUser;
+    }
+
+    public String getNewUser() {
+        return newUser;
+    }
+
+    public Map<String, String> getStatus() {
+        return status;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+}
---
+++ server/protocols/webadmin/webadmin-data/src/test/java/org/apache/james/webadmin/service/UsernameChangeTaskSerializationTest.java
@@ -0,0 +1,104 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.webadmin.service;
+
+import java.time.Instant;
+
+import org.apache.james.JsonSerializationVerifier;
+import org.apache.james.core.Username;
+import org.apache.james.user.api.UsernameChangeTaskStep;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import reactor.core.publisher.Mono;
+
+class UsernameChangeTaskSerializationTest {
+    private static final Instant TIMESTAMP = Instant.parse("2018-11-13T12:00:55Z");
+    private static final Username OLD_USERNAME = Username.of("user");
+    private static final Username NEW_USERNAME = Username.of("geraldine");
+    private static final UsernameChangeTaskStep.StepName STEP_A = new UsernameChangeTaskStep.StepName("A");
+    private static final UsernameChangeTaskStep.StepName STEP_B = new UsernameChangeTaskStep.StepName("B");
+    private static final UsernameChangeTaskStep.StepName STEP_C = new UsernameChangeTaskStep.StepName("C");
+    private static final UsernameChangeTaskStep.StepName STEP_D = new UsernameChangeTaskStep.StepName("D");
+    private static final UsernameChangeTaskStep A = asStep(STEP_A);
+    private static final UsernameChangeTaskStep B = asStep(STEP_B);
+    private static final UsernameChangeTaskStep C = asStep(STEP_C);
+    private static final UsernameChangeTaskStep D = asStep(STEP_D);
+
+    private static UsernameChangeTaskStep asStep(UsernameChangeTaskStep.StepName name) {
+        return new UsernameChangeTaskStep() {
+            @Override
+            public StepName name() {
+                return name;
+            }
+
+            @Override
+            public int priority() {
+                return 0;
+            }
+
+            @Override
+            public Publisher<Void> changeUsername(Username oldUsername, Username newUsername) {
+                return Mono.empty();
+            }
+        };
+    }
+
+    private static final String SERIALIZED_TASK = "{\"newUser\":\"geraldine\",\"oldUser\":\"user\",\"type\":\"UsernameChangeTask\"}";
+    private static final String SERIALIZED_ADDITIONAL_INFORMATION = "{" +
+        "  \"newUser\":\"geraldine\"," +
+        "  \"oldUser\":\"user\"," +
+        "  \"status\":{" +
+        "    \"A\":\"DONE\"," +
+        "    \"B\":\"WAITING\"," +
+        "    \"C\":\"FAILED\"," +
+        "    \"D\":\"ABORTED\"}," +
+        "  \"timestamp\":\"2018-11-13T12:00:55Z\"," +
+        "  \"type\":\"UsernameChangeTask\"" +
+        "}";
+
+    private static final UsernameChangeService SERVICE = new UsernameChangeService(ImmutableSet.of(A, B, C, D));
+
+    @Test
+    void taskShouldBeSerializable() throws Exception {
+        JsonSerializationVerifier.dtoModule(UsernameChangeTaskDTO.module(SERVICE))
+            .bean(new UsernameChangeTask(SERVICE, OLD_USERNAME, NEW_USERNAME))
+            .json(SERIALIZED_TASK)
+            .verify();
+    }
+
+    @Test
+    void additionalInformationShouldBeSerializable() throws Exception {
+        JsonSerializationVerifier.dtoModule(UsernameChangeTaskAdditionalInformationDTO.module())
+            .bean(new UsernameChangeTask.AdditionalInformation(
+                TIMESTAMP,
+                OLD_USERNAME,
+                NEW_USERNAME,
+                ImmutableMap.of(STEP_A, UsernameChangeService.StepState.DONE,
+                    STEP_B, UsernameChangeService.StepState.WAITING,
+                    STEP_C, UsernameChangeService.StepState.FAILED,
+                    STEP_D, UsernameChangeService.StepState.ABORTED)))
+            .json(SERIALIZED_ADDITIONAL_INFORMATION)
+            .verify();
+    }
+}
