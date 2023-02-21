ad14156e970cc8f70038a7128231f8959dd35cb3
Houssem Nasri <housi.housi2015@gmail.com>
2023-02-21 17:44:41 +0100

    Define an ldap test user for checking ldap server health

--- server/data/data-ldap/src/main/java/org/apache/james/user/ldap/LdapHealthCheck.java
+++ server/data/data-ldap/src/main/java/org/apache/james/user/ldap/LdapHealthCheck.java
@@ -12,6 +12,7 @@ import reactor.core.publisher.Mono;
 
 public class LdapHealthCheck implements HealthCheck {
     public static final ComponentName COMPONENT_NAME = new ComponentName("LDAP User Server");
+    public static final Username LDAP_TEST_USER = Username.of("ldap-test");
     private static final Logger LOGGER = LoggerFactory.getLogger(LdapHealthCheck.class);
 
     private final ReadOnlyUsersLDAPRepository ldapUserRepository;
@@ -28,7 +29,7 @@ public class LdapHealthCheck implements HealthCheck {
 
     @Override
     public Mono<Result> check() {
-        return Mono.fromCallable(() -> ldapUserRepository.getUserByName(Username.of("test")))
+        return Mono.fromCallable(() -> ldapUserRepository.getUserByName(LDAP_TEST_USER))
             .map(user -> Result.healthy(COMPONENT_NAME))
             .onErrorResume(e -> Mono.just(Result.unhealthy(COMPONENT_NAME, "Error checking LDAP server!", e)))
             .doOnError(e -> LOGGER.error("Error in LDAP server", e));
