a78ab7aa80ac205c38947d5efda2cf222399664c
Houssem Nasri <housi.housi2015@gmail.com>
2023-02-21 18:31:32 +0100

    Test Ldap health checker

---
+++ server/data/data-ldap/src/test/java/org/apache/james/user/ldap/LdapHealthCheckTest.java
@@ -0,0 +1,43 @@
+package org.apache.james.user.ldap;
+
+import org.apache.james.core.healthcheck.Result;
+import org.apache.james.domainlist.api.mock.SimpleDomainList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LdapHealthCheckTest {
+
+    LdapHealthCheck ldapHealthCheck;
+    ReadOnlyUsersLDAPRepository ldapUserRepository;
+    LdapGenericContainer ldapContainer;
+
+    @BeforeEach
+    public void setUp() {
+        ldapUserRepository = new ReadOnlyUsersLDAPRepository(new SimpleDomainList());
+        ldapHealthCheck = new LdapHealthCheck(ldapUserRepository);
+        ldapContainer = DockerLdapSingleton.ldapContainer;
+    }
+
+    @Test
+    void checkShouldReturnUnhealthyIfLdapIsDown() {
+        ldapContainer.stop();
+
+        Result checkResult = ldapHealthCheck.check().block();
+        assertNotNull(checkResult);
+        assertTrue(checkResult.isUnHealthy());
+
+        ldapContainer.start();
+    }
+
+    @Test
+    void checkShouldReturnHealthyIfLdapIsRunning() {
+        ldapContainer.start();
+
+        Result checkResult = ldapHealthCheck.check().block();
+        assertNotNull(checkResult);
+        assertTrue(checkResult.isHealthy());
+    }
+}
