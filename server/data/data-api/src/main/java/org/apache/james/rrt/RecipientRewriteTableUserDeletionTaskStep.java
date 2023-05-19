971d2d8da42ce22f999cd8f514b567bcc984c5ec
Benoit Tellier <btellier@linagora.com>
2023-05-19 11:42:47 +0700

    JAMES-3909 RecipientRewriteTableUserDeletionTaskStep

---
+++ server/data/data-api/src/main/java/org/apache/james/rrt/RecipientRewriteTableUserDeletionTaskStep.java
@@ -0,0 +1,89 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.rrt;
+
+import org.apache.james.core.Username;
+import org.apache.james.rrt.api.RecipientRewriteTable;
+import org.apache.james.rrt.lib.Mapping;
+import org.apache.james.rrt.lib.MappingSource;
+import org.apache.james.user.api.DeleteUserDataTaskStep;
+import org.apache.james.util.ReactorUtils;
+import org.reactivestreams.Publisher;
+
+import com.github.fge.lambdas.Throwing;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class RecipientRewriteTableUserDeletionTaskStep implements DeleteUserDataTaskStep {
+    private final RecipientRewriteTable rrt;
+
+    public RecipientRewriteTableUserDeletionTaskStep(RecipientRewriteTable rrt) {
+        this.rrt = rrt;
+    }
+
+    @Override
+    public StepName name() {
+        return new StepName("RecipientRewriteTableUserDeletionTaskStep");
+    }
+
+    @Override
+    public int priority() {
+        return 0;
+    }
+
+    @Override
+    public Publisher<Void> deleteUserData(Username username) {
+        return deleteRRT(username)
+            .then(deleteForwards(username))
+            .then(deleteGroup(username));
+    }
+
+    private Flux<Void> deleteRRT(Username username) {
+        MappingSource mappingSource = MappingSource.fromUser(username);
+
+        return Mono.fromCallable(() -> rrt.getStoredMappings(mappingSource))
+            .subscribeOn(ReactorUtils.BLOCKING_CALL_WRAPPER)
+            .flatMapMany(mappings -> Flux.fromStream(mappings.asStream()))
+            .flatMap(mapping -> deleteMapping(mappingSource, mapping));
+    }
+
+    private Mono<Void> deleteMapping(MappingSource mappingSource, Mapping mapping) {
+        return Mono.fromRunnable(Throwing.runnable(() -> rrt.removeMapping(mappingSource, mapping)))
+            .subscribeOn(ReactorUtils.BLOCKING_CALL_WRAPPER)
+            .then();
+    }
+
+    private Mono<Void> deleteForwards(Username username) {
+        return deleteSource(Mapping.forward(username.asString()));
+    }
+
+    private Mono<Void> deleteGroup(Username username) {
+        return deleteSource(Mapping.group(username.asString()));
+    }
+
+    private Mono<Void> deleteSource(Mapping mapping) {
+        return Mono.fromCallable(() -> rrt.listSources(mapping))
+            .subscribeOn(ReactorUtils.BLOCKING_CALL_WRAPPER)
+            .flatMapMany(Flux::fromStream)
+            .flatMap(source -> deleteMapping(source, mapping))
+            .then();
+    }
+}
---
+++ server/data/data-memory/src/test/java/org/apache/james/rrt/memory/RecipientRewriteTableUserDeletionTaskStepTest.java
@@ -0,0 +1,137 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+ package org.apache.james.rrt.memory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.mock;
+
+import org.apache.commons.configuration2.BaseHierarchicalConfiguration;
+import org.apache.james.UserEntityValidator;
+import org.apache.james.core.Domain;
+import org.apache.james.core.Username;
+import org.apache.james.dnsservice.api.DNSService;
+import org.apache.james.domainlist.lib.DomainListConfiguration;
+import org.apache.james.domainlist.memory.MemoryDomainList;
+import org.apache.james.rrt.RecipientRewriteTableUserDeletionTaskStep;
+import org.apache.james.rrt.lib.MappingSource;
+import org.apache.james.user.memory.MemoryUsersRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Mono;
+
+class RecipientRewriteTableUserDeletionTaskStepTest {
+    private static final Username BOB = Username.of("bob@domain.tld");
+    private MemoryRecipientRewriteTable rrt;
+    private RecipientRewriteTableUserDeletionTaskStep testee;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        DNSService dnsService = mock(DNSService.class);
+        MemoryDomainList domainList = new MemoryDomainList(dnsService);
+        domainList.configure(DomainListConfiguration.DEFAULT);
+        domainList.addDomain(Domain.of("domain.tld"));
+
+        rrt = new MemoryRecipientRewriteTable();
+        rrt.setUsersRepository(MemoryUsersRepository.withVirtualHosting(domainList));
+        rrt.setUserEntityValidator(UserEntityValidator.NOOP);
+        rrt.setDomainList(domainList);
+        rrt.configure(new BaseHierarchicalConfiguration());
+
+        testee = new RecipientRewriteTableUserDeletionTaskStep(rrt);
+    }
+
+    @Test
+    void shouldNotFailWhenNoMapping() {
+        assertThatCode(() -> Mono.from(testee.deleteUserData(BOB)).block())
+            .doesNotThrowAnyException();
+    }
+
+    @Test
+    void shouldDeleteForwardMappings() throws Exception {
+        rrt.addForwardMapping(MappingSource.fromUser(BOB), "alice@domain.tld");
+
+        Mono.from(testee.deleteUserData(BOB)).block();
+
+        assertThat(rrt.getAllMappings())
+            .isEmpty();
+    }
+
+    @Test
+    void shouldDeleteForwardMappingsWhenDestination() throws Exception {
+        rrt.addForwardMapping(MappingSource.fromUser("alice", "domain.tld"), BOB.asString());
+
+        Mono.from(testee.deleteUserData(BOB)).block();
+
+        assertThat(rrt.getAllMappings())
+            .isEmpty();
+    }
+
+    @Test
+    void shouldDeleteAliasMapping() throws Exception {
+        rrt.addAliasMapping(MappingSource.fromUser(BOB), "alice@domain.tld");
+
+        Mono.from(testee.deleteUserData(BOB)).block();
+
+        assertThat(rrt.getAllMappings())
+            .isEmpty();
+    }
+
+    @Test
+    void shouldDeleteAddressMapping() throws Exception {
+        rrt.addAddressMapping(MappingSource.fromUser(BOB), "alice@domain.tld");
+
+        Mono.from(testee.deleteUserData(BOB)).block();
+
+        assertThat(rrt.getAllMappings())
+            .isEmpty();
+    }
+
+    @Test
+    void shouldDeleteRegexMapping() throws Exception {
+        rrt.addRegexMapping(MappingSource.fromUser(BOB), "alice@domain.tld");
+
+        Mono.from(testee.deleteUserData(BOB)).block();
+
+        assertThat(rrt.getAllMappings())
+            .isEmpty();
+    }
+
+    @Test
+    void shouldDeleteErrorMapping() throws Exception {
+        rrt.addErrorMapping(MappingSource.fromUser(BOB), "alice@domain.tld");
+
+        Mono.from(testee.deleteUserData(BOB)).block();
+
+        assertThat(rrt.getAllMappings())
+            .isEmpty();
+    }
+
+    @Test
+    void shouldDeleteGroupMapping() throws Exception {
+        rrt.addGroupMapping(MappingSource.fromUser("alice", "domain.tld"), BOB.asString());
+
+        Mono.from(testee.deleteUserData(BOB)).block();
+
+        assertThat(rrt.getAllMappings())
+            .isEmpty();
+    }
+}
