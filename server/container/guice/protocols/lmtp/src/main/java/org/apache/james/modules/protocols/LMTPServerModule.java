3d15f0225aa3b6bed3e67b9fda70a6234967c2f2
Benoit Tellier <btellier@linagora.com>
2023-05-25 11:19:47 +0700

    JAMES-3906 Respect orthogonal architecture + centralise certificate renewal management

--- server/container/guice/protocols/lmtp/src/main/java/org/apache/james/modules/protocols/LMTPServerModule.java
+++ server/container/guice/protocols/lmtp/src/main/java/org/apache/james/modules/protocols/LMTPServerModule.java
@@ -24,14 +24,13 @@ import org.apache.james.RunArguments;
 import org.apache.james.filesystem.api.FileSystem;
 import org.apache.james.lifecycle.api.ConfigurationSanitizer;
 import org.apache.james.lmtpserver.netty.LMTPServerFactory;
-import org.apache.james.lmtpserver.webadmin.LMTPRoutes;
+import org.apache.james.protocols.lib.netty.AbstractServerFactory;
 import org.apache.james.server.core.configuration.ConfigurationProvider;
 import org.apache.james.util.LoggingLevel;
 import org.apache.james.utils.GuiceProbe;
 import org.apache.james.utils.InitializationOperation;
 import org.apache.james.utils.InitilizationOperationBuilder;
 import org.apache.james.utils.KeystoreCreator;
-import org.apache.james.webadmin.Routes;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
@@ -45,7 +44,7 @@ public class LMTPServerModule extends AbstractModule {
 
         Multibinder.newSetBinder(binder(), GuiceProbe.class).addBinding().to(LmtpGuiceProbe.class);
 
-        Multibinder.newSetBinder(binder(), Routes.class).addBinding().to(LMTPRoutes.class);
+        Multibinder.newSetBinder(binder(), AbstractServerFactory.class).addBinding().to(LMTPServerFactory.class);
     }
 
     @ProvidesIntoSet
