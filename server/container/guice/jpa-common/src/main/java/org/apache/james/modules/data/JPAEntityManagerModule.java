e6db86009852b4be581212a9aad0de5a00d3b39e
Wojtek <woj-tek@users.noreply.github.com>
2023-06-12 04:25:44 -0400

    Allow passing openjpa and underlying datasource (DBCP2) properties (#1580)

--- server/container/guice/jpa-common/src/main/java/org/apache/james/modules/data/JPAEntityManagerModule.java
+++ server/container/guice/jpa-common/src/main/java/org/apache/james/modules/data/JPAEntityManagerModule.java
@@ -21,7 +21,10 @@ package org.apache.james.modules.data;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.inject.Singleton;
 import javax.persistence.EntityManagerFactory;
@@ -41,25 +44,30 @@ public class JPAEntityManagerModule extends AbstractModule {
     public EntityManagerFactory provideEntityManagerFactory(JPAConfiguration jpaConfiguration) {
         HashMap<String, String> properties = new HashMap<>();
 
-        properties.put("openjpa.ConnectionDriverName", jpaConfiguration.getDriverName());
-        properties.put("openjpa.ConnectionURL", jpaConfiguration.getDriverURL());
+        properties.put(JPAConfiguration.JPA_CONNECTION_DRIVER_NAME, jpaConfiguration.getDriverName());
+        properties.put(JPAConfiguration.JPA_CONNECTION_URL, jpaConfiguration.getDriverURL());
         jpaConfiguration.getCredential()
             .ifPresent(credential -> {
-                properties.put("openjpa.ConnectionUserName", credential.getUsername());
-                properties.put("openjpa.ConnectionPassword", credential.getPassword());
+                properties.put(JPAConfiguration.JPA_CONNECTION_USERNAME, credential.getUsername());
+                properties.put(JPAConfiguration.JPA_CONNECTION_PASSWORD, credential.getPassword());
             });
 
-        List<String> connectionFactoryProperties = new ArrayList<>();
-        connectionFactoryProperties.add("TestOnBorrow=" + jpaConfiguration.isTestOnBorrow());
+        List<String> connectionProperties = new ArrayList<>();
+        jpaConfiguration.isTestOnBorrow().ifPresent(testOnBorrow -> connectionProperties.add("TestOnBorrow=" + testOnBorrow));
         jpaConfiguration.getValidationQueryTimeoutSec()
-            .ifPresent(timeoutSecond -> connectionFactoryProperties.add("ValidationTimeout=" + timeoutSecond * 1000));
+            .ifPresent(timeoutSecond -> connectionProperties.add("ValidationTimeout=" + timeoutSecond * 1000));
         jpaConfiguration.getValidationQuery()
-            .ifPresent(validationQuery -> connectionFactoryProperties.add("ValidationSQL='" + validationQuery + "'"));
-        jpaConfiguration.getMaxConnections().ifPresent(maxConnections ->
-                connectionFactoryProperties.add("MaxTotal=" + maxConnections)
-        );
+            .ifPresent(validationQuery -> connectionProperties.add("ValidationSQL='" + validationQuery + "'"));
+        jpaConfiguration.getMaxConnections()
+                .ifPresent(maxConnections -> connectionProperties.add("MaxTotal=" + maxConnections));
 
-        properties.put("openjpa.ConnectionFactoryProperties", Joiner.on(", ").join(connectionFactoryProperties));
+        connectionProperties.addAll(jpaConfiguration.getCustomDatasourceProperties().entrySet().stream().map(entry -> entry.getKey() + "=" + entry.getValue()).collect(Collectors.toList()));
+        properties.put(JPAConfiguration.JPA_CONNECTION_PROPERTIES, Joiner.on(",").join(connectionProperties));
+        properties.putAll(jpaConfiguration.getCustomOpenjpaProperties());
+
+        if (jpaConfiguration.isMultithreaded().isPresent()) {
+            properties.put(JPAConfiguration.JPA_MULTITHREADED, jpaConfiguration.isMultithreaded().get().toString());
+        }
 
         return Persistence.createEntityManagerFactory("Global", properties);
     }
@@ -68,15 +76,34 @@ public class JPAEntityManagerModule extends AbstractModule {
     @Singleton
     JPAConfiguration provideConfiguration(PropertiesProvider propertiesProvider) throws FileNotFoundException, ConfigurationException {
         Configuration dataSource = propertiesProvider.getConfiguration("james-database");
+
+        Map<String, String> openjpaProperties = getKeysForPrefix(dataSource, "openjpa", false);
+        Map<String, String> datasourceProperties = getKeysForPrefix(dataSource, "datasource", true);
+
+
         return JPAConfiguration.builder()
                 .driverName(dataSource.getString("database.driverClassName"))
                 .driverURL(dataSource.getString("database.url"))
                 .testOnBorrow(dataSource.getBoolean("datasource.testOnBorrow", false))
                 .validationQueryTimeoutSec(dataSource.getInteger("datasource.validationQueryTimeoutSec", null))
                 .validationQuery(dataSource.getString("datasource.validationQuery", null))
-                .maxConnections(dataSource.getInteger("datasource.maxTotal",null))
+                .maxConnections(dataSource.getInteger("datasource.maxTotal", null))
+                .multithreaded(dataSource.getBoolean(JPAConfiguration.JPA_MULTITHREADED, true))
                 .username(dataSource.getString("database.username"))
                 .password(dataSource.getString("database.password"))
+                .setCustomOpenjpaProperties(openjpaProperties)
+                .setCustomDatasourceProperties(datasourceProperties)
                 .build();
+    }
+
+    private static Map<String, String> getKeysForPrefix(Configuration dataSource, String prefix, boolean stripPrefix) {
+        Iterator<String> keys = dataSource.getKeys(prefix);
+        Map<String, String> properties = new HashMap<>();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            String propertyKey = stripPrefix ? key.replace(prefix + ".", "") : key;
+            properties.put(propertyKey, dataSource.getString(key));
+        }
+        return properties;
     }
 }
