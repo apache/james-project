376537795dafaf71ea62f033b550edbec227ec01
Maksim Meliashchuk <maksym873@gmail.com>
2023-06-24 10:54:18 +0300

    JAMES-2434 updated javadoc

--- mailet/standard/src/main/java/org/apache/james/transport/matchers/IsFromMailingList.java
+++ mailet/standard/src/main/java/org/apache/james/transport/matchers/IsFromMailingList.java
@@ -11,17 +11,33 @@ import java.util.Collection;
 import java.util.Collections;
 
 /**
- * Matches if mail is from a mailing list.
+ * <p>Matches if mail is from a mailing list.</p>
+ * <p>Implements the match method to check if the incoming mail is from a mailing list.
+ * If the mail is from a mailing list, then returns all the recipients of the mail.</p>
  */
+
 public class IsFromMailingList extends GenericMatcher {
 
+    /**
+     * Used to detect automatically sent mails.
+     */
     private final AutomaticallySentMailDetector automaticallySentMailDetector;
 
+    /**
+     * Constructor for IsFromMailingList.
+     * @param automaticallySentMailDetector Mail detector.
+     */
     @Inject
     public IsFromMailingList(AutomaticallySentMailDetector automaticallySentMailDetector) {
         this.automaticallySentMailDetector = automaticallySentMailDetector;
     }
 
+    /**
+     * Checks if the incoming mail is from a mailing list and returns all the recipients of the mail.
+     * @param mail Mail to be matched.
+     * @throws MessagingException if there is a problem while matching the mail.
+     * @return Collection of MailAddress if matches, else an empty Collection.
+     */
     @Override
     public Collection<MailAddress> match(Mail mail) throws MessagingException {
         if (automaticallySentMailDetector.isMailingList(mail)) {
