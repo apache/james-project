7afd0906e757173903838cbfc443632ab19e901e
Benoit Tellier <btellier@linagora.com>
2023-06-28 04:02:44 +0700

    fixup! fixup! JAMES-3867 Allow per IMAP server package specification

--- server/protocols/protocols-imap4/src/main/java/org/apache/james/imapserver/netty/IMAPServerFactory.java
+++ server/protocols/protocols-imap4/src/main/java/org/apache/james/imapserver/netty/IMAPServerFactory.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.inject.Provider;
 
 import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.apache.commons.configuration2.tree.ImmutableNode;
