3cd1125bb42087cd5dbca7239b60ba619e27db90
Benoit TELLIER <btellier@linagora.com>
2023-07-04 05:17:59 +0200

    JAMES-3922: Enforce non-null Property contract and add missing null-check in Prope (#1619) (#1622)
    Co-authored-by: Wojtek <woj-tek@users.noreply.github.com>

--- mailbox/store/src/test/java/org/apache/james/mailbox/store/mail/model/impl/PropertyBuilderTest.java
+++ mailbox/store/src/test/java/org/apache/james/mailbox/store/mail/model/impl/PropertyBuilderTest.java
@@ -26,10 +26,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.apache.james.mailbox.store.mail.model.Property;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 class PropertyBuilderTest {
     @Test
     void emptyPropertyBuilderShouldCreateEmptyProperties() {
         assertThat(new PropertyBuilder().toProperties()).isEmpty();
+    }
+
+    @Test
+    void nullValuePropertyBuilderShouldCreateEmptyProperties() {
+        List<String> listOfNulls = Arrays.asList(null, null, null);
+        Map <String,String> mapWithNullValues = Collections.singletonMap("k1", null);
+
+        PropertyBuilder builder = new PropertyBuilder();
+        builder.setContentLanguage(listOfNulls);
+        builder.setContentTypeParameters(mapWithNullValues);
+        builder.setCharset(null);
+
+        assertThat(builder.toProperties()).isEmpty();
     }
 
     @Test
