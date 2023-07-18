a2a39b498a91ae377640c0b5ff7142e1ca82a936
Tung Tran <vttran@linagora.com>
2023-07-18 18:41:18 +0700

    [UPGRADE] - Docker - Apache Tika -> 2.8.0.0

--- server/testing/src/main/java/org/apache/james/util/docker/Images.java
+++ server/testing/src/main/java/org/apache/james/util/docker/Images.java
@@ -26,7 +26,7 @@ public interface Images {
     String ELASTICSEARCH_6 = "docker.elastic.co/elasticsearch/elasticsearch:6.3.2";
     String ELASTICSEARCH_7 = "docker.elastic.co/elasticsearch/elasticsearch:7.10.2";
     String OPENSEARCH = "opensearchproject/opensearch:2.4.0";
-    String TIKA = "apache/tika:1.28.2";
+    String TIKA = "apache/tika:2.8.0.0";
     String MOCK_SMTP_SERVER = "linagora/mock-smtp-server:0.6";
     String OPEN_LDAP = "osixia/openldap:1.5.0";
 }
