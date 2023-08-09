b152c4c52bb9694453c95051a4ec77358e9d6742
Quan Tran <hqtran@linagora.com>
2023-08-09 16:36:16 +0700

    fixup! JAMES-3693 RedisHealthCheck

--- server/mailet/rate-limiter-redis/src/main/scala/org/apache/james/rate/limiter/redis/RedisHealthCheck.scala
+++ server/mailet/rate-limiter-redis/src/main/scala/org/apache/james/rate/limiter/redis/RedisHealthCheck.scala
@@ -1,3 +1,22 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ * http://www.apache.org/licenses/LICENSE-2.0                   *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
 package org.apache.james.rate.limiter.redis
 
 import java.time.Duration
@@ -17,17 +36,17 @@ sealed class RedisHealthCheckKey() extends RateLimitingKey {
 }
 
 class RedisHealthCheck @Inject()(redisRateLimiterFactory: RedisRateLimiterFactory) extends HealthCheck {
-  private val COMPONENT_NAME: ComponentName = new ComponentName("Redis")
-  private val REDIS_HEALTHCHECK_KEY: RedisHealthCheckKey = new RedisHealthCheckKey
-  private val FLEXIBLE_RATE_LIMIT_RULE = Rules(Seq(Rule(100000L, Duration.ofSeconds(60))))
-  private val REDIS_HEALTHCHECK_RATE_LIMITER = redisRateLimiterFactory.withSpecification(FLEXIBLE_RATE_LIMIT_RULE, None)
-  private val HEALTH_CHECK_TIMEOUT = new FiniteDuration(3, TimeUnit.SECONDS)
+  private val redisComponent: ComponentName = new ComponentName("Redis")
+  private val redisHealthcheckKey: RedisHealthCheckKey = new RedisHealthCheckKey
+  private val flexibleRateLimitRule = Rules(Seq(Rule(100000L, Duration.ofSeconds(60))))
+  private val redisHealthcheckRateLimiter = redisRateLimiterFactory.withSpecification(flexibleRateLimitRule, None)
+  private val healthcheckTimeout = new FiniteDuration(3, TimeUnit.SECONDS)
 
-  override def componentName(): ComponentName = COMPONENT_NAME
+  override def componentName(): ComponentName = redisComponent
 
   override def check(): Publisher[Result] =
-    SMono.fromPublisher(REDIS_HEALTHCHECK_RATE_LIMITER.rateLimit(REDIS_HEALTHCHECK_KEY, 1))
-      .timeout(HEALTH_CHECK_TIMEOUT)
-      .`then`(SMono.just(Result.healthy(COMPONENT_NAME)))
-      .onErrorResume(e => SMono.just(Result.unhealthy(COMPONENT_NAME, "Can not connect to Redis.", e)))
+    SMono.fromPublisher(redisHealthcheckRateLimiter.rateLimit(redisHealthcheckKey, 1))
+      .timeout(healthcheckTimeout)
+      .`then`(SMono.just(Result.healthy(redisComponent)))
+      .onErrorResume(e => SMono.just(Result.unhealthy(redisComponent, "Can not connect to Redis.", e)))
 }
