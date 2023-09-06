829a43995cea52531d3d1b7d39658d65fa30834a
hung phan <hphan@linagora.com>
2023-09-06 16:58:03 +0700

    Fixup! JAMES-3925 Recompute current values of JMAP upload

--- server/protocols/webadmin/webadmin-mailbox/src/main/java/org/apache/james/webadmin/routes/UserQuotaRoutes.java
+++ server/protocols/webadmin/webadmin-mailbox/src/main/java/org/apache/james/webadmin/routes/UserQuotaRoutes.java
@@ -23,7 +23,6 @@ import static org.apache.james.webadmin.routes.MailboxesRoutes.TASK_PARAMETER;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
