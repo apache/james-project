37e5e9b949daf498feb8af08b63d5ba541627002
hung phan <hphan@linagora.com>
2023-09-07 10:27:38 +0700

    Fixup! JAMES-3925 Recompute current values of JMAP upload

--- server/container/guice/protocols/jmap/src/main/java/org/apache/james/jmap/rfc8621/RFC8621MethodsModule.java
+++ server/container/guice/protocols/jmap/src/main/java/org/apache/james/jmap/rfc8621/RFC8621MethodsModule.java
@@ -31,6 +31,7 @@ import org.apache.james.jmap.JMAPRoutes;
 import org.apache.james.jmap.JMAPRoutesHandler;
 import org.apache.james.jmap.Version;
 import org.apache.james.jmap.api.model.TypeName;
+import org.apache.james.jmap.api.upload.JMAPCurrentUploadUsageRecomputator;
 import org.apache.james.jmap.api.upload.UploadService;
 import org.apache.james.jmap.api.upload.UploadServiceDefaultImpl;
 import org.apache.james.jmap.change.EmailDeliveryTypeName$;
@@ -137,6 +138,7 @@ public class RFC8621MethodsModule extends AbstractModule {
         bind(WebPushClient.class).to(DefaultWebPushClient.class);
 
         bind(UploadService.class).to(UploadServiceDefaultImpl.class).in(Scopes.SINGLETON);
+        bind(JMAPCurrentUploadUsageRecomputator.class).in(Scopes.SINGLETON);
 
         Multibinder<Method> methods = Multibinder.newSetBinder(binder(), Method.class);
         methods.addBinding().to(CoreEchoMethod.class);
