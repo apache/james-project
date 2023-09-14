71e31e4b6a552e91496b04f92395d35ffbb047d2
Tung Tran <vttran@linagora.com>
2023-09-14 17:02:19 +0700

    fixup! TestContainer – Avoid using static start container when invoking class

--- backends-common/opensearch/src/test/java/org/apache/james/backends/opensearch/DockerOpenSearchExtension.java
+++ backends-common/opensearch/src/test/java/org/apache/james/backends/opensearch/DockerOpenSearchExtension.java
@@ -19,15 +19,19 @@
 
 package org.apache.james.backends.opensearch;
 
-import org.apache.james.RegistrableExtension;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
 import org.opensearch.client.opensearch._types.query_dsl.MatchAllQuery;
 import org.opensearch.client.opensearch.core.SearchRequest;
 import org.testcontainers.shaded.org.awaitility.Awaitility;
 
-public class DockerOpenSearchExtension implements RegistrableExtension {
+public class DockerOpenSearchExtension implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback, AfterAllCallback, ParameterResolver {
 
     @FunctionalInterface
     interface CleanupStrategy {
