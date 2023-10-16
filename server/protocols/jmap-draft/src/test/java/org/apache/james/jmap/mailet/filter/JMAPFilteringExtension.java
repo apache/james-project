ff2a2e3899db76b8735b6aa7057a292e3a6616ca
hung phan <hphan@linagora.com>
2023-10-16 15:21:04 +0700

    fixup! JAMES-4877 FILTER: Forward action

--- server/protocols/jmap-draft/src/test/java/org/apache/james/jmap/mailet/filter/JMAPFilteringExtension.java
+++ server/protocols/jmap-draft/src/test/java/org/apache/james/jmap/mailet/filter/JMAPFilteringExtension.java
@@ -53,7 +53,6 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
-import org.mockito.Mockito;
 
 import com.google.common.collect.ImmutableList;
 
