4e8991312d09d5ef0abf96442fa7879cd31cca42
Thanh Bui <vtbui@linagora.com>
2023-10-26 15:22:41 +0700

    JAMES-3897: Crowdsec Extension Test

---
+++ third-party/crowdsec/src/test/java/org/apache/james/CrowdsecExtension.java
@@ -0,0 +1,66 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.UUID;
+
+import org.apache.james.util.docker.RateLimiters;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy;
+
+public class CrowdsecExtension implements GuiceModuleTestExtension {
+    public static final Duration STARTUP_TIMEOUT = Duration.ofMinutes(5);
+    public static final int CROWDSEC_PORT = 8080;
+    public static final String CROWDSEC_IMAGE = "crowdsecurity/crowdsec:v1.5.4";
+
+    private final GenericContainer<?> crowdsecContainer;
+
+    public CrowdsecExtension() {
+        this.crowdsecContainer = new GenericContainer<>(CROWDSEC_IMAGE)
+            .withCreateContainerCmdModifier(cmd -> cmd.withName("james-crowdsec-test-" + UUID.randomUUID()))
+            .withExposedPorts(CROWDSEC_PORT)
+            .withStartupTimeout(STARTUP_TIMEOUT)
+            .waitingFor(new HostPortWaitStrategy().withRateLimiter(RateLimiters.TWENTIES_PER_SECOND));;
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext context) {
+        if (!crowdsecContainer.isRunning()) {
+            crowdsecContainer.start();
+        }
+    }
+
+    @Override
+    public void afterEach(ExtensionContext extensionContext) throws IOException, InterruptedException {
+        crowdsecContainer.execInContainer("cscli", "decision", "delete", "--all");
+    }
+
+    @Override
+    public void afterAll(ExtensionContext extensionContext) {
+
+    }
+
+    public GenericContainer<?> getCrowdsecContainer() {
+        return crowdsecContainer;
+    }
+}
---
+++ third-party/crowdsec/src/test/java/org/apache/james/DockerCrowdsecExtensionTest.java
@@ -0,0 +1,76 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.testcontainers.containers.Container;
+
+class DockerCrowdsecExtensionTest {
+    @RegisterExtension
+    static CrowdsecExtension crowdsecExtension = new CrowdsecExtension();
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        crowdsecExtension.getCrowdsecContainer().execInContainer("cscli", "decision", "delete", "--all");
+    }
+
+    @Test
+    void addDecisionWithAnIPTest() throws IOException, InterruptedException {
+        crowdsecExtension.getCrowdsecContainer().execInContainer("cscli", "decision", "delete", "--all");
+        crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "add", "-i", "192.168.0.4");
+        Container.ExecResult result = crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "list");
+        assertThat(result.getStdout().contains("Ip:192.168.0.4") && result.getStdout().contains("manual 'ban' from 'localhost'")).isTrue();
+    }
+
+    @Test
+    void addDecisionWithAnIPRangeTest() throws IOException, InterruptedException {
+        crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "add", "-r", "192.168.0.0/16");
+        Container.ExecResult result = crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "list");
+        assertThat(result.getStdout().contains("Range:192.168.0.0/16") && result.getStdout().contains("manual 'ban' from 'localhost'")).isTrue();
+    }
+
+    @Test
+    void deleteDecisionWithAnIPTest() throws IOException, InterruptedException {
+        crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "add", "-i", "192.168.0.4");
+        Container.ExecResult result = crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "list");
+        assertThat(result.getStdout().contains("Ip:192.168.0.4") && result.getStdout().contains("manual 'ban' from 'localhost'")).isTrue();
+
+        crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "delete", "-i", "192.168.0.4");
+        result = crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "list");
+        assertThat(result.getStdout()).contains("No active decisions");
+    }
+
+    @Test
+    void deleteDecisionWithAnIRangeTest() throws IOException, InterruptedException {
+        crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "add", "-r", "192.168.0.0/16");
+        Container.ExecResult result = crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "list");
+        assertThat(result.getStdout().contains("Range:192.168.0.0/16") && result.getStdout().contains("manual 'ban' from 'localhost'")).isTrue();
+
+        crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "delete", "-r", "192.168.0.0/16");
+        result = crowdsecExtension.getCrowdsecContainer().execInContainer("cscli" , "decision", "list");
+        assertThat(result.getStdout()).contains("No active decisions");
+    }
+}
