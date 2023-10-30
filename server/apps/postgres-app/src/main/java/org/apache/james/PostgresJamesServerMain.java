b43158b3b3c6be0e295b64239c700ef573d9756f
vttran <vttran@linagora.com>
2023-10-30 11:45:56 +0700

    JAMES-2586 - Postgres - Init postgres app server
    - artifactId: james-server-postgres-app
    - Copy from  apps/jpa-app -> apps/postgres-app

---
+++ server/apps/postgres-app/src/main/java/org/apache/james/PostgresJamesServerMain.java
@@ -0,0 +1,119 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james;
+
+import org.apache.james.data.UsersRepositoryModuleChooser;
+import org.apache.james.modules.MailboxModule;
+import org.apache.james.modules.MailetProcessingModule;
+import org.apache.james.modules.RunArgumentsModule;
+import org.apache.james.modules.data.JPADataModule;
+import org.apache.james.modules.data.JPAUsersRepositoryModule;
+import org.apache.james.modules.data.SieveJPARepositoryModules;
+import org.apache.james.modules.mailbox.DefaultEventModule;
+import org.apache.james.modules.mailbox.JPAMailboxModule;
+import org.apache.james.modules.mailbox.LuceneSearchMailboxModule;
+import org.apache.james.modules.mailbox.MemoryDeadLetterModule;
+import org.apache.james.modules.protocols.IMAPServerModule;
+import org.apache.james.modules.protocols.LMTPServerModule;
+import org.apache.james.modules.protocols.ManageSieveServerModule;
+import org.apache.james.modules.protocols.POP3ServerModule;
+import org.apache.james.modules.protocols.ProtocolHandlerModule;
+import org.apache.james.modules.protocols.SMTPServerModule;
+import org.apache.james.modules.queue.activemq.ActiveMQQueueModule;
+import org.apache.james.modules.server.DataRoutesModules;
+import org.apache.james.modules.server.DefaultProcessorsConfigurationProviderModule;
+import org.apache.james.modules.server.InconsistencyQuotasSolvingRoutesModule;
+import org.apache.james.modules.server.JMXServerModule;
+import org.apache.james.modules.server.MailQueueRoutesModule;
+import org.apache.james.modules.server.MailRepositoriesRoutesModule;
+import org.apache.james.modules.server.MailboxRoutesModule;
+import org.apache.james.modules.server.NoJwtModule;
+import org.apache.james.modules.server.RawPostDequeueDecoratorModule;
+import org.apache.james.modules.server.ReIndexingModule;
+import org.apache.james.modules.server.SieveRoutesModule;
+import org.apache.james.modules.server.TaskManagerModule;
+import org.apache.james.modules.server.WebAdminReIndexingTaskSerializationModule;
+import org.apache.james.modules.server.WebAdminServerModule;
+
+import com.google.inject.Module;
+import com.google.inject.util.Modules;
+
+public class PostgresJamesServerMain implements JamesServerMain {
+
+    private static final Module WEBADMIN = Modules.combine(
+        new WebAdminServerModule(),
+        new DataRoutesModules(),
+        new InconsistencyQuotasSolvingRoutesModule(),
+        new MailboxRoutesModule(),
+        new MailQueueRoutesModule(),
+        new MailRepositoriesRoutesModule(),
+        new ReIndexingModule(),
+        new SieveRoutesModule(),
+        new WebAdminReIndexingTaskSerializationModule());
+
+    private static final Module PROTOCOLS = Modules.combine(
+        new IMAPServerModule(),
+        new LMTPServerModule(),
+        new ManageSieveServerModule(),
+        new POP3ServerModule(),
+        new ProtocolHandlerModule(),
+        new SMTPServerModule(),
+        WEBADMIN);
+
+    private static final Module JPA_SERVER_MODULE = Modules.combine(
+        new ActiveMQQueueModule(),
+        new NaiveDelegationStoreModule(),
+        new DefaultProcessorsConfigurationProviderModule(),
+        new JPADataModule(),
+        new JPAMailboxModule(),
+        new MailboxModule(),
+        new LuceneSearchMailboxModule(),
+        new NoJwtModule(),
+        new RawPostDequeueDecoratorModule(),
+        new SieveJPARepositoryModules(),
+        new DefaultEventModule(),
+        new TaskManagerModule(),
+        new MemoryDeadLetterModule());
+
+    private static final Module JPA_MODULE_AGGREGATE = Modules.combine(
+        new MailetProcessingModule(), JPA_SERVER_MODULE, PROTOCOLS);
+
+    public static void main(String[] args) throws Exception {
+        ExtraProperties.initialize();
+
+        PostgresJamesConfiguration configuration = PostgresJamesConfiguration.builder()
+            .useWorkingDirectoryEnvProperty()
+            .build();
+
+        LOGGER.info("Loading configuration {}", configuration.toString());
+        GuiceJamesServer server = createServer(configuration)
+            .combineWith(new JMXServerModule())
+            .overrideWith(new RunArgumentsModule(args));
+
+        JamesServerMain.main(server);
+    }
+
+    static GuiceJamesServer createServer(PostgresJamesConfiguration configuration) {
+        return GuiceJamesServer.forConfiguration(configuration)
+            .combineWith(JPA_MODULE_AGGREGATE)
+            .combineWith(new UsersRepositoryModuleChooser(new JPAUsersRepositoryModule())
+                .chooseModules(configuration.getUsersRepositoryImplementation()));
+    }
+}
