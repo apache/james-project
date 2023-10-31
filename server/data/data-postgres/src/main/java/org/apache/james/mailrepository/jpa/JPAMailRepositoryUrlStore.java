d0b2468266d14f926722323eb828693c01e1094c
Tung Tran <vttran@linagora.com>
2023-10-31 10:06:42 +0700

    JAMES-2586 - Postgres - Init james-server-data-postgres

---
+++ server/data/data-postgres/src/main/java/org/apache/james/mailrepository/jpa/JPAMailRepositoryUrlStore.java
@@ -0,0 +1,65 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailrepository.jpa;
+
+import java.util.stream.Stream;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.james.backends.jpa.TransactionRunner;
+import org.apache.james.mailrepository.api.MailRepositoryUrl;
+import org.apache.james.mailrepository.api.MailRepositoryUrlStore;
+import org.apache.james.mailrepository.jpa.model.JPAUrl;
+
+public class JPAMailRepositoryUrlStore implements MailRepositoryUrlStore {
+    private final TransactionRunner transactionRunner;
+
+    @Inject
+    public JPAMailRepositoryUrlStore(EntityManagerFactory entityManagerFactory) {
+        this.transactionRunner = new TransactionRunner(entityManagerFactory);
+    }
+
+    @Override
+    public void add(MailRepositoryUrl url) {
+        transactionRunner.run(entityManager ->
+            entityManager.merge(JPAUrl.from(url)));
+    }
+
+    @Override
+    public Stream<MailRepositoryUrl> listDistinct() {
+        return transactionRunner.runAndRetrieveResult(entityManager ->
+            entityManager
+                .createNamedQuery("listUrls", JPAUrl.class)
+                .getResultList()
+                .stream()
+                .map(JPAUrl::toMailRepositoryUrl));
+    }
+
+    @Override
+    public boolean contains(MailRepositoryUrl url) {
+        return transactionRunner.runAndRetrieveResult(entityManager ->
+            ! entityManager.createNamedQuery("getUrl", JPAUrl.class)
+                .setParameter("value", url.asString())
+                .getResultList()
+                .isEmpty());
+    }
+}
+
---
+++ server/data/data-postgres/src/test/java/org/apache/james/rrt/jpa/JPAStepdefs.java
@@ -0,0 +1,60 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.rrt.jpa;
+
+import org.apache.james.backends.jpa.JpaTestCluster;
+import org.apache.james.rrt.jpa.model.JPARecipientRewrite;
+import org.apache.james.rrt.lib.AbstractRecipientRewriteTable;
+import org.apache.james.rrt.lib.RecipientRewriteTableFixture;
+import org.apache.james.rrt.lib.RewriteTablesStepdefs;
+import org.apache.james.user.jpa.JPAUsersRepository;
+
+import com.github.fge.lambdas.Throwing;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class JPAStepdefs {
+
+    private static final JpaTestCluster JPA_TEST_CLUSTER = JpaTestCluster.create(JPARecipientRewrite.class);
+
+    private final RewriteTablesStepdefs mainStepdefs;
+
+    public JPAStepdefs(RewriteTablesStepdefs mainStepdefs) {
+        this.mainStepdefs = mainStepdefs;
+    }
+
+    @Before
+    public void setup() throws Throwable {
+        mainStepdefs.setUp(Throwing.supplier(this::getRecipientRewriteTable).sneakyThrow());
+    }
+
+    @After
+    public void tearDown() {
+        JPA_TEST_CLUSTER.clear(JPARecipientRewrite.JAMES_RECIPIENT_REWRITE);
+    }
+
+    private AbstractRecipientRewriteTable getRecipientRewriteTable() throws Exception {
+        JPARecipientRewriteTable localVirtualUserTable = new JPARecipientRewriteTable();
+        localVirtualUserTable.setEntityManagerFactory(JPA_TEST_CLUSTER.getEntityManagerFactory());
+        localVirtualUserTable.setUsersRepository(new JPAUsersRepository(RecipientRewriteTableFixture.domainListForCucumberTests()));
+        localVirtualUserTable.setDomainList(RecipientRewriteTableFixture.domainListForCucumberTests());
+        return localVirtualUserTable;
+    }
+}
