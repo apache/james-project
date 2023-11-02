77ce168a38f07892fc4738ce6d23b0cfeb1cd774
vttran <vttran@linagora.com>
2023-11-02 10:00:11 +0700

    JAMES-2586 PostgresTableManager support create table when enable row level security

--- backends-common/postgres/src/main/java/org/apache/james/backends/postgres/utils/PostgresExecutor.java
+++ backends-common/postgres/src/main/java/org/apache/james/backends/postgres/utils/PostgresExecutor.java
@@ -44,4 +44,8 @@ public class PostgresExecutor {
     public Mono<DSLContext> dslContext() {
         return connection.map(con -> DSL.using(con, PGSQL_DIALECT, SETTINGS));
     }
+
+    public Mono<Connection> connection() {
+        return connection;
+    }
 }
