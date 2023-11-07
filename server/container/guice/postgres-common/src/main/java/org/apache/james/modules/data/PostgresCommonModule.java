16c062acb1886b4cf3a83651531580f25f23ddc0
Quan Tran <hqtran@linagora.com>
2023-11-07 14:33:30 +0700

    JAMES-2586 Guice binding for PostgresTableManager
    tested manually the binding with the subscription module -> create subscription table upon James startup successfully.

--- server/container/guice/postgres-common/src/main/java/org/apache/james/modules/data/PostgresCommonModule.java
+++ server/container/guice/postgres-common/src/main/java/org/apache/james/modules/data/PostgresCommonModule.java
@@ -19,18 +19,25 @@
 package org.apache.james.modules.data;
 
 import java.io.FileNotFoundException;
+import java.util.Set;
 
 import javax.inject.Singleton;
 
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.james.backends.postgres.PostgresConfiguration;
+import org.apache.james.backends.postgres.PostgresModule;
+import org.apache.james.backends.postgres.PostgresTableManager;
 import org.apache.james.backends.postgres.utils.JamesPostgresConnectionFactory;
 import org.apache.james.backends.postgres.utils.SimpleJamesPostgresConnectionFactory;
+import org.apache.james.utils.InitializationOperation;
+import org.apache.james.utils.InitilizationOperationBuilder;
 import org.apache.james.utils.PropertiesProvider;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
+import com.google.inject.multibindings.Multibinder;
+import com.google.inject.multibindings.ProvidesIntoSet;
 
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
@@ -40,6 +47,8 @@ public class PostgresCommonModule extends AbstractModule {
     @Override
     public void configure() {
         bind(JamesPostgresConnectionFactory.class).to(SimpleJamesPostgresConnectionFactory.class);
+
+        Multibinder.newSetBinder(binder(), PostgresModule.class);
 
         bind(SimpleJamesPostgresConnectionFactory.class).in(Scopes.SINGLETON);
     }
@@ -61,5 +70,27 @@ public class PostgresCommonModule extends AbstractModule {
             .database(postgresConfiguration.getDatabaseName())
             .schema(postgresConfiguration.getDatabaseSchema())
             .build());
+    }
+
+    @Provides
+    @Singleton
+    PostgresModule composePostgresDataDefinitions(Set<PostgresModule> modules) {
+        return PostgresModule.aggregateModules(modules);
+    }
+
+    @Provides
+    @Singleton
+    PostgresTableManager postgresTableManager(JamesPostgresConnectionFactory jamesPostgresConnectionFactory,
+                                              PostgresModule postgresModule) {
+        return new PostgresTableManager(jamesPostgresConnectionFactory, postgresModule);
+    }
+
+    @ProvidesIntoSet
+    InitializationOperation provisionPostgresTablesAndIndexes(PostgresTableManager postgresTableManager) {
+        return InitilizationOperationBuilder
+            .forClass(PostgresTableManager.class)
+            .init(() -> postgresTableManager.initializeTables()
+                .then(postgresTableManager.initializeTableIndexes())
+                .block());
     }
 }
