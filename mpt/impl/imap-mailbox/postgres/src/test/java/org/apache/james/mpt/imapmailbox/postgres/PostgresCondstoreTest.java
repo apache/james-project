7633acabf5c4e49ce767eb469b8b2751e24bedc4
Tung Tran <vttran@linagora.com>
2023-11-10 15:48:09 +0700

    JAMES-2586 mpt-imapmailbox-postgres: bindings and setup in PGHostSystem
    - Adapt PostgresSubscriptionMapper

--- mpt/impl/imap-mailbox/postgres/src/test/java/org/apache/james/mpt/imapmailbox/postgres/PostgresCondstoreTest.java
+++ mpt/impl/imap-mailbox/postgres/src/test/java/org/apache/james/mpt/imapmailbox/postgres/PostgresCondstoreTest.java
@@ -26,7 +26,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 public class PostgresCondstoreTest extends Condstore {
     @RegisterExtension
-    public PostgresHostSystemExtension hostSystemExtension = new PostgresHostSystemExtension();
+    public static PostgresHostSystemExtension hostSystemExtension = new PostgresHostSystemExtension();
 
     @Override
     protected JamesImapHostSystem createJamesImapHostSystem() {
