2447d7285ba54559c05c30c39fb06c5b32ee688f
Tung Tran <vttran@linagora.com>
2023-11-13 17:42:41 +0700

    JAMES-2586 Introduce Single postgres connection factory when disable row level security

---
+++ backends-common/postgres/src/main/java/org/apache/james/backends/postgres/utils/SinglePostgresConnectionFactory.java
@@ -0,0 +1,40 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.backends.postgres.utils;
+
+import java.util.Optional;
+
+import org.apache.james.core.Domain;
+
+import io.r2dbc.spi.Connection;
+import reactor.core.publisher.Mono;
+
+public class SinglePostgresConnectionFactory implements JamesPostgresConnectionFactory {
+    private final Connection connection;
+
+    public SinglePostgresConnectionFactory(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public Mono<Connection> getConnection(Optional<Domain> domain) {
+        return Mono.just(connection);
+    }
+}
