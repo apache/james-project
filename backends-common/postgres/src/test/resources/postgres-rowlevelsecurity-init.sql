d8dec2f997594d002725cb071f13edae470e3bbd
Tung Tran <vttran@linagora.com>
2023-11-13 13:17:23 +0700

    fixup! JAMES-2586 Fix row-level security implementation

--- backends-common/postgres/src/test/resources/postgres-rowlevelsecurity-init.sql
+++
@@ -1,5 +0,0 @@
-create user rlsuser WITH PASSWORD 'secret1';
-create database rlsdb;
-grant all privileges on database rlsdb to rlsuser;
-\c rlsdb;
-create schema if not exists rlsschema authorization rlsuser;
