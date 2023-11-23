c08d62e30297e44961154e282ae6f149d7e935fb
hungphan227 <45198168+hungphan227@users.noreply.github.com>
2023-11-23 11:02:46 +0700

    JAMES-2586 postgres users dao and repository (#1803)

--- server/container/guice/postgres-common/src/main/java/org/apache/james/modules/data/PostgresDataModule.java
+++ server/container/guice/postgres-common/src/main/java/org/apache/james/modules/data/PostgresDataModule.java
@@ -16,29 +16,19 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
+
 package org.apache.james.modules.data;
 
-import org.apache.james.server.core.configuration.ConfigurationProvider;
-import org.apache.james.user.api.UsersRepository;
-import org.apache.james.user.jpa.JPAUsersRepository;
-import org.apache.james.utils.InitializationOperation;
-import org.apache.james.utils.InitilizationOperationBuilder;
+import org.apache.james.CoreDataModule;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Scopes;
-import com.google.inject.multibindings.ProvidesIntoSet;
 
-public class JPAUsersRepositoryModule extends AbstractModule {
+public class PostgresDataModule extends AbstractModule {
     @Override
-    public void configure() {
-        bind(JPAUsersRepository.class).in(Scopes.SINGLETON);
-        bind(UsersRepository.class).to(JPAUsersRepository.class);
-    }
-
-    @ProvidesIntoSet
-    InitializationOperation configureJpaUsers(ConfigurationProvider configurationProvider, JPAUsersRepository usersRepository) {
-        return InitilizationOperationBuilder
-            .forClass(JPAUsersRepository.class)
-            .init(() -> usersRepository.configure(configurationProvider.getConfiguration("usersrepository")));
+    protected void configure() {
+        install(new CoreDataModule());
+        install(new JPADomainListModule());
+        install(new JPARecipientRewriteTableModule());
+        install(new JPAMailRepositoryModule());
     }
 }
