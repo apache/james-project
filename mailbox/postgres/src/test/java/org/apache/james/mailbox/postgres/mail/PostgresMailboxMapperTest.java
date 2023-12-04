e446df36e7f2640ecfaacbc7ae288aa8c30a02b2
Tung Tran <vttran@linagora.com>
2023-12-04 16:06:17 +0700

    JAMES-2586 [PGSQL] Improve PostresMessageManager::getMetadata method

--- mailbox/postgres/src/test/java/org/apache/james/mailbox/postgres/mail/PostgresMailboxMapperTest.java
+++ mailbox/postgres/src/test/java/org/apache/james/mailbox/postgres/mail/PostgresMailboxMapperTest.java
@@ -19,12 +19,22 @@
 
 package org.apache.james.mailbox.postgres.mail;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.apache.james.backends.postgres.PostgresExtension;
+import org.apache.james.core.Username;
+import org.apache.james.mailbox.MailboxSessionUtil;
+import org.apache.james.mailbox.MessageUid;
+import org.apache.james.mailbox.ModSeq;
+import org.apache.james.mailbox.model.Mailbox;
 import org.apache.james.mailbox.model.MailboxId;
+import org.apache.james.mailbox.model.MailboxPath;
+import org.apache.james.mailbox.model.UidValidity;
 import org.apache.james.mailbox.postgres.PostgresMailboxId;
 import org.apache.james.mailbox.postgres.mail.dao.PostgresMailboxDAO;
 import org.apache.james.mailbox.store.mail.MailboxMapper;
 import org.apache.james.mailbox.store.mail.model.MailboxMapperTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 public class PostgresMailboxMapperTest extends MailboxMapperTest {
@@ -40,4 +50,36 @@ public class PostgresMailboxMapperTest extends MailboxMapperTest {
     protected MailboxId generateId() {
         return PostgresMailboxId.generate();
     }
+
+    @Test
+    void retrieveMailboxShouldReturnCorrectHighestModSeqAndLastUidWhenDefault() {
+        Mailbox mailbox = mailboxMapper.create(benwaInboxPath, UidValidity.of(43)).block();
+
+        PostgresMailbox metaData = (PostgresMailbox) mailboxMapper.findMailboxById(mailbox.getMailboxId()).block();
+
+        assertThat(metaData.getHighestModSeq()).isEqualTo(ModSeq.first());
+        assertThat(metaData.getLastUid()).isEqualTo(null);
+    }
+
+    @Test
+    void retrieveMailboxShouldReturnCorrectHighestModSeqAndLastUid() {
+        Username BENWA = Username.of("benwa");
+        MailboxPath benwaInboxPath = MailboxPath.forUser(BENWA, "INBOX");
+
+        Mailbox mailbox = mailboxMapper.create(benwaInboxPath, UidValidity.of(43)).block();
+
+        // increase modSeq
+        ModSeq nextModSeq = new PostgresModSeqProvider.Factory(postgresExtension.getExecutorFactory()).create(MailboxSessionUtil.create(BENWA))
+            .nextModSeqReactive(mailbox.getMailboxId()).block();
+
+        // increase lastUid
+        MessageUid nextUid = new PostgresUidProvider.Factory(postgresExtension.getExecutorFactory()).create(MailboxSessionUtil.create(BENWA))
+            .nextUidReactive(mailbox.getMailboxId()).block();
+
+        PostgresMailbox metaData = (PostgresMailbox) mailboxMapper.findMailboxById(mailbox.getMailboxId()).block();
+
+        assertThat(metaData.getHighestModSeq()).isEqualTo(nextModSeq);
+        assertThat(metaData.getLastUid()).isEqualTo(nextUid);
+    }
+
 }
