ccf4fa9b6fe60865904ffedcfb40f4cd76df775c
Benoit TELLIER <btellier@linagora.com>
2023-12-05 16:01:48 +0100

    JAMES-2586 Remove unused method in MessageManager

--- mailbox/store/src/main/java/org/apache/james/mailbox/store/StoreMessageManager.java
+++ mailbox/store/src/main/java/org/apache/james/mailbox/store/StoreMessageManager.java
@@ -996,9 +996,4 @@ public class StoreMessageManager implements MessageManager {
         return messageMapper.execute(
             () -> messageMapper.listAllMessageUids(mailbox));
     }
-
-    @Override
-    public EnumSet<MessageCapabilities> getSupportedMessageCapabilities() {
-        return messageCapabilities;
-    }
 }
