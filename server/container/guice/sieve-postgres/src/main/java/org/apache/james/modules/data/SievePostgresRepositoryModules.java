2695a9c8ec386c6827fa85d6300969dca3635d3b
Quan Tran <hqtran@linagora.com>
2023-12-12 16:07:14 +0700

    JAMES-2586 Guice binding for PostgresSieveScriptDAO

--- server/container/guice/sieve-postgres/src/main/java/org/apache/james/modules/data/SievePostgresRepositoryModules.java
+++ server/container/guice/sieve-postgres/src/main/java/org/apache/james/modules/data/SievePostgresRepositoryModules.java
@@ -19,18 +19,27 @@
 
 package org.apache.james.modules.data;
 
+import org.apache.james.backends.postgres.PostgresModule;
+import org.apache.james.sieve.postgres.PostgresSieveModule;
+import org.apache.james.sieve.postgres.PostgresSieveQuotaDAO;
 import org.apache.james.sieve.postgres.PostgresSieveRepository;
+import org.apache.james.sieve.postgres.PostgresSieveScriptDAO;
 import org.apache.james.sieverepository.api.SieveQuotaRepository;
 import org.apache.james.sieverepository.api.SieveRepository;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
+import com.google.inject.multibindings.Multibinder;
 
 public class SievePostgresRepositoryModules extends AbstractModule {
     @Override
     protected void configure() {
-        bind(PostgresSieveRepository.class).in(Scopes.SINGLETON);
+        Multibinder.newSetBinder(binder(), PostgresModule.class).addBinding().toInstance(PostgresSieveModule.MODULE);
 
+        bind(PostgresSieveQuotaDAO.class).in(Scopes.SINGLETON);
+        bind(PostgresSieveScriptDAO.class).in(Scopes.SINGLETON);
+
+        bind(PostgresSieveRepository.class).in(Scopes.SINGLETON);
         bind(SieveRepository.class).to(PostgresSieveRepository.class);
         bind(SieveQuotaRepository.class).to(PostgresSieveRepository.class);
     }
