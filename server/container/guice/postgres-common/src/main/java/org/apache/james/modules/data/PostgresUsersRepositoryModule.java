7bc5690abc0e7755b88a624ee3bf9f74804dab11
hungphan227 <45198168+hungphan227@users.noreply.github.com>
2023-12-13 16:24:11 +0700

    JAMES-2586 PostgresDelegationStore (#1851)

--- server/container/guice/postgres-common/src/main/java/org/apache/james/modules/data/PostgresUsersRepositoryModule.java
+++ server/container/guice/postgres-common/src/main/java/org/apache/james/modules/data/PostgresUsersRepositoryModule.java
@@ -19,23 +19,14 @@
 
 package org.apache.james.modules.data;
 
-import org.apache.commons.configuration2.ex.ConfigurationException;
-import org.apache.james.backends.postgres.PostgresModule;
 import org.apache.james.server.core.configuration.ConfigurationProvider;
 import org.apache.james.user.api.UsersRepository;
-import org.apache.james.user.lib.UsersDAO;
-import org.apache.james.user.postgres.PostgresUserModule;
-import org.apache.james.user.postgres.PostgresUsersDAO;
 import org.apache.james.user.postgres.PostgresUsersRepository;
-import org.apache.james.user.postgres.PostgresUsersRepositoryConfiguration;
 import org.apache.james.utils.InitializationOperation;
 import org.apache.james.utils.InitilizationOperationBuilder;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.google.inject.Scopes;
-import com.google.inject.Singleton;
-import com.google.inject.multibindings.Multibinder;
 import com.google.inject.multibindings.ProvidesIntoSet;
 
 public class PostgresUsersRepositoryModule extends AbstractModule {
@@ -43,19 +34,6 @@ public class PostgresUsersRepositoryModule extends AbstractModule {
     public void configure() {
         bind(PostgresUsersRepository.class).in(Scopes.SINGLETON);
         bind(UsersRepository.class).to(PostgresUsersRepository.class);
-
-        bind(PostgresUsersDAO.class).in(Scopes.SINGLETON);
-        bind(UsersDAO.class).to(PostgresUsersDAO.class);
-
-        Multibinder<PostgresModule> postgresDataDefinitions = Multibinder.newSetBinder(binder(), PostgresModule.class);
-        postgresDataDefinitions.addBinding().toInstance(PostgresUserModule.MODULE);
-    }
-
-    @Provides
-    @Singleton
-    public PostgresUsersRepositoryConfiguration provideConfiguration(ConfigurationProvider configurationProvider) throws ConfigurationException {
-        return PostgresUsersRepositoryConfiguration.from(
-            configurationProvider.getConfiguration("usersrepository"));
     }
 
     @ProvidesIntoSet
