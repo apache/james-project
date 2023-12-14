57817993f98f9465d384b79a90e6f7ef688da1f1
Benoit TELLIER <btellier@linagora.com>
2023-12-14 10:50:23 +0100

    [ENHANCEMENT] More tests for ReactiveThrottler

--- server/protocols/protocols-imap4/src/test/java/org/apache/james/imapserver/netty/ReactiveThrottlerTest.java
+++ server/protocols/protocols-imap4/src/test/java/org/apache/james/imapserver/netty/ReactiveThrottlerTest.java
@@ -29,9 +29,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.james.imap.api.ImapMessage;
 import org.apache.james.metrics.api.NoopGaugeRegistry;
+import org.apache.james.util.concurrency.ConcurrentTestRunner;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.shaded.org.awaitility.Awaitility;
 
+import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 
 class ReactiveThrottlerTest {
@@ -114,6 +117,82 @@ class ReactiveThrottlerTest {
             .isInstanceOf(ReactiveThrottler.RejectedException.class);
         // And the task is not executed
         assertThat(executed.get()).isFalse();
+    }
+    @Test
+    void throttleShouldRecoverFromABurst() throws Exception {
+        // Given a throttler
+        ReactiveThrottler testee = new ReactiveThrottler(new NoopGaugeRegistry(), 2, 2);
+
+        // When I submit too many tasks task
+        AtomicBoolean executed = new AtomicBoolean(false);
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+
+        Thread.sleep(500);
+        Mono.from(testee.throttle(Mono.fromRunnable(() -> executed.getAndSet(true)), NO_IMAP_MESSAGE)).block();
+        // And the task is not executed
+        assertThat(executed.get()).isTrue();
+    }
+    @Test
+    void throttleShouldHandleDisposal() throws Exception {
+        // Given a throttler
+        ReactiveThrottler testee = new ReactiveThrottler(new NoopGaugeRegistry(), 2, 2);
+
+        // When I submit too many tasks task
+        AtomicBoolean executed = new AtomicBoolean(false);
+        Disposable subscribe1 = Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Disposable subscribe2 = Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Disposable subscribe3 = Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Disposable subscribe4 = Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Disposable subscribe5 = Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Disposable subscribe6 = Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Disposable subscribe7 = Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        Disposable subscribe8 = Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE)).subscribe();
+        subscribe1.dispose();
+        subscribe2.dispose();
+        subscribe3.dispose();
+        subscribe4.dispose();
+        subscribe5.dispose();
+        subscribe6.dispose();
+        subscribe7.dispose();
+        subscribe8.dispose();
+
+        Thread.sleep(100);
+
+        Mono.from(testee.throttle(Mono.fromRunnable(() -> executed.getAndSet(true)), NO_IMAP_MESSAGE)).block();
+        // And the task is not executed
+        assertThat(executed.get()).isTrue();
+    }
+    @RepeatedTest(10)
+    void throttleShouldBeConcurrentFriendly() throws Exception {
+        // Given a throttler
+        ReactiveThrottler testee = new ReactiveThrottler(new NoopGaugeRegistry(), 2, 2);
+
+        ConcurrentTestRunner.builder()
+            .operation((a, b) -> Mono.from(testee.throttle(Mono.delay(Duration.ofMillis(50)).then(), NO_IMAP_MESSAGE))
+                .onErrorResume(ReactiveThrottler.RejectedException.class, e -> Mono.empty())
+                .block())
+            .threadCount(20)
+            .operationCount(5)
+            .runSuccessfullyWithin(Duration.ofSeconds(10));
+
+        Thread.sleep(100);
+
+        AtomicBoolean executed = new AtomicBoolean(false);
+        Mono.from(testee.throttle(Mono.fromRunnable(() -> executed.getAndSet(true)), NO_IMAP_MESSAGE)).block();
+        // And the task is not executed
+        assertThat(executed.get()).isTrue();
     }
 
     @Test
