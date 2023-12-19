23b3efa252812397d5ce014632adf62a12714c2f
Benoit TELLIER <btellier@linagora.com>
2023-12-19 15:59:28 +0100

    JAMES-2586 Add an Id for SieveScript (#1863)
    While not needed now this extra field would
    significantly ease a future JMAP Sieve
    implementation and avoid a migration.

---
+++ server/data/data-postgres/src/main/java/org/apache/james/sieve/postgres/model/PostgresSieveScriptId.java
@@ -0,0 +1,38 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.sieve.postgres.model;
+
+import java.util.UUID;
+
+public class PostgresSieveScriptId {
+    public static PostgresSieveScriptId generate() {
+        return new PostgresSieveScriptId(UUID.randomUUID());
+    }
+
+    private final UUID value;
+
+    public PostgresSieveScriptId(UUID value) {
+        this.value = value;
+    }
+
+    public UUID getValue() {
+        return value;
+    }
+}
