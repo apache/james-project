61f31ed10b454760ffc478c9a075231e60d7bf31
Quan Tran <hqtran@linagora.com>
2024-01-08 10:27:48 +0700

    JAMES-2586 Plug DeletedMessageVaultDeletionCallback into DeleteMessageListener
    missing attachment metadata though -> need to do it in https://github.com/linagora/james-project/issues/5011

--- server/container/guice/mailbox-postgres/src/main/java/org/apache/james/modules/mailbox/PostgresMailboxModule.java
+++ server/container/guice/mailbox-postgres/src/main/java/org/apache/james/modules/mailbox/PostgresMailboxModule.java
@@ -129,6 +129,7 @@ public class PostgresMailboxModule extends AbstractModule {
 
         Multibinder.newSetBinder(binder(), EventListener.ReactiveGroupEventListener.class)
             .addBinding().to(DeleteMessageListener.class);
+        Multibinder.newSetBinder(binder(), DeleteMessageListener.DeletionCallback.class);
 
         bind(MailboxManager.class).annotatedWith(Names.named(MAILBOXMANAGER_NAME)).to(MailboxManager.class);
         bind(MailboxManagerConfiguration.class).toInstance(MailboxManagerConfiguration.DEFAULT);
