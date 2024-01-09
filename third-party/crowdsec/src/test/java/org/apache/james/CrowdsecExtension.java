a444f47c9502466105cd0a88fe667ded5ab78e03
Quan Tran <hqtran@linagora.com>
2024-01-09 16:14:40 +0700

    JAMES-3897 CrowdSec integration test for SMTP

--- third-party/crowdsec/src/test/java/org/apache/james/CrowdsecExtension.java
+++ third-party/crowdsec/src/test/java/org/apache/james/CrowdsecExtension.java
@@ -19,21 +19,32 @@
 
 package org.apache.james;
 
+import static org.apache.james.model.CrowdsecClientConfiguration.DEFAULT_API_KEY;
+
 import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.UUID;
 
+import javax.inject.Singleton;
+
+import org.apache.james.model.CrowdsecClientConfiguration;
+import org.apache.james.model.CrowdsecHttpClient;
 import org.apache.james.util.docker.RateLimiters;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy;
+import org.testcontainers.utility.MountableFile;
 
-import com.google.common.collect.ImmutableList;
+import com.github.fge.lambdas.Throwing;
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.Provides;
 
 public class CrowdsecExtension implements GuiceModuleTestExtension {
     public static final Duration STARTUP_TIMEOUT = Duration.ofMinutes(5);
     public static final int CROWDSEC_PORT = 8080;
-    public static final int EXPOSED_PORT = 8082;
     public static final String CROWDSEC_IMAGE = "crowdsecurity/crowdsec:v1.5.4";
 
     private final GenericContainer<?> crowdsecContainer;
@@ -43,24 +54,72 @@ public class CrowdsecExtension implements GuiceModuleTestExtension {
             .withCreateContainerCmdModifier(cmd -> cmd.withName("james-crowdsec-test-" + UUID.randomUUID()))
             .withExposedPorts(CROWDSEC_PORT)
             .withStartupTimeout(STARTUP_TIMEOUT)
+            .withCopyFileToContainer(MountableFile.forClasspathResource("crowdsec/acquis.yaml"), "/etc/crowdsec/")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("crowdsec/parsers/syslog-logs.yaml"), "/etc/crowdsec/parsers/s00-raw/")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("crowdsec/parsers/james-auth.yaml"), "/etc/crowdsec/parsers/s01-parse/")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("crowdsec/scenarios/james-bf-auth.yaml"), "/etc/crowdsec/scenarios/")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("crowdsec/scenarios/james-dictionary-attack.yaml"), "/etc/crowdsec/scenarios/")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("crowdsec/collections/james.yaml"), "/etc/crowdsec/collections/")
+            .withFileSystemBind("src/test/resources/log", "/var/log", BindMode.READ_WRITE)
             .waitingFor(new HostPortWaitStrategy().withRateLimiter(RateLimiters.TWENTIES_PER_SECOND));
     }
 
     @Override
-    public void beforeAll(ExtensionContext context) {
+    public void beforeAll(ExtensionContext context) throws Exception {
         if (!crowdsecContainer.isRunning()) {
             crowdsecContainer.start();
         }
+
+        setApiKey();
+    }
+
+    private void setApiKey() throws IOException, InterruptedException {
+        crowdsecContainer.execInContainer("cscli", "bouncer", "add", "bouncer", "-k", DEFAULT_API_KEY);
     }
 
     @Override
     public void afterEach(ExtensionContext extensionContext) throws IOException, InterruptedException {
+        resetCrowdSecBanDecisions();
+        resetJamesLogFile();
+    }
+
+    private void resetCrowdSecBanDecisions() throws IOException, InterruptedException {
         crowdsecContainer.execInContainer("cscli", "decision", "delete", "--all");
+    }
+
+    private void resetJamesLogFile() throws IOException, InterruptedException {
+        crowdsecContainer.execInContainer("truncate", "-s", "0", "/var/log/james.log");
     }
 
     @Override
     public void afterAll(ExtensionContext extensionContext) {
 
+    }
+
+    @Override
+    public Module getModule() {
+        URL crowdSecUrl = getCrowdSecUrl();
+
+        return new AbstractModule() {
+            @Provides
+            @Singleton
+            public CrowdsecClientConfiguration crowdsecClientConfiguration() {
+                return new CrowdsecClientConfiguration(crowdSecUrl, DEFAULT_API_KEY);
+            }
+
+            @Provides
+            @Singleton
+            public CrowdsecHttpClient crowdsecHttpClient(CrowdsecClientConfiguration crowdsecClientConfiguration) {
+                return new CrowdsecHttpClient(crowdsecClientConfiguration);
+            }
+        };
+    }
+
+    public URL getCrowdSecUrl() {
+        return Throwing.supplier(() -> new URL("http",
+            crowdsecContainer.getHost(),
+            crowdsecContainer.getMappedPort(CROWDSEC_PORT),
+            "/v1")).get();
     }
 
     public GenericContainer<?> getCrowdsecContainer() {
