b19a3ed443a6ef2a7c4b7d6fcc44307db12eb618
hung phan <hphan@linagora.com>
2024-01-12 17:48:02 +0700

    JAMES-2586 add mailbox para for generateMessageUid method in MapperProvider

--- mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/mail/CassandraMapperProvider.java
+++ mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/mail/CassandraMapperProvider.java
@@ -40,6 +40,7 @@ import org.apache.james.mailbox.store.mail.AttachmentMapper;
 import org.apache.james.mailbox.store.mail.MailboxMapper;
 import org.apache.james.mailbox.store.mail.MessageIdMapper;
 import org.apache.james.mailbox.store.mail.MessageMapper;
+import org.apache.james.mailbox.store.mail.UidProvider;
 import org.apache.james.mailbox.store.mail.model.MapperProvider;
 import org.apache.james.mailbox.store.mail.model.MessageUidProvider;
 import org.apache.james.utils.UpdatableTickingClock;
@@ -51,7 +52,7 @@ public class CassandraMapperProvider implements MapperProvider {
     private static final Factory MESSAGE_ID_FACTORY = new CassandraMessageId.Factory();
 
     private final CassandraCluster cassandra;
-    private final MessageUidProvider messageUidProvider;
+    private final UidProvider messageUidProvider;
     private final CassandraModSeqProvider cassandraModSeqProvider;
     private final UpdatableTickingClock updatableTickingClock;
     private final MailboxSession mailboxSession = MailboxSessionUtil.create(Username.of("benwa"));
@@ -60,7 +61,7 @@ public class CassandraMapperProvider implements MapperProvider {
     public CassandraMapperProvider(CassandraCluster cassandra,
                                    CassandraConfiguration cassandraConfiguration) {
         this.cassandra = cassandra;
-        messageUidProvider = new MessageUidProvider();
+        messageUidProvider = new CassandraUidProvider(this.cassandra.getConf(), cassandraConfiguration);
         cassandraModSeqProvider = new CassandraModSeqProvider(
                 this.cassandra.getConf(),
                 cassandraConfiguration);
@@ -116,8 +117,12 @@ public class CassandraMapperProvider implements MapperProvider {
     }
 
     @Override
-    public MessageUid generateMessageUid() {
-        return messageUidProvider.next();
+    public MessageUid generateMessageUid(Mailbox mailbox) {
+        try {
+            return messageUidProvider.nextUid(mailbox);
+        } catch (MailboxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
