f79e4aca14cf627c3b635d844c30c85ec72b5da4
Thanh Bui <vtbui@linagora.com>
2024-01-14 23:43:46 +0700

    JAMES 3897: Add connectionCheckFactory bean into imapserver-context.xml

---
+++ protocols/imap/src/main/java/org/apache/james/imap/api/DefaultConnectionCheckFactory.java
@@ -0,0 +1,35 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.imap.api;
+
+import java.util.Set;
+
+import org.apache.commons.configuration2.HierarchicalConfiguration;
+import org.apache.commons.configuration2.tree.ImmutableNode;
+
+import com.google.common.collect.ImmutableSet;
+
+public class DefaultConnectionCheckFactory implements ConnectionCheckFactory {
+
+    @Override
+    public Set<ConnectionCheck> create(HierarchicalConfiguration<ImmutableNode> config) {
+        return ImmutableSet.of();
+    }
+}
