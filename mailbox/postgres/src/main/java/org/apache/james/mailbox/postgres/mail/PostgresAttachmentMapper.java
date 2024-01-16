eff3af8873c1fd685fb76d64ec205e467b63bff1
Tung Tran <vttran@linagora.com>
2024-01-16 11:40:24 +0700

    JAMES-2586 Implement PostgresAttachmentMapper, DAO and binding

---
+++ mailbox/postgres/src/main/java/org/apache/james/mailbox/postgres/mail/PostgresAttachmentMapper.java
@@ -0,0 +1,124 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailbox.postgres.mail;
+
+import static org.apache.james.blob.api.BlobStore.StoragePolicy.LOW_COST;
+import static org.apache.james.util.ReactorUtils.DEFAULT_CONCURRENCY;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.james.blob.api.BlobStore;
+import org.apache.james.mailbox.exception.AttachmentNotFoundException;
+import org.apache.james.mailbox.model.AttachmentId;
+import org.apache.james.mailbox.model.AttachmentMetadata;
+import org.apache.james.mailbox.model.MessageAttachmentMetadata;
+import org.apache.james.mailbox.model.MessageId;
+import org.apache.james.mailbox.model.ParsedAttachment;
+import org.apache.james.mailbox.postgres.mail.dao.PostgresAttachmentDAO;
+import org.apache.james.mailbox.store.mail.AttachmentMapper;
+
+import com.github.fge.lambdas.Throwing;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class PostgresAttachmentMapper implements AttachmentMapper {
+
+    private final PostgresAttachmentDAO postgresAttachmentDAO;
+    private final BlobStore blobStore;
+
+    public PostgresAttachmentMapper(PostgresAttachmentDAO postgresAttachmentDAO, BlobStore blobStore) {
+        this.postgresAttachmentDAO = postgresAttachmentDAO;
+        this.blobStore = blobStore;
+    }
+
+    @Override
+    public InputStream loadAttachmentContent(AttachmentId attachmentId) {
+        return loadAttachmentContentReactive(attachmentId)
+            .block();
+    }
+
+    @Override
+    public Mono<InputStream> loadAttachmentContentReactive(AttachmentId attachmentId) {
+        return postgresAttachmentDAO.getAttachment(attachmentId)
+            .flatMap(pair -> Mono.from(blobStore.readReactive(blobStore.getDefaultBucketName(), pair.getRight(), LOW_COST)))
+            .switchIfEmpty(Mono.error(() -> new AttachmentNotFoundException(attachmentId.toString())));
+    }
+
+    @Override
+    public AttachmentMetadata getAttachment(AttachmentId attachmentId) throws AttachmentNotFoundException {
+        Preconditions.checkArgument(attachmentId != null);
+        return postgresAttachmentDAO.getAttachment(attachmentId)
+            .map(Pair::getLeft)
+            .blockOptional()
+            .orElseThrow(() -> new AttachmentNotFoundException(attachmentId.getId()));
+    }
+
+    @Override
+    public Mono<AttachmentMetadata> getAttachmentReactive(AttachmentId attachmentId) {
+        Preconditions.checkArgument(attachmentId != null);
+        return postgresAttachmentDAO.getAttachment(attachmentId)
+            .map(Pair::getLeft)
+            .switchIfEmpty(Mono.error(() -> new AttachmentNotFoundException(attachmentId.getId())));
+    }
+
+    @Override
+    public List<AttachmentMetadata> getAttachments(Collection<AttachmentId> attachmentIds) {
+        Preconditions.checkArgument(attachmentIds != null);
+        return Flux.fromIterable(attachmentIds)
+            .flatMap(id -> postgresAttachmentDAO.getAttachment(id)
+                .map(Pair::getLeft), DEFAULT_CONCURRENCY)
+            .collect(ImmutableList.toImmutableList())
+            .block();
+    }
+
+    @Override
+    public List<MessageAttachmentMetadata> storeAttachments(Collection<ParsedAttachment> attachments, MessageId ownerMessageId) {
+        return storeAttachmentsReactive(attachments, ownerMessageId)
+            .block();
+    }
+
+    @Override
+    public Mono<List<MessageAttachmentMetadata>> storeAttachmentsReactive(Collection<ParsedAttachment> attachments, MessageId ownerMessageId) {
+        return Flux.fromIterable(attachments)
+            .concatMap(attachment -> storeAttachmentAsync(attachment, ownerMessageId))
+            .collectList();
+    }
+
+    private Mono<MessageAttachmentMetadata> storeAttachmentAsync(ParsedAttachment parsedAttachment, MessageId ownerMessageId) {
+        return Mono.fromCallable(parsedAttachment::getContent)
+            .flatMap(content -> Mono.from(blobStore.save(blobStore.getDefaultBucketName(), parsedAttachment.getContent(), BlobStore.StoragePolicy.LOW_COST))
+                .flatMap(blobId -> {
+                    AttachmentId attachmentId = AttachmentId.random();
+                    return postgresAttachmentDAO.storeAttachment(AttachmentMetadata.builder()
+                            .attachmentId(attachmentId)
+                            .type(parsedAttachment.getContentType())
+                            .size(Throwing.supplier(content::size).get())
+                            .messageId(ownerMessageId)
+                            .build(), blobId)
+                        .thenReturn(Throwing.supplier(() -> parsedAttachment.asMessageAttachment(attachmentId, ownerMessageId)).get());
+                }));
+    }
+}
