57fddebdd599d3b9cb3eba353f26675503374d4b
Benoit TELLIER <btellier@linagora.com>
2024-01-26 16:54:28 +0100

    Revert "[UPDATE] Update spark version: Jetty update"
    This reverts commit 69e45e4ddf164cff38764bcc4860b47b1274abb1.

--- server/protocols/webadmin/webadmin-mailrepository/src/main/java/org/apache/james/webadmin/routes/MailRepositoriesRoutes.java
+++ server/protocols/webadmin/webadmin-mailrepository/src/main/java/org/apache/james/webadmin/routes/MailRepositoriesRoutes.java
@@ -29,6 +29,7 @@ import java.util.function.Supplier;
 import javax.inject.Inject;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.output.CountingOutputStream;
 import org.apache.james.mailrepository.api.MailKey;
@@ -64,7 +65,6 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
-import jakarta.servlet.http.HttpServletResponse;
 import spark.HaltException;
 import spark.Request;
 import spark.Service;
