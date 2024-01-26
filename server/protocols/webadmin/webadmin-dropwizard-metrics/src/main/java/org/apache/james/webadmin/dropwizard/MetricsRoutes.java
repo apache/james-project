7d04b7737720616b5cc22ae11a759ba7f8cbde6a
Benoit TELLIER <btellier@linagora.com>
2024-01-26 16:54:28 +0100

    Revert "[UPDATE] Update spark version: Jetty update"
    This reverts commit 69e45e4ddf164cff38764bcc4860b47b1274abb1.

--- server/protocols/webadmin/webadmin-dropwizard-metrics/src/main/java/org/apache/james/webadmin/dropwizard/MetricsRoutes.java
+++ server/protocols/webadmin/webadmin-dropwizard-metrics/src/main/java/org/apache/james/webadmin/dropwizard/MetricsRoutes.java
@@ -27,6 +27,8 @@ import java.util.Optional;
 import java.util.Set;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.james.webadmin.PublicRoutes;
 
@@ -37,8 +39,6 @@ import com.google.common.collect.ImmutableSet;
 import io.prometheus.client.SampleNameFilter;
 import io.prometheus.client.dropwizard.DropwizardExports;
 import io.prometheus.client.exporter.common.TextFormat;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import spark.Request;
 import spark.Response;
 import spark.Service;
