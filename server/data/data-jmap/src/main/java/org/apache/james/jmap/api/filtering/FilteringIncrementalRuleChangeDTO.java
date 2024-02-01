1aa93a61c83512ae397522a4895f2bd6c292b9de
hung phan <hphan@linagora.com>
2024-02-01 11:43:51 +0700

    JAMES-2586 Implement PostgresFilteringProjection

--- server/data/data-jmap/src/main/java/org/apache/james/jmap/api/filtering/FilteringIncrementalRuleChangeDTO.java
+++ server/data/data-jmap/src/main/java/org/apache/james/jmap/api/filtering/FilteringIncrementalRuleChangeDTO.java
@@ -23,6 +23,8 @@ import java.util.Objects;
 
 import org.apache.james.eventsourcing.EventId;
 import org.apache.james.eventsourcing.eventstore.dto.EventDTO;
+import org.apache.james.jmap.api.filtering.Rule;
+import org.apache.james.jmap.api.filtering.RuleDTO;
 import org.apache.james.jmap.api.filtering.impl.FilteringAggregateId;
 import org.apache.james.jmap.api.filtering.impl.IncrementalRuleChange;
 
