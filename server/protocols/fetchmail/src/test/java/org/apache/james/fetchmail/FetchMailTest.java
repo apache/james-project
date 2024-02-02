61d319bdad6c4b03987832a1c33564f4e7b31411
Maxime Brunet <maxime.brunet@polyconseil.fr>
2024-02-02 11:19:28 +0100

    JAMES-2372 Fixing multiple accounts with Fetchmail

--- server/protocols/fetchmail/src/test/java/org/apache/james/fetchmail/FetchMailTest.java
+++ server/protocols/fetchmail/src/test/java/org/apache/james/fetchmail/FetchMailTest.java
@@ -22,8 +22,14 @@ package org.apache.james.fetchmail;
 import org.apache.commons.configuration2.BaseHierarchicalConfiguration;
 import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.apache.commons.configuration2.tree.ImmutableNode;
+import org.apache.james.core.Domain;
 import org.apache.james.core.Username;
+import org.apache.james.dnsservice.api.DNSService;
+import org.apache.james.domainlist.api.DomainList;
+import org.apache.james.domainlist.lib.DomainListConfiguration;
+import org.apache.james.domainlist.memory.MemoryDomainList;
 import org.apache.james.user.api.UsersRepository;
+import org.apache.james.user.memory.MemoryUsersRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
@@ -37,17 +43,21 @@ import static org.mockito.Mockito.when;
 
 class FetchMailTest {
 
+
     @Test
     void shouldAddAccountWithGoodDataFetchMail() throws Exception {
         try (MockedConstruction<ParsedConfiguration> mockParsedConfiguration = Mockito.mockConstruction(ParsedConfiguration.class)) {
-            UsersRepository fieldLocalUsers = mock(UsersRepository.class);
-            when(fieldLocalUsers.countUsers()).thenReturn(4);
-            when(fieldLocalUsers.list())
-                    .thenReturn(List.of(Username.of("user23@domain.com")).iterator())
-                    .thenReturn(List.of(Username.of("user23@domain.com")).iterator());
+            DNSService dnsService = mock(DNSService.class);
+            MemoryDomainList domainList = new MemoryDomainList(dnsService);
+            domainList.configure(DomainListConfiguration.DEFAULT);
+            domainList.addDomain(Domain.of("domain.com"));
+
+            UsersRepository usersRepository = MemoryUsersRepository.withVirtualHosting(domainList);
+            usersRepository.addUser(Username.of("user2@domain.com"), "");
+            usersRepository.addUser(Username.of("user3@domain.com"), "");
 
             FetchMail fetchMail = new FetchMail();
-            fetchMail.setUsersRepository(fieldLocalUsers);
+            fetchMail.setUsersRepository(usersRepository);
             fetchMail.configure(configuration());
 
             assertThat(mockParsedConfiguration.constructed().size()).isOne();
@@ -58,7 +68,7 @@ class FetchMailTest {
             assertThat(fetchMail.getDynamicAccounts().values())
                     .hasSize(2)
                     .extracting(DynamicAccount::getSequenceNumber, DynamicAccount::getUser)
-                    .containsExactlyInAnyOrder(tuple(2, "user23@domain.com"), tuple(3, "user23@domain.com"));
+                    .containsExactlyInAnyOrder(tuple(2, "user2@domain.com"), tuple(2, "user3@domain.com"));
         }
     }
 
@@ -68,9 +78,8 @@ class FetchMailTest {
         ImmutableNode account1 = new ImmutableNode.Builder().name("account").value("account1").addAttribute("ignorercpt-header", false).addAttribute("user", "user1").create();
 
         ImmutableNode account2 = new ImmutableNode.Builder().name("alllocal").value("account2").addAttribute("ignorercpt-header", false).create();
-        ImmutableNode account3 = new ImmutableNode.Builder().name("alllocal").value("account3").addAttribute("ignorercpt-header", false).create();
 
-        configuration.addNodes("accounts", List.of(account0, account1, account2, account3));
+        configuration.addNodes("accounts", List.of(account0, account1, account2));
         return configuration;
     }
 }
