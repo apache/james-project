bc14f9c063e718b0def398e311d14bb7052c221b
hung phan <hphan@linagora.com>
2024-02-07 17:54:51 +0700

    JAMES-2586 Implement PostgresEventStore

---
+++ event-sourcing/event-store-postgres/src/test/java/org/apache/james/eventsourcing/eventstore/postgres/PostgresEventStoreExtension.java
@@ -0,0 +1,72 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.eventsourcing.eventstore.postgres;
+
+import org.apache.james.backends.postgres.PostgresExtension;
+import org.apache.james.eventsourcing.eventstore.EventStore;
+import org.apache.james.eventsourcing.eventstore.JsonEventSerializer;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class PostgresEventStoreExtension implements AfterAllCallback, BeforeAllCallback, AfterEachCallback, BeforeEachCallback, ParameterResolver {
+    private PostgresExtension postgresExtension;
+    private JsonEventSerializer jsonEventSerializer;
+
+    public PostgresEventStoreExtension(JsonEventSerializer jsonEventSerializer) {
+        this.jsonEventSerializer = jsonEventSerializer;
+        this.postgresExtension = PostgresExtension.withoutRowLevelSecurity(PostgresEventStoreModule.MODULE);
+    }
+
+    @Override
+    public void afterAll(ExtensionContext extensionContext) {
+        postgresExtension.afterAll(extensionContext);
+    }
+
+    @Override
+    public void afterEach(ExtensionContext extensionContext) {
+        postgresExtension.afterEach(extensionContext);
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext extensionContext) throws Exception {
+        postgresExtension.beforeAll(extensionContext);
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext extensionContext) {
+        postgresExtension.beforeEach(extensionContext);
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType() == EventStore.class;
+    }
+
+    @Override
+    public PostgresEventStore resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return new PostgresEventStore(new PostgresEventStoreDAO(postgresExtension.getPostgresExecutor(), jsonEventSerializer));
+    }
+}
