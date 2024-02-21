16a21a874b7ebbb6c5ccd612a55d7902e298b1f1
Benoit TELLIER <btellier@linagora.com>
2024-02-21 14:21:22 +0100

    JAMES-4007 ImapChannelUpstreamHandler should always cleanup INBOUND message
    It needs to account for unexecuted messages, rejected messages,
    cancelled subscriptions.

--- server/protocols/protocols-imap4/src/main/java/org/apache/james/imapserver/netty/ImapChannelUpstreamHandler.java
+++ server/protocols/protocols-imap4/src/main/java/org/apache/james/imapserver/netty/ImapChannelUpstreamHandler.java
@@ -398,9 +398,6 @@ public class ImapChannelUpstreamHandler extends ChannelInboundHandlerAdapter imp
                     }
                     if (signal.isOnComplete() || signal.isOnError()) {
                         afterIDLEUponProcessing(ctx);
-                        if (message instanceof Closeable) {
-                            ((Closeable) message).close();
-                        }
                     }
                     if (signal.hasError()) {
                         ctx.fireExceptionCaught(signal.getThrowable());
@@ -412,6 +409,11 @@ public class ImapChannelUpstreamHandler extends ChannelInboundHandlerAdapter imp
                 .contextWrite(ReactorUtils.context("imap", mdc(session))), message)
             // Manage throttling errors
             .doOnError(ctx::fireExceptionCaught)
+            .doFinally(Throwing.consumer(any -> {
+                if (message instanceof Closeable) {
+                    ((Closeable) message).close();
+                }
+            }))
             .subscribe();
         disposableAttribute.set(disposable);
     }
