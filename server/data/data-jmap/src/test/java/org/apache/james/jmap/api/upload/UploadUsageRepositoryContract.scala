46b410c6c2fb0bc8b4ad24478ffd09d549d3a1ff
Tung Tran <vttran@linagora.com>
2024-02-29 09:19:44 +0700

    JAMES-3925 - JMAP Upload - Cleanup UploadUsageRepositoryContract

--- server/data/data-jmap/src/test/java/org/apache/james/jmap/api/upload/UploadUsageRepositoryContract.scala
+++ server/data/data-jmap/src/test/java/org/apache/james/jmap/api/upload/UploadUsageRepositoryContract.scala
@@ -21,30 +21,30 @@ trait UploadUsageRepositoryContract {
 
   @Test
   def increaseSpaceShouldIncreaseSuccessfully(): Unit = {
-    SMono.fromPublisher(uploadUsageRepository.increaseSpace(USER_NAME, QuotaSizeUsage.size(100))).block();
-    val expected = SMono.fromPublisher(uploadUsageRepository.getSpaceUsage(USER_NAME)).block();
-    assertThat(expected.asLong()).isEqualTo(100);
+    SMono.fromPublisher(uploadUsageRepository.increaseSpace(USER_NAME, QuotaSizeUsage.size(100))).block()
+    val expected = SMono.fromPublisher(uploadUsageRepository.getSpaceUsage(USER_NAME)).block()
+    assertThat(expected.asLong()).isEqualTo(100)
   }
 
   @Test
   def decreaseSpaceShouldDecreaseSuccessfully(): Unit = {
-    SMono.fromPublisher(uploadUsageRepository.increaseSpace(USER_NAME, QuotaSizeUsage.size(200))).block();
-    SMono.fromPublisher(uploadUsageRepository.decreaseSpace(USER_NAME, QuotaSizeUsage.size(100))).block();
-    val expected = SMono.fromPublisher(uploadUsageRepository.getSpaceUsage(USER_NAME)).block();
-    assertThat(expected.asLong()).isEqualTo(100);
+    SMono.fromPublisher(uploadUsageRepository.increaseSpace(USER_NAME, QuotaSizeUsage.size(200))).block()
+    SMono.fromPublisher(uploadUsageRepository.decreaseSpace(USER_NAME, QuotaSizeUsage.size(100))).block()
+    val expected = SMono.fromPublisher(uploadUsageRepository.getSpaceUsage(USER_NAME)).block()
+    assertThat(expected.asLong()).isEqualTo(100)
   }
 
   @Test
   def resetSpaceShouldResetSuccessfully(): Unit = {
-    SMono.fromPublisher(uploadUsageRepository.increaseSpace(USER_NAME, QuotaSizeUsage.size(200))).block();
-    SMono.fromPublisher(uploadUsageRepository.resetSpace(USER_NAME, QuotaSizeUsage.size(100))).block();
-    val expected = SMono.fromPublisher(uploadUsageRepository.getSpaceUsage(USER_NAME)).block();
-    assertThat(expected.asLong()).isEqualTo(100);
+    SMono.fromPublisher(uploadUsageRepository.increaseSpace(USER_NAME, QuotaSizeUsage.size(200))).block()
+    SMono.fromPublisher(uploadUsageRepository.resetSpace(USER_NAME, QuotaSizeUsage.size(99))).block()
+    val expected = SMono.fromPublisher(uploadUsageRepository.getSpaceUsage(USER_NAME)).block()
+    assertThat(expected.asLong()).isEqualTo(99)
   }
 
   @Test
   def getSpaceUsageShouldReturnZeroWhenRecordDoesNotExist(): Unit = {
-    assertThat(SMono.fromPublisher(uploadUsageRepository.getSpaceUsage(Username.of("aaa"))).block().asLong()).isEqualTo(0);
+    assertThat(SMono.fromPublisher(uploadUsageRepository.getSpaceUsage(Username.of("aaa"))).block().asLong()).isEqualTo(0)
   }
 
 }
