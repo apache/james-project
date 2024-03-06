aff064b3334d227467693c15adbfc83defdc68df
Tung Tran <vttran@linagora.com>
2024-03-06 10:55:47 +0700

    JAMES-2586 Optimize AttachmentLoader
    - get the list replaced to get each by each

--- mailbox/postgres/src/main/java/org/apache/james/mailbox/postgres/mail/AttachmentLoader.java
+++ mailbox/postgres/src/main/java/org/apache/james/mailbox/postgres/mail/AttachmentLoader.java
@@ -21,13 +21,20 @@ package org.apache.james.mailbox.postgres.mail;
 
 import static org.apache.james.mailbox.postgres.mail.PostgresMessageModule.MessageTable.ATTACHMENT_METADATA;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.james.mailbox.model.AttachmentId;
 import org.apache.james.mailbox.model.AttachmentMetadata;
 import org.apache.james.mailbox.model.MessageAttachmentMetadata;
+import org.apache.james.mailbox.postgres.mail.dto.AttachmentsDTO;
 import org.apache.james.mailbox.store.mail.MessageMapper;
 import org.apache.james.mailbox.store.mail.model.impl.SimpleMailboxMessage;
 import org.apache.james.util.ReactorUtils;
 import org.jooq.Record;
+
+import com.google.common.collect.ImmutableMap;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -45,10 +52,8 @@ public class AttachmentLoader {
         return findMessagePublisher.flatMap(pair -> {
             if (fetchType == MessageMapper.FetchType.FULL || fetchType == MessageMapper.FetchType.ATTACHMENTS_METADATA) {
                 return Mono.fromCallable(() -> pair.getRight().get(ATTACHMENT_METADATA))
-                    .flatMapMany(Flux::fromIterable)
-                    .flatMapSequential(attachmentRepresentation -> attachmentMapper.getAttachmentReactive(attachmentRepresentation.getAttachmentId())
-                        .map(attachment -> constructMessageAttachment(attachment, attachmentRepresentation)))
-                    .collectList()
+                    .map(e -> toMap((AttachmentsDTO) e))
+                    .flatMap(this::getAttachments)
                     .map(messageAttachmentMetadata -> {
                         pair.getLeft().addAttachments(messageAttachmentMetadata);
                         return pair;
@@ -57,6 +62,17 @@ public class AttachmentLoader {
                 return Mono.just(pair);
             }
         }, ReactorUtils.DEFAULT_CONCURRENCY);
+    }
+
+    private Map<AttachmentId, MessageRepresentation.AttachmentRepresentation> toMap(AttachmentsDTO attachmentRepresentations) {
+        return attachmentRepresentations.stream().collect(ImmutableMap.toImmutableMap(MessageRepresentation.AttachmentRepresentation::getAttachmentId, obj -> obj));
+    }
+
+    private Mono<List<MessageAttachmentMetadata>> getAttachments(Map<AttachmentId, MessageRepresentation.AttachmentRepresentation> mapAttachmentIdToAttachmentRepresentation) {
+        return Mono.fromCallable(mapAttachmentIdToAttachmentRepresentation::keySet)
+            .flatMapMany(attachmentMapper::getAttachmentsReactive)
+            .map(attachmentMetadata -> constructMessageAttachment(attachmentMetadata, mapAttachmentIdToAttachmentRepresentation.get(attachmentMetadata.getAttachmentId())))
+            .collectList();
     }
 
     private MessageAttachmentMetadata constructMessageAttachment(AttachmentMetadata attachment, MessageRepresentation.AttachmentRepresentation messageAttachmentRepresentation) {
