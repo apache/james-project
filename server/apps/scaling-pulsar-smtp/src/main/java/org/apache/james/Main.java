17a2857b759f14a314987b870b3a3cb1ab32ba97
Benoit TELLIER <btellier@linagora.com>
2024-03-11 07:40:41 +0100

    [FIX] Ship mail over web webadmin routes into more apps (#2099)

--- server/apps/scaling-pulsar-smtp/src/main/java/org/apache/james/Main.java
+++ server/apps/scaling-pulsar-smtp/src/main/java/org/apache/james/Main.java
@@ -45,6 +45,7 @@ import org.apache.james.modules.server.MailetContainerModule;
 import org.apache.james.modules.server.NoJwtModule;
 import org.apache.james.modules.server.RawPostDequeueDecoratorModule;
 import org.apache.james.modules.server.TaskManagerModule;
+import org.apache.james.modules.server.WebAdminMailOverWebModule;
 import org.apache.james.modules.server.WebAdminServerModule;
 import org.apache.james.queue.pulsar.module.PulsarQueueModule;
 import org.apache.james.server.blob.deduplication.PassThroughBlobStore;
@@ -56,55 +57,49 @@ import com.google.inject.util.Modules;
 
 public class Main implements JamesServerMain {
     public static final Module WEBADMIN = Modules.combine(
-            new DataRoutesModules(),
-            new MailQueueRoutesModule(),
-            new MailRepositoriesRoutesModule(),
-            new NoJwtModule(),
-            new WebAdminServerModule()
-    );
+        new DataRoutesModules(),
+        new MailQueueRoutesModule(),
+        new MailRepositoriesRoutesModule(),
+        new NoJwtModule(),
+        new WebAdminServerModule(),
+        new WebAdminMailOverWebModule());
     public static final Module PROTOCOLS = Modules.combine(
-            new SMTPServerModule(),
-            new ProtocolHandlerModule()
-    );
+        new SMTPServerModule(),
+        new ProtocolHandlerModule());
     private static final Module BLOB_MODULE = Modules.combine(
-            new BlobStoreAPIModule(),
-            new S3BlobStoreModule(),
-            new S3BucketModule(),
-            binder -> {
-                binder.bind(BlobStoreDAO.class).to(S3BlobStoreDAO.class);
-                binder.bind(BlobStore.class)
-                        .annotatedWith(Names.named(MetricableBlobStore.BLOB_STORE_IMPLEMENTATION))
-                        .to(PassThroughBlobStore.class);
-            }
-    );
+        new BlobStoreAPIModule(),
+        new S3BlobStoreModule(),
+        new S3BucketModule(),
+        binder -> {
+            binder.bind(BlobStoreDAO.class).to(S3BlobStoreDAO.class);
+            binder.bind(BlobStore.class)
+                .annotatedWith(Names.named(MetricableBlobStore.BLOB_STORE_IMPLEMENTATION))
+                .to(PassThroughBlobStore.class);
+        });
 
 
     public static final Module QUEUE_MODULES = Modules.combine(
-            new RawPostDequeueDecoratorModule(),
-            new PulsarQueueModule()
-    );
+        new RawPostDequeueDecoratorModule(),
+        new PulsarQueueModule());
 
     public static final Module SERVER_CORE_MODULES = Modules.combine(
-            new DefaultProcessorsConfigurationProviderModule(),
-            new JPAEntityManagerModule(),
-            new MailStoreRepositoryModule(),
-            new MailetContainerModule(),
-            new BlobstoreMailRepositoryModule(),
-            binder -> {
-                binder.bind(MailRepositoryUrlStore.class).to(JPAMailRepositoryUrlStore.class).in(Scopes.SINGLETON);
-            },
-            new CoreDataModule(),
-            new JPADomainListModule(),
-            new JPARecipientRewriteTableModule(),
-            new JPAUsersRepositoryModule(),
-            new MemoryDelegationStoreModule(),
-            new TaskManagerModule()
-    );
+        new DefaultProcessorsConfigurationProviderModule(),
+        new JPAEntityManagerModule(),
+        new MailStoreRepositoryModule(),
+        new MailetContainerModule(),
+        new BlobstoreMailRepositoryModule(),
+        binder -> binder.bind(MailRepositoryUrlStore.class).to(JPAMailRepositoryUrlStore.class).in(Scopes.SINGLETON),
+        new CoreDataModule(),
+        new JPADomainListModule(),
+        new JPARecipientRewriteTableModule(),
+        new JPAUsersRepositoryModule(),
+        new MemoryDelegationStoreModule(),
+        new TaskManagerModule());
 
     public static void main(String[] args) throws Exception {
         SMTPRelayConfiguration configuration = SMTPRelayConfiguration.builder()
-                .useWorkingDirectoryEnvProperty()
-                .build();
+            .useWorkingDirectoryEnvProperty()
+            .build();
 
         LOGGER.info("Loading configuration {}", configuration.toString());
         GuiceJamesServer server = createServer(configuration)
@@ -120,6 +115,6 @@ public class Main implements JamesServerMain {
 
     public static GuiceJamesServer createServer(SMTPRelayConfiguration configuration) {
         return GuiceJamesServer.forConfiguration(configuration)
-                .combineWith(SERVER_CORE_MODULES, BLOB_MODULE, QUEUE_MODULES, PROTOCOLS, WEBADMIN);
+            .combineWith(SERVER_CORE_MODULES, BLOB_MODULE, QUEUE_MODULES, PROTOCOLS, WEBADMIN);
     }
 }
