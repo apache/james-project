ef08acdbbeade2e42dd670a2c48040a62b5530f5
hung phan <hphan@linagora.com>
2024-03-14 16:39:31 +0700

    JAMES-2586 Fix PostgresMailboxSetMethodTest

--- server/protocols/jmap-rfc-8621-integration-tests/jmap-rfc-8621-integration-tests-common/src/main/scala/org/apache/james/jmap/rfc8621/contract/MailboxSetMethodContract.scala
+++ server/protocols/jmap-rfc-8621-integration-tests/jmap-rfc-8621-integration-tests-common/src/main/scala/org/apache/james/jmap/rfc8621/contract/MailboxSetMethodContract.scala
@@ -8168,6 +8168,7 @@ trait MailboxSetMethodContract {
                  |    }, "c1"]]
                  |}""".stripMargin))
 
+            ws.receive().asPayload
             List(ws.receive().asPayload)
         })
         .send(backend)
