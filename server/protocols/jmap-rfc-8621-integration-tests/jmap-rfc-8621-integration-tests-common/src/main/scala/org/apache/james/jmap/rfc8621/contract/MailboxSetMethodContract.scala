ccf5bb5e906086e744670875675ab62e9334afb2
Quan Tran <hqtran@linagora.com>
2024-03-14 15:53:12 +0700

    JAMES-2586 Reduce repeat count for some JMAP integration tests
    These are the tests took a lot of time to fully repeat.
    cf: https://ge.apache.org/s/dqudut5akzxr6/tests/goal/org.apache.james:postgres-jmap-rfc-8621-integration-tests:surefire:test@default-test/details/org.apache.james.jmap.rfc8621.postgres.PostgresUploadTest?top-execution=1
    cf: https://ge.apache.org/s/dqudut5akzxr6/tests/goal/org.apache.james:postgres-jmap-rfc-8621-integration-tests:surefire:test@default-test/details/org.apache.james.jmap.rfc8621.postgres.PostgresMailboxSetMethodTest?top-execution=1
    
    Likely overkill to repeat that much, we can reduce the repeat count to save some test runtime...

--- server/protocols/jmap-rfc-8621-integration-tests/jmap-rfc-8621-integration-tests-common/src/main/scala/org/apache/james/jmap/rfc8621/contract/MailboxSetMethodContract.scala
+++ server/protocols/jmap-rfc-8621-integration-tests/jmap-rfc-8621-integration-tests-common/src/main/scala/org/apache/james/jmap/rfc8621/contract/MailboxSetMethodContract.scala
@@ -6934,7 +6934,7 @@ trait MailboxSetMethodContract {
          |}""".stripMargin)
   }
 
-  @RepeatedTest(100)
+  @RepeatedTest(20)
   def concurrencyChecksUponParentIdUpdate(server: GuiceJamesServer): Unit = {
     val mailboxId1: MailboxId = server.getProbe(classOf[MailboxProbeImpl])
       .createMailbox(MailboxPath.forUser(BOB, "mailbox1"))
