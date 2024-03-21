304481cd260d9d1a967e92717a424d87528fd960
hung phan <hphan@linagora.com>
2024-03-21 10:19:58 +0700

    JAMES-2586 Add UuidBackedAttachmentIdFactory

--- mailbox/postgres/src/main/java/org/apache/james/mailbox/postgres/mail/dto/AttachmentsDTO.java
+++ mailbox/postgres/src/main/java/org/apache/james/mailbox/postgres/mail/dto/AttachmentsDTO.java
@@ -32,6 +32,7 @@ import java.util.stream.StreamSupport;
 import org.apache.james.mailbox.model.AttachmentId;
 import org.apache.james.mailbox.model.Cid;
 import org.apache.james.mailbox.model.MessageAttachmentMetadata;
+import org.apache.james.mailbox.model.UuidBackedAttachmentId;
 import org.apache.james.mailbox.postgres.mail.MessageRepresentation;
 import org.jooq.BindingGetResultSetContext;
 import org.jooq.BindingSetStatementContext;
@@ -94,7 +95,7 @@ public class AttachmentsDTO extends ArrayList<MessageRepresentation.AttachmentRe
         }
 
         private MessageRepresentation.AttachmentRepresentation fromJsonNode(JsonNode jsonNode) {
-            AttachmentId attachmentId = AttachmentId.from(jsonNode.get(ATTACHMENT_ID_PROPERTY).asText());
+            AttachmentId attachmentId = UuidBackedAttachmentId.from(jsonNode.get(ATTACHMENT_ID_PROPERTY).asText());
             Optional<String> name = Optional.ofNullable(jsonNode.get(NAME_PROPERTY)).map(JsonNode::asText);
             Optional<Cid> cid = Optional.ofNullable(jsonNode.get(CID_PROPERTY)).map(JsonNode::asText).map(Cid::from);
             boolean isInline = jsonNode.get(IN_LINE_PROPERTY).asBoolean();
