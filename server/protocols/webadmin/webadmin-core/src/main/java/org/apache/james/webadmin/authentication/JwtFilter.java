fcb352967b88cca5b5aae1ef3c19e74bf4759458
shn27 <sohanur1525sohan@gmail.com>
2024-05-21 19:11:45 +0600

    Webadmin Authentication added for both Dynamic and Static route. Tested.

--- server/protocols/webadmin/webadmin-core/src/main/java/org/apache/james/webadmin/authentication/JwtFilter.java
+++ server/protocols/webadmin/webadmin-core/src/main/java/org/apache/james/webadmin/authentication/JwtFilter.java
@@ -22,6 +22,7 @@ package org.apache.james.webadmin.authentication;
 import static spark.Spark.halt;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -58,8 +59,6 @@ public class JwtFilter implements AuthenticationFilter {
                 .filter(value -> value.startsWith(AUTHORIZATION_HEADER_PREFIX))
                 .map(value -> value.substring(AUTHORIZATION_HEADER_PREFIX.length()));
 
-            System.out.println("halaaaaaaaa     " + request.pathInfo() + "      " + request.requestMethod());
-
             checkHeaderPresent(bearer);
             String login = retrieveUser(bearer); // subject field can't be null
             checkIsAdmin(bearer); // admin field should be true
@@ -78,32 +77,60 @@ public class JwtFilter implements AuthenticationFilter {
 
         JsonNode subNode = payloadNode.get("sub");
         String sub = subNode != null ? subNode.asText() : "N/A";
-        if (sub == "admin") {
+        if (sub.equals("admin")) {
             return;
         } else if (!sub.equals("agent")) {
             halt(HttpStatus.UNAUTHORIZED_401, "Non authorized user.Subject is not agent");
         }
-        String pathValue = getProcessPath(request.pathInfo());
+        String pathProcessedValue = getProcessPath(request.pathInfo());
         JsonNode permissionsNode = payloadNode.get("permissions");
-        boolean hasGetPermission = false;
+
+        AtomicBoolean flag = new AtomicBoolean(false);
+        //Checking full match
         if (permissionsNode != null && permissionsNode.isArray()) {
             for (JsonNode permissionNode : permissionsNode) {
-                JsonNode valueNode = permissionNode.get(pathValue);
-
-                if (valueNode != null && request.requestMethod().equals(valueNode.asText())) {
-                    hasGetPermission = true;
-                    break;
+                JsonNode groupNode = permissionNode.get(pathProcessedValue);
+                if (groupNode != null && groupNode.isArray()) {
+                    for (JsonNode valueNode : groupNode) {
+                        if (request.requestMethod().equals(valueNode.asText())) {
+                            flag.set(true);
+                            return;
+                        }
+                    }
                 }
             }
         }
-        if (!hasGetPermission) {
-            halt(HttpStatus.UNAUTHORIZED_401, "Non authorized user.Do not have permission.");
+        if (flag.get()) {
+            return;
         }
+
+        String requestMethod = request.requestMethod().toString();
+        //Dynamic Check
+        JsonNode permissions = payloadNode.get("permissions");
+        if (permissions != null && permissions.isArray()) {
+            for (JsonNode permissionNode : permissions) {
+                permissionNode.fields().forEachRemaining(entry -> {
+                    String key = entry.getKey();
+                    if (match(key, "perm" + request.pathInfo())) {
+                        JsonNode groupValueNode = permissionNode.get(key);
+                        for (JsonNode valueNode : groupValueNode) {
+                            if (requestMethod.equals(valueNode.asText())) {
+                                flag.set(true);
+                                return;
+                            }
+                        }
+                    }
+                });
+            }
+        }
+        if (flag.get()) {
+            return;
+        }
+        halt(HttpStatus.UNAUTHORIZED_401, "Non authorized user.Do not have permission.");
     }
 
     private String getProcessPath(String path) {
-        String first15Chars = path.substring(0, 15);
-        String result = first15Chars.replace('/', '.');
+        String result = path.replace('/', '.');
         return "perm" + result;
     }
 
@@ -122,5 +149,31 @@ public class JwtFilter implements AuthenticationFilter {
         if (!jwtTokenVerifier.hasAttribute("admin", true, bearer.get())) {
             halt(HttpStatus.UNAUTHORIZED_401, "Non authorized user.");
         }
+    }
+
+    private boolean match(String key, String path) {
+        String key1 = key.replace('.', '@');
+        String[] keyArr = key1.split("@");
+        String[] pathArr = path.split("/");
+
+        if (pathArr.length < keyArr.length) {
+            return false;
+        }
+        Integer it = 0;
+        Integer it1 = 0;
+        Integer lenth = keyArr.length;
+        Integer lenth1 = pathArr.length;
+
+        while (it1 < lenth1) {
+            if (keyArr[it].equals(pathArr[it1])) {
+                it++; it1++;
+                if (it == lenth) {
+                    return true;
+                }
+            } else {
+                it1++;
+            }
+        }
+        return false;
     }
 }
