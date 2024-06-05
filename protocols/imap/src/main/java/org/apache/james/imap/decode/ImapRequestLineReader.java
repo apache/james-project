6dd3ad9ea1f6a9bc887d2c7af3f5aa30a60ec769
Benoit TELLIER <btellier@linagora.com>
2024-06-05 17:11:42 +0200

    [IMPROVEMENT] Better manage IMAP literals

--- protocols/imap/src/main/java/org/apache/james/imap/decode/ImapRequestLineReader.java
+++ protocols/imap/src/main/java/org/apache/james/imap/decode/ImapRequestLineReader.java
@@ -636,6 +636,8 @@ public abstract class ImapRequestLineReader {
                         }
                     }
                 }
+            } catch (DecodingException e) {
+                throw e;
             } catch (IOException e) {
                 throw new DecodingException(HumanReadableText.SOCKET_IO_FAILURE, "Could not read literal", e);
             }
