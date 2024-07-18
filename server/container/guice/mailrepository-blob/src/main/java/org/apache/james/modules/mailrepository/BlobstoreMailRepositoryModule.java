be95c46154d99e917f2792b5287a322d8f5f64c4
Jean Helou <jean@xn--gml-cma.com>
2024-07-18 23:22:46 +0200

    JAMES-3763 Use Blobstore#save with deterministic BlobId in BlobMailRepository
    Co-Authored-By: Matthieu Baechler <matthieu@apache.org>

--- server/container/guice/mailrepository-blob/src/main/java/org/apache/james/modules/mailrepository/BlobstoreMailRepositoryModule.java
+++ server/container/guice/mailrepository-blob/src/main/java/org/apache/james/modules/mailrepository/BlobstoreMailRepositoryModule.java
@@ -19,10 +19,13 @@
 
 package org.apache.james.modules.mailrepository;
 
+import jakarta.inject.Named;
+
 import org.apache.commons.configuration2.BaseHierarchicalConfiguration;
 import org.apache.james.blob.api.BlobId;
+import org.apache.james.blob.api.BlobStore;
 import org.apache.james.blob.api.BlobStoreDAO;
-import org.apache.james.blob.mail.MimeMessageStore;
+import org.apache.james.blob.api.BucketName;
 import org.apache.james.mailrepository.api.MailRepositoryFactory;
 import org.apache.james.mailrepository.api.MailRepositoryStore;
 import org.apache.james.mailrepository.api.Protocol;
@@ -52,8 +55,9 @@ public class BlobstoreMailRepositoryModule extends AbstractModule {
     @ProvidesIntoSet()
     public MailRepositoryFactory blobMailRepository(BlobStoreDAO blobStore,
                                                     BlobId.Factory blobIdFactory,
-                                                    MimeMessageStore.Factory mimeFactory) {
-        return new BlobMailRepositoryFactory(blobStore, blobIdFactory, mimeFactory);
+                                                    @Named(BlobStore.DEFAULT_BUCKET_NAME_QUALIFIER) BucketName defaultBucketName
+                                                    ) {
+        return new BlobMailRepositoryFactory(blobStore, blobIdFactory, defaultBucketName);
     }
 
 }
---
+++ server/mailrepository/mailrepository-blob/src/test/java/org/apache/james/mailrepository/blob/MailRepositoryBlobIdFactoryTest.java
@@ -0,0 +1,61 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailrepository.blob;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.james.blob.api.BlobId;
+import org.apache.james.blob.api.HashBlobId;
+import org.apache.james.mailrepository.api.MailRepositoryPath;
+import org.apache.james.mailrepository.api.MailRepositoryUrl;
+import org.apache.james.mailrepository.api.Protocol;
+import org.junit.jupiter.api.Test;
+
+class MailRepositoryBlobIdFactoryTest {
+    static String MAIL_REPOSITORY_PATH = "/var/mail/error";
+
+    BlobId.Factory blobIdFactory = new HashBlobId.Factory();
+    MailRepositoryUrl url = MailRepositoryUrl.fromPathAndProtocol(
+            new Protocol("blob"),
+            MailRepositoryPath.from(MAIL_REPOSITORY_PATH)
+    );
+
+    @Test
+    void ofShouldRelocateBlobIdUnderTheMailRepositoryPath() {
+        var id = "0c222abb-d115-4a88-9fbe-e65e951301f6";
+        MailRepositoryBlobIdFactory factory = new MailRepositoryBlobIdFactory(blobIdFactory, url);
+
+        BlobId actual = factory.of(id);
+
+        assertThat(actual.asString()).isEqualTo(MAIL_REPOSITORY_PATH + "/" + id);
+    }
+
+    @Test
+    void fromShouldNotRelocateBlobIdUnderTheMailRepositoryPath() {
+        var id = MAIL_REPOSITORY_PATH + "/" + "0c222abb-d115-4a88-9fbe-e65e951301f6";
+        MailRepositoryBlobIdFactory factory = new MailRepositoryBlobIdFactory(blobIdFactory, url);
+
+        BlobId actual = factory.from(id);
+
+        assertThat(actual.asString()).isEqualTo(id);
+    }
+
+
+}
