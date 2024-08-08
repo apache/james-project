ecd79e0bc359fe2220ddb7b310d28cd5fc0ba84c
hung phan <hphan@linagora.com>
2024-08-08 11:37:01 +0700

    JAMES-4055 Configure SMimeCheckSignature with PEM files

---
+++ mailet/crypto/src/main/java/org/apache/james/transport/KeyStoreHolderFactory.java
@@ -0,0 +1,125 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.transport;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.CertificateException;
+
+import jakarta.mail.MessagingException;
+
+import org.apache.commons.io.input.UnsynchronizedBufferedInputStream;
+import org.apache.mailet.MailetConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KeyStoreHolderFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KeyStoreHolderFactory.class);
+    private static final String KEYSTORE_FILE_TYPE = "keystore";
+    private static final String PEM_FILE_TYPE = "pem";
+
+    public static KeyStoreHolder createKeyStoreHolder(MailetConfig config) throws MessagingException {
+        try {
+            initJCE();
+            String fileType = config.getInitParameter("fileType");
+            if (fileType == null) {
+                return createFromKeyStoreFile(config);
+            }
+            switch (fileType) {
+                case KEYSTORE_FILE_TYPE:
+                    return createFromKeyStoreFile(config);
+                case PEM_FILE_TYPE:
+                    return createFromPemFile(config);
+                default:
+                    throw new RuntimeException("Unsupported file type " + fileType);
+            }
+        } catch (Exception e) {
+            throw new MessagingException("Error loading the trusted certificate store", e);
+        }
+    }
+
+    private static KeyStoreHolder createFromKeyStoreFile(MailetConfig config) throws Exception {
+        String type = config.getInitParameter("keyStoreType");
+        String fileName = config.getInitParameter("keyStoreFileName");
+        String password = config.getInitParameter("keyStorePassword");
+
+        if (fileName != null) {
+            return createFromKeyStoreFile(fileName, password, type);
+        } else {
+            LOGGER.info("No trusted store path specified, using default store.");
+            return createFromKeyStoreFile(password);
+        }
+    }
+
+    private static void initJCE() throws NoSuchProviderException {
+        try {
+            InitJCE.init();
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | InvocationTargetException | NoSuchMethodException e) {
+            NoSuchProviderException ex = new NoSuchProviderException("Error during cryptography provider initialization. Has bcprov-jdkxx-yyy.jar been copied in the lib directory or installed in the system?");
+            ex.initCause(e);
+            throw ex;
+        }
+    }
+
+    private static KeyStoreHolder createFromKeyStoreFile(String password) throws GeneralSecurityException, IOException {
+        return createFromKeyStoreFile(System.getProperty("java.home") + "/lib/security/cacerts".replace('/', File.separatorChar), password, KeyStore.getDefaultType());
+    }
+
+    private static KeyStoreHolder createFromKeyStoreFile(String keyStoreFileName, String keyStorePassword, String keyStoreType)
+        throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+
+        if (keyStorePassword == null) {
+            keyStorePassword = "";
+        }
+
+        if (keyStoreType == null) {
+            keyStoreType = KeyStore.getDefaultType();
+        }
+
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(UnsynchronizedBufferedInputStream
+            .builder()
+            .setInputStream(new FileInputStream(keyStoreFileName))
+            .get(), keyStorePassword.toCharArray());
+        if (keyStore.size() == 0) {
+            throw new KeyStoreException("The keystore must be not empty");
+        }
+        return new KeyStoreHolder(keyStore);
+    }
+
+    private static KeyStoreHolder createFromPemFile(MailetConfig config) throws Exception {
+        String fileName = config.getInitParameter("pemFileName");
+        if (fileName == null) {
+            throw new RuntimeException("pemFileName must not be empty");
+        }
+        KeyStore keyStore = PemReader.loadTrustStore(fileName);
+        if (keyStore.size() == 0) {
+            throw new KeyStoreException("The keystore must be not empty");
+        }
+        return new KeyStoreHolder(keyStore);
+    }
+}
--- server/mailet/integration-testing/src/test/java/org/apache/james/mailets/crypto/SMIMECheckSignatureIntegrationTest.java
+++ server/mailet/integration-testing/src/test/java/org/apache/james/mailets/crypto/SMIMECheckSignatureIntegrationTest.java
@@ -24,119 +24,67 @@ import static org.apache.james.mailets.configuration.Constants.LOCALHOST_IP;
 import static org.apache.james.mailets.configuration.Constants.awaitAtMostOneMinute;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.File;
 import java.time.ZonedDateTime;
 
-import org.apache.james.MemoryJamesServerMain;
 import org.apache.james.mailets.TemporaryJamesServer;
-import org.apache.james.mailets.configuration.CommonProcessors;
-import org.apache.james.mailets.configuration.MailetConfiguration;
-import org.apache.james.mailets.configuration.MailetContainer;
-import org.apache.james.mailets.configuration.ProcessorConfiguration;
 import org.apache.james.modules.protocols.ImapGuiceProbe;
 import org.apache.james.modules.protocols.SmtpGuiceProbe;
-import org.apache.james.transport.mailets.SMIMECheckSignature;
-import org.apache.james.transport.matchers.All;
 import org.apache.james.util.ClassLoaderUtils;
-import org.apache.james.util.date.ZonedDateTimeProvider;
-import org.apache.james.utils.DataProbeImpl;
 import org.apache.james.utils.SMTPMessageSender;
 import org.apache.james.utils.TestIMAPClient;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.jupiter.api.io.TempDir;
 
-public class SMIMECheckSignatureIntegrationTest {
-    private static final ZonedDateTime DATE_2015 = ZonedDateTime.parse("2015-10-15T14:10:00Z");
-    private static final String FROM = "user@" + DEFAULT_DOMAIN;
+public abstract class SMIMECheckSignatureIntegrationTest {
+    public static final ZonedDateTime DATE_2015 = ZonedDateTime.parse("2015-10-15T14:10:00Z");
+    public static final String FROM = "user@" + DEFAULT_DOMAIN;
     public static final String RECIPIENT = "user2@" + DEFAULT_DOMAIN;
-    private static final String PASSWORD = "secret";
+    public static final String PASSWORD = "secret";
 
-    @RegisterExtension
-    public TestIMAPClient testIMAPClient = new TestIMAPClient();
-    @RegisterExtension
-    public SMTPMessageSender messageSender = new SMTPMessageSender(DEFAULT_DOMAIN);
+    abstract TestIMAPClient testIMAPClient();
 
-    private TemporaryJamesServer jamesServer;
+    abstract SMTPMessageSender messageSender();
 
-    @BeforeEach
-    public void setup(@TempDir File temporaryFolder) throws Exception {
-        MailetContainer mailetContainer = MailetContainer.builder()
-            .putProcessor(CommonProcessors.root())
-            .putProcessor(CommonProcessors.error())
-            .putProcessor(ProcessorConfiguration.transport()
-                .addMailet(MailetConfiguration.BCC_STRIPPER)
-                .addMailet(MailetConfiguration.builder()
-                    .mailet(SMIMECheckSignature.class)
-                    .matcher(All.class)
-                    .addProperty("keyStoreFileName", temporaryFolder.toPath().resolve("conf").resolve("smime_cert_keystore").toAbsolutePath().toString())
-                    .addProperty("keyStorePassword", "secret")
-                    .addProperty("keyStoreType", "PKCS12")
-                    .addProperty("debug", "true"))
-                .addMailet(MailetConfiguration.LOCAL_DELIVERY))
-            .build();
-
-        jamesServer = TemporaryJamesServer.builder()
-            .withBase(MemoryJamesServerMain.SMTP_AND_IMAP_MODULE)
-            .withOverrides(binder -> binder.bind(ZonedDateTimeProvider.class).toInstance(() -> DATE_2015))
-            .withMailetContainer(mailetContainer)
-            .build(temporaryFolder);
-        jamesServer.start();
-
-        jamesServer.getProbe(DataProbeImpl.class)
-            .fluent()
-            .addDomain(DEFAULT_DOMAIN)
-            .addUser(FROM, PASSWORD)
-            .addUser(RECIPIENT, PASSWORD);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        jamesServer.shutdown();
-    }
+    abstract TemporaryJamesServer jamesServer();
 
     @Test
     public void checkSMIMESignatureShouldAddGoodSMIMEStatusWhenSignatureIsGood() throws Exception {
-        messageSender.connect(LOCALHOST_IP, jamesServer.getProbe(SmtpGuiceProbe.class).getSmtpAuthRequiredPort())
+        messageSender().connect(LOCALHOST_IP, jamesServer().getProbe(SmtpGuiceProbe.class).getSmtpAuthRequiredPort())
             .authenticate(FROM, PASSWORD)
             .sendMessageWithHeaders(FROM, RECIPIENT,
                 ClassLoaderUtils.getSystemResourceAsString("smime-test-resource-set/mail_with_signature.eml"));
 
-        testIMAPClient.connect(LOCALHOST_IP, jamesServer.getProbe(ImapGuiceProbe.class).getImapPort())
+        testIMAPClient().connect(LOCALHOST_IP, jamesServer().getProbe(ImapGuiceProbe.class).getImapPort())
             .login(RECIPIENT, PASSWORD)
             .select(TestIMAPClient.INBOX)
             .awaitMessage(awaitAtMostOneMinute);
-        assertThat(testIMAPClient.readFirstMessage()).containsSequence("X-SMIME-Status: Good signature");
+        assertThat(testIMAPClient().readFirstMessage()).containsSequence("X-SMIME-Status: Good signature");
     }
 
     @Test
     public void checkSMIMESignatureShouldAddBadSMIMEStatusWhenSignatureIsBad() throws Exception {
-        messageSender.connect(LOCALHOST_IP, jamesServer.getProbe(SmtpGuiceProbe.class).getSmtpAuthRequiredPort())
+        messageSender().connect(LOCALHOST_IP, jamesServer().getProbe(SmtpGuiceProbe.class).getSmtpAuthRequiredPort())
             .authenticate(FROM, PASSWORD)
             .sendMessageWithHeaders(FROM, RECIPIENT,
                 ClassLoaderUtils.getSystemResourceAsString("eml/mail_with_bad_signature.eml"));
 
-        testIMAPClient.connect(LOCALHOST_IP, jamesServer.getProbe(ImapGuiceProbe.class).getImapPort())
+        testIMAPClient().connect(LOCALHOST_IP, jamesServer().getProbe(ImapGuiceProbe.class).getImapPort())
             .login(RECIPIENT, PASSWORD)
             .select(TestIMAPClient.INBOX)
             .awaitMessage(awaitAtMostOneMinute);
-        assertThat(testIMAPClient.readFirstMessage()).containsSequence("X-SMIME-Status: Bad signature");
+        assertThat(testIMAPClient().readFirstMessage()).containsSequence("X-SMIME-Status: Bad signature");
     }
 
     @Test
     public void checkSMIMESignatureShouldAddNotSignedStatusWhenNoSignature() throws Exception {
-        messageSender.connect(LOCALHOST_IP, jamesServer.getProbe(SmtpGuiceProbe.class).getSmtpAuthRequiredPort())
+        messageSender().connect(LOCALHOST_IP, jamesServer().getProbe(SmtpGuiceProbe.class).getSmtpAuthRequiredPort())
             .authenticate(FROM, PASSWORD)
             .sendMessageWithHeaders(FROM, RECIPIENT,
                 ClassLoaderUtils.getSystemResourceAsString("eml/mail_with_no_signature.eml"));
 
-        testIMAPClient.connect(LOCALHOST_IP, jamesServer.getProbe(ImapGuiceProbe.class).getImapPort())
+        testIMAPClient().connect(LOCALHOST_IP, jamesServer().getProbe(ImapGuiceProbe.class).getImapPort())
             .login(RECIPIENT, PASSWORD)
             .select(TestIMAPClient.INBOX)
             .awaitMessage(awaitAtMostOneMinute);
-        assertThat(testIMAPClient.readFirstMessage()).containsSequence("X-SMIME-Status: Not signed");
+        assertThat(testIMAPClient().readFirstMessage()).containsSequence("X-SMIME-Status: Not signed");
     }
-
 }
