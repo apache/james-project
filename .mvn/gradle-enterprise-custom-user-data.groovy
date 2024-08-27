4a8b6185b351329b9b64cf07fc344eaf306b1f2f
Jean Helou <jean@xn--gml-cma.com>
2024-08-27 16:58:42 +0200

    [devscout] fixes syntax in profile extractor for gradle enteprise

--- .mvn/gradle-enterprise-custom-user-data.groovy
+++ .mvn/gradle-enterprise-custom-user-data.groovy
@@ -1,6 +1,6 @@
 //adds the profiles active in the reactor as buildScan tags
-session.getRequest().getActiveProfiles().stream().flatMap {it.getActiveProfiles()}.forEach { it->
-    buildScan.tag("P"+it.id)
+session.getRequest().activeProfiles.stream().forEach { it->
+    buildScan.tag("P"+it)
 }
 buildScan.value('parallel', session.parallel as String)
 buildScan.tag("T"+session.getRequest().getDegreeOfConcurrency())
