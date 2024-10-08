c979113b83c2c2f2fb28ea69b35b3cf24dd66702
Quan Tran <hqtran@linagora.com>
2024-10-08 14:53:45 +0700

    JAMES-2456 TikaHttpClientImpl: propagate error instead of Mono.empty
    Let the caller decide how to handle the error.
    If we return Mono.empty, the TextExtractor would turn it into ParsedContent.empty() and hide the error away (from possible valid empty content).

--- mailbox/tika/src/main/java/org/apache/james/mailbox/tika/TikaHttpClientImpl.java
+++ mailbox/tika/src/main/java/org/apache/james/mailbox/tika/TikaHttpClientImpl.java
@@ -84,10 +84,7 @@ public class TikaHttpClientImpl implements TikaHttpClient {
                 }
             })
             .timeout(Duration.ofMillis(tikaConfiguration.getTimeoutInMillis()))
-            .onErrorResume(e -> {
-                LOGGER.warn("Failing to call Tika for content type '{}'", contentType.asString(), e);
-                return Mono.empty();
-            });
+            .doOnError(e -> LOGGER.warn("Failing to call Tika for content type '{}'", contentType.asString(), e));
     }
 
 }
