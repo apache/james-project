6cee7a8c6080b8ccc19c6289c1e95e1eb6e63676
Benoit TELLIER <btellier@linagora.com>
2024-10-09 22:51:55 +0200

    JAMES-2182 Fix checkstyles

--- protocols/imap/src/main/java/org/apache/james/imap/encode/NamespaceResponseEncoder.java
+++ protocols/imap/src/main/java/org/apache/james/imap/encode/NamespaceResponseEncoder.java
@@ -20,7 +20,6 @@ package org.apache.james.imap.encode;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 
 import org.apache.james.imap.api.ImapConstants;
 import org.apache.james.imap.message.response.NamespaceResponse;
