dd6b3e53f9a2cd4853f60ae6a36f1d8945fc98f2
Benoit TELLIER <btellier@linagora.com>
2024-10-09 14:39:27 +0200

    JAMES-2182 PathConverter: handle virtual hosting
    As we can only access resources within a single domain the
    domain is implicit and thus do not need to be repeated in the
    IMAP wording.

--- protocols/imap/src/test/java/org/apache/james/imap/main/PathConverterTest.java
+++ protocols/imap/src/test/java/org/apache/james/imap/main/PathConverterTest.java
@@ -29,12 +29,16 @@ import org.apache.james.mailbox.MailboxSessionUtil;
 import org.apache.james.mailbox.model.MailboxConstants;
 import org.apache.james.mailbox.model.MailboxPath;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class PathConverterTest {
 
     private static final Username USERNAME = Username.of("username");
     private static final Username USERNAME2 = Username.of("username2");
+
+    private static final Username USERNAME_WITH_MAIL = Username.of("username@apache.org");
+    private static final Username USERNAME2_WITH_MAIL = Username.of("username2@apache.org");
     private static final char PATH_DELIMITER = '.';
     public static final boolean RELATIVE = true;
 
@@ -154,5 +158,115 @@ class PathConverterTest {
     void mailboxNameShouldReturnFQDNWhenNotRelativeAndSharedMailbox() {
         assertThat(pathConverter.mailboxName(!RELATIVE, new MailboxPath("#Shared", Username.of("marketing"), "abc"), mailboxSession))
             .isEqualTo("#Shared.marketing.abc");
+    }
+    @Nested
+    class WithEmail{
+        @BeforeEach
+        void setUp() {
+            imapSession = new FakeImapSession();
+            mailboxSession = MailboxSessionUtil.create(USERNAME_WITH_MAIL);
+            pathConverter = PathConverter.Factory.DEFAULT.forSession(imapSession);
+            imapSession.setMailboxSession(mailboxSession);
+        }
+
+        @Test
+        void buildFullPathShouldAcceptNull() {
+            assertThat(pathConverter.buildFullPath(null))
+                .isEqualTo(new MailboxPath("", USERNAME_WITH_MAIL, ""));
+        }
+
+        @Test
+        void buildPathShouldAcceptEmpty() {
+            assertThat(pathConverter.buildFullPath(""))
+                .isEqualTo(new MailboxPath("", USERNAME_WITH_MAIL, ""));
+        }
+
+        @Test
+        void buildPathShouldAcceptRelativeMailboxName() {
+            String mailboxName = "mailboxName";
+            assertThat(pathConverter.buildFullPath(mailboxName))
+                .isEqualTo(MailboxPath.forUser(USERNAME_WITH_MAIL, mailboxName));
+        }
+
+        @Test
+        void buildFullPathShouldAcceptUserNamespace() {
+            assertThat(pathConverter.buildFullPath(MailboxConstants.USER_NAMESPACE))
+                .isEqualTo(MailboxPath.forUser(USERNAME_WITH_MAIL, ""));
+        }
+
+        @Test
+        void buildFullPathShouldAcceptUserNamespaceAndDelimiter() {
+            assertThat(pathConverter.buildFullPath(MailboxConstants.USER_NAMESPACE + PATH_DELIMITER))
+                .isEqualTo(MailboxPath.forUser(USERNAME_WITH_MAIL, ""));
+        }
+
+        @Test
+        void buildFullPathShouldAcceptFullAbsoluteUserPath() {
+            String mailboxName = "mailboxName";
+            assertThat(pathConverter.buildFullPath(MailboxConstants.USER_NAMESPACE + PATH_DELIMITER + mailboxName))
+                .isEqualTo(MailboxPath.forUser(USERNAME_WITH_MAIL, mailboxName));
+        }
+
+        @Test
+        void buildFullPathShouldAcceptRelativePathWithSubFolder() {
+            String mailboxName = "mailboxName" + PATH_DELIMITER + "subFolder";
+            assertThat(pathConverter.buildFullPath(mailboxName))
+                .isEqualTo(MailboxPath.forUser(USERNAME_WITH_MAIL, mailboxName));
+        }
+
+        @Test
+        void buildFullPathShouldAcceptAbsoluteUserPathWithSubFolder() {
+            String mailboxName = "mailboxName.subFolder";
+            assertThat(pathConverter.buildFullPath(MailboxConstants.USER_NAMESPACE + PATH_DELIMITER + mailboxName))
+                .isEqualTo(MailboxPath.forUser(USERNAME_WITH_MAIL, mailboxName));
+        }
+
+        @Test
+        void buildFullPathShouldAcceptAbsoluteOtherUserPath() {
+            assertThat(pathConverter.buildFullPath("#user.username2.abc"))
+                .isEqualTo(MailboxPath.forUser(USERNAME2_WITH_MAIL, "abc"));
+        }
+
+        @Test
+        void buildFullPathShouldAcceptAbsoluteOtherUserPathWithSubfolder() {
+            assertThat(pathConverter.buildFullPath("#user.username2.abc.def"))
+                .isEqualTo(MailboxPath.forUser(USERNAME2_WITH_MAIL, "abc.def"));
+        }
+
+        @Test
+        void mailboxNameShouldReturnNameOnlyWhenRelativeAndUserMailbox() {
+            assertThat(pathConverter.mailboxName(RELATIVE, MailboxPath.forUser(USERNAME_WITH_MAIL, "abc"), mailboxSession))
+                .isEqualTo("abc");
+        }
+
+        @Test
+        void mailboxNameShouldReturnFQDNWhenRelativeAndOtherUserMailbox() {
+            assertThat(pathConverter.mailboxName(RELATIVE, MailboxPath.forUser(USERNAME2_WITH_MAIL, "abc"), mailboxSession))
+                .isEqualTo("#user.username2.abc");
+        }
+
+        @Test
+        void mailboxNameShouldReturnFQDNWhenRelativeAndSharedMailbox() {
+            assertThat(pathConverter.mailboxName(RELATIVE, new MailboxPath("#Shared", Username.of("marketing@apache.org"), "abc"), mailboxSession))
+                .isEqualTo("#Shared.marketing.abc");
+        }
+
+        @Test
+        void mailboxNameShouldReturnFQDNWhenNotRelativeAndUserMailbox() {
+            assertThat(pathConverter.mailboxName(!RELATIVE, MailboxPath.forUser(USERNAME_WITH_MAIL, "abc"), mailboxSession))
+                .isEqualTo("#private.abc");
+        }
+
+        @Test
+        void mailboxNameShouldReturnFQDNWhenNotRelativeAndOtherUserMailbox() {
+            assertThat(pathConverter.mailboxName(!RELATIVE, MailboxPath.forUser(USERNAME2_WITH_MAIL, "abc"), mailboxSession))
+                .isEqualTo("#user.username2.abc");
+        }
+
+        @Test
+        void mailboxNameShouldReturnFQDNWhenNotRelativeAndSharedMailbox() {
+            assertThat(pathConverter.mailboxName(!RELATIVE, new MailboxPath("#Shared", Username.of("marketing@apache.org"), "abc"), mailboxSession))
+                .isEqualTo("#Shared.marketing.abc");
+        }
     }
 }
