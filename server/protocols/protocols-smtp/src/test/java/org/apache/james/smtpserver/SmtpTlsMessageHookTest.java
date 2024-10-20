05a304cd3b4912244779bc16314794c9ff6b3f59
Maksim Meliashchuk <maksym873@gmail.com>
2024-10-20 21:39:11 +0300

    JAMES-3823 SMTP Require TLS Option

---
+++ server/protocols/protocols-smtp/src/test/java/org/apache/james/smtpserver/SmtpTlsMessageHookTest.java
@@ -0,0 +1,142 @@
+package org.apache.james.smtpserver;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.james.smtpserver.SMTPServerTestSystem.BOB;
+import static org.apache.james.smtpserver.SMTPServerTestSystem.PASSWORD;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Base64;
+
+import org.apache.commons.net.smtp.SMTPSClient;
+import org.apache.commons.net.smtp.SimpleSMTPHeader;
+import org.apache.james.protocols.api.utils.BogusSslContextFactory;
+import org.apache.james.protocols.api.utils.BogusTrustManagerFactory;
+import org.apache.mailet.Attribute;
+import org.apache.mailet.AttributeName;
+import org.apache.mailet.AttributeValue;
+import org.apache.mailet.Mail;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SmtpTlsMessageHookTest {
+    private final SMTPServerTestSystem testSystem = new SMTPServerTestSystem();
+    private static final String REQUIRETLS = "REQUIRETLS";
+    private static final AttributeName REQUIRETLS_ATTRIBUTE_NAME = AttributeName.of(REQUIRETLS);
+
+    @BeforeEach
+    void setUp() throws Exception {
+        testSystem.setUp("smtpserver-requireTls.xml");
+    }
+
+    @AfterEach
+    void tearDown() {
+        testSystem.smtpServer.destroy();
+    }
+
+    private SMTPSClient initSMTPSClient() throws IOException {
+        SMTPSClient client = new SMTPSClient(false, BogusSslContextFactory.getClientContext());
+        client.setTrustManager(BogusTrustManagerFactory.getTrustManagers()[0]);
+        InetSocketAddress bindedAddress = testSystem.getBindedAddress();
+        client.connect(bindedAddress.getAddress().getHostAddress(), bindedAddress.getPort());
+        return client;
+    }
+
+    @Test
+    void ehloShouldAdvertiseRequireTlsExtension() throws Exception {
+        SMTPSClient client = initSMTPSClient();
+        client.execTLS();
+        client.sendCommand("EHLO localhost");
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(client.getReplyCode()).isEqualTo(250);
+            softly.assertThat(client.getReplyString()).contains("250 REQUIRETLS");
+        });
+    }
+
+    @Test
+    void ehloShouldNotAdvertiseRequireTlsExtensionWithoutExecTLS() throws Exception {
+        SMTPSClient client = initSMTPSClient();
+        client.sendCommand("EHLO localhost");
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(client.getReplyCode()).isEqualTo(250);
+            softly.assertThat(client.getReplyString()).doesNotContain("250 REQUIRETLS");
+        });
+    }
+
+    @Test
+    void mailShouldBeRejectedWhenInvalidTlsRequiredParameter() throws Exception {
+        SMTPSClient client = initSMTPSClient();
+        client.execTLS();
+        authenticate(client);
+        client.sendCommand("EHLO localhost");
+        client.sendCommand("MAIL FROM: <bob@localhost> REQUIRETLS REQUIRETLS");
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(client.getReplyCode()).isEqualTo(501);
+            softly.assertThat(client.getReplyString()).contains("501 The Mail parameter cannot contain more than one REQUIRETLS parameter at the same time");
+        });
+    }
+
+    @Test
+    void requireTlsParameterShouldBeSetOnTheFinalEmail() throws Exception {
+        SMTPSClient client = initSMTPSClient();
+        client.execTLS();
+        authenticate(client);
+        client.sendCommand("EHLO localhost");
+        client.sendCommand("MAIL FROM:<bob@localhost> REQUIRETLS");
+        client.sendCommand("RCPT TO:<rcpt@localhost>");
+        client.sendShortMessageData("Subject: test mail\r\n\r\nTest body testSimpleMailSendWithMtPriority\r\n.\r\n");
+
+        Mail lastMail = testSystem.queue.getLastMail();
+
+        assertThat(lastMail.getAttribute(REQUIRETLS_ATTRIBUTE_NAME))
+            .hasValue(new Attribute(REQUIRETLS_ATTRIBUTE_NAME, AttributeValue.of(true)));
+
+    }
+
+    @Test
+    void requireTlsParameterShouldBeIgnoredInTheFinalEmail() throws Exception {
+        SMTPSClient client = initSMTPSClient();
+        authenticate(client);
+        client.sendCommand("EHLO localhost");
+        client.sendCommand("MAIL FROM:<bob@localhost> REQUIRETLS");
+        client.sendCommand("RCPT TO:<rcpt@localhost>");
+        client.sendShortMessageData("Subject: test mail\r\n\r\nTest body testSimpleMailSendWithMtPriority\r\n.\r\n");
+
+        Mail lastMail = testSystem.queue.getLastMail();
+
+        assertThat(lastMail.getAttribute(REQUIRETLS_ATTRIBUTE_NAME)).isEmpty();
+    }
+
+    @Test
+    void tlsRequiredHeaderFieldShouldBeIgnoredWhenTheRequireTlsMailFromParameterIsSpecified() throws Exception {
+        SMTPSClient client = initSMTPSClient();
+        client.execTLS();
+        authenticate(client);
+        client.sendCommand("EHLO localhost");
+        client.sendCommand("MAIL FROM:<bob@localhost> REQUIRETLS");
+        client.sendCommand("RCPT TO:<rcpt@localhost>");
+        SimpleSMTPHeader header = new SimpleSMTPHeader("bob@localhost", "rcpt@localhost", "Just testing");
+        header.addHeaderField("TLS-Required", "No");
+        client.sendShortMessageData(header + "Test body testRequireTlsEmail\r\n.\r\n");
+
+        Mail lastMail = testSystem.queue.getLastMail();
+
+        assertThat(lastMail.getAttribute(REQUIRETLS_ATTRIBUTE_NAME))
+            .hasValue(new Attribute(REQUIRETLS_ATTRIBUTE_NAME, AttributeValue.of(true)));
+    }
+
+    private void authenticate(SMTPSClient smtpProtocol) throws IOException {
+        smtpProtocol.sendCommand("AUTH PLAIN");
+        smtpProtocol.sendCommand(Base64.getEncoder().encodeToString(("\0" + BOB.asString() + "\0" + PASSWORD + "\0").getBytes(UTF_8)));
+        assertThat(smtpProtocol.getReplyCode())
+            .as("authenticated")
+            .isEqualTo(235);
+    }
+
+}
