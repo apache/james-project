2628a5b62e23b89d5a4d6e1bee356f28e6c71f57
hungphan227 <45198168+hungphan227@users.noreply.github.com>
2024-11-11 17:35:29 +0700

    JAMES-4086 Mailet for folding long lines (#2490)

---
+++ mailet/standard/src/main/java/org/apache/james/transport/mailets/FoldLongLines.java
@@ -0,0 +1,107 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.transport.mailets;
+
+import java.util.List;
+import java.util.Set;
+
+import jakarta.mail.Header;
+import jakarta.mail.MessagingException;
+
+import org.apache.commons.lang3.stream.Streams;
+import org.apache.mailet.Mail;
+import org.apache.mailet.base.GenericMailet;
+
+import com.github.fge.lambdas.Throwing;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * This mailet fold (wrap) any header lines of the mail that exceed the maximum number of characters.
+ * <br />
+ * It takes only one parameter:
+ * <ul>
+ * <li>maxCharacters: maximum number of characters. Default to 998.
+ * </ul>
+ */
+public class FoldLongLines extends GenericMailet {
+    public static final String MAX_CHARACTERS_PARAMETER_NAME = "maxCharacters";
+    public static final String HEADER_SEPARATOR = ": ";
+
+    private static final int DEFAULT_MAX_CHARACTERS = 998;
+    private static final String SEPARATOR = "\n";
+
+    private int maxCharacters;
+
+    @Override
+    public void init() throws MessagingException {
+        int maxCharacters = getInitParameterAsOptional(MAX_CHARACTERS_PARAMETER_NAME).map(Integer::parseInt).orElse(DEFAULT_MAX_CHARACTERS);
+        Preconditions.checkArgument(maxCharacters > 0, "maxCharacters must be positive");
+        this.maxCharacters = maxCharacters;
+    }
+
+    @Override
+    public void service(Mail mail) throws MessagingException {
+        Set<String> longHeaders = getHeadersExceedingMaxCharacters(mail);
+
+        if (!longHeaders.isEmpty()) {
+            List<Header> headers = getHeadersWithTheSameNameAsLongHeaders(mail, longHeaders);
+
+            // remove all long headers (as well as headers with same name)
+            longHeaders.forEach(Throwing.consumer(header -> mail.getMessage().removeHeader(header)));
+
+            headers.forEach(Throwing.consumer(header -> {
+                if (exceedLineLimit(header)) {
+                    mail.getMessage().addHeader(header.getName(), fold(header));
+                } else {
+                    mail.getMessage().addHeader(header.getName(), header.getValue());
+                }
+            }));
+            mail.getMessage().saveChanges();
+        }
+    }
+
+    private Set<String> getHeadersExceedingMaxCharacters(Mail mail) throws MessagingException {
+        return Streams.of(mail.getMessage().getAllHeaders().asIterator())
+            .filter(this::exceedLineLimit)
+            .map(Header::getName)
+            .collect(ImmutableSet.toImmutableSet());
+    }
+
+    private List<Header> getHeadersWithTheSameNameAsLongHeaders(Mail mail, Set<String> longHeaders) throws MessagingException {
+        return Streams.of(mail.getMessage().getAllHeaders().asIterator())
+            .filter(header -> longHeaders.contains(header.getName()))
+            .toList();
+    }
+
+    private String fold(Header header) {
+        int headerNameLength = header.getName().length() + HEADER_SEPARATOR.length();
+        // TODO After new release of mime4j with commit https://github.com/apache/james-mime4j/commit/66a09219457854c7a26e5b7c0e4c9dd59b4b0c32, update to use MimeUtil of mime4j and remove MimeUtil class file
+        return MimeUtil.fold(header.getValue(), headerNameLength, maxCharacters);
+    }
+
+    private boolean exceedLineLimit(Header header) {
+        String fullHeader = header.getName() + HEADER_SEPARATOR + header.getValue();
+        return Splitter.on(SEPARATOR)
+            .splitToStream(fullHeader)
+            .anyMatch(line -> line.length() > maxCharacters);
+    }
+}
---
+++ mailet/standard/src/test/java/org/apache/james/transport/mailets/FoldLongLinesTest.java
@@ -0,0 +1,185 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.transport.mailets;
+
+import static org.apache.james.transport.mailets.FoldLongLines.HEADER_SEPARATOR;
+import static org.apache.james.transport.mailets.FoldLongLines.MAX_CHARACTERS_PARAMETER_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import jakarta.mail.Header;
+import jakarta.mail.MessagingException;
+
+import org.apache.commons.lang3.stream.Streams;
+import org.apache.james.core.builder.MimeMessageBuilder;
+import org.apache.mailet.Mail;
+import org.apache.mailet.Mailet;
+import org.apache.mailet.MailetContext;
+import org.apache.mailet.base.test.FakeMail;
+import org.apache.mailet.base.test.FakeMailContext;
+import org.apache.mailet.base.test.FakeMailetConfig;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FoldLongLinesTest {
+    static final String HEADER_NAME = "References";
+    static final String HEADER_VALUE = "<a1@gmailcom> <a2@gmailcom> <a3@gmailcom>";
+    static final String FOLDED_LINE = "<a1@gmailcom> <a2@gmailcom>\r\n" +
+        " <a3@gmailcom>";
+
+    private Mailet foldMailet;
+    private MailetContext mailetContext;
+
+    @BeforeEach
+    void beforeEach() {
+        foldMailet = new FoldLongLines();
+        mailetContext = FakeMailContext.defaultContext();
+    }
+
+    @Test
+    void serviceShouldFoldLinesWhenTheyExceedMaxCharacters() throws MessagingException {
+        FakeMailetConfig mailetConfig = FakeMailetConfig.builder()
+            .mailetName("Test")
+            .mailetContext(mailetContext)
+            .setProperty(MAX_CHARACTERS_PARAMETER_NAME, String.valueOf(HEADER_NAME.length() + HEADER_SEPARATOR.length() + HEADER_VALUE.length() - 1))
+            .build();
+        foldMailet.init(mailetConfig);
+
+        Mail mail = FakeMail.builder()
+            .name("mail").mimeMessage(MimeMessageBuilder.mimeMessageBuilder().addHeader(HEADER_NAME, HEADER_VALUE).build())
+            .build();
+        foldMailet.service(mail);
+
+        List<Header> headers = Streams.of(mail.getMessage().getAllHeaders()).filter(header -> header.getName().equals(HEADER_NAME)).toList();
+        assertThat(headers).hasSize(1);
+        assertThat(headers.getFirst().getValue()).isEqualTo(FOLDED_LINE);
+    }
+
+    @Test
+    void serviceShouldFoldLinesWhenTheyExceedMaxCharactersAndTheHeaderHasMultiLines() throws MessagingException {
+        FakeMailetConfig mailetConfig = FakeMailetConfig.builder()
+            .mailetName("Test")
+            .mailetContext(mailetContext)
+            .setProperty(MAX_CHARACTERS_PARAMETER_NAME, "30")
+            .build();
+        foldMailet.init(mailetConfig);
+
+        Mail mail = FakeMail.builder()
+            .name("mail").mimeMessage(MimeMessageBuilder.mimeMessageBuilder().addHeader(HEADER_NAME, "<a1@gmailcom>\n<a2@gmailcom> <a3@gmailcom> <a4@gmailcom>").build())
+            .build();
+        foldMailet.service(mail);
+
+        List<Header> headers = Streams.of(mail.getMessage().getAllHeaders()).filter(header -> header.getName().equals(HEADER_NAME)).toList();
+        assertThat(headers).hasSize(1);
+        assertThat(headers.getFirst().getValue()).isEqualTo("<a1@gmailcom>\n" +
+            "<a2@gmailcom>\r\n" +
+            " <a3@gmailcom> <a4@gmailcom>");
+    }
+
+    @Test
+    void serviceShouldNotFoldLinesWhenTheyDoNotExceedMaxCharacters() throws MessagingException {
+        FakeMailetConfig mailetConfig = FakeMailetConfig.builder()
+            .mailetName("Test")
+            .mailetContext(mailetContext)
+            .setProperty(MAX_CHARACTERS_PARAMETER_NAME, "60")
+            .build();
+        foldMailet.init(mailetConfig);
+
+        Mail mail = FakeMail.builder()
+            .name("mail").mimeMessage(MimeMessageBuilder.mimeMessageBuilder().addHeader(HEADER_NAME, HEADER_VALUE).build())
+            .build();
+        foldMailet.service(mail);
+
+        List<Header> headers = Streams.of(mail.getMessage().getAllHeaders()).filter(header -> header.getName().equals(HEADER_NAME)).toList();
+        assertThat(headers).hasSize(1);
+        assertThat(headers.getFirst().getValue()).isEqualTo(HEADER_VALUE);
+    }
+
+    @Test
+    void serviceShouldNotFoldLinesWhenTheirLengthEqualToMaxCharacters() throws MessagingException {
+        FakeMailetConfig mailetConfig = FakeMailetConfig.builder()
+            .mailetName("Test")
+            .mailetContext(mailetContext)
+            .setProperty(MAX_CHARACTERS_PARAMETER_NAME, String.valueOf(HEADER_NAME.length() + HEADER_SEPARATOR.length() + HEADER_VALUE.length()))
+            .build();
+        foldMailet.init(mailetConfig);
+
+        Mail mail = FakeMail.builder()
+            .name("mail").mimeMessage(MimeMessageBuilder.mimeMessageBuilder().addHeader(HEADER_NAME, HEADER_VALUE).build())
+            .build();
+        foldMailet.service(mail);
+
+        List<Header> headers = Streams.of(mail.getMessage().getAllHeaders()).filter(header -> header.getName().equals(HEADER_NAME)).toList();
+        assertThat(headers).hasSize(1);
+        assertThat(headers.getFirst().getValue()).isEqualTo(HEADER_VALUE);
+    }
+
+    @Test
+    void serviceShouldNotRemoveTheHeaderThatHasTheSameNameAsHeadersWithLongLine() throws Exception {
+        FakeMailetConfig mailetConfig = FakeMailetConfig.builder()
+            .mailetName("Test")
+            .mailetContext(mailetContext)
+            .setProperty(MAX_CHARACTERS_PARAMETER_NAME, "40")
+            .build();
+        foldMailet.init(mailetConfig);
+
+        Mail mail = FakeMail.builder()
+            .name("mail").mimeMessage(MimeMessageBuilder.mimeMessageBuilder()
+                .addHeader(HEADER_NAME, "<b1@gmailcom>")
+                .addHeader(HEADER_NAME, HEADER_VALUE)
+                .build()
+            )
+            .build();
+        foldMailet.service(mail);
+
+        List<Header> headers = Streams.of(mail.getMessage().getAllHeaders()).filter(header -> header.getName().equals(HEADER_NAME)).toList();
+        assertThat(headers).hasSize(2);
+        SoftAssertions.assertSoftly(softly -> {
+            assertThat(headers.getFirst().getValue()).isEqualTo("<b1@gmailcom>");
+            assertThat(headers.getLast().getValue()).isEqualTo(FOLDED_LINE);
+        });
+    }
+
+    @Test
+    void serviceShouldNotChangeTheRelativePositionOfTheHeaderThatHasTheSameNameAsHeadersWithLongLine() throws Exception {
+        FakeMailetConfig mailetConfig = FakeMailetConfig.builder()
+            .mailetName("Test")
+            .mailetContext(mailetContext)
+            .setProperty(MAX_CHARACTERS_PARAMETER_NAME, "30")
+            .build();
+        foldMailet.init(mailetConfig);
+
+        Mail mail = FakeMail.builder()
+            .name("mail").mimeMessage(MimeMessageBuilder.mimeMessageBuilder()
+                .addHeader(HEADER_NAME, HEADER_VALUE)
+                .addHeader(HEADER_NAME, "<b1@gmailcom>")
+                .addHeader(HEADER_NAME, HEADER_VALUE)
+                .build()
+            )
+            .build();
+        foldMailet.service(mail);
+
+        List<Header> headers = Streams.of(mail.getMessage().getAllHeaders()).filter(header -> header.getName().equals(HEADER_NAME)).toList();
+        assertThat(headers).hasSize(3);
+        assertThat(headers.get(1).getValue()).isEqualTo("<b1@gmailcom>");
+    }
+}
