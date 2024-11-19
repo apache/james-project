b821041b5ead82950a6fb10a18bd4c45cbf4c663
TungTV <vttran@linagora.com>
2024-11-19 10:17:14 +0700

    JAMES-4085 Introduce S3MinioExtension - support https

---
+++ server/blob/blob-s3/src/test/java/org/apache/james/blob/objectstorage/aws/S3MinioDocker.java
@@ -0,0 +1,109 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.blob.objectstorage.aws;
+
+import java.time.Duration;
+import java.util.UUID;
+
+import org.apache.http.client.utils.URIBuilder;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+import com.github.fge.lambdas.Throwing;
+import com.google.common.base.Preconditions;
+
+public class S3MinioDocker {
+
+    public static final DockerImageName DOCKER_IMAGE_NAME = DockerImageName.parse("minio/minio")
+        .withTag("RELEASE.2024-10-13T13-34-11Z");
+
+    public static final int MINIO_PORT = 9000;
+    public static final int MINIO_WEB_ADMIN_PORT = 9090;
+    public static final String MINIO_ROOT_USER = "minio";
+    public static final String MINIO_ROOT_PASSWORD = "minio123";
+
+    private final GenericContainer<?> container;
+
+    public S3MinioDocker() {
+        this.container = getContainer();
+    }
+
+    public S3MinioDocker(Network network) {
+        this.container = getContainer()
+            .withNetwork(network);
+    }
+
+    private GenericContainer<?> getContainer() {
+        return new GenericContainer<>(DOCKER_IMAGE_NAME)
+            .withExposedPorts(MINIO_PORT, MINIO_WEB_ADMIN_PORT)
+            .withEnv("MINIO_ROOT_USER", MINIO_ROOT_USER)
+            .withEnv("MINIO_ROOT_PASSWORD", MINIO_ROOT_PASSWORD)
+            .withCommand("server", "--certs-dir", "/opt/minio/certs", "/data", "--console-address", ":" + MINIO_WEB_ADMIN_PORT)
+            .withClasspathResourceMapping("/minio/private.key",
+                "/opt/minio/certs/private.key",
+                BindMode.READ_ONLY)
+            .withClasspathResourceMapping("/minio/public.crt",
+                "/opt/minio/certs/public.crt",
+                BindMode.READ_ONLY)
+            .waitingFor(Wait.forLogMessage(".*MinIO Object Storage Server.*", 1)
+                .withStartupTimeout(Duration.ofMinutes(2)))
+            .withCreateContainerCmdModifier(createContainerCmd -> createContainerCmd.withName("james-minio-s3-test-" + UUID.randomUUID()));
+    }
+
+    public void start() {
+        if (!container.isRunning()) {
+            container.start();
+            setupMC();
+        }
+    }
+
+    public void stop() {
+        container.stop();
+    }
+
+    public AwsS3AuthConfiguration getAwsS3AuthConfiguration() {
+        Preconditions.checkArgument(container.isRunning(), "Container is not running");
+        return AwsS3AuthConfiguration.builder()
+            .endpoint(Throwing.supplier(() -> new URIBuilder()
+                .setScheme("https")
+                .setHost(container.getHost())
+                .setPort(container.getMappedPort(MINIO_PORT))
+                .build()).get())
+            .accessKeyId(MINIO_ROOT_USER)
+            .secretKey(MINIO_ROOT_PASSWORD)
+            .trustAll(true)
+            .build();
+    }
+
+    private void setupMC() {
+        Preconditions.checkArgument(container.isRunning(), "Container is not running");
+        Throwing.runnable(() -> container.execInContainer("mc", "alias", "set", "--insecure", "james", "https://localhost:9000", MINIO_ROOT_USER, MINIO_ROOT_PASSWORD)).run();
+    }
+
+    public void flushAll() {
+        // Remove all objects
+        Throwing.runnable(() -> container.execInContainer("mc", "--insecure", "rm", "--recursive", "--force", "--dangerous", "james/")).run();
+        // Remove all buckets
+        Throwing.runnable(() -> container.execInContainer("mc", "--insecure", "rb", "--force", "--dangerous", "james/")).run();
+    }
+}
--- server/blob/blob-s3/src/test/java/org/apache/james/blob/objectstorage/aws/S3MinioTest.java
+++ server/blob/blob-s3/src/test/java/org/apache/james/blob/objectstorage/aws/S3MinioTest.java
@@ -22,15 +22,11 @@ package org.apache.james.blob.objectstorage.aws;
 import static org.apache.james.blob.api.BlobStoreDAOFixture.SHORT_BYTEARRAY;
 import static org.apache.james.blob.api.BlobStoreDAOFixture.TEST_BLOB_ID;
 import static org.apache.james.blob.api.BlobStoreDAOFixture.TEST_BUCKET_NAME;
-import static org.apache.james.blob.objectstorage.aws.DockerAwsS3Container.ACCESS_KEY_ID;
-import static org.apache.james.blob.objectstorage.aws.DockerAwsS3Container.SECRET_ACCESS_KEY;
 import static org.apache.james.blob.objectstorage.aws.S3BlobStoreConfiguration.UPLOAD_RETRY_EXCEPTION_PREDICATE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.net.URI;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.james.blob.api.BlobId;
@@ -40,50 +36,31 @@ import org.apache.james.blob.api.TestBlobId;
 import org.apache.james.metrics.api.NoopGaugeRegistry;
 import org.apache.james.metrics.tests.RecordingMetricFactory;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
-@Testcontainers
 public class S3MinioTest implements BlobStoreDAOContract {
 
-    private static final String MINIO_IMAGE = "minio/minio";
-    private static final String MINIO_TAG = "RELEASE.2024-10-13T13-34-11Z";
-    private static final String MINIO_IMAGE_FULL = MINIO_IMAGE + ":" + MINIO_TAG;
-    private static final int MINIO_PORT = 9000;
+    @RegisterExtension
+    static S3MinioExtension minoExtension = new S3MinioExtension();
+
     private static S3BlobStoreDAO testee;
-
     private static S3ClientFactory s3ClientFactory;
-
-    @Container
-    private static final GenericContainer<?> minioContainer = new GenericContainer<>(MINIO_IMAGE_FULL)
-        .withExposedPorts(MINIO_PORT)
-        .withEnv("MINIO_ROOT_USER", ACCESS_KEY_ID)
-        .withEnv("MINIO_ROOT_PASSWORD", SECRET_ACCESS_KEY)
-        .withCommand("server", "/data", "--console-address", ":9090")
-        .withCreateContainerCmdModifier(createContainerCmd -> createContainerCmd.withName("james-minio-s3-test-" + UUID.randomUUID()));
-
 
     @BeforeAll
     static void setUp() {
-        AwsS3AuthConfiguration authConfiguration = AwsS3AuthConfiguration.builder()
-            .endpoint(URI.create(String.format("http://%s:%s/", minioContainer.getHost(), minioContainer.getMappedPort(MINIO_PORT))))
-            .accessKeyId(ACCESS_KEY_ID)
-            .secretKey(SECRET_ACCESS_KEY)
-            .build();
+        AwsS3AuthConfiguration awsS3AuthConfiguration = minoExtension.minioDocker().getAwsS3AuthConfiguration();
 
         S3BlobStoreConfiguration s3Configuration = S3BlobStoreConfiguration.builder()
-            .authConfiguration(authConfiguration)
+            .authConfiguration(awsS3AuthConfiguration)
             .region(DockerAwsS3Container.REGION)
             .uploadRetrySpec(Optional.of(Retry.backoff(3, java.time.Duration.ofSeconds(1))
                 .filter(UPLOAD_RETRY_EXCEPTION_PREDICATE)))
@@ -96,11 +73,6 @@ public class S3MinioTest implements BlobStoreDAOContract {
     @AfterAll
     static void tearDownClass() {
         s3ClientFactory.close();
-    }
-
-    @AfterEach
-    void tearDown() {
-        testee.deleteAllBuckets().block();
     }
 
     @BeforeEach
