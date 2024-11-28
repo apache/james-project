dcf78d3f02d26bc6be27965ed4af8a1703f32c7f
TungTV <vttran@linagora.com>
2024-11-28 09:05:41 +0700

    JAMES-2586 - Postgres mailbox messages support batch/pagination for some methods & introduce eager fetch mode

--- mailbox/postgres/src/test/java/org/apache/james/mailbox/postgres/PostgresMailboxManagerTest.java
+++ mailbox/postgres/src/test/java/org/apache/james/mailbox/postgres/PostgresMailboxManagerTest.java
@@ -18,16 +18,33 @@
  ****************************************************************/
 package org.apache.james.mailbox.postgres;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Optional;
 
+import jakarta.mail.Flags;
+
 import org.apache.james.backends.postgres.PostgresExtension;
+import org.apache.james.core.Username;
 import org.apache.james.events.EventBus;
 import org.apache.james.mailbox.MailboxManagerTest;
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.MessageManager;
+import org.apache.james.mailbox.MessageUid;
 import org.apache.james.mailbox.SubscriptionManager;
+import org.apache.james.mailbox.model.MailboxPath;
+import org.apache.james.mailbox.model.MessageRange;
 import org.apache.james.mailbox.store.PreDeletionHooks;
 import org.apache.james.mailbox.store.StoreSubscriptionManager;
 import org.apache.james.metrics.tests.RecordingMetricFactory;
+import org.apache.james.mime4j.dom.Message;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 class PostgresMailboxManagerTest extends MailboxManagerTest<PostgresMailboxManager> {
     @RegisterExtension
@@ -52,5 +69,30 @@ class PostgresMailboxManagerTest extends MailboxManagerTest<PostgresMailboxManag
     @Override
     protected EventBus retrieveEventBus(PostgresMailboxManager mailboxManager) {
         return mailboxManager.getEventBus();
+    }
+
+    @Test
+    void expungeMessageShouldCorrectWhenALotOfMessages() throws Exception {
+        // Given a mailbox with 6000 messages
+        Username username = Username.of("tung");
+        PostgresMailboxManager postgresMailboxManager = mailboxManager.get();
+        MailboxSession session = postgresMailboxManager.createSystemSession(username);
+        postgresMailboxManager.createMailbox(MailboxPath.inbox(username), session).get();
+        MessageManager inboxManager = postgresMailboxManager.getMailbox(MailboxPath.inbox(session), session);
+
+        int totalMessages = 6000;
+        Flux.range(0, totalMessages)
+            .flatMap(i -> Mono.fromCallable(() -> inboxManager.appendMessage(MessageManager.AppendCommand.builder().build(Message.Builder.of()
+                .setSubject("test" + i)
+                .setBody("testmail" + i, StandardCharsets.UTF_8)), session)), 100)
+            .collectList().block();
+        // When expunge all messages
+        inboxManager.setFlags(new Flags(Flags.Flag.DELETED), MessageManager.FlagsUpdateMode.ADD, MessageRange.all(), session);
+
+        List<MessageUid> expungeList = inboxManager.expungeReactive(MessageRange.all(), session)
+            .collectList().block();
+
+        // Then all messages are expunged
+        assertThat(expungeList).hasSize(totalMessages);
     }
 }
