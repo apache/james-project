106a14435fa9d4be51deb3ef108b4ebecb0fbf35
TungTV <vttran@linagora.com>
2025-02-19 09:15:58 +0700

    [FIX] make `updateACL` thread-safe in InMemoryMailboxMapper

--- mailbox/postgres/src/test/java/org/apache/james/mailbox/postgres/mail/PostgresMailboxMapperACLTest.java
+++ mailbox/postgres/src/test/java/org/apache/james/mailbox/postgres/mail/PostgresMailboxMapperACLTest.java
@@ -19,22 +19,11 @@
 
 package org.apache.james.mailbox.postgres.mail;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.time.Duration;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.IntStream;
-
 import org.apache.james.backends.postgres.PostgresExtension;
-import org.apache.james.mailbox.model.MailboxACL;
 import org.apache.james.mailbox.postgres.mail.dao.PostgresMailboxDAO;
 import org.apache.james.mailbox.store.mail.MailboxMapper;
 import org.apache.james.mailbox.store.mail.model.MailboxMapperACLTest;
-import org.apache.james.util.concurrency.ConcurrentTestRunner;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-
-import com.google.common.collect.ImmutableMap;
 
 class PostgresMailboxMapperACLTest extends MailboxMapperACLTest {
     @RegisterExtension
@@ -46,36 +35,5 @@ class PostgresMailboxMapperACLTest extends MailboxMapperACLTest {
     protected MailboxMapper createMailboxMapper() {
         mailboxMapper = new PostgresMailboxMapper(new PostgresMailboxDAO(postgresExtension.getDefaultPostgresExecutor()));
         return mailboxMapper;
-    }
-
-    @Test
-    protected void updateAclShouldWorkWellInMultiThreadEnv() throws ExecutionException, InterruptedException {
-        MailboxACL.Rfc4314Rights rights = new MailboxACL.Rfc4314Rights(MailboxACL.Right.Administer, MailboxACL.Right.Write);
-        MailboxACL.Rfc4314Rights newRights = new MailboxACL.Rfc4314Rights(MailboxACL.Right.Write);
-
-        ConcurrentTestRunner.builder()
-            .reactorOperation((threadNumber, step) -> {
-                int userNumber = threadNumber / 2;
-                MailboxACL.EntryKey key = MailboxACL.EntryKey.createUserEntryKey("user" + userNumber);
-                if (threadNumber % 2 == 0) {
-                    return mailboxMapper.updateACL(benwaInboxMailbox, MailboxACL.command().key(key).rights(rights).asReplacement())
-                        .then();
-                } else {
-                    return mailboxMapper.updateACL(benwaInboxMailbox, MailboxACL.command().key(key).rights(newRights).asAddition())
-                        .then();
-                }
-            })
-            .threadCount(10)
-            .operationCount(1)
-            .runSuccessfullyWithin(Duration.ofMinutes(1));
-
-        MailboxACL expectedMailboxACL = new MailboxACL(IntStream.range(0, 5).boxed()
-            .collect(ImmutableMap.toImmutableMap(userNumber -> MailboxACL.EntryKey.createUserEntryKey("user" + userNumber), userNumber -> rights)));
-
-        assertThat(
-            mailboxMapper.findMailboxById(benwaInboxMailbox.getMailboxId())
-                .block()
-                .getACL())
-            .isEqualTo(expectedMailboxACL);
     }
 }
