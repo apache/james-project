ee6482f456a80912383c158199bdcb135df55aec
Trần Hồng Quân <55171818+quantranhong1999@users.noreply.github.com>
2025-03-14 16:01:12 +0700

    [ENHANCEMENT] Add a concurrent test for IMAP ID command (#2673)

--- server/protocols/protocols-imap4/src/test/java/org/apache/james/imapserver/netty/IMAPServerTest.java
+++ server/protocols/protocols-imap4/src/test/java/org/apache/james/imapserver/netty/IMAPServerTest.java
@@ -3480,6 +3480,22 @@ class IMAPServerTest {
                 .contains("* ID (\"name\" \"Apache James\" \"version\" \"3.9.0\")")
                 .contains("OK ID completed.");
         }
+
+        @Test
+        void concurrentIdCommandsInTheSameSessionShouldSucceed() throws Exception {
+            imapServer = createImapServer("imapServer.xml");
+
+            testIMAPClient.connect("127.0.0.1", imapServer.getListenAddresses().getFirst().getPort());
+            ConcurrentTestRunner.builder()
+                .operation((threadNumber, step) -> {
+                    assertThat(testIMAPClient.sendCommand("ID (\"name\" \"Apache James\")"))
+                        .contains("* ID NIL")
+                        .contains("OK ID completed.");
+                })
+                .threadCount(20)
+                .operationCount(1)
+                .runSuccessfullyWithin(Duration.ofMinutes(5));
+        }
     }
 
 }
