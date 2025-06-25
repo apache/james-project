acff84c0a329b05bc0eaa654aedff2abcd89bf59
Rene Cordier <rcordier@linagora.com>
2025-06-25 14:34:04 +0700

    [UPDATE] Update tika image to 3.2.0.0

--- server/testing/src/main/java/org/apache/james/util/docker/Images.java
+++ server/testing/src/main/java/org/apache/james/util/docker/Images.java
@@ -25,7 +25,7 @@ public interface Images {
     String ELASTICSEARCH_2 = "elasticsearch:2.4.6";
     String ELASTICSEARCH_6 = "docker.elastic.co/elasticsearch/elasticsearch:6.3.2";
     String OPENSEARCH = "opensearchproject/opensearch:2.19.2";
-    String TIKA = "apache/tika:3.0.0.0";
+    String TIKA = "apache/tika:3.2.0.0";
     String MOCK_SMTP_SERVER = "linagora/mock-smtp-server:0.7";
     String OPEN_LDAP = "osixia/openldap:1.5.0";
 }
