8d5358b9447fbcab21535b703d00e2ae702ae08c
Felix Auringer <felix.auringer@protonmail.com>
2025-08-15 07:53:04 +0200

    feat(managesieve): add OAUTHBEARER authentication mechanism

--- protocols/managesieve/src/main/java/org/apache/james/managesieve/api/commands/Authenticate.java
+++ protocols/managesieve/src/main/java/org/apache/james/managesieve/api/commands/Authenticate.java
@@ -30,7 +30,7 @@ import org.apache.james.managesieve.api.UnknownSaslMechanism;
 public interface Authenticate {
 
     enum SupportedMechanism {
-        PLAIN, XOAUTH2;
+        PLAIN, XOAUTH2, OAUTHBEARER;
 
         public static SupportedMechanism retrieveMechanism(String serializedData) throws UnknownSaslMechanism {
             for (SupportedMechanism supportedMechanism : SupportedMechanism.values()) {
