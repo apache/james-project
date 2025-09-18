b8198a7cbb499996146d5b7c8759afcd8c74afc4
Quan Tran <hqtran@linagora.com>
2025-09-18 12:01:20 +0700

    [BUILD] [james-server-postgres-app] Compact scanning search tests for Postgres app
    Start with scanning search is a good enough test.

--- server/apps/postgres-app/src/test/java/org/apache/james/PostgresWithScanningSearchTest.java
+++ server/apps/postgres-app/src/test/java/org/apache/james/PostgresWithScanningSearchTest.java
@@ -20,12 +20,14 @@
 package org.apache.james;
 
 import static org.apache.james.data.UsersRepositoryModuleChooser.Implementation.DEFAULT;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.apache.james.PostgresJamesConfiguration.EventBusImpl;
 import org.apache.james.backends.postgres.PostgresExtension;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-public class WithScanningSearchImmutableTest implements JamesServerConcreteContract {
+public class PostgresWithScanningSearchTest {
     static PostgresExtension postgresExtension = PostgresExtension.empty();
 
     @RegisterExtension
@@ -41,4 +43,9 @@ public class WithScanningSearchImmutableTest implements JamesServerConcreteContr
         .extension(postgresExtension)
         .lifeCycle(JamesServerExtension.Lifecycle.PER_CLASS)
         .build();
+
+    @Test
+    void shouldStart(GuiceJamesServer server) {
+        assertThat(server.isStarted()).isTrue();
+    }
 }
