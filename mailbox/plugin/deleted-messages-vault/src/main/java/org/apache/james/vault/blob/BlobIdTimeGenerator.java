4591193352c02b88250ee9779aa80ef53d65eef2
Rene Cordier <rcordier@linagora.com>
2026-01-06 14:36:48 +0700

    JAMES-4156 add a BlobIdTimeGenerator

---
+++ mailbox/plugin/deleted-messages-vault/src/main/java/org/apache/james/vault/blob/BlobIdTimeGenerator.java
@@ -0,0 +1,50 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.vault.blob;
+
+import java.time.Clock;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+import jakarta.inject.Inject;
+
+import org.apache.james.blob.api.BlobId;
+import org.apache.james.blob.api.PlainBlobId;
+
+public class BlobIdTimeGenerator {
+    private static final String BLOB_ID_GENERATING_FORMAT = "%d/%02d/%s";
+
+    private final BlobId.Factory blobIdFactory;
+    private final Clock clock;
+
+    @Inject
+    public BlobIdTimeGenerator(BlobId.Factory blobIdFactory, Clock clock) {
+        this.blobIdFactory = blobIdFactory;
+        this.clock = clock;
+    }
+
+    BlobId currentBlobId() {
+        ZonedDateTime now = ZonedDateTime.now(clock);
+        int month = now.getMonthValue();
+        int year = now.getYear();
+
+        return new PlainBlobId(String.format(BLOB_ID_GENERATING_FORMAT, year, month, blobIdFactory.of(UUID.randomUUID().toString()).asString()));
+    }
+}
---
+++ mailbox/plugin/deleted-messages-vault/src/test/java/org/apache/james/vault/blob/BlobIdTimeGeneratorTest.java
@@ -0,0 +1,78 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.vault.blob;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Clock;
+import java.time.Instant;
+
+import org.apache.james.blob.api.BlobId;
+import org.apache.james.blob.api.PlainBlobId;
+import org.apache.james.server.blob.deduplication.GenerationAwareBlobId;
+import org.apache.james.server.blob.deduplication.MinIOGenerationAwareBlobId;
+import org.apache.james.utils.UpdatableTickingClock;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class BlobIdTimeGeneratorTest {
+    private static final Instant NOW = Instant.parse("2007-07-03T10:15:30.00Z");
+    private static final Clock CLOCK = new UpdatableTickingClock(NOW);
+
+    interface BlobIdTimeGeneratorContract {
+        BlobId.Factory blobIdFactory();
+
+        @Test
+        default void currentBlobIdShouldReturnBlobIdFormattedWithYearAndMonthPrefix() {
+            BlobIdTimeGenerator blobIdTimeGenerator = new BlobIdTimeGenerator(blobIdFactory(), CLOCK);
+            String currentBlobId = blobIdTimeGenerator.currentBlobId().asString();
+
+            int firstSlash = currentBlobId.indexOf('/');
+            int secondSlash = currentBlobId.indexOf('/', firstSlash + 1);
+            String prefix = currentBlobId.substring(0, secondSlash);
+
+            assertThat(prefix).isEqualTo("2007/07");
+        }
+    }
+
+    @Nested
+    class PlainBlobIdTimeGeneratorTest implements BlobIdTimeGeneratorContract {
+        @Override
+        public BlobId.Factory blobIdFactory() {
+            return new PlainBlobId.Factory();
+        }
+    }
+
+    @Nested
+    class GenerationAwareBlobIdTimeGeneratorTest implements BlobIdTimeGeneratorContract {
+        @Override
+        public BlobId.Factory blobIdFactory() {
+            return new GenerationAwareBlobId.Factory(CLOCK, new PlainBlobId.Factory(), GenerationAwareBlobId.Configuration.DEFAULT);
+        }
+    }
+
+    @Nested
+    class MinIOGenerationAwareBlobIdTimeGeneratorTest implements BlobIdTimeGeneratorContract {
+        @Override
+        public BlobId.Factory blobIdFactory() {
+            return new MinIOGenerationAwareBlobId.Factory(CLOCK, GenerationAwareBlobId.Configuration.DEFAULT, new PlainBlobId.Factory());
+        }
+    }
+}
