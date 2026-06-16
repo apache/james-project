d8b2861360f4fd03c14d954cfde97e65acd40f04
Benoit TELLIER <btellier@linagora.com>
2026-06-16 15:57:16 +0200

    [FIX] User rename was returning wrong quota when target address is not empty

--- server/container/mailbox-adapter/src/main/java/org/apache/james/adapter/mailbox/QuotaUsernameChangeTaskStep.java
+++ server/container/mailbox-adapter/src/main/java/org/apache/james/adapter/mailbox/QuotaUsernameChangeTaskStep.java
@@ -19,49 +19,37 @@
 
 package org.apache.james.adapter.mailbox;
 
-import java.time.Instant;
 import java.util.Optional;
 
 import jakarta.inject.Inject;
 
 import org.apache.james.core.Username;
-import org.apache.james.events.EventBus;
-import org.apache.james.events.RegistrationKey;
-import org.apache.james.mailbox.model.CurrentQuotas;
 import org.apache.james.mailbox.model.Quota;
-import org.apache.james.mailbox.model.QuotaOperation;
 import org.apache.james.mailbox.model.QuotaRoot;
-import org.apache.james.mailbox.quota.CurrentQuotaManager;
 import org.apache.james.mailbox.quota.MaxQuotaManager;
 import org.apache.james.mailbox.quota.QuotaManager;
 import org.apache.james.mailbox.quota.UserQuotaRootResolver;
-import org.apache.james.mailbox.store.event.EventFactory;
+import org.apache.james.mailbox.quota.task.RecomputeMailboxCurrentQuotasService;
 import org.apache.james.user.api.UsernameChangeTaskStep;
 import org.reactivestreams.Publisher;
-
-import com.google.common.collect.ImmutableSet;
 
 import reactor.core.publisher.Mono;
 
 public class QuotaUsernameChangeTaskStep implements UsernameChangeTaskStep {
-    private static final ImmutableSet<RegistrationKey> NO_REGISTRATION_KEYS = ImmutableSet.of();
     private final QuotaManager quotaManager;
     private final MaxQuotaManager maxQuotaManager;
-    private final CurrentQuotaManager currentQuotaManager;
+    private final RecomputeMailboxCurrentQuotasService recomputeMailboxCurrentQuotasService;
     private final UserQuotaRootResolver userQuotaRootResolver;
-    private final EventBus eventBus;
 
     @Inject
     public QuotaUsernameChangeTaskStep(QuotaManager quotaManager,
-                                       CurrentQuotaManager currentQuotaManager,
+                                       RecomputeMailboxCurrentQuotasService recomputeMailboxCurrentQuotasService,
                                        UserQuotaRootResolver userQuotaRootResolver,
-                                       MaxQuotaManager maxQuotaManager,
-                                       EventBus eventBus) {
+                                       MaxQuotaManager maxQuotaManager) {
         this.quotaManager = quotaManager;
-        this.currentQuotaManager = currentQuotaManager;
+        this.recomputeMailboxCurrentQuotasService = recomputeMailboxCurrentQuotasService;
         this.userQuotaRootResolver = userQuotaRootResolver;
         this.maxQuotaManager = maxQuotaManager;
-        this.eventBus = eventBus;
     }
 
     @Override
@@ -78,13 +66,8 @@ public class QuotaUsernameChangeTaskStep implements UsernameChangeTaskStep {
     public Publisher<Void> changeUsername(Username oldUsername, Username newUsername) {
         return Mono.from(quotaManager.getQuotasReactive(userQuotaRootResolver.forUser(oldUsername)))
             .flatMap(quotas -> Mono.fromCallable(() -> userQuotaRootResolver.forUser(newUsername))
-                .flatMap(newUserQuotaRoot -> setQuotaForNewUser(newUserQuotaRoot, quotas)
-                    .then(dispatchNewEventQuota(newUserQuotaRoot, newUsername))));
-    }
-
-    private Mono<Void> setQuotaForNewUser(QuotaRoot quotaRoot, QuotaManager.Quotas quotas) {
-        return setMaxQuota(quotaRoot, quotas)
-            .then(setCurrentQuota(quotaRoot, quotas));
+                .flatMap(newUserQuotaRoot -> setMaxQuota(newUserQuotaRoot, quotas)))
+            .then(Mono.from(recomputeMailboxCurrentQuotasService.recomputeCurrentQuotas(newUsername)));
     }
 
     private Mono<Void> setMaxQuota(QuotaRoot quotaRoot, QuotaManager.Quotas quotas) {
@@ -103,24 +86,5 @@ public class QuotaUsernameChangeTaskStep implements UsernameChangeTaskStep {
         return Mono.justOrEmpty(Optional.ofNullable(quotas.getMessageQuota().getLimitByScope()
                 .get(Quota.Scope.User)))
             .flatMap(quotaCountLimit -> Mono.from(maxQuotaManager.setMaxMessageReactive(quotaRoot, quotaCountLimit)));
-    }
-
-    private Mono<Void> setCurrentQuota(QuotaRoot quotaRoot, QuotaManager.Quotas quotas) {
-        return Mono.from(currentQuotaManager.setCurrentQuotas(QuotaOperation.from(quotaRoot,
-            new CurrentQuotas(quotas.getMessageQuota().getUsed(), quotas.getStorageQuota().getUsed()))));
-    }
-
-    private Mono<Void> dispatchNewEventQuota(QuotaRoot quotaRoot, Username username) {
-        return Mono.from(quotaManager.getQuotasReactive(quotaRoot))
-            .flatMap(quotas -> eventBus.dispatch(
-                EventFactory.quotaUpdated()
-                    .randomEventId()
-                    .user(username)
-                    .quotaRoot(quotaRoot)
-                    .quotaCount(quotas.getMessageQuota())
-                    .quotaSize(quotas.getStorageQuota())
-                    .instant(Instant.now())
-                    .build(),
-                NO_REGISTRATION_KEYS));
     }
 }
