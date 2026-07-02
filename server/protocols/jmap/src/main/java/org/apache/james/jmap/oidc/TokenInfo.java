0d3c56fa8023afc09f8e991ed329ada9c2becf47
Quan Tran <hqtran@linagora.com>
2026-07-02 16:13:14 +0700

    JAMES-4195 Add JMAP OIDC token cache abstraction
    Adds OIDC token value objects, cache configuration, cache interface, Caffeine cache implementation, and cache contract tests.

---
+++ server/protocols/jmap/src/main/java/org/apache/james/jmap/oidc/TokenInfo.java
@@ -0,0 +1,42 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.oidc;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+import com.google.common.base.MoreObjects;
+
+public record TokenInfo(String email, Optional<Sid> sid, Instant exp, Optional<List<Aud>> aud) {
+
+    public String asString() {
+        return MoreObjects.toStringHelper(this)
+            .add("email", email)
+            .add("sid", Optional.ofNullable(sid).flatMap(sidValue -> sidValue.map(Sid::value)).orElse(null))
+            .add("exp", exp)
+            .add("aud", aud.map(audList -> audList
+                    .stream()
+                    .map(Aud::value)
+                    .toList())
+                .orElse(null))
+            .toString();
+    }
+}
