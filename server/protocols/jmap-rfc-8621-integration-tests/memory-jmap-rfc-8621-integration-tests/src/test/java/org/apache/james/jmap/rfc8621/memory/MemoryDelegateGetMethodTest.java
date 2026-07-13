5b978cacbe01024fc305d104196327782b3d37a4
Quan Tran <hqtran@linagora.com>
2026-07-13 19:47:50 +0700

    [BUILD] DelegateGetMethodContract: isolate using different users

--- server/protocols/jmap-rfc-8621-integration-tests/memory-jmap-rfc-8621-integration-tests/src/test/java/org/apache/james/jmap/rfc8621/memory/MemoryDelegateGetMethodTest.java
+++ server/protocols/jmap-rfc-8621-integration-tests/memory-jmap-rfc-8621-integration-tests/src/test/java/org/apache/james/jmap/rfc8621/memory/MemoryDelegateGetMethodTest.java
@@ -21,6 +21,6 @@ package org.apache.james.jmap.rfc8621.memory;
 
 import org.apache.james.jmap.rfc8621.contract.DelegateGetMethodContract;
 
-public class MemoryDelegateGetMethodTest extends MemoryBase  implements DelegateGetMethodContract {
+public class MemoryDelegateGetMethodTest extends PerClassMemoryBase implements DelegateGetMethodContract {
 
 }
