883f25db2c8093825cd71859a53dcda917891615
Quan Tran <hqtran@linagora.com>
2026-07-17 11:52:07 +0700

    JAMES-4210 Wire configurable POP3 SASL mechanisms
    Load built-in and custom SASL factories with Guice and wire Spring to the default PLAIN and OAuth mechanisms.

--- server/protocols/protocols-pop3/src/main/java/org/apache/james/pop3server/netty/POP3ServerFactory.java
+++ server/protocols/protocols-pop3/src/main/java/org/apache/james/pop3server/netty/POP3ServerFactory.java
@@ -25,18 +25,64 @@ import java.util.List;
 import jakarta.inject.Inject;
 
 import org.apache.commons.configuration2.HierarchicalConfiguration;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.configuration2.tree.ImmutableNode;
 import org.apache.james.filesystem.api.FileSystem;
+import org.apache.james.protocols.api.sasl.SaslMechanism;
+import org.apache.james.protocols.api.sasl.SaslMechanismFactory;
 import org.apache.james.protocols.lib.handler.ProtocolHandlerLoader;
 import org.apache.james.protocols.lib.netty.AbstractConfigurableAsyncServer;
 import org.apache.james.protocols.lib.netty.AbstractServerFactory;
 import org.apache.james.protocols.netty.Encryption;
+import org.apache.james.protocols.sasl.BuiltInSaslMechanismFactories;
+import org.apache.james.protocols.sasl.OauthBearerSaslMechanismFactory;
+import org.apache.james.protocols.sasl.PlainSaslMechanismFactory;
+import org.apache.james.protocols.sasl.XOauth2SaslMechanismFactory;
+
+import com.github.fge.lambdas.Throwing;
+import com.google.common.collect.ImmutableList;
 
 public class POP3ServerFactory extends AbstractServerFactory {
+    @FunctionalInterface
+    public interface Pop3SaslMechanismLoader {
+        static Pop3SaslMechanismLoader defaultLoader() {
+            ImmutableList<SaslMechanismFactory> defaultFactories = ImmutableList.of(
+                new PlainSaslMechanismFactory(),
+                new OauthBearerSaslMechanismFactory(),
+                new XOauth2SaslMechanismFactory());
+            return configuration -> loadBuiltInMechanisms(defaultFactories, configuration);
+        }
+
+        ImmutableList<SaslMechanism> load(HierarchicalConfiguration<ImmutableNode> configuration) throws ConfigurationException;
+    }
+
+    private static ImmutableList<SaslMechanism> loadBuiltInMechanisms(ImmutableList<SaslMechanismFactory> defaultFactories,
+                                                                       HierarchicalConfiguration<ImmutableNode> configuration) throws ConfigurationException {
+        try {
+            return BuiltInSaslMechanismFactories.enabledForServer(defaultFactories, configuration)
+                .stream()
+                .map(Throwing.function(factory -> factory.create(configuration)))
+                .collect(ImmutableList.toImmutableList());
+        } catch (RuntimeException e) {
+            if (e.getCause() instanceof ConfigurationException configurationException) {
+                throw configurationException;
+            }
+            throw e;
+        }
+    }
 
     private ProtocolHandlerLoader loader;
     private FileSystem fileSystem;
     private Encryption.Factory encryptionFactory;
+    private Pop3SaslMechanismLoader saslMechanismLoader = Pop3SaslMechanismLoader.defaultLoader();
+
+    public POP3ServerFactory() {
+    }
+
+    @Inject
+    public POP3ServerFactory(Pop3SaslMechanismLoader saslMechanismLoader) {
+        this.saslMechanismLoader = saslMechanismLoader;
+    }
 
     @Inject
     public void setProtocolHandlerLoader(ProtocolHandlerLoader loader) {
@@ -68,6 +114,7 @@ public class POP3ServerFactory extends AbstractServerFactory {
             server.setProtocolHandlerLoader(loader);
             server.setFileSystem(fileSystem);
             server.setEncryptionFactory(encryptionFactory);
+            server.setSaslMechanisms(saslMechanismLoader.load(serverConfig));
             server.configure(serverConfig);
             servers.add(server);
         }
