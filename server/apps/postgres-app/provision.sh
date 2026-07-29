b01151420b2d3f818c06255b1d8d7169bd8e0f84
Quan Tran <hqtran@linagora.com>
2026-07-29 10:20:56 +0700

    [DOCUMENTATION] Webadmin: authenticate Postgres provisioning scripts
    Now Postgres app requires webadmin password by default, the script should have an optional webadmin password param.

--- server/apps/postgres-app/provision.sh
+++ server/apps/postgres-app/provision.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+set -eu
+
 export WEBADMIN_BASE_URL="http://localhost:8000"
 export DOMAIN_NAME="domain.org"
 export USERS_COUNT=1000
+
+call_webadmin() {
+  if [ -n "${WEBADMIN_PASSWORD:-}" ]; then
+    curl --fail --header "Password: ${WEBADMIN_PASSWORD}" "$@"
+  else
+    curl --fail "$@"
+  fi
+}
 
 echo "Start provisioning users."
 
@@ -15,14 +25,14 @@ if [ -e "$user_file" ]; then
 fi
 
 # Create domain
-curl -X PUT ${WEBADMIN_BASE_URL}/domains/${DOMAIN_NAME}
+call_webadmin -X PUT ${WEBADMIN_BASE_URL}/domains/${DOMAIN_NAME}
 
 for i in $(seq 1 $USERS_COUNT)
 do
   # Create user
    echo "Creating user $i"
    username=user${i}@$DOMAIN_NAME
-   curl -XPUT ${WEBADMIN_BASE_URL}/users/$username \
+   call_webadmin -XPUT ${WEBADMIN_BASE_URL}/users/$username \
      -d '{"password":"secret"}' \
      -H "Content-Type: application/json"
 
@@ -37,4 +47,3 @@ echo "Start provisioning IMAP mailboxes and messages..."
 docker run --rm -it --name james-provisioning --network host -v ./imap-provision-conf/provisioning.properties:/conf/provisioning.properties \
 -v $user_file:/conf/users.csv linagora/james-provisioning:latest
 echo "Finished provisioning IMAP mailboxes and messages."
-
