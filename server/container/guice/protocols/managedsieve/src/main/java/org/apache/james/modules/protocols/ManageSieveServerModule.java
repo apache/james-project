236e808a59e4934007a6183c3200c50b4be676d6
Quan Tran <hqtran@linagora.com>
2026-08-13 09:44:10 +0700

    JAMES-4210 Resolve configured ManageSieve SASL factories with Guice
    Reuse the shared Guice SASL resolver for configured factory classes while preserving ManageSieve default mechanisms and their stable order.

--- server/container/guice/protocols/managedsieve/src/main/java/org/apache/james/modules/protocols/ManageSieveServerModule.java
+++ server/container/guice/protocols/managedsieve/src/main/java/org/apache/james/modules/protocols/ManageSieveServerModule.java
@@ -22,19 +22,27 @@ import org.apache.james.ProtocolConfigurationSanitizer;
 import org.apache.james.RunArguments;
 import org.apache.james.filesystem.api.FileSystem;
 import org.apache.james.lifecycle.api.ConfigurationSanitizer;
-import org.apache.james.managesieve.api.commands.CoreCommands;
-import org.apache.james.managesieve.core.CoreProcessor;
 import org.apache.james.managesieveserver.netty.ManageSieveServerFactory;
+import org.apache.james.managesieveserver.netty.ManageSieveServerFactory.ManageSieveSaslMechanismLoader;
+import org.apache.james.protocols.api.sasl.SaslMechanismFactory;
 import org.apache.james.protocols.lib.netty.CertificateReloadable;
+import org.apache.james.protocols.sasl.BuiltInSaslMechanismFactories;
+import org.apache.james.protocols.sasl.OauthBearerSaslMechanismFactory;
+import org.apache.james.protocols.sasl.PlainSaslMechanismFactory;
+import org.apache.james.protocols.sasl.XOauth2SaslMechanismFactory;
 import org.apache.james.server.core.configuration.ConfigurationProvider;
 import org.apache.james.util.LoggingLevel;
 import org.apache.james.utils.GuiceProbe;
+import org.apache.james.utils.GuiceSaslMechanismResolver;
 import org.apache.james.utils.InitializationOperation;
 import org.apache.james.utils.InitilizationOperationBuilder;
 import org.apache.james.utils.KeystoreCreator;
 
+import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.Scopes;
+import com.google.inject.Singleton;
 import com.google.inject.multibindings.Multibinder;
 import com.google.inject.multibindings.ProvidesIntoSet;
 
@@ -44,10 +52,27 @@ public class ManageSieveServerModule extends AbstractModule {
     protected void configure() {
         install(new SieveModule());
         bind(ManageSieveServerFactory.class).in(Scopes.SINGLETON);
-        bind(CoreCommands.class).to(CoreProcessor.class);
         Multibinder.newSetBinder(binder(), GuiceProbe.class).addBinding().to(SieveProbeImpl.class);
 
         Multibinder.newSetBinder(binder(), CertificateReloadable.Factory.class).addBinding().to(ManageSieveServerFactory.class);
+    }
+
+    @Provides
+    @Singleton
+    @ManageSieveDefaultSaslMechanismFactories
+    ImmutableList<SaslMechanismFactory> provideDefaultManageSieveSaslMechanismFactories(OauthBearerSaslMechanismFactory oauthBearer,
+                                                                                        XOauth2SaslMechanismFactory xoauth2) {
+        return ImmutableList.of(new PlainSaslMechanismFactory(false), oauthBearer, xoauth2);
+    }
+
+    @Provides
+    @Singleton
+    ManageSieveSaslMechanismLoader provideManageSieveSaslMechanismLoader(GuiceSaslMechanismResolver resolver,
+                                                                         @ManageSieveDefaultSaslMechanismFactories ImmutableList<SaslMechanismFactory> defaultFactories) {
+        return configuration -> resolver.resolve(
+            ManageSieveServerFactory.retrieveSaslMechanismFactoryClassNames(configuration),
+            BuiltInSaslMechanismFactories.enabledForServer(defaultFactories, configuration),
+            configuration);
     }
 
     @ProvidesIntoSet
