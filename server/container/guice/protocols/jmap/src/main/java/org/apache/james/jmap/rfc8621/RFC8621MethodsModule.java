bff83be7407409cd0d5542fba31c71f17eea03c7
Benoit TELLIER <btellier@linagora.com>
2026-09-10 13:37:11 +0200

    JAMES-4228 Plug EmailSubmissionSetValidation into EmailSubmissionSetMethod

--- server/container/guice/protocols/jmap/src/main/java/org/apache/james/jmap/rfc8621/RFC8621MethodsModule.java
+++ server/container/guice/protocols/jmap/src/main/java/org/apache/james/jmap/rfc8621/RFC8621MethodsModule.java
@@ -67,6 +67,7 @@ import org.apache.james.jmap.method.EmailQueryOptimizer;
 import org.apache.james.jmap.method.EmailQueryViewOptimizer;
 import org.apache.james.jmap.method.EmailSetMethod;
 import org.apache.james.jmap.method.EmailSubmissionSetMethod;
+import org.apache.james.jmap.method.EmailSubmissionSetValidation;
 import org.apache.james.jmap.method.IdentityChangesMethod;
 import org.apache.james.jmap.method.IdentityGetMethod;
 import org.apache.james.jmap.method.IdentitySetMethod;
@@ -89,6 +90,7 @@ import org.apache.james.jmap.method.ThreadChangesMethod;
 import org.apache.james.jmap.method.ThreadGetMethod;
 import org.apache.james.jmap.method.VacationResponseGetMethod;
 import org.apache.james.jmap.method.VacationResponseSetMethod;
+import org.apache.james.jmap.method.ValidRcptEmailSubmissionSetValidation;
 import org.apache.james.jmap.method.ZoneIdProvider;
 import org.apache.james.jmap.pushsubscription.DefaultWebPushClient;
 import org.apache.james.jmap.pushsubscription.PushClientConfiguration;
@@ -100,6 +102,7 @@ import org.apache.james.jmap.routes.SessionRoutes;
 import org.apache.james.jmap.routes.UploadRoutes;
 import org.apache.james.jmap.routes.WebSocketRoutes;
 import org.apache.james.metrics.api.MetricFactory;
+import org.apache.james.rrt.api.RecipientValidator;
 import org.apache.james.utils.ClassName;
 import org.apache.james.utils.GuiceLoader;
 import org.apache.james.utils.InitializationOperation;
@@ -114,6 +117,7 @@ import com.github.fge.lambdas.Throwing;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
 import com.google.inject.Singleton;
@@ -239,6 +243,16 @@ public class RFC8621MethodsModule extends AbstractModule {
             .map(Throwing.function(guiceLoader.<AuthenticationStrategy>withNamingSheme(
                 new NamingScheme.OptionalPackagePrefix(IMPLICIT_AUTHENTICATION_STRATEGY_FQDN_PREFIX))::instantiate))
             .collect(ImmutableSet.toImmutableSet());
+    }
+
+    @Provides
+    @Singleton
+    public Set<EmailSubmissionSetValidation> provideEmailSubmissionSetValidations(JmapRfc8621Configuration configuration,
+                                                                                 Provider<RecipientValidator> recipientValidator) {
+        if (!configuration.validateRecipientsOnSend()) {
+            return ImmutableSet.of();
+        }
+        return ImmutableSet.of(new ValidRcptEmailSubmissionSetValidation(recipientValidator.get(), configuration.recipientValidationPolicy()));
     }
 
     @Provides
--- server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/core/JmapRfc8621Configuration.scala
+++ server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/core/JmapRfc8621Configuration.scala
@@ -29,6 +29,7 @@ import org.apache.commons.configuration2.Configuration
 import org.apache.james.jmap.core.CapabilityIdentifier.CapabilityIdentifier
 import org.apache.james.jmap.core.JmapRfc8621Configuration.{JMAP_EMAIL_GET_FULL_MAX_SIZE_DEFAULT, JMAP_MAX_OBJECT_IN_GET, JMAP_MAX_OBJECT_IN_SET, JMAP_UPLOAD_QUOTA_LIMIT_DEFAULT, MAX_SIZE_ATTACHMENTS_PER_MAIL_DEFAULT, UPLOAD_LIMIT_DEFAULT}
 import org.apache.james.jmap.pushsubscription.PushClientConfiguration
+import org.apache.james.rrt.api.RecipientValidator
 import org.apache.james.util.{DurationParser, Size}
 
 import scala.concurrent.duration.Duration
@@ -54,6 +55,7 @@ object JmapConfigProperties {
   val JMAP_EMAIL_GET_FULL_MAX_SIZE_PROPERTY: String = "email.get.full.max.size"
   val JMAP_GET_MAX_SIZE_PROPERTY: String = "get.max.size"
   val JMAP_SET_MAX_SIZE_PROPERTY: String = "set.max.size"
+  val SEND_VALIDATE_RCPT_PROPERTY: String = "send.validate.rcpt"
 }
 
 object JmapRfc8621Configuration {
@@ -105,6 +107,8 @@ object JmapRfc8621Configuration {
       maxObjectsInSet = Option(configuration.getLong(JMAP_SET_MAX_SIZE_PROPERTY, null))
         .map(value => MaxObjectsInSet(UnsignedInt.liftOrThrow(value)))
         .getOrElse(JMAP_MAX_OBJECT_IN_SET),
+      validateRecipientsOnSend = configuration.getBoolean(SEND_VALIDATE_RCPT_PROPERTY, false),
+      recipientValidationPolicy = RecipientValidator.Policy.from(configuration.subset(SEND_VALIDATE_RCPT_PROPERTY)),
       webPushEnabled = configuration.getBoolean(WEB_PUSH_ENABLED_PROPERTY, true),
       maxTimeoutSeconds = Optional.ofNullable(configuration.getInteger(WEB_PUSH_MAX_TIMEOUT_SECONDS_PROPERTY, null)).map(Integer2int).toScala,
       maxConnections = Optional.ofNullable(configuration.getInteger(WEB_PUSH_MAX_CONNECTIONS_PROPERTY, null)).map(Integer2int).toScala,
@@ -129,6 +133,8 @@ case class JmapRfc8621Configuration(urlPrefixString: String,
                                     jmapEmailGetFullMaxSize: JmapEmailGetFullMaxSize = JMAP_EMAIL_GET_FULL_MAX_SIZE_DEFAULT,
                                     maxObjectsInGet: MaxObjectsInGet = JMAP_MAX_OBJECT_IN_GET,
                                     maxObjectsInSet: MaxObjectsInSet = JMAP_MAX_OBJECT_IN_SET,
+                                    validateRecipientsOnSend: Boolean = false,
+                                    recipientValidationPolicy: RecipientValidator.Policy = RecipientValidator.Policy.DEFAULT,
                                     webPushEnabled: Boolean = true,
                                     maxTimeoutSeconds: Option[Int] = None,
                                     maxConnections: Option[Int] = None,
