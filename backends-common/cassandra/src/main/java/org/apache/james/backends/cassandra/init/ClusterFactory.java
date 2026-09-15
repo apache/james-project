20f566aacc659ce7137dffe7482be6595edb05a5
Benoit TELLIER <btellier@linagora.com>
2026-09-15 08:50:27 +0200

    [FIX] Cassandra 5 support dateof(now()) -> toTimestamp(now())

--- backends-common/cassandra/src/main/java/org/apache/james/backends/cassandra/init/ClusterFactory.java
+++ backends-common/cassandra/src/main/java/org/apache/james/backends/cassandra/init/ClusterFactory.java
@@ -114,6 +114,6 @@ public class ClusterFactory {
     }
 
     private static void ensureContactable(CqlSession session) {
-        session.execute("SELECT dateof(now()) FROM system.local ;");
+        session.execute("SELECT toTimestamp(now()) FROM system.local;");
     }
 }
