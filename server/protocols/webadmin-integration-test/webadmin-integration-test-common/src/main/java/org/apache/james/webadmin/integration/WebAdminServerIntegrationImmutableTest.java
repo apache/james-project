3fdb7d18fd77b2e7b83f84c21327b4993c7470fc
Benoit TELLIER <btellier@linagora.com>
2026-09-17 14:43:58 +0200

    [BUILD] Webadmin Integration test: move tests as immutable when possible

--- server/protocols/webadmin-integration-test/webadmin-integration-test-common/src/main/java/org/apache/james/webadmin/integration/WebAdminServerIntegrationImmutableTest.java
+++ server/protocols/webadmin-integration-test/webadmin-integration-test-common/src/main/java/org/apache/james/webadmin/integration/WebAdminServerIntegrationImmutableTest.java
@@ -22,9 +22,16 @@ package org.apache.james.webadmin.integration;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static io.restassured.RestAssured.with;
+import static org.apache.james.webadmin.Constants.JSON_CONTENT_TYPE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.io.ByteArrayOutputStream;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.james.GuiceJamesServer;
 import org.apache.james.probe.DataProbe;
@@ -44,10 +51,15 @@ import io.restassured.RestAssured;
 public abstract class WebAdminServerIntegrationImmutableTest {
     private static final String DOMAIN = "domain";
     protected static final String USERNAME = "username@" + DOMAIN;
+    private static final String JMAP_USER = "jmap@" + DOMAIN;
+    private static final String EXPORT_USER = "export@" + DOMAIN;
+    private static final String RESTORE_USER = "restore@" + DOMAIN;
+
+    private static DataProbe dataProbe;
 
     @BeforeAll
     static void setUp(GuiceJamesServer guiceJamesServer) throws Exception {
-        DataProbe dataProbe = guiceJamesServer.getProbe(DataProbeImpl.class);
+        dataProbe = guiceJamesServer.getProbe(DataProbeImpl.class);
         dataProbe.addDomain(DOMAIN);
         WebAdminGuiceProbe webAdminGuiceProbe = guiceJamesServer.getProbe(WebAdminGuiceProbe.class);
 
@@ -160,5 +172,168 @@ public abstract class WebAdminServerIntegrationImmutableTest {
         .then()
             .body("status", is("completed"))
             .body("type", is("PopulateFilteringProjectionTask"));
+    }
+
+    @Test
+    void getUserDefaultIdentityShouldReturnNotFoundByDefault() {
+        when()
+            .get(String.format("/users/%s/identities?default=true", USERNAME))
+        .then()
+            .statusCode(HttpStatus.NOT_FOUND_404)
+            .contentType(JSON_CONTENT_TYPE)
+            .body("message", is("Default identity can not be found"));
+    }
+
+    @Test
+    void getIdentitiesOfInvalidUserShouldReturnBadRequest() {
+        given()
+            .urlEncodingEnabled(true)
+            .get(String.format("/users/%s/identities?default=true", "John Doe"))
+        .then()
+            .statusCode(HttpStatus.BAD_REQUEST_400);
+    }
+
+    @Test
+    void createIdentitiesForInvalidUserShouldReturnBadRequest() {
+        given()
+            .urlEncodingEnabled(true)
+            .body("{\n" +
+                "  \"name\": \"create name 1\",\n" +
+                "  \"email\": \"bob@domain.tld\",\n" +
+                "  \"textSignature\": \"create textSignature1\",\n" +
+                "  \"htmlSignature\": \"create htmlSignature1\",\n" +
+                "  \"sortOrder\": 99,\n" +
+                "  \"bcc\": [\n" +
+                "    {\n" +
+                "      \"name\": \"create bcc 1\",\n" +
+                "      \"email\": \"create_boss_bcc_1@domain.tld\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"replyTo\": [\n" +
+                "    {\n" +
+                "      \"name\": \"create replyTo 1\",\n" +
+                "      \"email\": \"create_boss1@domain.tld\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}")
+            .post(String.format("/users/%s/identities", "John Doe"))
+        .then()
+            .statusCode(HttpStatus.BAD_REQUEST_400);
+    }
+
+    @Test
+    void updateIdentitiesForInvalidUserShouldReturnBadRequest() {
+        given()
+            .urlEncodingEnabled(true)
+            .body("{\n" +
+                "  \"name\": \"create name 1\",\n" +
+                "  \"email\": \"bob@domain.tld\",\n" +
+                "  \"textSignature\": \"create textSignature1\",\n" +
+                "  \"htmlSignature\": \"create htmlSignature1\",\n" +
+                "  \"sortOrder\": 99,\n" +
+                "  \"bcc\": [\n" +
+                "    {\n" +
+                "      \"name\": \"create bcc 1\",\n" +
+                "      \"email\": \"create_boss_bcc_1@domain.tld\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"replyTo\": [\n" +
+                "    {\n" +
+                "      \"name\": \"create replyTo 1\",\n" +
+                "      \"email\": \"create_boss1@domain.tld\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}")
+            .put(String.format("/users/%s/identities/b1c924a3-5b86-44fa-a036-77825ec0e3e6", "John Doe"))
+        .then()
+            .statusCode(HttpStatus.BAD_REQUEST_400);
+    }
+
+    @Test
+    void jmapUserTasksShouldBeExposed() throws Exception {
+        dataProbe.addUser(JMAP_USER, "anyPassword");
+
+        String taskId = with()
+            .queryParam("task", "recomputeFastViewProjectionItems")
+            .post("/users/" + JMAP_USER + "/mailboxes")
+            .jsonPath()
+            .get("taskId");
+
+        given()
+            .basePath(TasksRoutes.BASE)
+        .when()
+            .get(taskId + "/await")
+        .then()
+            .body("status", is("completed"))
+            .body("type", is("RecomputeUserFastViewProjectionItemsTask"));
+    }
+
+    @Test
+    void mailboxesExportTasksShouldBeExposed() throws Exception {
+        dataProbe.addUser(EXPORT_USER, "anyPassword");
+
+        String taskId = with()
+            .queryParam("task", "export")
+            .post("/users/" + EXPORT_USER + "/mailboxes")
+            .jsonPath()
+            .get("taskId");
+
+        given()
+            .basePath(TasksRoutes.BASE)
+        .when()
+            .get(taskId + "/await")
+        .then()
+            .body("status", is("completed"))
+            .body("type", is("MailboxesExportTask"));
+    }
+
+    @Test
+    void mailboxesRestoreTasksShouldBeExposed() throws Exception {
+        dataProbe.addUser(RESTORE_USER, "anyPassword");
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (ZipOutputStream zos = new ZipOutputStream(baos)) {
+            // empty zip
+        }
+        byte[] emptyZip = baos.toByteArray();
+
+        String taskId = with()
+            .queryParam("task", "restore")
+            .body(emptyZip)
+            .post("/users/" + RESTORE_USER + "/mailboxes")
+            .jsonPath()
+            .get("taskId");
+
+        given()
+            .basePath(TasksRoutes.BASE)
+        .when()
+            .get(taskId + "/await")
+        .then()
+            .body("status", is("completed"))
+            .body("type", is("MailboxesRestoreTask"));
+    }
+
+    @Test
+    void createMissParentsTasksShouldBeExposed() {
+        String taskId = with()
+            .queryParam("task", "createMissingParents")
+            .post("/mailboxes")
+            .jsonPath()
+            .get("taskId");
+
+        given()
+            .basePath(TasksRoutes.BASE)
+            .when()
+            .get(taskId + "/await")
+            .then()
+            .body("status", is("completed"))
+            .body("type", is("CreateMissingParentsTask"))
+            .body("additionalInformation.created", hasSize(0))
+            .body("additionalInformation.totalCreated", is(0))
+            .body("additionalInformation.failures", empty())
+            .body("additionalInformation.totalFailure", is(0))
+            .body("startedDate", is(notNullValue()))
+            .body("submitDate", is(notNullValue()))
+            .body("completedDate", is(notNullValue()));
     }
 }
