1fc63d3372415e380b847ce14385def342a42c66
ilya terskov <prosgarz35@gmail.com>
2026-09-24 13:48:27 +0700

    Import DisabledOnOs and OS in FileBlobStoreGCAlgorithmTest

--- server/blob/blob-file/src/test/java/org/apache/james/blob/file/FileBlobStoreGCAlgorithmTest.java
+++ server/blob/blob-file/src/test/java/org/apache/james/blob/file/FileBlobStoreGCAlgorithmTest.java
@@ -24,6 +24,8 @@ import org.apache.james.blob.api.PlainBlobId;
 import org.apache.james.server.blob.deduplication.BloomFilterGCAlgorithmContract;
 import org.apache.james.server.core.filesystem.FileSystemImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 public class FileBlobStoreGCAlgorithmTest implements BloomFilterGCAlgorithmContract {
 
@@ -40,7 +42,7 @@ public class FileBlobStoreGCAlgorithmTest implements BloomFilterGCAlgorithmContr
     }
 
     @Override
-    @org.junit.jupiter.api.condition.DisabledOnOs(org.junit.jupiter.api.condition.OS.WINDOWS)
+    @DisabledOnOs(OS.WINDOWS)
     public void allOrphanBlobIdsShouldRemovedAfterMultipleRunningTimesGC() {
         BloomFilterGCAlgorithmContract.super.allOrphanBlobIdsShouldRemovedAfterMultipleRunningTimesGC();
     }
